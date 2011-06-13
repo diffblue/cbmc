f61c8124ee5b343f6da2f494acea1a6633512607
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-06-13 16:43:33 +0000

    revert to minisat2 without simplifier

--- src/solvers/sat/satcheck.h
+++ src/solvers/sat/satcheck.h
@@ -42,7 +42,7 @@ typedef satcheck_minisatt satcheckt;
 
 #include "satcheck_minisat2.h"
 
-typedef satcheck_minisat_simpt satcheckt;
+typedef satcheck_minisat_coret satcheckt;
 
 #else
 #error NO SAT CHECKER
