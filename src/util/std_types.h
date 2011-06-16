099f695196dfdfee79d64ef098a744c233c508da
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-06-16 11:18:52 +0000

    C_is_bit_field
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@213 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/std_types.h
+++ src/util/std_types.h
@@ -169,6 +169,16 @@ public:
     {
       return set(ID_C_is_padding, is_padding);
     }
+
+    inline const bool get_is_bit_field() const
+    {
+      return get_bool(ID_C_is_bit_field);
+    }
+
+    inline void set_is_bit_field(bool is_bit_field)
+    {
+      return set(ID_C_is_bit_field, is_bit_field);
+    }
   };
 
   typedef std::vector<componentt> componentst;
