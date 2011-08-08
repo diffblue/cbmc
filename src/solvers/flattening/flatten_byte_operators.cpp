a5fff1f1f810b7ee35b6f22779b670e260648164
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-08-08 14:41:59 +0000

    more

--- src/solvers/flattening/flatten_byte_operators.cpp
+++ src/solvers/flattening/flatten_byte_operators.cpp
@@ -6,11 +6,13 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
-#ifndef CPROVER_FLATTEN_BYTE_OPERATORS_H
-#define CPROVER_FLATTEN_BYTE_OPERATORS_H
-
 #include <expr.h>
 #include <std_types.h>
+#include <std_expr.h>
+#include <arith_tools.h>
+#include <pointer_offset_size.h>
+
+#include "flatten_byte_operators.h"
 
 /*******************************************************************\
 
@@ -28,9 +30,51 @@ exprt flatten_byte_extract(
   const exprt &src,
   const namespacet &ns)
 {
-  assert(src.id()==ID_byte_extract_little_endian);
-  assert(src.id()==ID_byte_extract_big_endian);
+  assert(src.id()==ID_byte_update_little_endian ||
+         src.id()==ID_byte_update_big_endian);
   assert(src.operands().size()==2);
+
+  unsigned width=
+    integer2long(pointer_offset_size(ns, src.type()));
+  
+  const typet &t=src.op0().type();
+  
+  if(t.id()==ID_array)
+  {
+    const array_typet &array_type=to_array_type(t);
+    const typet &subtype=array_type.subtype();
+    
+    // byte-array?
+    if((subtype.id()==ID_unsignedbv ||
+        subtype.id()==ID_signedbv) &&
+       subtype.get_int(ID_width)==8)
+    {
+      // get 'width'-bytes, and concatenate
+      exprt::operandst op;
+      op.resize(width);
+      
+      for(unsigned i=0; i<width; i++)
+      {
+        index_exprt index_expr(subtype);
+        index_expr.array()=src.op0();
+        index_expr.index()=from_integer(i, integer_typet());
+        op[i]=index_expr;
+      }
+      
+      if(width==1)
+        return op[0];
+      else
+      {
+        concatenation_exprt concatenation(src.type());
+        concatenation.operands().swap(op);
+        return concatenation;
+      }
+    }
+    else
+      throw "flatten_byte_extract can only do byte-array right now";
+  }
+  else
+    throw "flatten_byte_extract can only do array right now";
 }
 
 /*******************************************************************\
@@ -49,11 +93,12 @@ exprt flatten_byte_update(
   const exprt &src,
   const namespacet &ns)
 {
-  assert(src.id()==ID_byte_update_little_endian);
-  assert(src.id()==ID_byte_update_big_endian);
+  assert(src.id()==ID_byte_update_little_endian ||
+         src.id()==ID_byte_update_big_endian);
   assert(src.operands().size()==3);
 
-  mp_integer width=pointer_offset_size(src.type());
+  unsigned width=
+    integer2long(pointer_offset_size(ns, src.type()));
   
   const typet &t=src.op0().type();
   
@@ -67,12 +112,33 @@ exprt flatten_byte_update(
         subtype.id()==ID_signedbv) &&
        subtype.get_int(ID_width)==8)
     {
+      // apply 'with' width times
+      exprt result=src;
+      
+      for(unsigned i=0; i<width; i++)
+      {
+        exprt byte_extract_expr(
+          src.id()==ID_byte_update_little_endian?ID_byte_extract_little_endian:
+          src.id()==ID_byte_update_big_endian?ID_byte_extract_big_endian:
+          throw "unexpected src.id()",
+          subtype);
+          
+        exprt i_expr=from_integer(i, ns.follow(src.op1().type()));
+          
+        byte_extract_expr.copy_to_operands(src.op1(), i_expr);
+          
+        with_exprt with_expr;
+        with_expr.type()=src.type();
+        with_expr.old()=result;
+        with_expr.where()=plus_exprt(src.op1(), i_expr);
+        with_expr.new_value()=byte_extract_expr;
+      }
+      
+      return result;
     }
     else
-      throw "flatten byte update can only do byte-array right now";
+      throw "flatten_byte_update can only do byte-array right now";
   }
   else
-    throw "flatten byte update can only do array right now";
+    throw "flatten_byte_update can only do array right now";
 }
-
-#endif
--- src/solvers/flattening/flatten_byte_operators.h
+++ src/solvers/flattening/flatten_byte_operators.h
@@ -10,8 +10,9 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_FLATTEN_BYTE_OPERATORS_H
 
 #include <expr.h>
+#include <namespace.h>
 
-exprt flatten_byte_extract(const exprt &src);
-exprt flatten_byte_update(const exprt &src);
+exprt flatten_byte_extract(const exprt &src, const namespacet &ns);
+exprt flatten_byte_update(const exprt &src, const namespacet &ns);
 
 #endif
