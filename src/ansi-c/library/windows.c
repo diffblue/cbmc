9633a8b1aadae16ffd6078aa3f338da855ac29a4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-09-07 09:44:20 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@475 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/library/windows.c
+++ src/ansi-c/library/windows.c
@@ -22,3 +22,26 @@ VOID ExitThread(DWORD dwExitCode)
   // never returns
   __CPROVER_assume(0);
 }
+
+/* FUNCTION: CreateThread */
+
+#include <windows.h>
+
+HANDLE CreateThread(
+  LPSECURITY_ATTRIBUTES lpThreadAttributes,
+  SIZE_T dwStackSize,
+  LPTHREAD_START_ROUTINE lpStartAddress,
+  LPVOID lpParameter,
+  DWORD dwCreationFlags,
+  LPDWORD lpThreadId
+)
+{
+  __CPROVER_HIDE:;
+  DWORD thread_id;
+
+  if(lpThreadId) *lpThreadId=thread_id;
+  __CPROVER_ASYNC_1: lpStartAddress(lpParameter);
+  
+  HANDLE handle;
+  return handle;
+}
