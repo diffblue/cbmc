86ca47ff3b4fdcd2e5d67bcc5acf8dc5955d00db
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-10-02 18:09:25 +0000

    interrupt weaving

---
+++ src/goto-instrument/interrupt.h
@@ -0,0 +1,24 @@
+/*******************************************************************\
+
+Module: Interrupt Instrumentation for Goto Programs
+
+Author: Daniel Kroening
+
+Date: September 2011
+
+\*******************************************************************/
+
+#ifndef CPROVER_GOTO_INSTRUMENT_INTERRUPT_H
+#define CPROVER_GOTO_INSTRUMENT_INTERRUPT_H
+
+#include <pointer-analysis/value_sets.h>
+
+#include <goto-programs/goto_functions.h>
+
+void interrupt(
+  value_setst &value_sets,
+  const class contextt &context,
+  goto_functionst &goto_functions,
+  const irep_idt &interrupt_handler);
+
+#endif
