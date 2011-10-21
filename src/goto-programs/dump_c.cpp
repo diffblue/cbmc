02cbb3c141ce8004004557ddff336b4c939fd44e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-10-21 19:50:47 +0000

    code from scoot
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@605 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-programs/dump_c.cpp
+++ src/goto-programs/dump_c.cpp
@@ -196,6 +196,88 @@ void dump_c(
 
 /*******************************************************************\
 
+Module: 
+
+Author: 
+
+\*******************************************************************/
+
+#include <map>
+#include <ostream>
+#include <sstream>
+#include <cctype>
+
+#include <namespace.h>
+
+class goto2cppt
+{
+public:
+  goto2cppt(
+    const namespacet& ns,
+    const goto_functionst& goto_functions,
+    std::ostream& os):
+      goto_functions(goto_functions), ns(ns){}
+
+  void convert(std::ostream& os);
+
+protected:
+
+   std::string type_to_string(const typet&, bool keep_int = false);
+   std::string expr_to_string(const exprt&,
+                              const std::map<irep_idt,irep_idt>&, bool keep_int = false);
+
+   std::string implicit_declarations(const exprt& expr,
+                      std::map<irep_idt,irep_idt>& local_renaming);
+
+   void convert_compound_declarations(std::ostream& os_decl,
+                                      std::ostream& os_body);
+   void convert_global_variables(std::ostream& os);
+   void convert_function_declarations(std::ostream& os);
+
+
+   void convert_instructions(const goto_programt& goto_program,
+                             std::map<irep_idt, irep_idt>&, std::ostream& os);
+
+   void convert_compound_rec(const struct_typet& struct_type, std::ostream& os);
+
+   const goto_functionst& goto_functions;
+
+   irep_idt unique_name(irep_idt name);
+
+
+   irep_idt get_global_constant(irep_idt cst, irep_idt type_id)
+   {
+       irep_idt key = id2string(type_id)+"("+id2string(cst)+")";
+
+       std::map<irep_idt, irep_idt>::const_iterator it_find
+            = global_constants.find(key);
+
+        if(it_find != global_constants.end())
+            return it_find->second;
+
+        irep_idt new_cst_id = unique_name( "__SCOOT_constant");
+
+        global_constants[key] = new_cst_id;
+        global_constant_stream << "const " <<
+            id2string(type_id) <<" "<< new_cst_id
+            <<"(\"" << id2string(cst) << "\");" << std::endl;
+
+        return new_cst_id;
+   }
+
+   std::map<typet, irep_idt> typedef_map;
+
+   std::map<irep_idt, irep_idt> global_constants;
+
+   const namespacet& ns;
+   std::map<irep_idt,irep_idt> renaming;
+
+   std::stringstream typedef_stream;          // for types
+   std::stringstream global_constant_stream;  // for numerical constants
+};
+
+/*******************************************************************\
+
 Function: dump_c
 
   Inputs:
@@ -211,8 +293,14 @@ void dump_c(
   const namespacet &ns,
   std::ostream &out)
 {
+  #if 0
   forall_goto_functions(it, src)
     if(it->second.body_available &&
-       it->first!="main")
+       it->first!=ID_main)
       dump_c(it, ns, out);
+  #endif
+
+  goto2cppt goto2cpp(ns, src, out);
+  goto2cpp.convert(out);
 }
+
