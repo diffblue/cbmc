c046445e918ae63696cef6b378f4098af8c7b98c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-11-05 18:33:27 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@630 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/library/setjmp.c
+++ src/ansi-c/library/setjmp.c
@@ -12,6 +12,19 @@ inline void longjmp(jmp_buf env, int val)
   __CPROVER_assume(0);
 }
        
+/* FUNCTION: _longjmp */
+
+#ifndef __CPROVER_SETJMP_H_INCLUDED
+#include <setjmp.h>
+#define __CPROVER_SETJMP_H_INCLUDED
+#endif
+
+inline void _longjmp(jmp_buf env, int val)
+{
+  // does not return
+  __CPROVER_assume(0);
+}
+       
 /* FUNCTION: siglongjmp */
 
 #ifndef __CPROVER_SETJMP_H_INCLUDED
@@ -24,5 +37,16 @@ inline void siglongjmp(sigjmp_buf env, int val)
   // does not return
   __CPROVER_assume(0);
 }
-       
-              
+
+/* FUNCTION: setjmp */
+
+#ifndef __CPROVER_SETJMP_H_INCLUDED
+#include <setjmp.h>
+#define __CPROVER_SETJMP_H_INCLUDED
+#endif
+
+inline int setjmp(jmp_buf env)
+{
+  // store PC
+}
+
