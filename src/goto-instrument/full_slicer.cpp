b027adab046c03c2d07a6144b6ad80169753c346
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-11-13 20:54:34 +0000

    function calls

--- src/goto-instrument/full_slicer.cpp
+++ src/goto-instrument/full_slicer.cpp
@@ -28,6 +28,7 @@ void full_slicert::fixedpoint()
 {
   queuet queue;
 
+  // fill queue with asserts
   for(cfgt::entry_mapt::iterator
       e_it=cfg.entry_map.begin();
       e_it!=cfg.entry_map.end();
@@ -38,6 +39,7 @@ void full_slicert::fixedpoint()
       queue.push(&e_it->second);
     }
 
+  // process queue until empty
   while(!queue.empty())
   {
     cfgt::iterator e=queue.top();
@@ -113,7 +115,8 @@ Function: full_slicert::transform
 
 object_id_sett full_slicert::transform(cfgt::iterator e)
 {
-  object_id_sett result=e->required_objects;
+  const object_id_sett &old_set=e->required_objects;
+  object_id_sett new_set=e->required_objects; // copy!
   
   const goto_programt::instructiont &instruction=*(e->PC);
 
@@ -127,18 +130,40 @@ object_id_sett full_slicert::transform(cfgt::iterator e)
       
       bool found=false;
 
-      // does it write something we need?
+      // does it constrain something we need?
       for(object_id_sett::const_iterator
           o_it1=w.begin(); o_it1!=w.end(); o_it1++)
-        if(result.find(*o_it1)!=result.end())
+        if(old_set.find(*o_it1)!=old_set.end())
           found=true;
 
       if(found)
       {
         e->node_required=true;
-        result.insert(w.begin(), w.end());
+        new_set.insert(w.begin(), w.end());
       }
     }    
+    break;
+    
+  // these introduce control-flow dependencies
+  case GOTO:
+    {
+      object_id_sett w;
+      get_objects(instruction.guard, w);
+      
+      bool found=false;
+
+      // does it constrain something we need?
+      for(object_id_sett::const_iterator
+          o_it1=w.begin(); o_it1!=w.end(); o_it1++)
+        if(old_set.find(*o_it1)!=old_set.end())
+          found=true;
+
+      if(found)
+      {
+        e->node_required=true;
+        new_set.insert(w.begin(), w.end());
+      }
+    }
     break;
   
   case OTHER:
@@ -161,12 +186,12 @@ object_id_sett full_slicert::transform(cfgt::iterator e)
       for(object_id_sett::const_iterator
           o_it1=w.begin(); o_it1!=w.end(); o_it1++)
       {
-        object_id_sett::iterator o_it2=result.find(*o_it1);
-        if(o_it2!=result.end())
+        object_id_sett::iterator o_it2=old_set.find(*o_it1);
+        if(o_it2!=old_set.end())
         {
           found=true;
           // remove what is written
-          result.erase(o_it2);
+          new_set.erase(o_it2);
         }
       }
 
@@ -174,7 +199,7 @@ object_id_sett full_slicert::transform(cfgt::iterator e)
       {
         e->node_required=true;
         // add what it reads
-        get_objects_r(code_assign, result);
+        get_objects_r(code_assign, new_set);
       }
     }    
     break;
@@ -182,54 +207,47 @@ object_id_sett full_slicert::transform(cfgt::iterator e)
   case DECL: // this cuts off dependencies
     {
       object_idt object_id(to_symbol_expr(instruction.code.op0()));
-      if(result.find(object_id)!=result.end())
+      if(old_set.find(object_id)!=old_set.end())
       {
         e->node_required=true;
-        result.erase(object_id);
+        new_set.erase(object_id);
       }
     }
     break;
   
   case FUNCTION_CALL:
     {
-      #if 0      
-      // these are like assignments for the arguments
-      // and for the LHS
+      // These are like assignments for the arguments.
+      // The LHS is dealt with on return.
       const code_function_callt &code_function_call=
         to_code_function_call(instruction.code);
 
-      object_id_sett w;
-      get_objects_w(code_function_call, w);
+      assert(code_function_call.function().type().id()==ID_code);
       
-      bool found=false;
+      const code_typet &code_type=
+        to_code_type(code_function_call.function().type());
 
-      // does it write something we need?
-      for(object_id_sett::const_iterator
-          o_it1=w.begin(); o_it1!=w.end(); o_it1++)
+      assert(code_function_call.arguments().size()>=
+             code_type.arguments().size());
+
+      for(unsigned i=0; i<code_type.arguments().size(); i++)
       {
-        object_id_sett::iterator o_it2=result.find(*o_it1);
-        if(o_it2!=result.end())
+        object_idt lhs_id=object_idt(code_type.arguments()[i].get_identifier());
+
+        if(old_set.find(lhs_id)!=old_set.end())
         {
-          found=true;
-          // remove what is written
-          result.erase(o_it2);
+          new_set.erase(lhs_id);
+          e->node_required=true;
+          get_objects(code_function_call.arguments()[i], new_set);
         }
       }
-
-      if(found)
-      {
-        e->node_required=true;
-        // add what it reads
-        get_objects_r(code_assign, result);
-      }
-      #endif
     }    
     break;
   
   default:;
   }
   
-  return result;
+  return new_set;
 }
 
 /*******************************************************************\
