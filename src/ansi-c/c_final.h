481d38cb133a0d4119debf9686fd22d0daf13f1e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-11-14 15:03:11 +0000

    c_final is retired

--- src/ansi-c/c_final.h
+++ src/ansi-c/c_final.h
@@ -1,19 +1,1 @@
-/*******************************************************************\
 
-Module:
-
-Author: Daniel Kroening, kroening@kroening.com
-
-\*******************************************************************/
-
-#ifndef CPROVER_C_FINAL_H
-#define CPROVER_C_FINAL_H
-
-#include <iostream>
-
-#include <context.h>
-#include <message.h>
-
-bool c_final(contextt &context, message_handlert &message_handler);
-
-#endif
