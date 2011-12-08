d2ed1902e8d63ca74e93af05c8328dd32f1bcabd
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2011-12-08 07:56:39 +0000

    renaming str2 -> string2

--- src/util/string2int.h
+++ src/util/string2int.h
@@ -9,7 +9,7 @@ Author: Michael Tautschnig, michael.tautschnig@cs.ox.ac.uk
 #ifndef CPROVER_STRING2INT_H
 #define CPROVER_STRING2INT_H
 
-int safe_str2int(const char *str, int base=10);
-unsigned safe_str2unsigned(const char *str, int base=10);
+int safe_string2int(const char *str, int base=10);
+unsigned safe_string2unsigned(const char *str, int base=10);
 
 #endif
