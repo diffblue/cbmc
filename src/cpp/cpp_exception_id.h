4a06aaf3d4cef7f2619f9941bb2f59b06501ec3f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-01-02 15:47:38 +0000

    better typechecking for catch()

---
+++ src/cpp/cpp_exception_id.h
@@ -0,0 +1,20 @@
+/*******************************************************************\
+
+Module: C++ Language Type Checking
+
+Author: Daniel Kroening, kroening@cs.cmu.edu
+
+\*******************************************************************/
+
+#ifndef CPROVER_CPP_EXCEPTION_ID_H
+#define CPROVER_CPP_EXCEPTION_ID_H
+
+// turns a type into an exception ID
+
+#include <namespace.h>
+#include <type.h>
+
+irep_idt cpp_exception_id(const typet &, const namespacet &);
+irept cpp_exception_list(const typet &, const namespacet &);
+
+#endif
