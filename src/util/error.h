bce7a75a31adc16f4fb8d0d686b460b20407c412
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-02-06 13:04:26 +0000

    error_str
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@967 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/error.h
+++ src/util/error.h
@@ -11,10 +11,11 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <exception>
 #include <sstream>
+#include <iostream>
 
 #include "location.h"
 
-class error_baset:public exception
+class error_baset:public std::exception
 {
 public:
   virtual const char* what() const throw()
@@ -22,16 +23,63 @@ public:
     return "";
   }
   
+  virtual ~error_baset() throw ()
+  {
+  }
+  
+  inline error_baset()
+  {
+  }
+  
+  inline explicit error_baset(const locationt &_location):location(_location)
+  {
+  }
+  
   locationt location;
 };          
 
-class error:public error_baset, public std::ostringstream
+class error_str:public error_baset, public std::ostringstream
 {
 public:
   virtual const char* what() const throw()
   {
     return str().c_str();
   }
-};          
+  
+  virtual ~error_str() throw ()
+  {
+  }
+  
+  inline error_str()
+  {
+  }
+  
+  explicit inline error_str(const locationt &_location):
+    error_baset(_location), std::ostringstream()
+  {
+  }
+  
+  explicit inline error_str(const char *string)
+  {
+    str(string);
+  }
+
+  explicit inline error_str(const std::string &string)
+  {
+    str(string);
+  }
+  
+  inline error_str(const error_str &other):std::ostringstream()
+  {
+    str(other.str());
+    location=other.location;
+  }
+
+  inline error_str(const locationt &_location, const std::string &string):
+    error_baset(_location)
+  {
+    str(string);
+  }
+};
 
 #endif
