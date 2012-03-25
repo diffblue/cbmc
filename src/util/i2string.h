36e16e432f6f9e163be1d1b4e922c5b632b0ed39
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-03-25 11:50:18 +0000

    64 bit integers for Visual Studio
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1084 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/i2string.h
+++ src/util/i2string.h
@@ -12,3 +12,10 @@ std::string i2string(int i);
 std::string i2string(signed long int i);
 std::string i2string(unsigned i);
 std::string i2string(unsigned long int i);
+
+// 64 bit integers
+
+#ifdef _MSC_VER
+std::string i2string(signed __int64 i);
+std::string i2string(unsigned __int64 i);
+#endif
