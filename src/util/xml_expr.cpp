509aae038236d4d62971e512969aebc6a555c122
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-04-09 13:56:00 +0000

    expr -> XML
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1141 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/util/xml_expr.cpp
@@ -0,0 +1,115 @@
+/*******************************************************************\
+
+Module: Expressions in XML
+
+Author: Daniel Kroening
+
+  Date: November 2005
+
+\*******************************************************************/
+
+#include "namespace.h"
+#include "expr.h"
+#include "xml.h"
+#include "arith_tools.h"
+#include "ieee_float.h"
+#include "fixedbv.h"
+
+#include "xml_expr.h"
+
+/*******************************************************************\
+
+Function: convert
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+xmlt convert(
+  const exprt &expr,
+  const namespacet &ns)
+{
+  const typet &type=ns.follow(expr.type());
+  xmlt result;
+  
+  if(expr.id()==ID_constant)
+  {
+    if(type.id()==ID_unsignedbv ||
+       type.id()==ID_signedbv ||
+       type.id()==ID_bv)
+    {
+      result.name="integer";
+      result.set_attribute("binary", expr.get_string(ID_value));
+
+      mp_integer i;
+      if(!to_integer(expr, i))
+        result.data=integer2string(i);
+    }
+    else if(type.id()==ID_fixedbv)
+    {
+      result.name="fixed";
+      result.set_attribute("binary", expr.get_string(ID_value));
+      result.data=fixedbvt(expr).to_ansi_c_string();
+    }
+    else if(type.id()==ID_floatbv)
+    {
+      result.name="float";
+      result.set_attribute("binary", expr.get_string(ID_value));
+      result.data=ieee_floatt(expr).to_ansi_c_string();
+    }
+    else if(type.id()==ID_pointer)
+    {
+      result.name="pointer";
+      result.set_attribute("binary", expr.get_string(ID_value));
+      if(expr.get(ID_value)==ID_NULL)
+        result.data="NULL";
+    }
+    else if(type.id()==ID_bool)
+    {
+      result.name="boolean";
+      result.data=expr.is_true()?"TRUE":"FALSE";
+    }
+  }
+  else if(expr.id()==ID_array)
+  {
+    result.name="array";
+    
+    unsigned index=0;
+    
+    forall_operands(it, expr)
+    {
+      xmlt &e=result.new_element("element");
+      e.set_attribute("index", index);
+      e.new_element(convert(*it, ns));
+      index++;
+    }
+  }
+  else if(expr.id()==ID_struct)
+  {
+    result.name="struct";
+  
+    forall_operands(it, expr)
+    {
+      xmlt &e=result.new_element("member");
+      e.new_element(convert(*it, ns));
+    }
+  }
+  else if(expr.id()==ID_union)
+  { 
+    result.name="union";
+    
+    assert(expr.operands().size()==1);
+    
+    xmlt &e=result.new_element("member");
+    e.new_element(convert(expr.op0(), ns));
+  }
+  else
+    result.name="unknown";
+
+  return result;
+}
+
