b202aa1a96bc137a2b61d67577ce37d8da733798
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-05-06 22:00:28 +0000

    more

--- src/ansi-c/remove_internal_symbols.cpp
+++ src/ansi-c/remove_internal_symbols.cpp
@@ -10,6 +10,8 @@ Author: Daniel Kroening
 #include <namespace.h>
 #include <find_symbols.h>
 
+#include <std_types.h>
+
 #include "remove_internal_symbols.h"
 
 /*******************************************************************\
@@ -30,18 +32,33 @@ void get_symbols_rec(
   find_symbols_sett &dest)
 {
   dest.insert(symbol.name);
-
+  
   find_symbols_sett new_symbols;
 
   find_type_and_expr_symbols(symbol.type, new_symbols);
   find_type_and_expr_symbols(symbol.value, new_symbols);
+  
+  if(symbol.type.id()==ID_code)
+  {
+    const code_typet &code_type=to_code_type(symbol.type);
+    const code_typet::argumentst &arguments=code_type.arguments();
+    for(code_typet::argumentst::const_iterator
+        it=arguments.begin();
+        it!=arguments.end();
+        it++)
+    {
+      irep_idt id=it->get_identifier();
+      const symbolt *s;
+      if(!ns.lookup(id, s)) new_symbols.insert(id);
+    }
+  }
 
   for(find_symbols_sett::const_iterator
       it=new_symbols.begin();
       it!=new_symbols.end();
       it++)
   {
-    if(dest.find(*it)!=dest.end())
+    if(dest.find(*it)==dest.end())
     {
       dest.insert(*it);
       get_symbols_rec(ns, ns.lookup(*it), dest); // recursive call
@@ -61,6 +78,10 @@ Function: remove_internal_symbols
           * non-static function with body that is not extern inline
           * symbol used in an EXPORTED symbol
           * type used in an EXPORTED symbol
+          
+          Read
+          http://gcc.gnu.org/ml/gcc/2006-11/msg00006.html
+          on "extern inline"
 
 \*******************************************************************/
 
@@ -69,6 +90,13 @@ void remove_internal_symbols(
 {
   namespacet ns(context);
   find_symbols_sett exported;
+
+  // we retain certain special ones
+  find_symbols_sett special;
+  special.insert("c::argc'");  
+  special.insert("c::argv'");  
+  special.insert("c::__CPROVER_memory");  
+  special.insert("c::__CPROVER_initialize");
   
   for(contextt::symbolst::const_iterator
       it=context.symbols.begin();
@@ -81,12 +109,20 @@ void remove_internal_symbols(
 
     // not marked yet  
     const symbolt &symbol=it->second;
+
+    if(special.find(symbol.name)!=special.end())
+    {
+      get_symbols_rec(ns, symbol, exported);
+      continue;
+    }
     
     bool is_function=symbol.type.id()==ID_code;
     bool is_file_local=symbol.file_local;
     bool is_type=symbol.is_type;
     bool has_body=symbol.value.is_not_nil();
-    bool has_initializer=symbol.value.get_bool(ID_C_zero_initializer);
+    bool has_initializer=
+      symbol.value.is_not_nil() &&
+      !symbol.value.get_bool(ID_C_zero_initializer);
 
     if(is_type)
     {
@@ -104,6 +140,18 @@ void remove_internal_symbols(
       if(has_initializer)
         get_symbols_rec(ns, symbol, exported);
     }
+  }
+
+  // remove all that are _not_ exported!
+  for(contextt::symbolst::iterator
+      it=context.symbols.begin();
+      it!=context.symbols.end();
+      ) // no it++
+  {
+    if(exported.find(it->first)==exported.end())
+      it=context.symbols.erase(it);
+    else
+      it++;
   }
 }
 
