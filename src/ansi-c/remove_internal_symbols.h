e8d698715ac612146429e06de1f36a3befd8d063
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-05-06 08:48:00 +0000

    remove internal symbols
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1159 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/ansi-c/remove_internal_symbols.h
@@ -0,0 +1,15 @@
+/*******************************************************************\
+
+Module: Remove symbols that are internal only
+
+Author: Daniel Kroening
+
+\*******************************************************************/
+
+#ifndef CPROVER_GOTO_PROGRAMS_REMOVE_INTERNAL_SYMBOLS_H
+#define CPROVER_GOTO_PROGRAMS_REMOVE_INTERNAL_SYMBOLS_H
+
+void remove_internal_symbols(
+  class contextt &context);
+
+#endif
