7a0e670341dd3d55b39bbaf3dabf909a163a46f1
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-05-09 13:53:23 +0000

    header
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1190 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/internal_additions.h
+++ src/ansi-c/internal_additions.h
@@ -10,3 +10,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 void ansi_c_internal_additions(std::string &code);
 void ansi_c_architecture_strings(std::string &code);
+
+extern const char gcc_builtin_headers[];
+extern const char arm_builtin_headers[];
+
