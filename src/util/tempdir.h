cba65caebf655d182de8aa62e2fc8fd5a8bc5ae4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-05-28 22:15:49 +0000

    temp_working_dirt
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1225 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/tempdir.h
+++ src/util/tempdir.h
@@ -13,4 +13,14 @@ Author: CM Wintersteiger
 
 std::string get_temporary_directory(const std::string &name_template);
 
+class temp_working_dirt
+{
+public:
+  std::string old_working_directory;
+  std::string path;
+  
+  explicit temp_working_dirt(const std::string &name_template);
+  ~temp_working_dirt();
+};
+
 #endif
