f09af75c596bc0730862804d5fd127f97f7d3db4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-06-14 23:01:13 +0000

    double instrumentation on write races

--- src/goto-instrument/interrupt.cpp
+++ src/goto-instrument/interrupt.cpp
@@ -14,20 +14,12 @@ Date: September 2011
 #include <prefix.h>
 #include <context.h>
 
-#if 0
-#include <hash_cont.h>
-#include <expr_util.h>
-#include <guard.h>
-
-#include <goto-programs/remove_skip.h>
-#endif
-
 #include "interrupt.h"
 #include "rw_set.h"
 
 /*******************************************************************\
 
-Function: poential_race
+Function: poential_race_on_read
 
   Inputs:
 
@@ -37,19 +29,38 @@ Function: poential_race
 
 \*******************************************************************/
 
-bool potential_race(
-  const rw_set_baset &rw_set,
+bool potential_race_on_read(
+  const rw_set_baset &code_rw_set,
   const rw_set_baset &isr_rw_set)
 {
   // R/W race?
-  forall_rw_set_r_entries(e_it, rw_set)
+  forall_rw_set_r_entries(e_it, code_rw_set)
   {
     if(isr_rw_set.has_w_entry(e_it->first))
       return true;
   }
   
+  return false;
+}
+
+/*******************************************************************\
+
+Function: poential_race_on_write
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool potential_race_on_write(
+  const rw_set_baset &code_rw_set,
+  const rw_set_baset &isr_rw_set)
+{
   // W/R or W/W?
-  forall_rw_set_w_entries(e_it, rw_set)
+  forall_rw_set_w_entries(e_it, code_rw_set)
   {
     if(isr_rw_set.has_r_entry(e_it->first))
       return true;
@@ -89,35 +100,71 @@ void interrupt(
     rw_set_loct rw_set(ns, value_sets, i_it);
 
     // potential race?
-    if(!potential_race(rw_set, isr_rw_set))
+    bool race_on_read=potential_race_on_read(rw_set, isr_rw_set);
+    bool race_on_write=potential_race_on_write(rw_set, isr_rw_set);
+    
+    if(!race_on_read && !race_on_write)
       continue;
       
-    // insert the call to the ISR
-    goto_programt::instructiont original_instruction;
-    original_instruction.swap(instruction);
+    // Insert the call to the ISR.
+    // We do before for races on Read, and before and after
+    // for races on Write.
 
-    const locationt &location=original_instruction.location;
+    if(race_on_read || race_on_write)
+    {    
+      goto_programt::instructiont original_instruction;
+      original_instruction.swap(instruction);
+
+      const locationt &location=original_instruction.location;
+      
+      code_function_callt isr_call;
+      isr_call.location()=location;
+      isr_call.function()=interrupt_handler;
+      
+      goto_programt::targett t_goto=i_it;      
+      goto_programt::targett t_call=goto_program.insert_after(t_goto);
+      goto_programt::targett t_orig=goto_program.insert_after(t_call);
+
+      t_goto->make_goto(t_orig);
+      t_goto->location=location;
+      t_goto->guard=side_effect_expr_nondett(bool_typet());
+      t_goto->function=original_instruction.function;
+
+      t_call->make_function_call(isr_call);
+      t_call->location=location;
+      t_call->function=original_instruction.function;
+
+      t_orig->swap(original_instruction);
+      
+      i_it=t_orig; // the for loop already counts us up
+    }
     
-    code_function_callt isr_call;
-    isr_call.location()=location;
-    isr_call.function()=interrupt_handler;
-    
-    goto_programt::targett t_goto=i_it;      
-    goto_programt::targett t_call=goto_program.insert_after(t_goto);
-    goto_programt::targett t_orig=goto_program.insert_after(t_call);
+    if(race_on_write)
+    {
+      // insert _after_ the instruction with race
+      goto_programt::targett t_orig=i_it;
+      t_orig++;
+      
+      goto_programt::targett t_goto=goto_program.insert_after(i_it);
+      goto_programt::targett t_call=goto_program.insert_after(t_goto);
+      
+      const locationt &location=i_it->location;
+      
+      code_function_callt isr_call;
+      isr_call.location()=location;
+      isr_call.function()=interrupt_handler;
+      
+      t_goto->make_goto(t_orig);
+      t_goto->location=location;
+      t_goto->guard=side_effect_expr_nondett(bool_typet());
+      t_goto->function=i_it->function;
 
-    t_goto->make_goto(t_orig);
-    t_goto->location=location;
-    t_goto->guard=side_effect_expr_nondett(bool_typet());
-    t_goto->function=original_instruction.function;
+      t_call->make_function_call(isr_call);
+      t_call->location=location;
+      t_call->function=i_it->function;
 
-    t_call->make_function_call(isr_call);
-    t_call->location=location;
-    t_call->function=original_instruction.function;
-
-    t_orig->swap(original_instruction);
-
-    i_it=t_orig; // the for loop already counts us up
+      i_it=t_call; // the for loop already counts us up      
+    }
   }
 }
 
