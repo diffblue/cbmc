09e6d441e48df4651234f565429bf56cad28f788
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-07-16 12:15:51 +0000

    memory-model option

--- src/cbmc/parseoptions.h
+++ src/cbmc/parseoptions.h
@@ -41,6 +41,7 @@ class optionst;
   "(error-label):(verbosity):(no-library)" \
   "(version)" \
   "(cover-assertions)" \
+  "(memory-model):" \
   "(i386-linux)(i386-macos)(i386-win32)(win32)(winx64)" \
   "(ppc-macos)(unsigned-char)" \
   "(arrays-uf-always)(arrays-uf-never)" \
