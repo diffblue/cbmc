e7cedd5f43cc527ace6098f2c7903a2cfaa10788
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-04 21:29:28 +0000

    unicode file names
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1385 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/cbmc/main.cpp
+++ src/cbmc/main.cpp
@@ -33,7 +33,7 @@ Function: main
 #ifdef _MSC_VER
 int wmain(int argc, const wchar_t **argv_wide)
 {
-  const char **argv=narrow_argv(argv_wide);
+  const char **argv=narrow_argv(argc, argv_wide);
   cbmc_parseoptionst parseoptions(argc, argv);
   return parseoptions.main();
 }
