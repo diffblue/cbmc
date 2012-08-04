08a842aea20891bfa9a43c52b6829ca37d0eecaa
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-04 14:18:26 +0000

    unicode
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1369 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-cc/ms_cl_cmdline.cpp
+++ src/goto-cc/ms_cl_cmdline.cpp
@@ -27,65 +27,67 @@ Function: ms_cl_cmdlinet::parse
  
 \*******************************************************************/
 
-const char *non_ms_cl_options[]=
+const wchar_t *non_ms_cl_options[]=
 {
-  "--dot",
-  "--show-symbol-table",
-  "--show-function-table",
-  "--ppc-macos",
-  "--i386-linux",
-  "--i386-win32",
-  "--i386-macos",
-  "--string-abstraction",
-  "--no-library",
-  "--16",
-  "--32",
-  "--64",
-  "--little-endian",
-  "--big-endian",
-  "--unsigned-char",
-  "--no-arch",            
-  "--help",
-  "--xml",
-  "--partial-inlining",
-  "--verbosity",
-  "--function",
+  L"--dot",
+  L"--show-symbol-table",
+  L"--show-function-table",
+  L"--ppc-macos",
+  L"--i386-linux",
+  L"--i386-win32",
+  L"--i386-macos",
+  L"--string-abstraction",
+  L"--no-library",
+  L"--16",
+  L"--32",
+  L"--64",
+  L"--little-endian",
+  L"--big-endian",
+  L"--unsigned-char",
+  L"--no-arch",            
+  L"--help",
+  L"--xml",
+  L"--partial-inlining",
+  L"--verbosity",
+  L"--function",
   NULL
 };
 
-bool ms_cl_cmdlinet::parse(const std::vector<std::string> &options)
+bool ms_cl_cmdlinet::parse(const std::vector<std::wstring> &options)
 {
   for(unsigned i=0; i<options.size(); i++)
   {
     // is it a non-cl option?
-    if(strncmp(options[i].c_str(), "--", 2)==0)
+    if(std::wstring(options[i], 0, 2)==L"--")
     {
       process_non_cl_option(options[i]);
 
-      if(options[i]=="--verbosity" ||
-          options[i]=="--function")
+      if(options[i]==L"--verbosity" ||
+         options[i]==L"--function")
         if(i<options.size()-1)
         {
-          set(options[i].substr(2), options[i+1]);
+          set(w2s(options[i]).substr(2), w2s(options[i+1]));
           i++; // skip ahead
         }
     }
     else if(!options[i].empty() && options[i][0]=='@')
     {
-      process_response_file(std::string(options[i], 1, std::string::npos));
+      // potentially recursive
+      process_response_file(
+        std::string(w2s(options[i]), 1, std::string::npos));
     }
-    else if(options[i]=="/link" ||
-            options[i]=="-link")
+    else if(options[i]==L"/link" ||
+            options[i]==L"-link")
     {
       // anything that follows goes to the linker
       i=options.size()-1;
     }
     else if(options[i].size()==2 &&
-            (options[i]=="/D" || options[i]=="-D") &&
+            (options[i]==L"/D" || options[i]==L"-D") &&
             i!=options.size()-1)
     {
       // this requires special treatment, as you can do "/D something"
-      std::string tmp="/D"+options[i+1];
+      std::wstring tmp=L"/D"+options[i+1];
       i++;
       process_cl_option(tmp);
     }
@@ -111,10 +113,22 @@ Function: ms_cl_cmdlinet::parse_env
 void ms_cl_cmdlinet::parse_env()
 {
   // first do environment
-  const char *CL_env=getenv("CL");
   
+  #ifdef _WIN32
+
+  const wchar_t *CL_env=_wgetenv("CL");
+
   if(CL_env!=NULL)
-    process_response_file_line(std::string(CL_env));
+    process_response_file_line(std::wstring(CL_env));
+
+  #else
+
+  const char *CL_env=getenv("CL");
+
+  if(CL_env!=NULL)
+    process_response_file_line(s2w(std::string(CL_env)));
+
+  #endif  
 }
 
 /*******************************************************************\
@@ -131,11 +145,13 @@ Function: ms_cl_cmdlinet::parse
 
 bool ms_cl_cmdlinet::parse(int argc, const char **argv)
 {
-  std::vector<std::string> options;
+  // should really use "wide" argv from wmain()
+
+  std::vector<std::wstring> options;
 
   // skip argv[0]
   for(int i=1; i<argc; i++)
-    options.push_back(argv[i]);
+    options.push_back(s2w(argv[i]));
   
   return parse(options);
 }
@@ -152,6 +168,7 @@ Function: my_wgetline
  
 \*******************************************************************/
 
+#if 0
 static std::istream &my_wgetline(std::istream &in, std::string &dest)
 {
   // We should support this properly,
@@ -187,6 +204,7 @@ static std::istream &my_wgetline(std::istream &in, std::string &dest)
         
   return in;
 }
+#endif
 
 /*******************************************************************\
  
@@ -217,18 +235,29 @@ void ms_cl_cmdlinet::process_response_file(const std::string &file)
   if(line[0]==char(0xff) && line[1]==char(0xfe))
   {
     // Unicode!
+    
+    #if 0
     // re-open -- should be using wifstream
     std::ifstream infile2(file.c_str());
     infile2.seekg(2);
     
     while(my_wgetline(infile2, line))
       process_response_file_line(line);
+    #else
+    
+    std::wifstream infile2(file.c_str());
+    std::wstring wline;
+    
+    while(std::getline(infile2, wline))
+      process_response_file_line(wline);
+    
+    #endif
   }
   else
   {
     // normal ASCII
     while(getline(infile, line))
-      process_response_file_line(line);
+      process_response_file_line(s2w(line));
   }
 }
 
@@ -244,7 +273,7 @@ Function: ms_cl_cmdlinet::process_response_file_line
  
 \*******************************************************************/
 
-void ms_cl_cmdlinet::process_response_file_line(const std::string &line)
+void ms_cl_cmdlinet::process_response_file_line(const std::wstring &line)
 {
   // In a response file, multiple compiler options and source-code files can
   // appear on one line.  A single compiler-option specification must appear
@@ -252,10 +281,10 @@ void ms_cl_cmdlinet::process_response_file_line(const std::string &line)
   // comments that begin with the # symbol.
 
   if(line.empty()) return;
-  if(line[0]=='#') return;
+  if(line[0]=='#') return; // comment
 
-  std::vector<std::string> options;
-  std::string option;
+  std::vector<std::wstring> options;
+  std::wstring option;
   bool in_quotes=false;
 
   for(unsigned i=0; i<line.size(); i++)
@@ -293,13 +322,13 @@ Function: ms_cl_cmdlinet::process_non_cl_option
 \*******************************************************************/
 
 void ms_cl_cmdlinet::process_non_cl_option(
-  const std::string &s)
+  const std::wstring &s)
 {
   cmdlinet::optiont option;
 
   option.isset=true;
   option.islong=true;
-  option.optstring=std::string(s, 2, std::string::npos);
+  option.optstring=w2s(std::wstring(s, 2, std::string::npos));
   option.optchar=0;
 
   int optnr=getoptnr(option.optstring);
@@ -317,8 +346,8 @@ void ms_cl_cmdlinet::process_non_cl_option(
       return;
 
   // unrecognized option
-  std::cout << "Warning: uninterpreted non-CL option `" 
-            << s << "'" << std::endl;
+  std::wcout << L"Warning: uninterpreted non-CL option `" 
+             << s << L"'" << std::endl;
 }
 
 /*******************************************************************\
@@ -333,143 +362,143 @@ Function: ms_cl_cmdlinet::process_cl_option
  
 \*******************************************************************/
 
-const char *ms_cl_flags[]=
+const wchar_t *ms_cl_flags[]=
 {
-  "c", // compile only
+  L"c", // compile only
   NULL
 };
 
-const char *ms_cl_prefixes[]=
+const wchar_t *ms_cl_prefixes[]=
 {
-  "O1", // minimize space
-  "O2", // maximize speed
-  "Ob", // <n> inline expansion (default n=0)
-  "Od", // disable optimizations (default)
-  "Og", // enable global optimization
-  "Oi", // [-] enable intrinsic functions
-  "Os", // favor code space
-  "Ot", // favor code speed
-  "Ox", // maximum optimizations
-  "Oy", // [-] enable frame pointer omission
-  "GF", // enable read-only string pooling
-  "Gm", // [-] enable minimal rebuild
-  "Gy", // [-] separate functions for linker
-  "GS", // [-] enable security checks
-  "GR", // [-] enable C++ RTTI
-  "GX", // [-] enable C++ EH (same as /EHsc)
-  "EHs", // enable C++ EH (no SEH exceptions)
-  "EHa", // enable C++ EH (w/ SEH exceptions)
-  "EHc", // extern "C" defaults to nothrow
-  "fp", // floating-point model
-  "GL", // [-] enable link-time code generation
-  "GA", // optimize for Windows Application
-  "Ge", // force stack checking for all funcs
-  "Gs", // [num] control stack checking calls
-  "Gh", // enable _penter function call
-  "GH", // enable _pexit function call
-  "GT", // generate fiber-safe TLS accesses
-  "RTC1", // Enable fast checks (/RTCsu)
-  "RTCc", // Convert to smaller type checks
-  "RTCs", // Stack Frame runtime checking
-  "RTCu", // Uninitialized local usage checks
-  "clr", // compile for common language runtime
-  "Gd", // __cdecl calling convention
-  "Gr", // __fastcall calling convention
-  "Gz", // __stdcall calling convention
-  "GZ", // Enable stack checks (/RTCs)
-  "QIfist", // [-] use FIST instead of ftol()
-  "hotpatch", // ensure function padding for hotpatchable images
-  "arch:", // <SSE|SSE2> minimum CPU architecture requirements
-  "Fa", // [file] name assembly listing file
-  "FA", // [scu] configure assembly listing
-  "Fd", // [file] name .PDB file
-  "Fe", // <file> name executable file
-  "Fm", // [file] name map file
-  "Fo", // <file> name object file
-  "Fp", // <file> name precompiled header file
-  "Fr", // [file] name source browser file
-  "FR", // [file] name extended .SBR file
-  "doc", // [file] process XML documentation comments
-  "AI", // <dir> add to assembly search path
-  "FU", // <file> forced using assembly/module
-  "C", //  don't strip comments
-  "D", // <name>{=|#}<text> define macro
-  "E", //  preprocess to stdout
-  "EP", //  preprocess to stdout, no #line
-  "P", //  preprocess to file
-  "Fx", //  merge injected code to file
-  "FI", // <file> name forced include file
-  "U", // <name> remove predefined macro
-  "u", //  remove all predefined macros
-  "I", // <dir> add to include search path
-  "X", //  ignore "standard places"
-  "Zi", //  enable debugging information
-  "Z7", //  enable old-style debug info
-  "Zp", // [n] pack structs on n-byte boundary
-  "Za", //  disable extensions
-  "Ze", //  enable extensions (default)
-  "Zl", //  omit default library name in .OBJ
-  "Zg", //  generate function prototypes
-  "Zs", //  syntax check only
-  "vd", // {0|1|2} disable/enable vtordisp
-  "vm", // <x> type of pointers to members
-  "Zc:", // arg1[,arg2] C++ language conformance, where arguments can be:
-  "ZI", //  enable Edit and Continue debug info
-  "openmp", //  enable OpenMP 2.0 language extensions
-  "analyze",
-  "errorReport",
-  "?",
-  "help", //  print this help message
-  "FC", //  use full pathnames in diagnostics /H<num> max external name length
-  "J", //  default char type is unsigned
-  "nologo", //  suppress copyright message
-  "show", // Includes show include file names
-  "Tc", // <source file> compile file as .c
-  "Tp", // <source file> compile file as .cpp
-  "TC", // compile all files as .c
-  "TP", // compile all files as .cpp
-  "V", // <string> set version string
-  "w", // disable all warnings
-  "wd", // <n> disable warning n
-  "we", // <n> treat warning n as an error
-  "wo", // <n> issue warning n once
-  "w", // <l><n> set warning level 1-4 for n
-  "W", // <n> set warning level (default n=1)
-  "Wall", // enable all warnings
-  "WL", // enable one line diagnostics
-  "WX", // treat warnings as errors
-  "Yc", // [file] create .PCH file
-  "Yd", // put debug info in every .OBJ
-  "Yl", // [sym] inject .PCH ref for debug lib
-  "Yu", // [file] use .PCH file
-  "Y", // - disable all PCH options
-  "Zm", // <n> max memory alloc (% of default)
-  "Wp64", // enable 64 bit porting warnings
-  "LD", //  Create .DLL
-  "LDd", //  Create .DLL debug library
-  "LN", //  Create a .netmodule
-  "F", // <num> set stack size
-  "link", //  [linker options and libraries]
-  "MD", //  link with MSVCRT.LIB
-  "MT", //  link with LIBCMT.LIB
-  "MDd", //  link with MSVCRTD.LIB debug lib
-  "MTd", //  link with LIBCMTD.LIB debug lib
+  L"O1", // minimize space
+  L"O2", // maximize speed
+  L"Ob", // <n> inline expansion (default n=0)
+  L"Od", // disable optimizations (default)
+  L"Og", // enable global optimization
+  L"Oi", // [-] enable intrinsic functions
+  L"Os", // favor code space
+  L"Ot", // favor code speed
+  L"Ox", // maximum optimizations
+  L"Oy", // [-] enable frame pointer omission
+  L"GF", // enable read-only string pooling
+  L"Gm", // [-] enable minimal rebuild
+  L"Gy", // [-] separate functions for linker
+  L"GS", // [-] enable security checks
+  L"GR", // [-] enable C++ RTTI
+  L"GX", // [-] enable C++ EH (same as /EHsc)
+  L"EHs", // enable C++ EH (no SEH exceptions)
+  L"EHa", // enable C++ EH (w/ SEH exceptions)
+  L"EHc", // extern "C" defaults to nothrow
+  L"fp", // floating-point model
+  L"GL", // [-] enable link-time code generation
+  L"GA", // optimize for Windows Application
+  L"Ge", // force stack checking for all funcs
+  L"Gs", // [num] control stack checking calls
+  L"Gh", // enable _penter function call
+  L"GH", // enable _pexit function call
+  L"GT", // generate fiber-safe TLS accesses
+  L"RTC1", // Enable fast checks (/RTCsu)
+  L"RTCc", // Convert to smaller type checks
+  L"RTCs", // Stack Frame runtime checking
+  L"RTCu", // Uninitialized local usage checks
+  L"clr", // compile for common language runtime
+  L"Gd", // __cdecl calling convention
+  L"Gr", // __fastcall calling convention
+  L"Gz", // __stdcall calling convention
+  L"GZ", // Enable stack checks (/RTCs)
+  L"QIfist", // [-] use FIST instead of ftol()
+  L"hotpatch", // ensure function padding for hotpatchable images
+  L"arch:", // <SSE|SSE2> minimum CPU architecture requirements
+  L"Fa", // [file] name assembly listing file
+  L"FA", // [scu] configure assembly listing
+  L"Fd", // [file] name .PDB file
+  L"Fe", // <file> name executable file
+  L"Fm", // [file] name map file
+  L"Fo", // <file> name object file
+  L"Fp", // <file> name precompiled header file
+  L"Fr", // [file] name source browser file
+  L"FR", // [file] name extended .SBR file
+  L"doc", // [file] process XML documentation comments
+  L"AI", // <dir> add to assembly search path
+  L"FU", // <file> forced using assembly/module
+  L"C", //  don't strip comments
+  L"D", // <name>{=|#}<text> define macro
+  L"E", //  preprocess to stdout
+  L"EP", //  preprocess to stdout, no #line
+  L"P", //  preprocess to file
+  L"Fx", //  merge injected code to file
+  L"FI", // <file> name forced include file
+  L"U", // <name> remove predefined macro
+  L"u", //  remove all predefined macros
+  L"I", // <dir> add to include search path
+  L"X", //  ignore "standard places"
+  L"Zi", //  enable debugging information
+  L"Z7", //  enable old-style debug info
+  L"Zp", // [n] pack structs on n-byte boundary
+  L"Za", //  disable extensions
+  L"Ze", //  enable extensions (default)
+  L"Zl", //  omit default library name in .OBJ
+  L"Zg", //  generate function prototypes
+  L"Zs", //  syntax check only
+  L"vd", // {0|1|2} disable/enable vtordisp
+  L"vm", // <x> type of pointers to members
+  L"Zc:", // arg1[,arg2] C++ language conformance, where arguments can be:
+  L"ZI", //  enable Edit and Continue debug info
+  L"openmp", //  enable OpenMP 2.0 language extensions
+  L"analyze",
+  L"errorReport",
+  L"?",
+  L"help", //  print this help message
+  L"FC", //  use full pathnames in diagnostics /H<num> max external name length
+  L"J", //  default char type is unsigned
+  L"nologo", //  suppress copyright message
+  L"show", // Includes show include file names
+  L"Tc", // <source file> compile file as .c
+  L"Tp", // <source file> compile file as .cpp
+  L"TC", // compile all files as .c
+  L"TP", // compile all files as .cpp
+  L"V", // <string> set version string
+  L"w", // disable all warnings
+  L"wd", // <n> disable warning n
+  L"we", // <n> treat warning n as an error
+  L"wo", // <n> issue warning n once
+  L"w", // <l><n> set warning level 1-4 for n
+  L"W", // <n> set warning level (default n=1)
+  L"Wall", // enable all warnings
+  L"WL", // enable one line diagnostics
+  L"WX", // treat warnings as errors
+  L"Yc", // [file] create .PCH file
+  L"Yd", // put debug info in every .OBJ
+  L"Yl", // [sym] inject .PCH ref for debug lib
+  L"Yu", // [file] use .PCH file
+  L"Y", // - disable all PCH options
+  L"Zm", // <n> max memory alloc (% of default)
+  L"Wp64", // enable 64 bit porting warnings
+  L"LD", //  Create .DLL
+  L"LDd", //  Create .DLL debug library
+  L"LN", //  Create a .netmodule
+  L"F", // <num> set stack size
+  L"link", //  [linker options and libraries]
+  L"MD", //  link with MSVCRT.LIB
+  L"MT", //  link with LIBCMT.LIB
+  L"MDd", //  link with MSVCRTD.LIB debug lib
+  L"MTd", //  link with LIBCMTD.LIB debug lib
   NULL
 };
 
-void ms_cl_cmdlinet::process_cl_option(const std::string &s)
+void ms_cl_cmdlinet::process_cl_option(const std::wstring &s)
 {
-  if(s=="") return;
+  if(s==L"") return;
 
   if(s[0]!='/' && s[0]!='-')
   {
-    args.push_back(s);
+    args.push_back(w2s(s)); // loss!
     return;
   }
 
   for(unsigned j=0; ms_cl_flags[j]!=NULL; j++)
   {
-    if(std::string(s, 1, std::string::npos)==ms_cl_flags[j])
+    if(std::wstring(s, 1, std::string::npos)==ms_cl_flags[j])
     {
       cmdlinet::optiont option;
 
@@ -482,7 +511,7 @@ void ms_cl_cmdlinet::process_cl_option(const std::string &s)
       else
       {
         option.islong=true;
-        option.optstring=std::string(s, 1, std::string::npos);
+        option.optstring=w2s(std::wstring(s, 1, std::string::npos)); // loss
         option.optchar=0;
       }
 
@@ -501,14 +530,14 @@ void ms_cl_cmdlinet::process_cl_option(const std::string &s)
   
   for(unsigned j=0; ms_cl_prefixes[j]!=NULL; j++)
   {
-    unsigned length=strlen(ms_cl_prefixes[j]);
-    if(std::string(s, 1, length)==ms_cl_prefixes[j])
+    std::wstring ms_cl_prefix=ms_cl_prefixes[j];
+    if(std::wstring(s, 1, ms_cl_prefix.size())==ms_cl_prefix)
     {
       cmdlinet::optiont option;
       
       int optnr;
 
-      if(length==1)
+      if(ms_cl_prefix.size()==1)
       {
         option.islong=false;
         option.optstring="";
@@ -518,7 +547,7 @@ void ms_cl_cmdlinet::process_cl_option(const std::string &s)
       else
       {
         option.islong=true;
-        option.optstring=std::string(s, 1, length);
+        option.optstring=w2s(std::wstring(s, 1, ms_cl_prefix.size())); // loss
         option.optchar=0;
         optnr=getoptnr(option.optstring);
       }
@@ -530,12 +559,13 @@ void ms_cl_cmdlinet::process_cl_option(const std::string &s)
       }
 
       options[optnr].isset=true;
-      options[optnr].values.push_back(std::string(s, length+1, std::string::npos));
+      options[optnr].values.push_back(
+        w2s(std::wstring(s, ms_cl_prefix.size()+1, std::string::npos))); // loss
       return;
     }
   }
 
   // unrecognized option
-  std::cout << "Warning: uninterpreted CL option `" 
-            << s << "'" << std::endl;
+  std::wcout << L"Warning: uninterpreted CL option `" 
+             << s << L"'" << std::endl;
 }
