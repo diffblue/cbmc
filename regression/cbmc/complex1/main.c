ed69236eb7b5d696070fb84c6b7afd544953af3c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-10 09:15:25 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1410 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/cbmc/complex1/main.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdio.h>
+
+int main()
+{
+  _Complex c;
+  c=1.0i+2;
+
+  _Complex double d;
+  
+  _Complex char char_complex, char_complex2;
+  
+  char_complex=3i-2;
+  
+  assert(sizeof(d)==sizeof(c));
+  assert(sizeof(char_complex)==sizeof(char)*2);
+  
+  assert(__real__ c == 2);
+  assert(__imag__ c == 1);
+
+  assert(__real__ char_complex == -2);
+  assert(__imag__ char_complex == 3);
+  
+  // complex conjugate
+  char_complex2 = ~ char_complex;
+}
