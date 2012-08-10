af7e8f8c28c4908dcda41455e5aaed7622af2a75
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-10 22:49:12 +0000

    more

---
+++ regression/ansi-c/sizeof1/main.c
@@ -0,0 +1,43 @@
+#include <wchar.h>
+
+#define STATIC_ASSERT(condition) \
+  int some_array##__LINE__[(condition) ? 1 : -1];    
+
+#ifdef _WIN32
+
+#ifdef _WIN64
+
+STATIC_ASSERT(sizeof(void *)==8);
+STATIC_ASSERT(sizeof(int)==4);
+STATIC_ASSERT(sizeof(long int)==4);
+STATIC_ASSERT(sizeof(long long int)==8);
+STATIC_ASSERT(sizeof(wchar_t)==2);
+STATIC_ASSERT(sizeof(long double)==8);
+
+#else
+
+STATIC_ASSERT(sizeof(void *)==4);
+STATIC_ASSERT(sizeof(int)==4);
+STATIC_ASSERT(sizeof(long int)==4);
+STATIC_ASSERT(sizeof(long long int)==8);
+STATIC_ASSERT(sizeof(wchar_t)==2);
+STATIC_ASSERT(sizeof(long double)==8);
+
+#endif
+
+#else
+
+// the following does both 32 and 64 bit architectures
+
+STATIC_ASSERT(sizeof(void *)==sizeof(long int));
+STATIC_ASSERT(sizeof(int)==4);
+STATIC_ASSERT(sizeof(long int)==4 || sizeof(long int)==8);
+STATIC_ASSERT(sizeof(long long int)==8);
+STATIC_ASSERT(sizeof(wchar_t)==4);
+STATIC_ASSERT(sizeof(long double)==sizeof(void *)*2);
+
+#endif
+
+int main()
+{
+}
