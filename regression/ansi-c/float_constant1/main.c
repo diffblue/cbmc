33289a8410e972eb5b8f655d59a649e82eb44b32
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-10 08:21:37 +0000

    imaginary constants

--- regression/ansi-c/float_constant1/main.c
+++ regression/ansi-c/float_constant1/main.c
@@ -1,8 +1,18 @@
 #define STATIC_ASSERT(condition) \
   int some_array##__LINE__[(condition) ? 1 : -1];    
 
+// hex-based constants
 STATIC_ASSERT(0x1.0p-95f == 2.524355e-29f);
+
+_Complex c;
 
 int main()
 {
+  // imaginary constants
+  c=(__extension__ 1.0iF);
+  c=(__extension__ 1.0Fi);
+  c=(__extension__ 1.0jF);
+  c=(__extension__ 1.0j);
+  c=(__extension__ 1.0jL);
+  c=(__extension__ 1.0il);
 }
