75f93bee91d14cefcb1dafd8dc4f47fce81b507f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-11 12:31:38 +0000

    more

---
+++ regression/ansi-c/_Bool1/main.c
@@ -0,0 +1,19 @@
+#define STATIC_ASSERT(condition) \
+  int some_array##__LINE__[(condition) ? 1 : -1];    
+
+// C11:
+// 6.3.1.2 Boolean type
+// When any scalar value is converted to _Bool, the result is 0 if the value
+// compares equal to 0; otherwise, the result is 1.
+
+STATIC_ASSERT((_Bool)10);
+STATIC_ASSERT(((_Bool)10)==1);
+STATIC_ASSERT(!(_Bool)0);
+STATIC_ASSERT((_Bool)0.1);
+STATIC_ASSERT(!(_Bool)(int)0.1);
+STATIC_ASSERT(!(_Bool)0.0);
+STATIC_ASSERT(!(_Bool)-0.0);
+
+int main()
+{
+}
