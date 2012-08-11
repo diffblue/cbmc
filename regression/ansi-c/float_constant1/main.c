abd0eefd5756cedd1ba31d222b51dc5ff5f2fdd6
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-11 13:41:47 +0000

    ifdefs

--- regression/ansi-c/float_constant1/main.c
+++ regression/ansi-c/float_constant1/main.c
@@ -7,15 +7,19 @@ STATIC_ASSERT(0x1.0p-95f == 2.524355e-29f);
 // also with upper case X, P, F
 STATIC_ASSERT(0X1.0P-95F == 2.524355e-29f);
 
+#ifdef __GNUC__
 _Complex c;
+#endif
 
 int main()
 {
-  // imaginary constants
+  // imaginary constants, these are GCC only
+  #ifdef __GNUC__
   c=(__extension__ 1.0iF);
   c=(__extension__ 1.0Fi);
   c=(__extension__ 1.0jF);
   c=(__extension__ 1.0j);
   c=(__extension__ 1.0jL);
   c=(__extension__ 1.0il);
+  #endif
 }
