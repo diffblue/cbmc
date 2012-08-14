3d4586b249b6b9cbb0fe79d168b4d9a672abc955
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-08-14 13:14:10 +0000

    ELF 64

--- src/goto-programs/elf_reader.cpp
+++ src/goto-programs/elf_reader.cpp
@@ -22,51 +22,102 @@ Function: elf_readert::elf_readert
 
 elf_readert::elf_readert(std::istream &_in):in(_in)
 {
-  // read header
-  in.read((char *)&elf_header, sizeof(elf_header));
+  // read 32-bit header
+  in.read((char *)&elf32_header, sizeof(elf32_header));
 
   if(!in)
     throw "failed to read ELF header";
 
-  if(elf_header.e_ident[0]!=0x7f ||
-     elf_header.e_ident[1]!='E' ||
-     elf_header.e_ident[2]!='L' ||
-     elf_header.e_ident[3]!='F')
+  if(elf32_header.e_ident[0]!=0x7f ||
+     elf32_header.e_ident[1]!='E' ||
+     elf32_header.e_ident[2]!='L' ||
+     elf32_header.e_ident[3]!='F')
    throw "ELF header malformed (magic)";
-  
-  char ei_data=elf_header.e_ident[5];
-  
-  bool little_endian;
-  
-  if(ei_data==1)
-    little_endian=true;
-  else if(ei_data==2)
-    little_endian=false;
-  else
-    throw "ELF header malformed (EI_DATA)";
-  
-  // get offset for section header
-  if(elf_header.e_shoff==0)
-    throw "ELF without section header";
-  
-  section_header_table.resize(elf_header.e_shnum);
 
-  // iterate over these
-  for(unsigned i=0; i<section_header_table.size(); i++)
+  elf_class=(elf_classt)elf32_header.e_ident[4];
+  
+  if(elf_class==ELF32)
   {
-    // go to right place
-    in.seekg(elf_header.e_shoff+i*elf_header.e_shentsize);
+    char ei_data=elf32_header.e_ident[5];
+    
+    if(ei_data==1)
+      little_endian=true;
+    else if(ei_data==2)
+      little_endian=false;
+    else
+      throw "ELF32 header malformed (EI_DATA)";
 
-    // read section header
-    in.read((char *)&section_header_table[i], sizeof(Elf32_Shdr));
+    if(elf32_header.e_version!=1)
+      throw "unknown ELF32 version";
+      
+    // get offset for section header
+    if(elf32_header.e_shoff==0 ||
+       elf32_header.e_shnum==0)
+      throw "ELF32 without section header";
+
+    elf32_section_header_table.resize(elf32_header.e_shnum);
+    number_of_sections=elf32_header.e_shnum;
+
+    // iterate over these
+    for(unsigned i=0; i<elf32_section_header_table.size(); i++)
+    {
+      // go to right place
+      in.seekg(elf32_header.e_shoff+i*elf32_header.e_shentsize);
+
+      // read section header
+      in.read((char *)&elf32_section_header_table[i], sizeof(Elf32_Shdr));
+    }
+    
+    // string table
+    unsigned string_table_nr=elf32_header.e_shstrndx;
+    if(string_table_nr>=elf32_section_header_table.size())
+      throw "ELF32 without string table";
+
+    string_table_offset=section_offset(string_table_nr);
   }
-  
-  // string table
-  unsigned string_table_nr=elf_header.e_shstrndx;
-  if(string_table_nr>=section_header_table.size())
-    throw "ELF without string table";
+  else if(elf_class==ELF64)
+  {
+    // read 64-bit header
+    in.seekg(0);
+    in.read((char *)&elf64_header, sizeof(elf64_header));
 
-  string_table_offset=section_header_table[string_table_nr].sh_offset;
+    char ei_data=elf64_header.e_ident[5];
+    
+    if(ei_data==1)
+      little_endian=true;
+    else if(ei_data==2)
+      little_endian=false;
+    else
+      throw "ELF64 header malformed (EI_DATA)";
+
+    if(elf64_header.e_version!=1)
+      throw "unknown ELF64 version";
+      
+    // get offset for section header
+    if(elf64_header.e_shoff==0 ||
+       elf64_header.e_shnum==0)
+      throw "ELF64 without section header";
+
+    elf64_section_header_table.resize(elf64_header.e_shnum);
+    number_of_sections=elf64_header.e_shnum;
+
+    // iterate over these
+    for(unsigned i=0; i<elf64_section_header_table.size(); i++)
+    {
+      // go to right place
+      in.seekg(elf64_header.e_shoff+i*elf64_header.e_shentsize);
+
+      // read section header
+      in.read((char *)&elf64_section_header_table[i], sizeof(Elf64_Shdr));
+    }
+    
+    // string table
+    unsigned string_table_nr=elf64_header.e_shstrndx;
+    if(string_table_nr>=elf64_section_header_table.size())
+      throw "ELF64 without string table";
+
+    string_table_offset=section_offset(string_table_nr);
+  }
 }
 
 /*******************************************************************\
@@ -81,7 +132,7 @@ Function: elf_readert::get_string
 
 \*******************************************************************/
 
-std::string elf_readert::get_string(unsigned index)
+std::string elf_readert::get_string(std::streampos index)
 {
   in.seekg(string_table_offset+index);
 
