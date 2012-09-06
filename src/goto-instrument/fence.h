c1dde813531f3f13a0b13c4a05e6c559182590d8
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-06 16:31:31 +0000

    new code from Vincent

---
+++ src/goto-instrument/fence.h
@@ -0,0 +1,27 @@
+/*******************************************************************\
+
+Module: Fences for instrumentation
+
+Author: Vincent Nimal
+
+Date: February 2012
+
+\*******************************************************************/
+
+#ifndef FENCE_H
+#define FENCE_H
+
+#include <context.h>
+#include <goto-programs/goto_program.h>
+
+bool is_fence(
+  goto_programt::instructiont instruction,
+  contextt &context
+);
+
+bool is_lwfence(
+  goto_programt::instructiont instruction,
+  contextt &context
+);
+
+#endif
