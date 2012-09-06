d01074e540d26637cb618d13de8030b262baad4b
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-06 16:30:02 +0000

    new code from Vincent
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1546 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/weak_memory.h
+++ src/goto-instrument/weak_memory.h
@@ -16,16 +16,26 @@ Date: September 2011
 #include <goto-programs/goto_program.h>
 #include <goto-programs/goto_functions.h>
 
+#ifndef MEMORY_MODEL
+#define MEMORY_MODEL
 typedef enum { TSO, PSO, RMO, POWER } weak_memory_modelt;
+#endif
 
 void weak_memory(
   weak_memory_modelt model,
-  value_setst &value_sets,
-  class contextt &context,
-  goto_functionst &goto_functions,
-  bool one_partition,
+  value_setst& value_sets,
+  contextt& context,
+  goto_functionst& goto_functions,
+  bool SCC,
   bool one_event_per_cycle,
   bool my_events,
-  unsigned unwinding_bound);
+  unsigned unwinding_bound,
+  bool no_cfg_kill,
+  bool no_dependencies,
+  unsigned max_var,
+  unsigned max_po_trans,
+  bool render_po,
+  bool render_file,
+  bool render_function);
 
 #endif
