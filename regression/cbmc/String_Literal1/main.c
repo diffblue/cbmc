9111955dd5a2df66e714e49a115126e25156fdea
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-14 08:57:03 +0000

    C11 unicode test

--- regression/cbmc/String_Literal1/main.c
+++ regression/cbmc/String_Literal1/main.c
@@ -16,4 +16,16 @@ int main()
   assert(L"abc"[1]=='b');
   assert(L"abc"[3]==0);
   assert(L"\x1234"[0]==0x1234);
+  
+  // the following is a C11 UTF-8 string literal
+  const char euro_sign[]=u8"\x20ac";
+  assert((unsigned char)euro_sign[0]==0xe2);
+  assert((unsigned char)euro_sign[1]==0x82);
+  assert((unsigned char)euro_sign[2]==0xac);
+  assert(euro_sign[3]==0);
+  
+  // C11 unicode string literals
+  assert(sizeof(u8""[0])==sizeof(char));
+  assert(sizeof(u""[0])==2);
+  assert(sizeof(U""[0])==4);
 }
