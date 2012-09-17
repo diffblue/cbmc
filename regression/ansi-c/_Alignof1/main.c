20f0e24a8633498a3b5a2eab290adf5d47cefa61
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-17 16:10:19 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1699 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/ansi-c/_Alignof1/main.c
@@ -0,0 +1,21 @@
+#define STATIC_ASSERT(condition) \
+  int some_array##__LINE__[(condition) ? 1 : -1];    
+
+// C11: _Alignof
+// 6.5.3.4
+// The _Alignof operator yields the alignment requirement of its operand
+// type.  The operand is not evaluated and the result is an integer
+// constant.  When applied to an array type, the result is the alignment
+// requirement of the element type.
+
+// The _Alignof operator yields the alignment requirement of its operand
+// type.  The operand is not evaluated and the result is an integer
+// constant.  When applied to an array type, the result is the alignment
+// requirement of the element type.
+
+STATIC_ASSERT(_Alignof(char)==1);
+STATIC_ASSERT(_Alignof(char[10])==1);
+
+int main()
+{
+}
