3ccab2408e3810aa696027430436b2d22e1c3649
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-18 11:49:50 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1708 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc/Array_Initialization3/main.c
+++ regression/cbmc/Array_Initialization3/main.c
@@ -14,6 +14,10 @@ int array4[2] = { [1] 42 };
 int array5[2][2] = { { 42, 42 }, {[1] 42 } };
 int array6[2][2] = { [1] {[1] 42 } };
 
+typedef int var_a[];
+var_a array7 = { 1, 2 };
+var_a array8 = { 1, 2, 3 };
+
 int main(void)
 {
   assert(array1[0][1] ==2);
@@ -25,6 +29,9 @@ int main(void)
   assert(sizeof(array2)==sizeof(int)*11);
   
   assert(sizeof(array3)==sizeof(int)*8);
+  
+  assert(sizeof(array7)==sizeof(int)*2);
+  assert(sizeof(array8)==sizeof(int)*3);
   
   return 0;
 }
