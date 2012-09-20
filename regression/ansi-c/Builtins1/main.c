da582e469d71e8d8ac0a867a0849f89a4c1492ef
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-09-20 14:03:14 +0000

    check all usual includes
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1742 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/ansi-c/Builtins1/main.c
@@ -0,0 +1,28 @@
+#include <alloca.h>
+#include <assert.h>
+#include <complex.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <math.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#ifndef _WIN32
+#include <unistd.h>
+#include <pthread.h>
+#endif
+
+int main()
+{
+  return 0;
+}
+
