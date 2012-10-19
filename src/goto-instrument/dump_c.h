d9d07c1dfbdcbd07842760b35a0c6f97a4644b0a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-10-19 09:13:51 +0000

    moved dump_c to goto-instrument
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1827 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/dump_c.h
+++ src/goto-instrument/dump_c.h
@@ -9,7 +9,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_GOTO_PROGRAM_DUMP_C_H
 #define CPROVER_GOTO_PROGRAM_DUMP_C_H
 
-#include "goto_functions.h"
+#include <goto-programs/goto_functions.h>
 
 void dump_c(
   const goto_functionst &src,
