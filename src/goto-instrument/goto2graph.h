af32c1ae4c3c3c196138fb21462cb71bfb160579
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-10-27 14:59:30 +0000

    new version from Vincent

--- src/goto-instrument/goto2graph.h
+++ src/goto-instrument/goto2graph.h
@@ -51,12 +51,14 @@ protected:
   bool render_by_file;
   bool render_by_function;
 
-  inline bool local(const irep_idt& id);
+  bool inline local(const irep_idt& id);
 
   void inline add_instr_to_interleaving (
     goto_programt::instructionst::iterator it,
     goto_programt& interleaving);
   bool is_cfg_spurious(const event_grapht::critical_cyclet& cyc);
+
+  unsigned d(const event_grapht::critical_cyclet::delayt& e);
 
   typedef std::set<event_grapht::critical_cyclet> set_of_cyclest;
   void inline instrument_all_inserter(
@@ -70,8 +72,7 @@ protected:
   void inline instrument_minimum_interference_inserter(
     const set_of_cyclest& set);
   void inline instrument_my_events_inserter(
-    const set_of_cyclest& set,
-    const std::set<unsigned>& events);
+    const set_of_cyclest& set, const std::set<unsigned>& events);
 
   void inline print_outputs_local(
     const std::set<event_grapht::critical_cyclet>& set,
@@ -79,6 +80,106 @@ protected:
     std::ofstream& ref,
     std::ofstream& output,
     weak_memory_modelt model);
+
+  typedef std::set<goto_programt::instructiont::targett> target_sett;
+
+  class cfg_visitort 
+  {
+  protected: 
+    namespacet& ns;
+    instrumentert& instrumenter;
+    event_grapht& egraph;
+    std::vector<std::set<unsigned> >& egraph_SCCs;    
+    graph<abstract_eventt>& egraph_alt;  
+
+    /* for thread marking (dynamic) */
+    unsigned current_thread;
+    unsigned coming_from;
+
+  public:
+    unsigned max_thread;
+
+    /* relations between irep and Reads/Writes */
+    typedef std::multimap<irep_idt,unsigned> id2nodet;
+    typedef std::pair<irep_idt,unsigned> id2node_pairt;
+    id2nodet map_reads, map_writes;
+
+    unsigned write_counter;
+    unsigned read_counter;
+
+    /* previous nodes (fwd analysis) */
+    typedef std::pair<unsigned,unsigned> nodet;
+    typedef std::map<goto_programt::instructiont::targett,std::set<nodet> > 
+      incoming_post;
+
+    incoming_post in_pos;
+    std::set<goto_programt::instructiont::targett> updated;
+
+    /* "next nodes" (bwd steps in fwd/bck analysis) */
+    incoming_post out_pos;    
+
+    #define add_all_pos(it, target, source) \
+    for(std::set<nodet>::const_iterator \
+      it=(source).begin(); \
+      it!=(source).end(); ++it) \
+      (target).insert(*it);
+
+    /* current thread number */
+    unsigned thread;
+
+    /* dependencies */
+    data_dpt data_dp;
+
+    /* set of functions visited so far -- we don't handle recursive functions */
+    std::set<irep_idt> functions_met;
+
+    cfg_visitort(namespacet& _ns, instrumentert& _instrumenter)
+    :ns(_ns), instrumenter(_instrumenter), egraph(_instrumenter.egraph),
+      egraph_SCCs(_instrumenter.egraph_SCCs), 
+      egraph_alt(_instrumenter.egraph_alt)
+    {
+      write_counter = 0;
+      read_counter = 0;
+      thread = 0;
+      current_thread = 0;
+      max_thread = 0;
+      coming_from = 0;
+    }
+
+    void inline visit_function(const irep_idt& function)
+    {
+      if(functions_met.find(function)!=functions_met.end())
+        throw ("Sorry, doesn't handle recursive function for the moment");
+      functions_met.insert(function);
+    }
+
+    void inline leave_function(const irep_idt& function)
+    {
+      functions_met.erase(function);
+    }
+
+    void inline visit_cfg(
+      value_setst& value_sets,
+      weak_memory_modelt model,
+      bool no_dependencies,
+      const irep_idt& function)
+    {
+      visit_function(function);
+      visit_cfg_impl(value_sets, model, no_dependencies, function,
+        std::set<nodet>());
+      leave_function(function);
+    }
+
+    // TODO: move the visitor outside, and inherit
+    virtual const std::set<nodet>& visit_cfg_impl(
+      value_setst& value_sets,
+      weak_memory_modelt model,
+      bool no_dependencies,
+      const irep_idt& function,
+      const std::set<nodet>& initial_vertex);
+
+    bool inline local(const irep_idt& i);
+  };
 
 public:
   /* graph */
@@ -96,7 +197,7 @@ public:
 
   /* variables to instrument, locations of variables to instrument on 
      the cycles, and locations of all the variables on the critical cycles */
-  /* TO FIX: those maps are here to interface easily with weak_mem.cpp, 
+  /* TODO: those maps are here to interface easily with weak_mem.cpp, 
      but a rewriting of weak_mem can eliminate them */
   std::set<irep_idt> var_to_instr;
   std::multimap<irep_idt,locationt> id2loc;
@@ -108,8 +209,9 @@ public:
   {
   }
 
-  /* abstracts goto-programs in abstract event graph */
-  void goto2graph(
+  /* abstracts goto-programs in abstract event graph, and computes
+     the thread numbering and returns the max number */
+  unsigned goto2graph_cfg(
     value_setst& value_sets,
     weak_memory_modelt model,
     bool no_dependencies);
