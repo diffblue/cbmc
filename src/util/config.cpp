85b6ccee4cc924746fd1cadb91c17af502b8181e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-11-03 16:26:26 +0000

    FLOATBV cleanup
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1888 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/config.cpp
+++ src/util/config.cpp
@@ -253,12 +253,7 @@ bool configt::set(const cmdlinet &cmdline)
   else
     ansi_c.set_32();
     
-  #ifdef HAVE_FLOATBV
   ansi_c.use_fixed_for_float=false;
-  #else
-  ansi_c.use_fixed_for_float=true;
-  #endif
-
   ansi_c.endianness=ansi_ct::NO_ENDIANNESS;
   ansi_c.os=ansi_ct::NO_OS;
   ansi_c.arch=ansi_ct::NO_ARCH;
