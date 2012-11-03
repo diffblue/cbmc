3e5dbe11484fee0266148b35a7dd386735ebcdbd
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-11-03 14:25:31 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1878 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cpp/typecast_ambiguity1/main.cpp
+++ regression/cpp/typecast_ambiguity1/main.cpp
@@ -16,4 +16,7 @@ int main()
   (Y) & z; // this is a bitwise and
   
   (X) * p; // this is a typecast
+
+  z=(int)(p) & 0x1fff; // this is bitwise and
+  // z=(int)(p) & z; // this is bitwise and
 }
