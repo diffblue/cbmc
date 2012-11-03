42b1dbf1037ca52b30494d01224990cfe537fbe9
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-11-03 15:44:41 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1883 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cpp/static_assert1/main.cpp
+++ regression/cpp/static_assert1/main.cpp
@@ -5,9 +5,9 @@ static_assert(sizeof(int)==sizeof(int), "sizeof(int)==sizeof(int)");
 class C1
 {
   static_assert(2==2, "2==2");
-  static_assert(sizeof(T)==sizeof(int), "sizeof(T)==sizeof(int)");
   
   typedef int T;
+  static_assert(sizeof(T)==sizeof(int), "sizeof(T)==sizeof(int)");
 };
 
 template<typename T>
@@ -16,8 +16,10 @@ class C2
   static_assert(sizeof(T)==sizeof(char), "sizeof(T)==sizeof(char)");
 };
 
+C2<char> C2_inst;
+
 int main()
 {
   typedef double T;
-  static_assert(sizeof(T)==sizeof(char), "sizeof(T)==sizeof(double)");
+  static_assert(sizeof(T)==sizeof(double), "sizeof(T)==sizeof(double)");
 }
