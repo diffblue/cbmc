040fc47ff298e067df456c763cda5a7ea000dec7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-11-03 11:19:28 +0000

    goto_convertt file cleanup

--- src/goto-programs/goto_convert.cpp
+++ src/goto-programs/goto_convert.cpp
@@ -15,6 +15,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <std_expr.h>
 #include <context.h>
 #include <simplify_expr.h>
+#include <rename.h>
 
 #include <ansi-c/c_types.h>
 
@@ -2503,3 +2504,118 @@ void goto_convertt::make_temp_symbol(
   expr=symbol_expr(new_symbol);  
 }
 
+/*******************************************************************\
+
+Function: goto_convertt::new_name
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void goto_convertt::new_name(symbolt &symbol)
+{
+  // rename it
+  get_new_name(symbol, ns);
+
+  // store in context
+  context.add(symbol);
+}
+
+/*******************************************************************\
+
+Function: goto_convertt::lookup
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+const symbolt &goto_convertt::lookup(const irep_idt &identifier) const
+{
+  const symbolt *symbol;
+  if(ns.lookup(identifier, symbol))
+    throw "failed to find symbol "+id2string(identifier);
+  return *symbol;
+}
+
+/*******************************************************************\
+
+Function: goto_convert
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void goto_convert(
+  const codet &code,
+  contextt &context,
+  const optionst &options,
+  goto_programt &dest,
+  message_handlert &message_handler)
+{
+  goto_convertt goto_convert(context, options, message_handler);
+
+  try
+  {
+    goto_convert.goto_convert(code, dest);
+  }
+  
+  catch(int)
+  {
+    goto_convert.error();
+  }
+
+  catch(const char *e)
+  {
+    goto_convert.error(e);
+  }
+
+  catch(const std::string &e)
+  {
+    goto_convert.error(e);
+  }
+
+  if(goto_convert.get_error_found())
+    throw 0;
+}
+
+/*******************************************************************\
+
+Function: goto_convert
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void goto_convert(
+  contextt &context,
+  const optionst &options,
+  goto_programt &dest,
+  message_handlert &message_handler)
+{
+  // find main symbol
+  const contextt::symbolst::const_iterator s_it=
+    context.symbols.find("main");
+  
+  if(s_it==context.symbols.end())
+    throw "failed to find main symbol";
+  
+  const symbolt &symbol=s_it->second;
+  
+  ::goto_convert(to_code(symbol.value), context, options, dest, message_handler);
+}
