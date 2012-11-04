c3dd8d7e9d0480cecc3946fc94c7fe950b97366a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2012-11-04 17:49:29 +0000

    more

--- regression/cpp/ModeC2/main.cpp
+++ regression/cpp/ModeC2/main.cpp
@@ -16,10 +16,26 @@ extern "C" void g(char)
 {
 }
 
+// same again inside a namespace
+namespace SOME
+{
+  extern "C" void somef(char)
+  {
+  }
+  
+  extern "C++" void somef(int)
+  {
+  }
+}
+
+using SOME::somef;
+
 int main()
 {
   f(int(1));
   f(char(1));
   g(int(1));
   g(char(1));
+  somef(int(1));
+  somef(char(1));
 }
