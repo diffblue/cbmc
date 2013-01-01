94badb5cb0ccbbd3bd996315fc30ecbc62273c0f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-01-01 14:19:50 +0000

    added missing pointer_predicates.h
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@1968 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/goto-programs/pointer_predicates.h
@@ -0,0 +1,30 @@
+/*******************************************************************\
+
+Module: Various predicates over pointers in programs
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_GOTO_PROGRAMS_POINTER_PREDICATES_H
+#define CPROVER_GOTO_PROGRAMS_POINTER_PREDIATES_H
+
+#include <namespace.h>
+
+exprt same_object(const exprt &p1, const exprt &p2);
+exprt deallocated(const exprt &pointer, const namespacet &ns);
+exprt dynamic_size(const namespacet &ns);
+exprt object_size(const exprt &pointer);
+exprt pointer_object_has_type(const exprt &pointer, const typet &type, const namespacet &ns);
+exprt dynamic_object(const exprt &pointer);
+exprt good_pointer(const exprt &pointer, const typet &dereference_type);
+exprt good_pointer_def(const exprt &pointer, const typet &dereference_type, const namespacet &ns);
+exprt null_object(const exprt &pointer);
+exprt null_pointer(const exprt &pointer);
+exprt invalid_pointer(const exprt &pointer);
+exprt dynamic_object_lower_bound(const exprt &pointer);
+exprt dynamic_object_upper_bound(const exprt &pointer, const typet &dereference_type, const namespacet &ns);
+exprt object_lower_bound(const exprt &pointer);
+exprt object_upper_bound(const exprt &pointer, const typet &dereference_type, const namespacet &ns);
+
+#endif
