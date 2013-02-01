ac4062fd5b3cb594388a6bb2a97826c24e158292
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-01 22:58:51 +0000

    goto_modelt now has a symbol table
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2043 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-programs/goto_model.h
+++ src/goto-programs/goto_model.h
@@ -1,4 +1,4 @@
-/*******************************************************************\
+k/*******************************************************************\
 
 Module: Symbol Table + CFG
 
@@ -9,6 +9,8 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_GOTO_MODEL_H
 #define CPROVER_GOTO_MODEL_H
 
+#include <symbol_table.h>
+
 #include "goto_functions.h"
 
 // A model is a pair consisting of a symbol table
@@ -17,16 +19,8 @@ Author: Daniel Kroening, kroening@kroening.com
 class goto_modelt
 {
 public:
-  const namespacet &ns;
-  const goto_functionst &goto_functions;
-  
-  goto_modelt(
-    const namespacet &_ns,
-    const goto_functionst &_goto_functions):
-    ns(_ns),
-    goto_functions(_goto_functions)
-  {
-  }
+  symbol_tablet symbol_table;
+  goto_functionst goto_functions;
 };
 
 #endif
