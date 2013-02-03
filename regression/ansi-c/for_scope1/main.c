953e60579af3a4abcb0698d1a39f235fdf8f9047
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-03 19:24:35 +0000

    no for scope in Visual Studio

--- regression/ansi-c/for_scope1/main.c
+++ regression/ansi-c/for_scope1/main.c
@@ -1,7 +1,15 @@
 int main()
 {
+  #ifdef _MSC_VER
+
+  // Visual Studio won't even parse the stuff below
+
+  #else
+
   // the following is ok in C99 and upwards
   
   for(unsigned i=0; i<10; i++);  
   for(char i=0; i<10; i++);
+  
+  #endif
 }
