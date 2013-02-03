04e732e0c71199e08e75c0d8efd0f350b2c1ddc2
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-03 19:35:24 +0000

    no isinf and no isnormal in Visual Studio
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2075 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc/Float-flags-no-simp1/main.c
+++ regression/cbmc/Float-flags-no-simp1/main.c
@@ -7,15 +7,20 @@ int main()
   double d;
 
   // first check constants
+  
+  #ifndef _MSC_VER
   assert(isnormal(FLT_MAX));
   assert(isinf(HUGE_VAL));
   assert(isinf(HUGE_VALF));
 //  assert(isinf(HUGE_VALL));
   assert(isinf(INFINITY));
+  #endif
   assert(isnan(NAN));
 
   // check +
+  #ifndef _MSC_VER
   assert(isinf(INFINITY+INFINITY));
+  #endif
   assert(isnan(-INFINITY+INFINITY));
   assert(INFINITY+INFINITY>0);
   assert(isnan(NAN+d));
@@ -23,14 +28,18 @@ int main()
 
   // check -
   assert(isnan(INFINITY-INFINITY));
+  #ifndef _MSC_VER
   assert(isinf(-INFINITY-INFINITY));
+  #endif
   assert(-INFINITY-INFINITY<0);
   assert(isnan(NAN-d));
   assert(isnan(NAN-INFINITY));
 
   // check *
+  #ifndef _MSC_VER
   assert(isinf(INFINITY*INFINITY));
   assert(isinf(-INFINITY*INFINITY));
+  #endif
   assert(INFINITY*INFINITY>0);
   assert(-INFINITY*INFINITY<0);
   assert(isnan(NAN*d));
@@ -42,14 +51,18 @@ int main()
   // check /
   assert(isnan(INFINITY/INFINITY));
   assert(isnan(-INFINITY/INFINITY));
+  #ifndef _MSC_VER
   assert(isinf(INFINITY/0));
+  #endif
   assert(0.0/INFINITY==0);
   assert(1.0/INFINITY==0);
   assert(signbit(-1.0/INFINITY));
   assert(signbit(1.0/-INFINITY));
-  assert(isinf(INFINITY/2));
   assert(INFINITY/-2<0);
+  #ifndef _MSC_VER
   assert(isinf(1.0/0.0));
+  assert(isinf(INFINITY/2));
+  #endif
   assert(isnan(0.0/0.0));
   assert(isnan(NAN/d));
   assert(isnan(NAN/INFINITY));
