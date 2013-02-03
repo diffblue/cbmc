7a1b202da6c4b4941acf1d63ee2f45f78788aad8
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-03 16:58:19 +0000

    more

--- regression/cbmc-from-CVS/Division/main.c
+++ regression/cbmc-from-CVS/Division/main.c
@@ -1,5 +1,16 @@
-int main() {
-	int k = 5 / 2;
+#include <assert.h>
 
-	assert(k == 2);
+int main()
+{
+  int k1 = 5 / 2;
+  assert(k1 == 2);
+  
+  int k2 = -5 / 2;
+  assert(k2 == -2);
+
+  int k3 = 5 / -2;
+  assert(k3 == -2);
+
+  int k4 = -5 / -2;
+  assert(k4 == 2);
 }
