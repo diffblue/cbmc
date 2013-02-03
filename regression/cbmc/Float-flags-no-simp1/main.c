8d999d547df2239be8b5e0f1019245bf432457a0
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-03 19:38:35 +0000

    no float stuff in Visual Studio

--- regression/cbmc/Float-flags-no-simp1/main.c
+++ regression/cbmc/Float-flags-no-simp1/main.c
@@ -6,50 +6,38 @@ int main()
 {
   double d;
 
+  #ifndef _MSC_VER
+
   // first check constants
   
-  #ifndef _MSC_VER
   assert(isnormal(FLT_MAX));
   assert(isinf(HUGE_VAL));
   assert(isinf(HUGE_VALF));
 //  assert(isinf(HUGE_VALL));
   assert(isinf(INFINITY));
   assert(isnan(NAN));
-  #endif
 
   // check +
-  #ifndef _MSC_VER
   assert(isinf(INFINITY+INFINITY));
-  #endif
   assert(isnan(-INFINITY+INFINITY));
   assert(INFINITY+INFINITY>0);
-  #ifndef _MSC_VER
   assert(isnan(NAN+d));
   assert(isnan(NAN+INFINITY));
-  #endif
 
   // check -
   assert(isnan(INFINITY-INFINITY));
-  #ifndef _MSC_VER
   assert(isinf(-INFINITY-INFINITY));
-  #endif
   assert(-INFINITY-INFINITY<0);
-  #ifndef _MSC_VER
   assert(isnan(NAN-d));
   assert(isnan(NAN-INFINITY));
-  #endif
 
   // check *
-  #ifndef _MSC_VER
   assert(isinf(INFINITY*INFINITY));
   assert(isinf(-INFINITY*INFINITY));
-  #endif
   assert(INFINITY*INFINITY>0);
   assert(-INFINITY*INFINITY<0);
-  #ifndef _MSC_VER
   assert(isnan(NAN*d));
   assert(isnan(NAN*INFINITY));
-  #endif
   assert(isnan(INFINITY*0));
   assert(signbit(1.0*-0.0));
   assert(!signbit(1.0*0.0));
@@ -57,22 +45,18 @@ int main()
   // check /
   assert(isnan(INFINITY/INFINITY));
   assert(isnan(-INFINITY/INFINITY));
-  #ifndef _MSC_VER
   assert(isinf(INFINITY/0));
-  #endif
   assert(0.0/INFINITY==0);
   assert(1.0/INFINITY==0);
   assert(signbit(-1.0/INFINITY));
   assert(signbit(1.0/-INFINITY));
   assert(INFINITY/-2<0);
-  #ifndef _MSC_VER
   assert(isinf(1.0/0.0));
   assert(isinf(INFINITY/2));
-  #endif
   assert(isnan(0.0/0.0));
-  #ifndef _MSC_VER
   assert(isnan(NAN/d));
   assert(isnan(NAN/INFINITY));
-  #endif
   assert(signbit(-0.0/1));
+
+  #endif
 }
