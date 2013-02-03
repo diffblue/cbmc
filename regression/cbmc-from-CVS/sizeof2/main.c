067deb39b7dcc9abcad8b56fbab715f3d427591c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-03 17:09:50 +0000

    more

--- regression/cbmc-from-CVS/sizeof2/main.c
+++ regression/cbmc-from-CVS/sizeof2/main.c
@@ -1,15 +1,12 @@
-#include <assert.h>
+// sizeof is unsigned
+_Static_assert( 1 - sizeof(int) >=0, "sizeof is unsigned" );
 
-long int f() {
-  // sizeof is unsigned
-  return (1 - sizeof(int)) >> 64;
-}
-              
+// same with cast to signed
+_Static_assert( 1 - (int)sizeof(int) <0, "int is signed" );
+
+// the size of sizeof!
+_Static_assert(sizeof(sizeof(int)) == sizeof(void *), "size of sizeof" );
+
 int main()
 {
-  long int ret;
-
-  ret=f();
-  
-  assert(ret==0);
 }
