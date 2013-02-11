08b7fbdef0c3310119e4873d53fd254d94283044
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-02-11 18:04:26 +0000

    added model for __sync_synchronize

--- src/ansi-c/library/gcc.c
+++ src/ansi-c/library/gcc.c
@@ -18,3 +18,15 @@ inline void __builtin_ia32_mfence(void)
 {
   __asm("mfence");
 }
+
+/* FUNCTION: __sync_synchronize */
+
+inline void __sync_synchronize(void)
+{
+  // WARNING: this was a NOP before gcc 4.3.1,
+  // but is now believed to be the strongest possible barrier.
+          
+  __CPROVER_HIDE:;
+  __CPROVER_fence("WWfence", "RRfence", "RWfence", "WRfence",
+                  "WWcumul", "RRcumul", "RWcumul", "WRcumul");
+}
