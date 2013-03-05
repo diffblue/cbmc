e3d6d8c3b38790d040fa61aa17922959bf8e6c43
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-03-05 19:02:37 +0000

    added havoc loops

---
+++ src/goto-instrument/havoc_loops.h
@@ -0,0 +1,16 @@
+/*******************************************************************\
+
+Module: Havoc Loops
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_HAVOC_LOOPS_H
+#define CPROVER_HAVOC_LOOPS_H
+
+#include <goto-programs/goto_functions.h>
+
+void havoc_loops(goto_functionst &goto_functions);
+
+#endif
