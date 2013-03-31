248df2c19104400b489e2b168ae3bbc32c69f2e2
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-03-31 11:28:48 +0000

    SC thread patch from Michael

--- src/goto-symex/symex_start_thread.cpp
+++ src/goto-symex/symex_start_thread.cpp
@@ -38,11 +38,14 @@ void goto_symext::symex_start_thread(statet &state)
   // put into thread vector
   unsigned t=state.threads.size();
   state.threads.push_back(statet::threadt());
+  //statet::threadt &cur_thread=state.threads[state.source.thread_nr];
   statet::threadt &new_thread=state.threads.back();
   new_thread.pc=thread_target;
   new_thread.guard=state.guard;
   new_thread.call_stack.push_back(state.top());
-  new_thread.parent_thread=state.source.thread_nr;
+  #if 0
+  new_thread.abstract_events=&(target.new_thread(cur_thread.abstract_events));
+  #endif
 
   // create a copy of the local variables for the new thread
   statet::framet &frame=state.top();
@@ -56,19 +59,23 @@ void goto_symext::symex_start_thread(statet &state)
     irep_idt original_name=state.get_original_name(*it);
   
     // get L0 name for current thread
-    irep_idt l0_name=state.level0.name(original_name, t);
+    irep_idt l0_name=state.level0.rename(original_name, t);
     
     // setup L1 name
     state.level1.rename(l0_name, 0);
     irep_idt l1_name=state.level1.current_name(l0_name);
     state.l1_history.insert(l1_name);
+
+    // make sure the L2 name with current index exists for future renaming
+    state.level2(l1_name);
     
     // make copy
     typet type=ns.lookup(original_name).type;
     symbol_exprt lhs(l1_name, type);
     symbol_exprt rhs(*it, type);
 
-    symex_assign_symbol(state, lhs, nil_exprt(), rhs, state.guard, HIDDEN);
+    guardt guard;
+    symex_assign_symbol(state, lhs, nil_exprt(), rhs, guard, HIDDEN);
   }
 }
 
