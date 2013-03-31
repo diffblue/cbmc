7383b1b3cf4e783781725c390764e78db18cff0a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-03-31 18:37:54 +0000

    __thread
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2301 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc-concurrency/sc3/main.c
+++ regression/cbmc-concurrency/sc3/main.c
@@ -1,17 +1,26 @@
 __CPROVER_thread_local int local1;
 
+#ifdef __GNUC__
+__thread int local2;
+#else
+__CPROVER_thread_local int local2;
+#endif
+
 void f()
 {
-  int local2;
+  int local3;
   
   local1=1;
   local2=1;
+  local3=1;
   
   local1++;
   local2++;
+  local3++;
   
   assert(local1==2);
   assert(local2==2);
+  assert(local3==2);
 }
 
 int main()
