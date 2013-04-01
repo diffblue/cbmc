9b941dfd050951d418d15f7a618e411ff0468229
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-04-01 08:30:14 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2302 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-symex/memory_model.h
+++ src/goto-symex/memory_model.h
@@ -16,19 +16,26 @@ Author: Michael Tautschnig, michael.tautschnig@cs.ox.ac.uk
 class memory_model_baset:public messaget
 {
 public:
-  virtual void operator()(symex_target_equationt &)
-  {
-  }
-  
+  virtual void operator()(symex_target_equationt &)=0;
   virtual ~memory_model_baset();
 
+  typedef symex_target_equationt::SSA_stept eventt;
+  typedef symex_target_equationt::SSA_stepst eventst;
+  
 protected:
+  typedef std::vector<const eventt *> event_listt;
+  event_listt reads, writes;
+  
+  void build_event_lists(symex_target_equationt &);
 };
 
 class memory_model_sct:public memory_model_baset
 {
+public:
+  virtual void operator()(symex_target_equationt &equation);
+  
 protected:
-  virtual void read_from();  
+  virtual void read_from(symex_target_equationt &equation);  
 };
 
 #endif
