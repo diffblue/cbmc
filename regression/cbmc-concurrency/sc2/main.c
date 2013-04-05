a9783d32642f4a0888db40e216e22d754ee3167d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-04-05 12:25:16 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2317 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc-concurrency/sc2/main.c
+++ regression/cbmc-concurrency/sc2/main.c
@@ -1,9 +1,20 @@
 int global;
 
+void thread()
+{
+  int local;
+  local=global; // #6
+  if(local==3)
+  {
+    global=1;   // #7
+  }
+}
+
 int main()
 {
-  __CPROVER_ASYNC_1: if(global==3) global=1;
-  global=2;
-  assert(global==2); // safe
-  global=3;
+  __CPROVER_ASYNC_1: thread();
+  global=2;                   // #2
+  assert(global!=3); // safe  // #4
+  global=3;                   // #5
 }
+
