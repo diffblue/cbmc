40d9d2eb847ee5aeca5a36d609fbe2ad1ae057ad
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-04-12 20:23:18 +0000

    CFG dominators and natural loops are now template-ized

--- src/analyses/cfg_dominators.h
+++ src/analyses/cfg_dominators.h
@@ -10,47 +10,273 @@ Author: Georg Weissenbacher, georg@weissenbacher.name
 #define CPROVER_CFG_DOMINATORS_H
 
 #include <set>
+#include <list>
 #include <map>
 #include <ostream>
 
 #include <goto-programs/goto_functions.h>
 #include <goto-programs/goto_program.h>
 
-class cfg_dominatorst
+template<class P, class T>
+class cfg_dominators_templatet
 {
 public:
-  typedef std::set<goto_programt::const_targett> const_target_sett;
+  typedef std::set<T> target_sett;
 
   struct nodet
   {
-    const_target_sett dominators;
-    goto_programt::const_targetst successors, predecessors;
+    target_sett dominators;
+    std::list<T> successors, predecessors;
 
-    goto_programt::const_targett PC;
+    T PC;
   };
 
-  typedef std::map<goto_programt::const_targett, nodet> node_mapt;
+  typedef std::map<T, nodet> node_mapt;
   node_mapt node_map;
 
-  void operator()(const goto_programt &program)
-  {
-    initialise(program);
-    fixedpoint(program);
-  }
+  void operator()(P &program);
 
-  const_target_sett top;
-  goto_programt::const_targett entry_node;
+  target_sett top;
+  T entry_node;
 
   void output(std::ostream&) const;
 
 protected:
-  void initialise(const goto_programt &);
-  void construct_cfg(const goto_programt &);
-  void construct_cfg(const goto_programt &,
-                     goto_programt::const_targett PC);
-  void fixedpoint(const goto_programt &);
+  void initialise(P &program);
+  void construct_cfg(P &program);
+  void construct_cfg(P &program, T PC);
+  void fixedpoint(P &program);
 };
 
-std::ostream &operator << (std::ostream &out, const cfg_dominatorst &);
+/*******************************************************************\
+
+Function: operator <<
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Print the result of the dominator computation
+
+\*******************************************************************/
+
+template <class P, class T>
+std::ostream &operator << (
+  std::ostream &out,
+  const cfg_dominators_templatet<P, T> &cfg_dominators)
+{
+  cfg_dominators.output(out);
+  return out;
+}
+
+
+template<class P, class T>
+void cfg_dominators_templatet<P, T>::operator()(P &program)
+{
+  initialise(program);
+  fixedpoint(program);
+}
+
+/*******************************************************************\
+
+Function: cfg_dominators_templatet::initialise
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Initialises the elements of the fixed point analysis
+
+\*******************************************************************/
+
+template<class P, class T>
+void cfg_dominators_templatet<P, T>::initialise(P &program)
+{
+  construct_cfg(program);
+
+  // initialise top element
+  for(typename node_mapt::const_iterator e_it=node_map.begin();
+      e_it!=node_map.end(); ++e_it)
+    top.insert(e_it->first);
+}
+
+/*******************************************************************\
+
+Function: cfg_dominators_templatet::construct_cfg
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Initialises the predecessor and successor sets
+
+\*******************************************************************/
+
+template<class P, class T>
+void cfg_dominators_templatet<P, T>::construct_cfg(P &program)
+{
+  for (T it = program.instructions.begin();
+       it != program.instructions.end();
+       ++it)
+  {
+    construct_cfg(program, it);
+  }
+}
+
+/*******************************************************************\
+
+Function: cfg_dominators_templatet::construct_cfg
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Initialises the predecessor and successor sets
+
+\*******************************************************************/
+
+template <class P, class T>
+void cfg_dominators_templatet<P, T>::construct_cfg(P &program, T PC)
+{
+  nodet &node=node_map[PC];
+  node.PC=PC;
+  
+  program.get_successors(PC, node.successors);
+
+  // now do backward edges
+  for(typename std::list<T>::const_iterator
+        s_it=node.successors.begin();
+      s_it!=node.successors.end();
+      s_it++)
+  {
+    node_map[*s_it].predecessors.push_back(node.PC);
+  }
+
+}
+
+/*******************************************************************\
+
+Function: cfg_dominators_templatet::fixedpoint
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Computes the MOP for the dominator analysis
+
+\*******************************************************************/
+
+template<class P, class T>
+void cfg_dominators_templatet<P, T>::fixedpoint(P &program)
+{
+  std::list<T> worklist;
+
+  entry_node = program.instructions.begin();
+  nodet &n=node_map[entry_node];
+  n.dominators.insert(entry_node);
+  for(typename std::list<T>::const_iterator 
+      s_it=n.successors.begin();
+      s_it!=n.successors.end();
+      ++s_it)
+    worklist.push_back(*s_it);
+
+  while(!worklist.empty())
+  {
+    // get node from worklist
+    T current=worklist.front();
+    worklist.pop_front();
+
+    bool changed=false;
+    nodet &node=node_map[current];
+    if(node.dominators.empty())
+      for(typename std::list<T>::const_iterator 
+          p_it=node.predecessors.begin();
+          !changed && p_it!=node.predecessors.end();
+          ++p_it)
+        if(!node_map[*p_it].dominators.empty())
+        {
+          node.dominators=node_map[*p_it].dominators;
+          node.dominators.insert(current);
+          changed=true;
+        }
+
+    // compute intersection of predecessors
+    for(typename std::list<T>::const_iterator 
+          p_it=node.predecessors.begin();
+        p_it!=node.predecessors.end();
+        ++p_it)
+    {   
+      const target_sett &other=node_map[*p_it].dominators;
+      if(other.empty())
+        continue;
+
+      typename target_sett::const_iterator n_it=node.dominators.begin();
+      typename target_sett::const_iterator o_it=other.begin();
+
+      // in-place intersection. not safe to use set_intersect
+      while(n_it!=node.dominators.end() && o_it!=other.end())
+      {
+        if(*n_it==current) ++n_it;
+        else if(*n_it<*o_it) { changed=true; node.dominators.erase(n_it++); }
+        else if(*o_it<*n_it) ++o_it;
+        else { ++n_it; ++o_it; }
+      }
+      while(n_it!=node.dominators.end())
+        if(*n_it==current)
+          ++n_it;
+        else
+        {
+          changed=true;
+          node.dominators.erase(n_it++);
+        }
+    }
+
+    if(changed) // fixed point for node reached?
+    {
+      for(typename std::list<T>::const_iterator 
+            s_it=node.successors.begin();
+          s_it!=node.successors.end();
+          ++s_it)
+      {
+        worklist.push_back(*s_it);
+      }
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: cfg_dominators_templatet::output
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Print the result of the dominator computation
+
+\*******************************************************************/
+
+template <class P, class T>
+void cfg_dominators_templatet<P, T>::output(std::ostream &out) const
+{
+  for(typename node_mapt::const_iterator it=node_map.begin();
+      it!=node_map.end(); ++it)
+  {
+    unsigned n=it->first->location_number;
+    
+    out << n << " dominated by ";
+    for(typename target_sett::const_iterator d_it=it->second.dominators.begin();
+        d_it!=it->second.dominators.end();)
+    {
+      out << (*d_it)->location_number;
+      if (++d_it!=it->second.dominators.end()) 
+        out << ", ";
+    }
+    out << std::endl;
+  }
+}
+
+typedef cfg_dominators_templatet<const goto_programt, goto_programt::const_targett>
+        cfg_dominatorst;
 
 #endif
--- src/analyses/natural_loops.h
+++ src/analyses/natural_loops.h
@@ -9,6 +9,8 @@ Author: Georg Weissenbacher, georg@weissenbacher.name
 #ifndef CPROVER_NATURAL_LOOPS_H
 #define CPROVER_NATURAL_LOOPS_H
 
+#include <stack>
+#include <ostream>
 #include <set>
 
 #include <goto-programs/goto_program.h>
@@ -16,47 +18,187 @@ Author: Georg Weissenbacher, georg@weissenbacher.name
 
 #include "cfg_dominators.h"
 
-class natural_loopst
+template<class P, class T>
+class natural_loops_templatet
 {
 public:
-  typedef std::set<goto_programt::const_targett> natural_loopt;
+  typedef std::set<T> natural_loopt;
 
   // map loop headers to loops
-  typedef std::map<goto_programt::const_targett, natural_loopt> loop_mapt;
+  typedef std::map<T, natural_loopt> loop_mapt;
   
   loop_mapt loop_map;
 
-  void operator()(const goto_programt &program)
-  {
-    compute(program);
-  }
+  void operator()(P &program);
 
   void output(std::ostream &) const;
   
-  natural_loopst()
-  {
-  }
-
-  explicit natural_loopst(const goto_programt &program)
-  {
-    compute(program);
-  }
-
-  const cfg_dominatorst& get_dominator_info() const
+  const cfg_dominators_templatet<P, T>& get_dominator_info() const
   {
     return cfg_dominators;
   }
 
 protected:
-  cfg_dominatorst cfg_dominators;
+  cfg_dominators_templatet<P, T> cfg_dominators;
 
-  void compute(const goto_programt &program);
+  void compute(P &program);
 
-  void compute_natural_loop(goto_programt::const_targett, 
-                            goto_programt::const_targett);
+  void compute_natural_loop(T, T);
   
 };
 
 void show_natural_loops(const goto_functionst &goto_functions);
+
+class natural_loopst:
+    public natural_loops_templatet<const goto_programt, goto_programt::const_targett>
+{
+};
+
+typedef natural_loops_templatet<goto_programt, goto_programt::targett>
+    natural_loops_mutablet;
+
+template<class P, class T>
+void natural_loops_templatet<P, T>::operator()(P &program)
+{
+  compute(program);
+}
+
+/*******************************************************************\
+
+Function: natural_loops_templatet::compute
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Finds all back-edges and computes the natural loops
+
+\*******************************************************************/
+
+//#define DEBUG
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
+template<class P, class T>
+void natural_loops_templatet<P, T>::compute(P &program)
+{
+  cfg_dominators(program);
+#ifdef DEBUG
+  dominators.output(std::cout);
+#endif
+
+  // find back-edges m->n
+  for (T m_it = program.instructions.begin();
+       m_it != program.instructions.end();
+       ++m_it)
+  {
+    if(m_it->is_backwards_goto())
+    {
+      for(goto_programt::targetst::const_iterator n_it=m_it->targets.begin();
+          n_it!=m_it->targets.end(); ++n_it)
+      {
+        if((*n_it)->location_number<=m_it->location_number)
+        {
+          const typename cfg_dominators_templatet<P, T>::nodet &node=
+            cfg_dominators.node_map[m_it];
+          
+#ifdef DEBUG
+          std::cout << "Computing loop for " 
+                    << m_it->location_number << " -> " 
+                    << (*n_it)->location_number << std::endl;
+#endif
+          if(node.dominators.find(*n_it)!=node.dominators.end())
+          {
+            compute_natural_loop(m_it, *n_it);
+          }
+        }
+      }
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: natural_loops_templatet::compute_natural_loop
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Computes the natural loop for a given back-edge
+          (see Muchnick section 7.4)
+
+\*******************************************************************/
+
+template<class P, class T>
+void natural_loops_templatet<P, T>::compute_natural_loop(T m, T n)
+{
+  assert(n->location_number<=m->location_number);
+  
+  std::stack<T> stack;
+
+  natural_loopt &loop=loop_map[n];
+
+  loop.insert(n);
+  loop.insert(m);
+
+  if (n!=m)
+    stack.push(m);
+
+  while(!stack.empty())
+  {
+    T p=stack.top();
+    stack.pop();
+
+    typename cfg_dominators_templatet<P, T>::nodet &node=
+      cfg_dominators.node_map[p];
+
+    for(typename std::list<T>::const_iterator
+          q_it=node.predecessors.begin();
+        q_it!=node.predecessors.end();
+        ++q_it)
+    {
+      T q=*q_it;
+      std::pair<typename natural_loopt::const_iterator, bool> result=
+          loop.insert(q);
+      if(result.second)
+        stack.push(q);
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: natural_loops_templatet::output
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Print all natural loops that were found
+
+\*******************************************************************/
+
+template<class P, class T>
+void natural_loops_templatet<P, T>::output(std::ostream &out) const
+{
+  for(typename loop_mapt::const_iterator h_it=loop_map.begin();
+      h_it!=loop_map.end(); ++h_it)
+  {
+    unsigned n=h_it->first->location_number;
+    
+    out << n << " is head of { ";
+    for(typename natural_loopt::const_iterator l_it=h_it->second.begin();
+        l_it!=h_it->second.end();)
+    {
+      out << (*l_it)->location_number;
+      if (++l_it!=h_it->second.end()) 
+        out << ", ";
+    }
+    out << " } " << std::endl;
+  }
+}
 
 #endif
