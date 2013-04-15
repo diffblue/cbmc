c5c2ceb88be761e3e11152a8e7a4957d4848bf46
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-04-15 08:10:35 +0000

    more

--- regression/cbmc/Function10/main.c
+++ regression/cbmc/Function10/main.c
@@ -1,3 +1,7 @@
+#include <assert.h>
+
+int global;
+
 void other_func1(int z, int my_func(int b, int c))
 {
   my_func(1, 2);
@@ -13,10 +17,16 @@ void my_f1(int array[])
 {
 }
 
+int whatnot(int p1, int p2)
+{
+  global=p2;
+}
+
 int main()
 {
   int *p;
   my_f1(p);
   
-  other_func1(1, 0);
+  other_func1(1, whatnot);
+  assert(global==2);
 }
