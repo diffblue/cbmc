b561c240acdf43bbb886d1ed5ae31218a6f144b4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-04-17 09:06:34 +0000

    Ofer's function call sequences

---
+++ src/goto-instrument/call_sequences.h
@@ -0,0 +1,18 @@
+/*******************************************************************\
+
+Module: Memory-mapped I/O Instrumentation for Goto Programs
+
+Author: Daniel Kroening
+
+Date: September 2011
+
+\*******************************************************************/
+
+#ifndef CPROVER_GOTO_INSTRUMENT_CALL_SEQUENCES_H
+#define CPROVER_GOTO_INSTRUMENT_CALL_SEQUENCES_H
+
+#include <goto-programs/goto_functions.h>
+
+void show_call_sequences(const goto_functionst &goto_functions);
+
+#endif
