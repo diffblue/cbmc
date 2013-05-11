b3ba8814dd21ffe972b3dfae60a4eab37e4ec712
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-11 15:31:01 +0000

    interval analysis instrumentation

---
+++ src/analyses/interval_analysis.h
@@ -0,0 +1,16 @@
+/*******************************************************************\
+
+Module: Interval Analysis
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_INTERVAL_ANALYSIS_H
+#define CPROVER_INTERVAL_ANALYSIS_H
+
+#include <goto-programs/goto_functions.h>
+
+void interval_analysis(goto_functionst &goto_functions);
+
+#endif
