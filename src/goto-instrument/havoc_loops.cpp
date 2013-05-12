356a429cff6641e11472a1d2e5a3833302cc84a6
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-12 10:43:01 +0000

    more havoc
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2432 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/havoc_loops.cpp
+++ src/goto-instrument/havoc_loops.cpp
@@ -16,22 +16,80 @@ class havoc_loopst
 public:
   typedef goto_functionst::goto_functiont goto_functiont;
   
-  explicit havoc_loopst(goto_functiont &goto_function):
-    local_may_alias(goto_function),
-    natural_loops(goto_function.body)
+  explicit havoc_loopst(goto_functiont &_goto_function):
+    goto_function(_goto_function),
+    local_may_alias(_goto_function),
+    natural_loops(_goto_function.body)
   {
     havoc_loops();
   }
 
 protected:
+  goto_functiont &goto_function;
   local_may_aliast local_may_alias;
   natural_loops_mutablet natural_loops;
   
-  void havoc_loops();
-  void havoc_loop(const natural_loops_mutablet::natural_loopt &);
-
   typedef std::set<exprt> modifiest;
+  typedef const natural_loops_mutablet::natural_loopt loopt;
+
+  void havoc_loops();
+
+  void havoc_loop(
+    const goto_programt::targett loop_head,
+    const loopt &);
+
+  void build_havoc_code(
+    const goto_programt::targett loop_head,
+    const modifiest &modifies,
+    goto_programt &dest);
+  
+  void get_modifies(const loopt &, modifiest &);
+
+  goto_programt::targett get_loop_exit(const loopt &);
 };
+
+/*******************************************************************\
+
+Function: havoc_loopst::get_loop_exit
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+goto_programt::targett havoc_loopst::get_loop_exit(const loopt &loop)
+{
+  assert(!loop.empty());
+  return *loop.begin();
+}
+
+/*******************************************************************\
+
+Function: havoc_loopst::build_havoc_code
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void havoc_loopst::build_havoc_code(
+  const goto_programt::targett loop_head,
+  const modifiest &modifies,
+  goto_programt &dest)
+{
+  for(modifiest::const_iterator
+      m_it=modifies.begin();
+      m_it!=modifies.end();
+      m_it++)
+  {
+  }
+}
 
 /*******************************************************************\
 
@@ -45,13 +103,64 @@ Function: havoc_loopst::havoc_loop
 
 \*******************************************************************/
 
-void havoc_loopst::havoc_loop(const natural_loops_mutablet::natural_loopt &loop)
+void havoc_loopst::havoc_loop(
+  const goto_programt::targett loop_head,
+  const loopt &loop)
 {
-  // first find out what can get changed in the loop
-  
+  assert(!loop.empty());
+
+  // first find out what can get changed in the loop  
   modifiest modifies;
+  get_modifies(loop, modifies);
   
-  for(natural_loops_mutablet::natural_loopt::const_iterator
+  // build the havoc-ing code
+  goto_programt havoc_code;
+  build_havoc_code(loop_head, modifies, havoc_code);
+  
+  // Now havoc at the loop head. Use insert_swap to
+  // preserve jumps to loop head.
+  goto_function.body.insert_before_swap(loop_head, havoc_code);
+  
+  // compute the loop exit
+  goto_programt::targett loop_exit=
+    get_loop_exit(loop);
+
+  // divert all gotos to the loop head to the loop exit
+  for(loopt::const_iterator
+      i_it=loop.begin(); i_it!=loop.end(); i_it++)
+  {
+    goto_programt::instructiont &instruction=**i_it;
+    if(instruction.is_goto())
+    {
+      for(goto_programt::targetst::iterator
+          t_it=instruction.targets.begin();
+          t_it!=instruction.targets.end();
+          t_it++)
+      {
+        if(*t_it==loop_head)
+          *t_it=loop_exit; // divert
+      }
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: havoc_loopst::get_modifies
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void havoc_loopst::get_modifies(
+  const loopt &loop,
+  modifiest &modifies)
+{
+  for(loopt::const_iterator
       i_it=loop.begin(); i_it!=loop.end(); i_it++)
   {
     const goto_programt::instructiont &instruction=**i_it;
@@ -69,19 +178,6 @@ void havoc_loopst::havoc_loop(const natural_loops_mutablet::natural_loopt &loop)
       modifies.insert(m.begin(), m.end());
     }
   }
-  
-  // now havoc on all backwards gotos
-
-  for(natural_loops_mutablet::natural_loopt::const_iterator
-      i_it=loop.begin(); i_it!=loop.end(); i_it++)
-  {
-    const goto_programt::instructiont &instruction=**i_it;
-    if(instruction.is_backwards_goto())
-    {
-      
-
-    }    
-  }
 }
 
 /*******************************************************************\
@@ -98,13 +194,13 @@ Function: havoc_loopst::havoc_loops
 
 void havoc_loopst::havoc_loops()
 {
-  // iterate over the (natural) loops
+  // iterate over the (natural) loops in the function
   
   for(natural_loops_mutablet::loop_mapt::const_iterator
       l_it=natural_loops.loop_map.begin();
       l_it!=natural_loops.loop_map.end();
       l_it++)
-    havoc_loop(l_it->second);
+    havoc_loop(l_it->first, l_it->second);
 }
 
 /*******************************************************************\
