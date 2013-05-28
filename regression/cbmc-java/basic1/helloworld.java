b10a18df85f4919f825cdf4b87df2283d4987acc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-28 15:36:28 +0000

    first regression
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2476 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/cbmc-java/basic1/helloworld.java
@@ -0,0 +1,10 @@
+// from
+// http://www.thegeekstuff.com/2010/02/java-hello-world-example-how-to-write-and-execute-java-program-on-unix-os/
+
+/* Hello World Java Program */
+class helloworld  {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
