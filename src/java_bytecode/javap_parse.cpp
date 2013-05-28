ab2c446219062e3336adeeb8fb9d4f5f39e1629e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-28 21:45:17 +0000

    include

--- src/java_bytecode/javap_parse.cpp
+++ src/java_bytecode/javap_parse.cpp
@@ -6,6 +6,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <cstdlib>
 #include <fstream>
 
 #include <util/parser.h>
