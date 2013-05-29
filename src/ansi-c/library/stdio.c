bcb167e94b1a83c3cc032ffdf3e08f9040d109b4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-29 14:03:26 +0000

    stupidity fix

--- src/ansi-c/library/stdio.c
+++ src/ansi-c/library/stdio.c
@@ -263,7 +263,7 @@ inline ssize_t read(int fildes, void *buf, size_t nbyte)
   __CPROVER_HIDE:;
   ssize_t nread;
   size_t i;
-  (void)filedes;
+  (void)fildes;
   __CPROVER_assume((size_t)nread<=nbyte);
 
   for(i=0; i<nbyte; i++)
