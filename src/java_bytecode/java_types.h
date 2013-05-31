bda2019a7fc8fe7ded2b8a138178b86014f856fc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-05-31 16:18:04 +0000

    more bytecode conversion

---
+++ src/java_bytecode/java_types.h
@@ -0,0 +1,36 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_JAVA_TYPES_H
+#define CPROVER_JAVA_TYPES_H
+
+#include <util/type.h>
+
+typet java_int_type();
+typet java_long_type();
+typet java_short_type();
+typet java_byte_type();
+typet java_char_type();
+typet java_float_type();
+typet java_double_type();
+typet java_boolean_type();
+typet java_reference_type();
+
+// i  integer
+// l  long
+// s  short
+// b  byte
+// c  character
+// f  float
+// d  double
+// z  boolean
+// a  reference
+
+typet java_type(char t);
+
+#endif
