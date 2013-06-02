adf8723589ea89fe84cb26f5098fd936325b1fe7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-02 08:27:37 +0000

    CBMC for Java regression testing

---
+++ regression/cbmc-java/assert1/assert1.java
@@ -0,0 +1,13 @@
+class assert1
+{
+  public static void main(String[] args)
+  {
+    java.util.Random random = new java.util.Random(42);
+    
+    int i=random.nextInt();
+    
+    if(i>=10)
+      assert i>=20; // should hold
+  }
+}
+
