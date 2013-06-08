91253d46345d4a5308d6ade9032a71e19c979aa9
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-08 21:22:28 +0000

    use cpp_new
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2577 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/java_bytecode/internal_additions.cpp
+++ src/java_bytecode/internal_additions.cpp
@@ -6,6 +6,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <util/std_types.h>
 #include <ansi-c/c_types.h>
 
 #include "internal_additions.h"
@@ -25,6 +26,7 @@ Function: java_internal_additions
 void java_internal_additions(symbol_tablet &dest)
 {
   // add c::__CPROVER_rounding_mode
+
   {
     symbolt symbol;
     symbol.base_name="__CPROVER_rounding_mode";
@@ -33,6 +35,22 @@ void java_internal_additions(symbol_tablet &dest)
     symbol.is_lvalue=true;
     symbol.is_state_var=true;
     symbol.is_thread_local=true;
+    dest.add(symbol);
+  }
+
+  // add c::__new
+  // void *__new(__typeof__(sizeof(int)));
+
+  {
+    code_typet type;
+    type.return_type()=pointer_typet(void_typet());
+    type.parameters().resize(1);
+    type.parameters()[0].type()=size_type();
+  
+    symbolt symbol;
+    symbol.base_name="__new";
+    symbol.name="c::__new";
+    symbol.type=type;
     dest.add(symbol);
   }
 }
