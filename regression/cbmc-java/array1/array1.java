593bab30c888946b162ce20e245cd5a85036f14f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-09 11:32:25 +0000

    array assertion
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2581 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/cbmc-java/array1/array1.java
@@ -0,0 +1,20 @@
+class what_not
+{
+};
+
+class array1
+{
+  public static void main(String[] args)
+  {
+    int size=10;
+    int int_array[]=new int[size];
+    
+    for(int i=0; i<size; i++)
+      int_array[i]=i;
+
+    what_not what_not_array[]=new what_not[size];
+    
+    assert what_not_array.length == size;
+  }
+}
+
