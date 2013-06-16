14c374538a517656ba0371c2e7f6139361288df4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-16 14:35:23 +0000

    avoid sprintf

--- src/util/c_misc.cpp
+++ src/util/c_misc.cpp
@@ -81,7 +81,7 @@ void MetaChar(std::string &out, char c, bool inString)
     if(((unsigned char)c < ' ') || (c == 127))
     {
       char octbuf[8];
-      sprintf(octbuf, "%03o", (unsigned char) c);
+      snprintf(octbuf, sizeof(octbuf), "%03o", (unsigned char) c);
       out+="\\";
       out+=octbuf;
     }
