fd59a40c99ac27f53ad663322460ca0b78f9c2f9
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-16 18:47:40 +0000

    avoid source file name collisions

--- src/goto-cc/goto_cc_languages.cpp
+++ src/goto-cc/goto_cc_languages.cpp
@@ -9,10 +9,7 @@ Author: CM Wintersteiger
 #include <langapi/mode.h>
 
 #include <ansi-c/ansi_c_language.h>
-
-#ifdef HAVE_CPP
 #include <cpp/cpp_language.h>
-#endif
 
 #ifdef HAVE_SPECC
 #include <specc/specc_language.h>
@@ -35,10 +32,7 @@ Function: goto_cc_modet::register_languages
 void goto_cc_modet::register_languages()
 {
   register_language(new_ansi_c_language);
-
-  #ifdef HAVE_CPP
   register_language(new_cpp_language);
-  #endif
 
   #ifdef HAVE_SPECC
   register_language(new_specc_language);
