70b3ffa7550de7924b1bafbf83753e9bae3d229d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-06-25 21:03:07 +0000

    fix for same_set

--- src/util/union_find.h
+++ src/util/union_find.h
@@ -132,7 +132,7 @@ public:
   }
   
   // are 'a' and 'b' in the same set?
-  inline bool same_set(const T &a, const T &b) const
+  inline bool same_set(const T &a, const T &b)
   {
     unsigned na=number(a), nb=number(b);
     return uuf.same_set(na, nb);
