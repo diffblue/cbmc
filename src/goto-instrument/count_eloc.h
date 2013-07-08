348b0b1844a5b626fd97dc9d579b04d9cc78bb81
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-07-08 11:26:06 +0000

    Added --count-eloc option for goto-instrument

---
+++ src/goto-instrument/count_eloc.h
@@ -0,0 +1,18 @@
+/*******************************************************************\
+
+Module: Count effective lines of code
+
+Author: Michael Tautschnig
+
+Date: December 2012
+
+\*******************************************************************/
+
+#ifndef CPROVER_GOTO_INSTRUMENT_COUNT_ELOC_H
+#define CPROVER_GOTO_INSTRUMENT_COUNT_ELOC_H
+
+#include <goto-programs/goto_functions.h>
+
+void count_eloc(const goto_functionst &goto_functions);
+
+#endif
