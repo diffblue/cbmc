012c8423becb54c7b9ebe003d5ae012e637a7fe0
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-07-08 11:26:06 +0000

    Added --count-eloc option for goto-instrument
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2735 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/goto-instrument/count_eloc.cpp
@@ -0,0 +1,61 @@
+/*******************************************************************\
+
+Module: Count effective lines of code
+
+Author: Michael Tautschnig
+
+Date: December 2012
+
+\*******************************************************************/
+
+#include <iostream>
+
+#include <util/prefix.h>
+
+#include "count_eloc.h"
+
+/*******************************************************************\
+
+Function: count_eloc
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+unsigned count_eloc(const goto_programt &goto_program)
+{
+  hash_set_cont<irep_idt, irep_id_hash> lines;
+
+  forall_goto_program_instructions(it, goto_program)
+    if(it->location.is_not_nil() &&
+        !has_prefix(id2string(it->location.get_file()), "<built-in-"))
+      lines.insert(it->location.get_line());
+
+  return lines.size();
+}
+
+/*******************************************************************\
+
+Function: count_eloc
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void count_eloc(const goto_functionst &goto_functions)
+{
+  unsigned eloc=0;
+  forall_goto_functions(f_it, goto_functions)
+    eloc+=count_eloc(f_it->second.body);
+
+  std::cout << "Effective lines of code: " << eloc << std::endl;
+}
+
