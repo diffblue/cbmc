348ffb73410ff8ab1665698db2f4f4b099261700
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-07-11 14:12:40 +0000

    added conjunction(...) and disjunction(...)

--- src/util/std_expr.cpp
+++ src/util/std_expr.cpp
@@ -7,3 +7,56 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include "std_expr.h"
+
+/*******************************************************************\
+
+Function: disjunction
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt disjunction(const exprt::operandst &op)
+{
+  if(op.empty())
+    return false_exprt();
+  else if(op.size()==1)
+    return op.front();
+  else
+  {
+    or_exprt result;
+    result.operands()=op;
+    return result;
+  }
+}
+
+/*******************************************************************\
+
+Function: conjunction
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt conjunction(const exprt::operandst &op)
+{
+  if(op.empty())
+    return true_exprt();
+  else if(op.size()==1)
+    return op.front();
+  else
+  {
+    and_exprt result;
+    result.operands()=op;
+    return result;
+  }
+}
+
