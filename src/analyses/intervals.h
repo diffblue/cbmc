c59e0d7c1b5fc80d9ca2adfffb19513fab75f332
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-07-26 11:08:24 +0000

    meet(), join(), is_bottom() for intervals
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2812 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/analyses/intervals.h
+++ src/analyses/intervals.h
@@ -12,6 +12,8 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/ieee_float.h>
 #include <util/mp_arith.h>
 
+// a generic weak (but possibly open) interval
+
 template<typename T>
 class interval_templatet
 {
@@ -34,11 +36,91 @@ public:
     upper=value;
   }
   
+  // is this 'true'?
   inline bool is_top() const
   {
     return !lower_set && !upper_set;
   }
+  
+  // is this 'false'?
+  inline bool is_bottom() const
+  {
+    return lower_set && upper_set && lower>upper;
+  }
+  
+  // Union or disjunction
+  bool join(const interval_templatet<T> &other);
+
+  // Intersection or conjunction
+  bool meet(const interval_templatet<T> &other);
 };
+
+// Union or disjunction
+// return 'true' if there is change
+template<typename T>
+bool interval_templatet<T>::join(const interval_templatet<T> &other)
+{
+  bool result=false;
+
+  if(upper_set)
+  {
+    if(!other.upper_set)
+    {
+      upper_set=false;
+      result=true;
+    }
+    else if(upper<other.upper)
+    {
+      // new, looser upper bound
+      set_upper(other.upper);
+      result=true;
+    }
+  }
+
+  if(lower_set)
+  {
+    if(!other.lower_set)
+    {
+      lower_set=false;
+      result=true;
+    }
+    else if(lower>other.lower)
+    {
+      // new, looser lower bound
+      set_lower(other.lower);
+      result=true;
+    }
+  }
+
+  return result;
+}
+
+// Intersection or conjunction
+// return 'true' if there is change
+
+template<typename T>
+bool interval_templatet<T>::meet(const interval_templatet<T> &other)
+{
+  bool result=false;
+
+  if(other.upper_set)
+    if(!upper_set || upper>other.upper)
+    {
+      // new, tighter upper bound
+      set_upper(other.upper);
+      result=true;
+    }
+
+  if(other.lower_set)
+    if(!lower_set || lower<other.lower)
+    {
+      // new, tighter lower bound
+      set_lower(other.lower);
+      result=true;
+    }
+
+  return result;
+}
 
 typedef interval_templatet<mp_integer> integer_intervalt;
 typedef interval_templatet<ieee_floatt> ieee_float_intervalt;
