a98802b78dca845e8ee5355e72feac016c73b654
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-08-15 07:51:41 +0000

    add missing #include <iterator>
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2879 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/analyses/local_may_alias.cpp
+++ src/analyses/local_may_alias.cpp
@@ -6,6 +6,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <iterator>
 #include <algorithm>
 
 #include <util/std_expr.h>
