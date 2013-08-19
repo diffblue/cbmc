b30563d487b7222861da9b9897614e80beeba35f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-08-19 12:59:18 +0000

    basic AIG -> prop solver

--- src/solvers/prop/aig_prop.cpp
+++ src/solvers/prop/aig_prop.cpp
@@ -12,7 +12,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 /*******************************************************************\
 
-Function: aig_propt::lcnf
+Function: aig_prop_baset::land
 
   Inputs:
 
@@ -22,24 +22,7 @@ Function: aig_propt::lcnf
 
 \*******************************************************************/
 
-void aig_propt::lcnf(const bvt &clause)
-{
-  l_set_to_true(lor(clause));
-}
-  
-/*******************************************************************\
-
-Function: aig_propt::land
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-literalt aig_propt::land(const bvt &bv)
+literalt aig_prop_baset::land(const bvt &bv)
 {
   literalt literal=const_literal(true);
 
@@ -51,7 +34,7 @@ literalt aig_propt::land(const bvt &bv)
   
 /*******************************************************************\
 
-Function: aig_propt::lor
+Function: aig_prop_baset::lor
 
   Inputs:
 
@@ -61,7 +44,7 @@ Function: aig_propt::lor
 
 \*******************************************************************/
 
-literalt aig_propt::lor(const bvt &bv)
+literalt aig_prop_baset::lor(const bvt &bv)
 {
   literalt literal=const_literal(false);
 
@@ -73,7 +56,7 @@ literalt aig_propt::lor(const bvt &bv)
   
 /*******************************************************************\
 
-Function: aig_propt::lxor
+Function: aig_prop_baset::lxor
 
   Inputs:
 
@@ -83,7 +66,7 @@ Function: aig_propt::lxor
 
 \*******************************************************************/
 
-literalt aig_propt::lxor(const bvt &bv)
+literalt aig_prop_baset::lxor(const bvt &bv)
 {
   literalt literal=const_literal(false);
 
@@ -95,7 +78,7 @@ literalt aig_propt::lxor(const bvt &bv)
   
 /*******************************************************************\
 
-Function: aig_propt::land
+Function: aig_prop_baset::land
 
   Inputs:
 
@@ -105,7 +88,7 @@ Function: aig_propt::land
 
 \*******************************************************************/
 
-literalt aig_propt::land(literalt a, literalt b)
+literalt aig_prop_baset::land(literalt a, literalt b)
 {
   if(a.is_true()) return b;
   if(b.is_true()) return a;
@@ -114,13 +97,13 @@ literalt aig_propt::land(literalt a, literalt b)
 
   if(a==neg(b)) return const_literal(false);
   if(a==b) return a;
-
+  
   return dest.new_and_node(a, b);
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lor
+Function: aig_prop_baset::lor
 
   Inputs:
 
@@ -130,14 +113,14 @@ Function: aig_propt::lor
 
 \*******************************************************************/
 
-literalt aig_propt::lor(literalt a, literalt b)
+literalt aig_prop_baset::lor(literalt a, literalt b)
 {
   return neg(land(neg(a), neg(b))); // De Morgan's
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lnot
+Function: aig_prop_baset::lnot
 
   Inputs:
 
@@ -147,14 +130,14 @@ Function: aig_propt::lnot
 
 \*******************************************************************/
 
-literalt aig_propt::lnot(literalt a)
+literalt aig_prop_baset::lnot(literalt a)
 {
   return neg(a);
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lxor
+Function: aig_prop_baset::lxor
 
   Inputs:
 
@@ -164,7 +147,7 @@ Function: aig_propt::lxor
 
 \*******************************************************************/
 
-literalt aig_propt::lxor(literalt a, literalt b)
+literalt aig_prop_baset::lxor(literalt a, literalt b)
 {
   if(a.is_false()) return b;
   if(b.is_false()) return a;
@@ -180,7 +163,7 @@ literalt aig_propt::lxor(literalt a, literalt b)
 
 /*******************************************************************\
 
-Function: aig_propt::lnand
+Function: aig_prop_baset::lnand
 
   Inputs:
 
@@ -190,14 +173,14 @@ Function: aig_propt::lnand
 
 \*******************************************************************/
 
-literalt aig_propt::lnand(literalt a, literalt b)
+literalt aig_prop_baset::lnand(literalt a, literalt b)
 {
   return land(a, b).negation();
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lnor
+Function: aig_prop_baset::lnor
 
   Inputs:
 
@@ -207,14 +190,14 @@ Function: aig_propt::lnor
 
 \*******************************************************************/
 
-literalt aig_propt::lnor(literalt a, literalt b)
+literalt aig_prop_baset::lnor(literalt a, literalt b)
 {
   return lor(a, b).negation();
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lequal
+Function: aig_prop_baset::lequal
 
   Inputs:
 
@@ -224,14 +207,14 @@ Function: aig_propt::lequal
 
 \*******************************************************************/
 
-literalt aig_propt::lequal(literalt a, literalt b)
+literalt aig_prop_baset::lequal(literalt a, literalt b)
 {
   return lxor(a, b).negation();
 }
 
 /*******************************************************************\
 
-Function: aig_propt::limplies
+Function: aig_prop_baset::limplies
 
   Inputs:
 
@@ -241,14 +224,14 @@ Function: aig_propt::limplies
 
 \*******************************************************************/
 
-literalt aig_propt::limplies(literalt a, literalt b)
+literalt aig_prop_baset::limplies(literalt a, literalt b)
 {
   return lor(neg(a), b);
 }
 
 /*******************************************************************\
 
-Function: aig_propt::lselect
+Function: aig_prop_baset::lselect
 
   Inputs:
 
@@ -258,7 +241,7 @@ Function: aig_propt::lselect
 
 \*******************************************************************/
 
-literalt aig_propt::lselect(literalt a, literalt b, literalt c)
+literalt aig_prop_baset::lselect(literalt a, literalt b, literalt c)
 {  // a?b:c = (a AND b) OR (/a AND c)
   if(a.is_true()) return b;
   if(a.is_false()) return c;
@@ -269,7 +252,7 @@ literalt aig_propt::lselect(literalt a, literalt b, literalt c)
 
 /*******************************************************************\
 
-Function: aig_propt::set_equal
+Function: aig_prop_constraintt::lcnf
 
   Inputs:
 
@@ -279,11 +262,127 @@ Function: aig_propt::set_equal
 
 \*******************************************************************/
 
-void aig_propt::set_equal(literalt a, literalt b)
+void aig_prop_constraintt::lcnf(const bvt &clause)
+{
+  l_set_to_true(lor(clause));
+}
+  
+/*******************************************************************\
+
+Function: aig_prop_baset::set_equal
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void aig_prop_baset::set_equal(literalt a, literalt b)
 {
   // we produce two constraints:
   // a|!b   !a|b
 
-  l_set_to_true(land(pos(a), neg(b)));
-  l_set_to_true(land(neg(a), pos(b)));
+  l_set_to_true(lor(pos(a), neg(b)));
+  l_set_to_true(lor(neg(a), pos(b)));
+}
+
+/*******************************************************************\
+
+Function: aig_prop_solvert::l_get
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+tvt aig_prop_solvert::l_get(literalt a) const
+{
+  return solver.l_get(a);
+}
+
+/*******************************************************************\
+
+Function: aig_prop_solvert::prop_solve
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+propt::resultt aig_prop_solvert::prop_solve()
+{
+  status() << "converting AIG, "
+           << aig.nodes.size() << " nodes" << eom;
+  convert_aig();
+
+  return solver.prop_solve();
+}
+
+/*******************************************************************\
+
+Function: aig_prop_solvert::convert_aig
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void aig_prop_solvert::convert_aig()
+{
+  // 1. Do variables
+  
+  while(solver.no_variables()<=aig.nodes.size())
+    solver.new_variable();
+
+  // 2. Do nodes
+
+  for(unsigned n=0; n<aig.nodes.size(); n++)
+  {
+    const aigt::nodet &node=aig.nodes[n];
+
+    if(node.is_and())
+    {
+      literalt o=literalt(n, false);
+      literalt a=node.a;
+      literalt b=node.b;
+      
+      bvt lits(2);
+   
+      lits[0]=pos(a);
+      lits[1]=neg(o);
+      solver.lcnf(lits);
+
+      lits[0]=pos(b);
+      lits[1]=neg(o);
+      solver.lcnf(lits);
+
+      lits.clear();
+      lits.reserve(3);
+      lits.push_back(neg(a));
+      lits.push_back(neg(b));
+      lits.push_back(pos(o));
+      solver.lcnf(lits);
+    }
+  }
+  
+  // 3. Do constraints
+  
+  for(constraintst::const_iterator
+      c_it=constraints.begin();
+      c_it!=constraints.end();
+      c_it++)
+  {
+    solver.l_set_to(*c_it, true);
+  }
 }
