91e7856b945c991774b36e735eb222f4e7073bf7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-08-24 14:39:39 +0000

    show max_size_in_use on the Mac
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2922 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/memory_info.cpp
+++ src/util/memory_info.cpp
@@ -9,6 +9,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifdef __APPLE__
 #include <mach/task.h>
 #include <mach/mach_init.h>
+#include <malloc/malloc.h>
 #endif
 
 #ifdef __linux__
@@ -40,15 +41,15 @@ void memory_info(std::ostream &out)
 {
   #ifdef __linux__
   struct mallinfo m = mallinfo();
-  out << "  non-mmapped space allocated from system: " << m.arena << std::endl;
-  out << "  number of free chunks: " << m.ordblks << std::endl;
-  out << "  number of fastbin blocks: " << m.smblks << std::endl;
-  out << "  number of mmapped regions: " << m.hblks << std::endl;
-  out << "  space in mmapped regions: " << m.hblkhd << std::endl;
-  out << "  maximum total allocated space: " << m.usmblks << std::endl;
-  out << "  space available in freed fastbin blocks: " << m.fsmblks << std::endl;
-  out << "  total allocated space: " << m.uordblks << std::endl;
-  out << "  total free space: " << m.fordblks << std::endl;
+  out << "  non-mmapped space allocated from system: " << m.arena << "\n";
+  out << "  number of free chunks: " << m.ordblks << "\n";
+  out << "  number of fastbin blocks: " << m.smblks << "\n";
+  out << "  number of mmapped regions: " << m.hblks << "\n";
+  out << "  space in mmapped regions: " << m.hblkhd << "\n";
+  out << "  maximum total allocated space: " << m.usmblks << "\n";
+  out << "  space available in freed fastbin blocks: " << m.fsmblks << "\n";
+  out << "  total allocated space: " << m.uordblks << "\n";
+  out << "  total free space: " << m.fordblks << "\n";
   #endif
   
   #ifdef _WIN32
@@ -56,8 +57,8 @@ void memory_info(std::ostream &out)
   PROCESS_MEMORY_COUNTERS pmc;
   if(GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc)))
   {
-    out << "  PeakWorkingSetSize: " << pmc.PeakWorkingSetSize << std::endl;
-    out << "  WorkingSetSize: " << pmc.WorkingSetSize << std::endl;
+    out << "  PeakWorkingSetSize: " << pmc.PeakWorkingSetSize << "\n";
+    out << "  WorkingSetSize: " << pmc.WorkingSetSize << "\n";
   }
   #endif
   #endif
@@ -66,6 +67,10 @@ void memory_info(std::ostream &out)
   struct task_basic_info t_info;
   mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
   task_info(current_task(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
-  out << "  virtual size: " << t_info.virtual_size/1000000 << "m" << std::endl;
+  out << "  virtual size: " << (double)t_info.virtual_size/1000000 << "m\n";
+
+  malloc_statistics_t t;
+  malloc_zone_statistics(NULL, &t);
+  out << "  max_size_in_use: " << (double)t.max_size_in_use/1000000 << "m\n";
   #endif
 }
