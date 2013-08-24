45396ff5dbd03f13804c5a50ba887f73d3167e37
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-08-24 15:11:30 +0000

    merge_full_irept

--- src/util/merge_irep.cpp
+++ src/util/merge_irep.cpp
@@ -39,3 +39,39 @@ void merge_irept::operator()(irept &irep)
 
   irep=*irep_store.insert(irep).first;
 }
+
+/*******************************************************************\
+
+Function: merge_full_irept::operator()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void merge_full_irept::operator()(irept &irep)
+{
+  irep_storet::const_iterator entry=irep_store.find(irep);
+  if(entry!=irep_store.end())
+  {
+    irep=*entry;
+    return;
+  }
+
+  irept::subt &sub=irep.get_sub();
+  Forall_irep(it, sub)
+    operator()(*it); // recursive call
+
+  irept::named_subt &named_sub=irep.get_named_sub();
+  Forall_named_irep(it, named_sub)
+    operator()(it->second); // recursive call
+
+  irept::named_subt &comments=irep.get_comments();
+  Forall_named_irep(it, comments)
+    operator()(it->second); // recursive call
+
+  irep=*irep_store.insert(irep).first;
+}
