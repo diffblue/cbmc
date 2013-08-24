3d08d9b749f7dbafb53d43aedb32f17fc9de9339
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-08-24 15:11:30 +0000

    merge_full_irept
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2925 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/merge_irep.h
+++ src/util/merge_irep.h
@@ -13,7 +13,8 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "hash_cont.h"
 
 // Warning: the below uses irep_hash, as opposed to irep_full_hash,
-// i.e., any comments will be disregarded during merging.
+// i.e., any comments will be disregarded during merging. Use
+// merge_full_irept if any comments are of importance.
 
 class merge_irept
 {
@@ -22,6 +23,16 @@ public:
 
 protected:
   typedef hash_set_cont<irept, irep_hash> irep_storet;
+  irep_storet irep_store;     
+};
+
+class merge_full_irept
+{
+public:
+  void operator()(irept &);
+
+protected:
+  typedef hash_set_cont<irept, irep_full_hash, irep_full_eq> irep_storet;
   irep_storet irep_store;     
 };
 
