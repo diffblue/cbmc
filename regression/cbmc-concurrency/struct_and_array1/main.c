51bfa62638f4852d18efd53dcf15eb04e863f636
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-09-05 07:47:39 +0000

    check for accesses to shared structs and arrays
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2943 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ regression/cbmc-concurrency/struct_and_array1/main.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+
+typedef struct st_t
+{
+  unsigned char x;
+  unsigned char y;
+} ST;
+
+ST st;
+
+char my_array[10];
+
+_Bool done1, done2;
+
+void *foo1(void *arg1)
+{
+  st.x = 1;
+  my_array[1]=1;
+  done1 = 1;
+}
+
+void *foo2(void *arg2)
+{
+  st.y = 1;
+  my_array[2]=2;
+  done2 = 1;
+}
+
+int main()
+{
+  pthread_t t;
+  pthread_create(&t,NULL,foo1,NULL);
+  pthread_create(&t,NULL,foo2,NULL);
+  
+  if(done1 && done2)
+  {
+    assert(st.x==st.y);
+    assert(my_array[1]==my_array[2]);
+  }
+}
