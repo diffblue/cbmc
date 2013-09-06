2cdcd39dccc25d6159c771fa81e12150cb06150c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-09-06 07:27:00 +0000

    regression tests for float to integer conversion with different rounding modes

---
+++ regression/cbmc/Float-to-int1/main.c
@@ -0,0 +1,62 @@
+#include <fenv.h>
+#include <assert.h>
+
+float castWithRounding (int rounding_mode, int value)
+{
+  int returnValue = fesetround(rounding_mode);
+
+  assert(returnValue == 0);
+
+  return (float)value;
+}
+
+int main (void)
+{
+  float low = 0x1p+25f;          // 33554432
+  float high = 0x1.000002p+25f;  // 33554436
+  float higher = 0x1.000004p+25f;// 33554440
+
+  // Constant folding tests
+  assert(castWithRounding(FE_TONEAREST,33554432) == low);
+  assert(castWithRounding(FE_TONEAREST,33554433) == low);
+  assert(castWithRounding(FE_TONEAREST,33554434) == low); // Even is low
+  assert(castWithRounding(FE_TONEAREST,33554435) == high);
+  assert(castWithRounding(FE_TONEAREST,33554436) == high);
+  assert(castWithRounding(FE_TONEAREST,33554437) == high);
+  assert(castWithRounding(FE_TONEAREST,33554438) == higher); // Even is high
+  assert(castWithRounding(FE_TONEAREST,33554439) == higher);
+  assert(castWithRounding(FE_TONEAREST,33554440) == higher);
+
+  assert(castWithRounding(FE_UPWARD,33554432) == low);
+  assert(castWithRounding(FE_UPWARD,33554433) == high);
+  assert(castWithRounding(FE_UPWARD,33554434) == high);
+  assert(castWithRounding(FE_UPWARD,33554435) == high);
+  assert(castWithRounding(FE_UPWARD,33554436) == high);
+  assert(castWithRounding(FE_UPWARD,-33554432) == -low);
+  assert(castWithRounding(FE_UPWARD,-33554433) == -low);
+  assert(castWithRounding(FE_UPWARD,-33554434) == -low);
+  assert(castWithRounding(FE_UPWARD,-33554435) == -low);
+  assert(castWithRounding(FE_UPWARD,-33554436) == -high);
+
+  assert(castWithRounding(FE_DOWNWARD,33554432) == low);
+  assert(castWithRounding(FE_DOWNWARD,33554433) == low);
+  assert(castWithRounding(FE_DOWNWARD,33554434) == low);
+  assert(castWithRounding(FE_DOWNWARD,33554435) == low);
+  assert(castWithRounding(FE_DOWNWARD,33554436) == high);
+  assert(castWithRounding(FE_DOWNWARD,-33554432) == -low);
+  assert(castWithRounding(FE_DOWNWARD,-33554433) == -high);
+  assert(castWithRounding(FE_DOWNWARD,-33554434) == -high);
+  assert(castWithRounding(FE_DOWNWARD,-33554435) == -high);
+  assert(castWithRounding(FE_DOWNWARD,-33554436) == -high);
+
+  assert(castWithRounding(FE_TOWARDZERO,33554432) == low);
+  assert(castWithRounding(FE_TOWARDZERO,33554433) == low);
+  assert(castWithRounding(FE_TOWARDZERO,33554434) == low);
+  assert(castWithRounding(FE_TOWARDZERO,33554435) == low);
+  assert(castWithRounding(FE_TOWARDZERO,33554436) == high);
+  assert(castWithRounding(FE_TOWARDZERO,-33554432) == -low);
+  assert(castWithRounding(FE_TOWARDZERO,-33554433) == -low);
+  assert(castWithRounding(FE_TOWARDZERO,-33554434) == -low);
+  assert(castWithRounding(FE_TOWARDZERO,-33554435) == -low);
+  assert(castWithRounding(FE_TOWARDZERO,-33554436) == -high);
+}
