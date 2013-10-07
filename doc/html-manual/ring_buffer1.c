cb487cfc22e804b7e49f7bcd5ad62950e160b6fc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-10-07 18:00:54 +0000

    ring buffer example
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@2997 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ doc/html-manual/ring_buffer1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define MAX 1000
+#define SIZE 10
+
+unsigned int sample()
+{
+  return random()%(MAX+1);
+}
+
+unsigned int ring_buffer[SIZE];
+
+int main()
+{
+  unsigned index=0;
+  
+  while(1)
+  {
+    unsigned output;
+    output=ring_buffer[index];
+    printf("%u\n", output);
+    assert(output<=MAX);
+    
+    ring_buffer[index]=sample();
+
+    index=(index+1)%SIZE;
+    assert(index<SIZE);
+  }
+}
