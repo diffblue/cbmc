a1512e171a82e92139fb8bb8ed58764183e0880b
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-10-10 11:00:16 +0000

    symex_goto needs to use clean_expr
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3023 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -31,7 +31,7 @@ void goto_symext::symex_goto(statet &state)
   const goto_programt::instructiont &instruction=*state.source.pc;
   
   exprt old_guard=instruction.guard;
-  dereference(old_guard, state, false);
+  clean_expr(old_guard, state, false);
 
   exprt new_guard=old_guard;
   state.rename(new_guard, ns);
