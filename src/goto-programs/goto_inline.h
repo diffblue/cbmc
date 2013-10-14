e28278d78b44248512dc261f372325c642eec927
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-10-14 09:06:19 +0000

    added goto_model versions of goto_inline and goto_partial_inline

--- src/goto-programs/goto_inline.h
+++ src/goto-programs/goto_inline.h
@@ -11,13 +11,25 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <util/message_stream.h>
 
-#include "goto_functions.h"
+#include "goto_model.h"
 
 // do a full inlining
 void goto_inline(
   goto_functionst &goto_functions,
   const namespacet &ns,
   message_handlert &message_handler);
+
+// do a full inlining
+void goto_inline(
+  goto_modelt &goto_model,
+  message_handlert &message_handler);
+
+// inline those functions marked as "inlined"
+// and functions with less than _smallfunc_limit instructions
+void goto_partial_inline(
+  goto_modelt &goto_model,
+  message_handlert &message_handler,
+  unsigned _smallfunc_limit = 0);
 
 // inline those functions marked as "inlined"
 // and functions with less than _smallfunc_limit instructions
