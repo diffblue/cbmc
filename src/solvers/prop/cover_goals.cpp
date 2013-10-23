a766dbec085f8af5d7e3716ff859b50ef0cecf1c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-10-23 14:45:27 +0000

    cover_goalst now avoids calling prop_convt::post_process more than once
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3063 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/solvers/prop/cover_goals.cpp
+++ src/solvers/prop/cover_goals.cpp
@@ -116,6 +116,46 @@ Function: cover_goalst::operator()
 void cover_goalst::operator()()
 {
   _iterations=_number_covered=0;
+  
+  #if 1
+  propt::resultt prop_result;
+  
+  // We use incremental solving, so need to freeze some variables
+  // to prevent them from being eliminated.      
+  freeze_goal_variables();
+
+  // Do post-processing in decision procedure, just once
+  prop_conv.post_process();
+
+  do
+  {
+    // We want (at least) one of the remaining goals, please!
+    _iterations++;
+    
+    constraint();
+    prop_result=prop.prop_solve();
+    
+    switch(prop_result)
+    {
+    case propt::P_UNSATISFIABLE: // DONE
+      break;
+
+    case propt::P_SATISFIABLE:
+      // mark the goals we got
+      mark(); 
+      
+      // notify
+      assignment();
+      break;
+
+    default:
+      error("propositional solver has failed");
+      return;
+    }
+  }
+  while(prop_result==propt::P_SATISFIABLE &&
+        number_covered()<size());
+  #else
   decision_proceduret::resultt dec_result;
   
   // We use incremental solving, so need to freeze some variables
@@ -126,6 +166,7 @@ void cover_goalst::operator()()
   {
     // We want (at least) one of the remaining goals, please!
     _iterations++;
+    
     constraint();
     dec_result=prop_conv.dec_solve();
     
@@ -149,5 +190,6 @@ void cover_goalst::operator()()
   }
   while(dec_result==decision_proceduret::D_SATISFIABLE &&
         number_covered()<size());
+  #endif
 }
 
