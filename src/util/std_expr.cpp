e376eab9caf21f6d36dd93ae1b09c822552bd2c0
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2013-12-03 11:51:15 +0000

    Michael's ssa_exprt

--- src/util/std_expr.cpp
+++ src/util/std_expr.cpp
@@ -6,6 +6,10 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <sstream>
+
+#include "arith_tools.h"
+
 #include "std_expr.h"
 
 /*******************************************************************\
@@ -76,5 +80,85 @@ exprt conjunction(const exprt::operandst &op)
     result.operands()=op;
     return result;
   }
+}
+
+/*******************************************************************\
+
+Function: build_identifier_rec
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+static void build_identifier_rec(
+  const exprt &expr,
+  const irep_idt &l0,
+  const irep_idt &l1,
+  std::ostringstream &oss)
+{
+  if(expr.id()==ID_member)
+  {
+    const member_exprt &member=to_member_expr(expr);
+
+    build_identifier_rec(member.struct_op(), l0, l1, oss);
+
+    oss << '.' << member.get_component_name();
+  }
+  else if(expr.id()==ID_index)
+  {
+    const index_exprt &index=to_index_expr(expr);
+
+    build_identifier_rec(index.array(), l0, l1, oss);
+
+    mp_integer idx;
+    if(to_integer(to_constant_expr(index.index()), idx))
+      assert(false);
+
+    oss << '[' << idx << ']';
+  }
+  else if(expr.id()==ID_symbol)
+  {
+    oss << to_symbol_expr(expr).get_identifier();
+
+    if(!l0.empty())
+      oss << '!' << l0;
+
+    if(!l1.empty())
+      oss << '@' << l1;
+  }
+  else
+    assert(false);
+}
+
+/*******************************************************************\
+
+Function: ssa_exprt::update_identifier
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void ssa_exprt::update_identifier()
+{
+  std::ostringstream oss;
+
+  const irep_idt &l0=get_level_0();
+  const irep_idt &l1=get_level_1();
+  const irep_idt &l2=get_level_2();
+
+  build_identifier_rec(get_original_expr(), l0, l1, oss);
+
+  if(!l2.empty())
+    oss << '#' << l2;
+
+  return set(ID_identifier, oss.str());
 }
 
