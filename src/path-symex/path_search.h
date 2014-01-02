22b6e48321662e7af85295ce61d8f280f97fcc62
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-02 12:46:55 +0000

    drop states based on depth or number of context switches
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3287 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/path-symex/path_search.h
+++ src/path-symex/path_search.h
@@ -18,7 +18,9 @@ class path_searcht:public safety_checkert
 public:
   explicit inline path_searcht(const namespacet &_ns):
     safety_checkert(_ns),
-    show_vcc(false)
+    show_vcc(false),
+    depth_limit(-1), // no limit
+    context_bound(-1)
   {
   }
 
@@ -26,6 +28,10 @@ public:
     const goto_functionst &goto_functions);
 
   bool show_vcc;
+  
+  unsigned depth_limit;
+  unsigned context_bound;
+  unsigned number_of_dropped_states;
 
 protected:
   typedef path_symex_statet statet;
@@ -40,6 +46,8 @@ protected:
   
   bool check_assertion(statet &state, const namespacet &);
   void do_show_vcc(statet &state, const namespacet &);
+  
+  bool drop_state(const statet &state) const;
 };
 
 #endif
