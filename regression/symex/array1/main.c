5d33fbaedff1c120ce4ae9b30c7b5e8cd962d156
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-03 14:50:18 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3311 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/symex/array1/main.c
+++ regression/symex/array1/main.c
@@ -1,10 +1,16 @@
 struct some_struct
 {
   int x, y, z;
-} some_struct_array[100];
+} some_struct_array[10];
 
-int some_int_array[100];
+int some_int_array[10];
 
 int main()
 {
+  int i;
+
+  // zero initialization
+  assert(some_int_array[1]==0); 
+//  if(i>=0 && i<10) assert(some_int_array[i]==0);
+
 }
