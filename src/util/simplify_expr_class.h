1313844bae96d01ff45b11c2a90ad6546b4ecfde
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-04 20:02:07 +0000

    constant-fold absolute value

--- src/util/simplify_expr_class.h
+++ src/util/simplify_expr_class.h
@@ -88,6 +88,7 @@ public:
   bool simplify_isinf(exprt &expr);
   bool simplify_isnan(exprt &expr);
   bool simplify_isnormal(exprt &expr);
+  bool simplify_abs(exprt &expr);
 
   // auxiliary
   bool simplify_if_implies(exprt &expr, const exprt &cond, bool truth, bool &new_truth);
