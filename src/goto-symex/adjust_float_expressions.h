93685ab99866fcc019e1235a3ddbfaa4e442959f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-04 16:17:07 +0000

    factored out adjust_float_expressions
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3314 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/goto-symex/adjust_float_expressions.h
@@ -0,0 +1,14 @@
+/*******************************************************************\
+
+Module: Symbolic Execution
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <util/expr.h>
+#include <util/namespace.h>
+
+exprt adjust_float_expressions(
+  const exprt &expr,
+  const namespacet &ns);
