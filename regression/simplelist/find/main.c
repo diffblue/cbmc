12908829170d26d9057713675962cf67d95076c2
schram <schram@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-17 03:46:32 +0000

    find fixed
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/branches/ESOP2014-heap@3498 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/simplelist/find/main.c
+++ regression/simplelist/find/main.c
@@ -13,7 +13,7 @@ typedef struct list* list_t;
 
 void main() {
   list_t x, tmp;
-  list_t a, res, one, zero, res, err;
+  list_t a, res, one, zero, err, retval;
 
   __CPROVER_assume(res!=err);
   __CPROVER_assume(one!=zero);
@@ -27,9 +27,9 @@ void main() {
 
 
   if(tmp != NULL)
-    res = one;
+    retval = one;
   else
-    res = zero;
+    retval = zero;
 
   assert(__CPROVER_HEAP_path(x, tmp, "next"));
   assert(res != err);
--- regression/simplelist/find_bug1/main.c
+++ regression/simplelist/find_bug1/main.c
@@ -13,7 +13,7 @@ typedef struct list* list_t;
 
 void main() {
   list_t x, tmp, res, err;
-  list_t a, res, one, zero;
+  list_t a, retval, one, zero;
 
   __CPROVER_assume(res!=err);
   __CPROVER_assume(one!=zero);
@@ -27,9 +27,9 @@ void main() {
 
 
   if(tmp != NULL)
-    res = one;
+    retval = one;
   else
-    res = zero;
+    retval = zero;
 
   assert(__CPROVER_HEAP_path(x, tmp, "next"));
   assert(res != err);
