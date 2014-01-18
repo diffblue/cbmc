13790d59d3277e0bf35efc39504c9c08211859a8
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-01-18 17:32:17 +0000

    Added object_descriptor_exprt::build to set up root_object() and offset() appropriately
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3511 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/std_expr.cpp
+++ src/util/std_expr.cpp
@@ -7,6 +7,13 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <sstream>
+#include <cassert>
+
+#include "arith_tools.h"
+#include "byte_operators.h"
+#include "config.h"
+#include "namespace.h"
+#include "pointer_offset_size.h"
 
 #include "arith_tools.h"
 
@@ -160,5 +167,101 @@ void ssa_exprt::update_identifier()
     oss << '#' << l2;
 
   set_identifier(oss.str());
+}
+
+/*******************************************************************\
+
+Function: build_object_descriptor_rec
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Build an object_descriptor_exprt from a given expr
+
+\*******************************************************************/
+
+static void build_object_descriptor_rec(
+  const namespacet &ns,
+  const exprt &expr,
+  object_descriptor_exprt &dest)
+{
+  const signedbv_typet index_type(config.ansi_c.pointer_width);
+
+  if(expr.id()==ID_index)
+  {
+    const index_exprt &index=to_index_expr(expr);
+
+    build_object_descriptor_rec(ns, index.array(), dest);
+
+    mp_integer sub_size=pointer_offset_size(ns, expr.type());
+    assert(sub_size>0);
+
+    dest.offset()=
+      plus_exprt(dest.offset(),
+                 mult_exprt(typecast_exprt(index.index(), index_type),
+                            from_integer(sub_size, index_type)));
+  }
+  else if(expr.id()==ID_member)
+  {
+    const member_exprt &member=to_member_expr(expr);
+    const exprt &struct_op=member.struct_op();
+    const typet &struct_type=ns.follow(struct_op.type());
+
+    build_object_descriptor_rec(ns, struct_op, dest);
+
+    if(struct_type.id()==ID_union)
+      return;
+
+    mp_integer offset=
+      member_offset(ns,
+                    to_struct_type(struct_type),
+                    member.get_component_name());
+    assert(offset>=0);
+
+    dest.offset()=
+      plus_exprt(dest.offset(), from_integer(offset, index_type));
+  }
+  else if(expr.id()==ID_byte_extract_little_endian ||
+          expr.id()==ID_byte_extract_big_endian)
+  {
+    const byte_extract_exprt &be=to_byte_extract_expr(expr);
+
+    dest.object()=be.op();
+
+    build_object_descriptor_rec(ns, be.op(), dest);
+
+    dest.offset()=
+      plus_exprt(dest.offset(),
+                 typecast_exprt(to_byte_extract_expr(expr).offset(),
+                                index_type));
+  }
+}
+
+/*******************************************************************\
+
+Function: object_descriptor_exprt::build
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Build an object_descriptor_exprt from a given expr
+
+\*******************************************************************/
+
+void object_descriptor_exprt::build(
+  const exprt &expr,
+  const namespacet &ns)
+{
+  assert(object().id()==ID_unknown);
+  object()=expr;
+
+  if(offset().id()==ID_unknown)
+    offset()=from_integer(0, signedbv_typet(config.ansi_c.pointer_width));
+
+  build_object_descriptor_rec(ns, expr, *this);
+
+  assert(root_object().type().id()!=ID_empty);
 }
 
