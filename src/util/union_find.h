ca548403714fd9640e229bef29d7bf5fcd2b8829
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-05 12:05:37 +0000

    Use size_t as union find's numbering type

--- src/util/union_find.h
+++ src/util/union_find.h
@@ -19,12 +19,29 @@ Author: Daniel Kroening, kroening@kroening.com
 
 class unsigned_union_find
 {
+protected:
+  struct nodet
+  {
+    size_t count; // set size
+    size_t parent;
+
+    // constructs a root node
+    explicit nodet(size_t index):count(1), parent(index)
+    {
+    }
+  };
+
+  // This is mutable to allow path compression in find().
+  mutable std::vector<nodet> nodes;
+
 public:
+  typedef std::vector<nodet>::size_type size_type;
+
   // merge the sets 'a' and 'b'
-  void make_union(unsigned a, unsigned b);
+  void make_union(size_t a, size_t b);
 
   // find the root of the set 'a' belongs to
-  unsigned find(unsigned a) const;  
+  size_t find(size_t a) const;
 
   // Makes 'this' the union-find with the following:
   // any union in 'this' will be present in both source sets,
@@ -33,14 +50,14 @@ public:
   void intersection(const unsigned_union_find &other);
 
   // remove from any sets
-  void isolate(unsigned a);
+  void isolate(size_t a);
 
   inline void swap(unsigned_union_find &other)
   {
     other.nodes.swap(nodes);
   }
 
-  inline void resize(unsigned size)
+  inline void resize(size_type size)
   {
     // We only enlarge. Shrinking is yet to be implemented.
     assert(nodes.size()<=size);
@@ -55,7 +72,7 @@ public:
   }
 
   // is 'a' a root?  
-  inline bool is_root(unsigned a) const
+  inline bool is_root(size_t a) const
   {
     if(a>=size()) return true;
     // a root is its own parent
@@ -63,59 +80,44 @@ public:
   }
 
   // are 'a' and 'b' in the same set?
-  inline bool same_set(unsigned a, unsigned b) const
+  inline bool same_set(size_t a, size_t b) const
   {
     return find(a)==find(b);
   }
 
   // total number of elements  
-  inline unsigned size() const
+  inline size_type size() const
   {
     return nodes.size();
   }
 
   // size of the set that 'a' is in  
-  inline unsigned count(unsigned a) const
+  inline size_t count(size_t a) const
   {
     if(a>=size()) return 1;
     return nodes[find(a)].count;
   }
 
   // make the array large enough to contain 'a'  
-  inline void check_index(unsigned a)
+  inline void check_index(size_t a)
   {
     if(a>=size()) resize(a+1);
   }
 
   // number of disjoint sets  
-  unsigned count_roots() const
+  size_t count_roots() const
   {
-    unsigned c=0;
-    for(unsigned i=0; i<nodes.size(); i++)
+    size_t c=0;
+    for(size_t i=0; i<nodes.size(); i++)
       if(is_root(i)) c++;
     return c;
   }
 
   // makes 'new_root' the root of the set 'old'  
-  void re_root(unsigned old, unsigned new_root);
+  void re_root(size_t old, size_t new_root);
 
   // find a different member of the same set
-  unsigned get_other(unsigned a);
-  
-protected:  
-  struct nodet
-  {
-    unsigned count; // set size
-    unsigned parent;
-
-    // constructs a root node        
-    explicit nodet(unsigned index):count(1), parent(index)
-    {
-    }
-  };
-
-  // This is mutable to allow path compression in find().
-  mutable std::vector<nodet> nodes;
+  size_t get_other(size_t a);
 };
 
 template <typename T>
@@ -125,7 +127,7 @@ public:
   // true == already in same set
   bool make_union(const T &a, const T &b)
   {
-    unsigned na=number(a), nb=number(b);
+    size_t na=number(a), nb=number(b);
     bool is_union=find_number(na)==find_number(nb);
     uuf.make_union(na, nb);
     return is_union;
@@ -134,7 +136,7 @@ public:
   // are 'a' and 'b' in the same set?
   inline bool same_set(const T &a, const T &b)
   {
-    unsigned na=number(a), nb=number(b);
+    size_t na=number(a), nb=number(b);
     return uuf.same_set(na, nb);
   }
 
@@ -143,17 +145,17 @@ public:
     return find(number(a));
   }
   
-  inline unsigned find_number(unsigned a) const
+  inline size_t find_number(size_t a) const
   {
     return uuf.find(a);
   }
 
-  inline unsigned find_number(const T &a)
+  inline size_t find_number(const T &a)
   {
     return uuf.find(number(a));
   }
   
-  inline bool is_root_number(unsigned a) const
+  inline bool is_root_number(size_t a) const
   {
     return uuf.is_root(a);
   }
@@ -163,9 +165,9 @@ public:
     return is_root(number(a));
   }
 
-  inline unsigned number(const T &a)
+  inline size_t number(const T &a)
   {
-    unsigned n=subt::number(a);
+    size_t n=subt::number(a);
   
     if(n>=uuf.size())
       uuf.resize(this->size());
