449ca92f62c232de67ff7c2b9b1db7ce68944d78
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-21 19:52:47 +0000

    various builtins are compile-time constants

---
+++ regression/ansi-c/gcc_builtins1/main.c
@@ -0,0 +1,11 @@
+enum { E1 = __builtin_isinf(1.0) };
+enum { E2 = __builtin_isnan(1.0) };
+enum { E3 = __builtin_isinf(__builtin_inf()) };
+enum { E4 = __builtin_signbit(-1.0) };
+enum { E5 = __builtin_classify_type(1) };
+enum { E6 = sizeof(int) };
+enum { E7 = __alignof__(int) };
+
+int main()
+{
+}
