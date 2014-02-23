9862259083b58767d73f8a151e170f8148db8c47
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-23 14:52:42 +0000

    avoid space after backslash
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3607 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-programs/read_goto_binary.cpp
+++ src/goto-programs/read_goto_binary.cpp
@@ -9,7 +9,7 @@ Author:
 #if defined(__linux__) || \
     defined(__FreeBSD_kernel__) || \
     defined(__GNU__) || \
-    defined(__unix__) || \    
+    defined(__unix__) || \
     defined(__CYGWIN__) || \
     defined(__MACH__)
 #include <unistd.h>
