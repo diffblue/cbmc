c5466141c4c9ee97e02c7266d864f548e56289f9
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-24 10:46:00 +0000

    add back cstdlib header for exit()
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3624 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/cbmc/cbmc_parseoptions.cpp
+++ src/cbmc/cbmc_parseoptions.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <fstream>
+#include <cstdlib> // exit()
 #include <iostream>
 #include <memory>
 
