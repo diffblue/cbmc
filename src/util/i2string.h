75d73b9bbd8e15d9bb2bb48ddd09515604b0653d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-24 10:28:31 +0000

    C++11 comment

--- src/util/i2string.h
+++ src/util/i2string.h
@@ -8,6 +8,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <string>
 
+// One day, the below will be replaced by C++11's std::to_string.
+
 std::string i2string(int i);
 std::string i2string(signed long int i);
 std::string i2string(signed long long int i);
