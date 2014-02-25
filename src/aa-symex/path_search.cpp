806947b8b8de5f99732787313443ea838028bc9c
ahorn <ahorn@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-02-25 18:00:04 +0000

    Aggressively optimize for space when path_symex_states are lazy
    The experiments must compare the most extreme designs: either
    every state is stored, or none at all. The middle ground includes
    things such as storing the entire forest of previously explored
    steps (i.e. histories) whereas the composite/collection fields
    in patch_symex_states could be populated lazily by traversing
    the appropriate history tree from an interior node to the root.
    But this would require careful manipulation of steps and states,
    certainly harder to maintain than both of the current designs.
    
    
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3633 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/aa-symex/path_search.cpp
+++ src/aa-symex/path_search.cpp
@@ -59,20 +59,32 @@ path_searcht::resultt path_searcht::operator()(
     // according to some heuristic.
     queuet::iterator state=pick_state();
 
-    // restore all fields of a lazy state by symbolic
-    // execution along previously recorded branch decisions
-    const queuet::size_type queue_size=queue.size();
-    for(; state->is_lazy() && state->is_executable();
-          number_of_fast_forward_steps++)
+    // fast forwarding required?
+    if(state->is_lazy())
     {
-      path_symex(*state, queue);
-      status() << "Fast forward thread " << state->get_current_thread()
-               << "/" << state->threads.size()
-               << " PC " << state->pc() << messaget::eom;
-    }
+      assert(state->is_executable());
 
-    // queue does not change during fast forwarding
-    assert(queue.size() == queue_size);
+      // keep allocated memory, this is faster than
+      // instantiating a new empty vector and map
+      history.clear();
+      var_map.clear();
+      state->history=path_symex_step_reft(history);
+
+      // restore all fields of a lazy state by symbolic
+      // execution along previously recorded branches
+      const queuet::size_type queue_size=queue.size();
+      do
+      {
+        number_of_fast_forward_steps++;
+
+        path_symex(*state, queue);
+        status() << "Fast forward thread " << state->get_current_thread()
+                 << "/" << state->threads.size()
+                 << " PC " << state->pc() << messaget::eom;
+      }
+      while(state->is_lazy() && state->is_executable());
+      assert(queue.size() == queue_size);
+    }
     
     // TODO: check lazy states before fast forwarding, or perhaps it
     // is better to even check before inserting into queue
