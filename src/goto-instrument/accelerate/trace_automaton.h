d84c8d775f43bf1709b9e0322c8553e30719021a
matlew <matlew@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-03-22 16:30:48 +0000

    First cut at implementation of trace automata
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3669 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/accelerate/trace_automaton.h
+++ src/goto-instrument/accelerate/trace_automaton.h
@@ -22,15 +22,20 @@ class automatont {
   statet add_state();
   void add_trans(statet s, goto_programt::targett a, statet t);
 
-  bool is_accepting(statet s);
-  void set_accepting(statet s);
+  bool is_accepting(statet s) {
+    return accept_states.find(s) != accept_states.end();
+  }
+
+  void set_accepting(statet s) {
+    accept_states.insert(s);
+  }
 
   void move(statet s, goto_programt::targett a, state_sett &t);
   void move(state_sett &s, goto_programt::targett a, state_sett &t);
 
   statet init_state;
 
- protected:
+ //protected:
   typedef std::multimap<goto_programt::targett, statet> transitionst;
   typedef std::pair<transitionst::iterator, transitionst::iterator> transition_ranget;
   typedef std::vector<transitionst> transition_tablet;
@@ -47,11 +52,32 @@ class trace_automatont {
   {
     build_alphabet(goto_program);
     init_nta();
+
+    epsilon = goto_program.instructions.end();
+    epsilon++;
   }
 
   void add_path(patht &path);
 
   void build();
+
+  int init_state() {
+    return dta.init_state;
+  }
+
+  void accept_states(state_sett &states) {
+    states.insert(dta.accept_states.begin(), dta.accept_states.end());
+  }
+
+  typedef std::pair<statet, statet> state_pairt;
+  typedef std::multimap<goto_programt::targett, state_pairt> sym_mapt;
+  typedef std::pair<sym_mapt::iterator, sym_mapt::iterator> sym_range_pairt;
+
+  void get_transitions(sym_mapt &transitions);
+
+
+  typedef std::set<goto_programt::targett> alphabett;
+  alphabett alphabet;
 
  protected:
   void build_alphabet(goto_programt &program);
@@ -70,17 +96,19 @@ class trace_automatont {
   statet find_dstate(state_sett &s);
   void add_dtrans(state_sett &s, goto_programt::targett a, state_sett &t);
 
-  typedef std::set<goto_programt::targett> alphabett;
+  void build_sym_map();
+
   typedef std::map<state_sett, statet> state_mapt;
 
   goto_programt &goto_program;
-  alphabett alphabet;
   goto_programt::targett epsilon;
   std::vector<state_sett> unmarked_dstates;
   state_mapt dstates;
 
   automatont nta;
   automatont dta;
+
+  sym_mapt sym_map;
 };
 
 #endif // TRACE_AUTOMATON_H
