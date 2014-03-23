114ded2c8636cdbe6adcad6daf2e35df141dcb89
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-03-23 14:19:33 +0000

    recursion limits

--- src/cbmc/symex_bmc.cpp
+++ src/cbmc/symex_bmc.cpp
@@ -107,7 +107,12 @@ bool symex_bmct::get_unwind(
 
   statistics() << (abort?"Not unwinding":"Unwinding")
                << " loop " << id << " iteration "
-               << unwind << " " << source.pc->location
+               << unwind;
+
+  if(this_loop_limit!=std::numeric_limits<unsigned>::max())
+    statistics() << " (" << this_loop_limit << " max)";
+
+  statistics() << " " << source.pc->location
                << " thread " << source.thread_nr << eom;
 
   return abort;
@@ -130,29 +135,44 @@ bool symex_bmct::get_unwind_recursion(
   const unsigned thread_nr,
   unsigned unwind)
 {
-  const long this_loop_max_unwind=
-    std::max(max_unwind,
-             std::max(thread_loop_limits[(unsigned)-1][id],
-                      thread_loop_limits[thread_nr][id]));
+  // We use the most specific limit we have,
+  // and 'infinity' when we have none.
 
-  #if 1
-  if(unwind!=0)
+  unsigned this_loop_limit=std::numeric_limits<unsigned>::max();
+
+  loop_limitst &this_thread_limits=
+    thread_loop_limits[thread_nr];
+    
+  loop_limitst::const_iterator l_it=this_thread_limits.find(id);
+  if(l_it!=this_thread_limits.end())
+    this_loop_limit=l_it->second;
+  else
+  {
+    l_it=loop_limits.find(id);
+    if(l_it!=loop_limits.end())
+      this_loop_limit=l_it->second;
+    else if(max_unwind_is_set)
+      this_loop_limit=max_unwind;
+  }
+
+  bool abort=unwind>this_loop_limit;
+
+  if(unwind>0 || abort)
   {
     const symbolt &symbol=ns.lookup(id);
 
-    statistics() << "Unwinding recursion "
+    statistics() << (abort?"Not unwinding":"Unwinding")
+                 << " recursion "
                  << symbol.display_name()
                  << " iteration " << unwind;
-      
-    if(this_loop_max_unwind!=0)
-      statistics() << " (" << this_loop_max_unwind << " max)";
+    
+    if(this_loop_limit!=std::numeric_limits<unsigned>::max())
+      statistics() << " (" << this_loop_limit << " max)";
 
     statistics() << eom;
   }
-  #endif
 
-  return this_loop_max_unwind!=0 &&
-         unwind>=this_loop_max_unwind;
+  return abort;
 }
 
 /*******************************************************************\
