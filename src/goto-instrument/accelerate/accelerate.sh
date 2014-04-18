30b858d5b765b8411a682b85a5f4a50c3f24127f
matlew <matlew@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-04-18 11:15:45 +0000

    Fix acceleration driver script for unsafe tests
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3733 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/accelerate/accelerate.sh
+++ src/goto-instrument/accelerate/accelerate.sh
@@ -4,13 +4,27 @@ bindir=`dirname $0`
 goto_cc="$bindir/../../goto-cc/goto-cc"
 goto_instrument="$bindir/../goto-instrument"
 cbmc="$bindir/../../cbmc/cbmc"
-cfile=$1
+cfile=""
+cbmcargs=""
 ofile=`mktemp`
 accfile=`mktemp`
 
+for a in "$@"
+do
+case $a in
+  --*)
+    cbmcargs="$cbmcargs $a"
+    ;;
+  *)
+    cfile=$a
+    ;;
+esac
+done
+
 $goto_cc $cfile -o $ofile
 $goto_instrument --accelerate $ofile $accfile
-$cbmc --unwind 2 $accfile
+
+$cbmc --unwind 2 $cbmcargs $accfile
 retcode=$?
 
 rm $ofile $accfile
