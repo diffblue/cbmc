0b0f0f74c9ef1643f382f1ec303742658796f5d2
cdavid <cdavid@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-05-12 16:03:38 +0000

    + not-dangling assumption
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/branches/ESOP2014-heap@3819 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/simplelist/concat/main.c
+++ regression/simplelist/concat/main.c
@@ -15,6 +15,7 @@ void main() {
   list_t x, y,tmp, res, err;
 
   __CPROVER_assume(res!=err);
+  __CPROVER_assume(!__CPROVER_HEAP_dangling(x));
 
   tmp = x;
 
@@ -27,6 +28,7 @@ void main() {
   while(tmp->next != NULL) {
     not_null(tmp);
     tmp = tmp->next;
+    __CPROVER_assume(!__CPROVER_HEAP_dangling(tmp));
   }
 
   not_null(tmp);
