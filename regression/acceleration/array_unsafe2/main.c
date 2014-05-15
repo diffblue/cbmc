2e7650089dae14d60fd403737d949b4aa921cd51
matlew <matlew@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-05-15 13:01:28 +0000

    Fix array acceleration tests

--- regression/acceleration/array_unsafe2/main.c
+++ regression/acceleration/array_unsafe2/main.c
@@ -1,10 +1,12 @@
+#define SZ 2048
+
 int main(void) {
-  int A[10];
-  int B[10];
+  int A[SZ];
+  int B[SZ];
   int i;
   int tmp;
 
-  for (i = 0; i < 10 && tmp != 0; i++) {
+  for (i = 0; i < SZ; i++) {
     tmp = A[i];
     B[i] = tmp;
   }
