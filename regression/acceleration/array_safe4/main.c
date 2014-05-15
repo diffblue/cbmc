84bf37a7ede7d6ca10e71bb1da84748f0c01280a
matlew <matlew@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-05-15 13:03:03 +0000

    Added 4 more array acceleration tests

---
+++ regression/acceleration/array_safe4/main.c
@@ -0,0 +1,13 @@
+#define N 10
+
+int main(void) {
+  int A[N];
+  int i;
+
+  A[N-1] = 0;
+
+  for (i = 0; A[i] != 0; i++) {
+  }
+
+  assert(i <= N);
+}
---
+++ regression/acceleration/array_unsafe4/main.c
@@ -0,0 +1,11 @@
+#define N 10
+
+int main(void) {
+  int A[N];
+  int i;
+
+  for (i = 0; A[i] != 0; i++) {
+  }
+
+  assert(i <= N);
+}
