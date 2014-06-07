1f6cbc28f2318485544b69649c357bde0204166e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-07 20:00:21 +0000

    prefer size_type over size_t

--- src/util/expanding_vector.h
+++ src/util/expanding_vector.h
@@ -15,13 +15,13 @@ template<typename T>
 class expanding_vector:public std::vector<T>
 {
 public:
-  inline T & operator[] (std::size_t n)
+  inline T & operator[] (typename std::vector<T>::size_type n)
   {
     check_index(n);
     return subt::operator[](n);
   }
   
-  inline const T & operator[] (std::size_t n) const
+  inline const T & operator[] (typename std::vector<T>::size_type n) const
   {
     // hack-ish const cast
     const_cast<expanding_vector *>(this)->check_index(n);
@@ -32,7 +32,7 @@ protected:
   typedef std::vector<T> subt;
 
   // make the vector large enough to contain 'n'
-  inline void check_index(std::size_t n)
+  inline void check_index(typename std::vector<T>::size_type n)
   {
     if(n>=subt::size()) subt::resize(n+1);
   }
