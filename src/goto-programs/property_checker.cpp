d88ffd43f9a694301f36a2ed16c10dc7d9ac1850
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-07 16:53:28 +0000

    property_checkert interface
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3910 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/goto-programs/property_checker.cpp
@@ -0,0 +1,28 @@
+/*******************************************************************\
+
+Module: Property Checker Interface
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "property_checker.h"
+
+/*******************************************************************\
+
+Function: property_checkert::property_checkert
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+property_checkert::property_checkert(
+  message_handlert &_message_handler):
+  messaget(_message_handler)
+{
+}
+
