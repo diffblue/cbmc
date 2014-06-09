9bf9aa4ffe041171738b926344dcbad9a6358719
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-09 13:47:14 +0000

    inline the mini methods

--- src/ansi-c/ansi_c_declaration.h
+++ src/ansi-c/ansi_c_declaration.h
@@ -16,144 +16,146 @@ Author: Daniel Kroening, kroening@kroening.com
 class ansi_c_declarationt:public exprt
 {
 public:
-  ansi_c_declarationt();
+  inline ansi_c_declarationt():exprt(ID_declaration)
+  {
+  }
   
-  exprt &value()
+  inline exprt &value()
   {
     return static_cast<exprt &>(add(ID_value));
   }
   
-  const exprt &value() const
+  inline const exprt &value() const
   {
     return static_cast<const exprt &>(find(ID_value));
   }
   
-  void set_name(const irep_idt &name)
+  inline void set_name(const irep_idt &name)
   {
     return set(ID_name, name);
   }
   
-  irep_idt get_name() const
+  inline irep_idt get_name() const
   {
     return get(ID_name);
   }
   
-  irep_idt get_base_name() const
+  inline irep_idt get_base_name() const
   {
     return get(ID_base_name);
   }
   
-  void set_base_name(const irep_idt &base_name)
+  inline void set_base_name(const irep_idt &base_name)
   {
     return set(ID_base_name, base_name);
   }
   
-  bool get_is_type() const
+  inline bool get_is_type() const
   {
     return get_bool(ID_is_type);
   }
   
-  void set_is_type(bool is_type)
+  inline void set_is_type(bool is_type)
   {
     set(ID_is_type, is_type);
   }
   
-  bool get_is_typedef() const
+  inline bool get_is_typedef() const
   {
     return get_bool(ID_is_typedef);
   }
   
-  void set_is_typedef(bool is_typedef)
+  inline void set_is_typedef(bool is_typedef)
   {
     set(ID_is_typedef, is_typedef);
   }
   
-  bool get_is_macro() const
+  inline bool get_is_macro() const
   {
     return get_bool(ID_is_macro);
   }
   
-  void set_is_macro(bool is_macro)
+  inline void set_is_macro(bool is_macro)
   {
     set(ID_is_macro, is_macro);
   }
   
-  bool get_is_static() const
+  inline bool get_is_static() const
   {
     return get_bool(ID_is_static);
   }
   
-  void set_is_static(bool is_static)
+  inline void set_is_static(bool is_static)
   {
     set(ID_is_static, is_static);
   }
   
-  bool get_is_parameter() const
+  inline bool get_is_parameter() const
   {
     return get_bool(ID_is_parameter);
   }
   
-  void set_is_parameter(bool is_parameter)
+  inline void set_is_parameter(bool is_parameter)
   {
     set(ID_is_parameter, is_parameter);
   }
   
-  bool get_is_global() const
+  inline bool get_is_global() const
   {
     return get_bool(ID_is_global);
   }
   
-  void set_is_global(bool is_global)
+  inline void set_is_global(bool is_global)
   {
     set(ID_is_global, is_global);
   }
   
-  bool get_is_register() const
+  inline bool get_is_register() const
   {
     return get_bool(ID_is_register);
   }
   
-  void set_is_register(bool is_register)
+  inline void set_is_register(bool is_register)
   {
     set(ID_is_register, is_register);
   }
   
-  bool get_is_thread_local() const
+  inline bool get_is_thread_local() const
   {
     return get_bool(ID_is_thread_local);
   }
   
-  void set_is_thread_local(bool is_thread_local)
+  inline void set_is_thread_local(bool is_thread_local)
   {
     set(ID_is_thread_local, is_thread_local);
   }
   
-  bool get_is_inline() const
+  inline bool get_is_inline() const
   {
     return get_bool(ID_is_inline);
   }
   
-  void set_is_inline(bool is_inline)
+  inline void set_is_inline(bool is_inline)
   {
     set(ID_is_inline, is_inline);
   }
   
-  bool get_is_extern() const
+  inline bool get_is_extern() const
   {
     return get_bool(ID_is_extern);
   }
   
-  void set_is_extern(bool is_extern)
+  inline void set_is_extern(bool is_extern)
   {
     set(ID_is_extern, is_extern);
   }
   
-  bool get_is_static_assert() const
+  inline bool get_is_static_assert() const
   {
     return get_bool(ID_is_static_assert);
   }
   
-  void set_is_static_assert(bool is_static_assert)
+  inline void set_is_static_assert(bool is_static_assert)
   {
     set(ID_is_static_assert, is_static_assert);
   }
