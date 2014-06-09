715bfbc4b9cf600062ae8b13800c06b21878ac2a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-09 12:29:55 +0000

    Implement GCC's (extern) inline semantics incl  __attribute__((__gnu_inline__))
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@3920 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/ansi_c_declaration.cpp
+++ src/ansi-c/ansi_c_declaration.cpp
@@ -8,6 +8,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <cassert>
 
+#include <util/config.h>
+
 #include "ansi_c_declaration.h"
 
 /*******************************************************************\
@@ -74,4 +76,22 @@ void ansi_c_declarationt::to_symbol(symbolt &symbol) const
   
   if(get_is_inline())
     symbol.type.set(ID_C_inlined, true);
+
+  // GCC extern inline cleanup, to enable remove_internal_symbols
+  // do its full job
+  // https://gcc.gnu.org/ml/gcc/2006-11/msg00006.html
+  if(is_code && get_is_inline())
+  {
+    // __attribute__((__gnu_inline__))
+    if(get_is_static() && get_is_extern())
+      symbol.is_extern=false;
+    // C99 and above
+    else if(config.ansi_c.for_has_scope ||
+            (config.ansi_c.mode!=configt::ansi_ct::MODE_GCC_C &&
+             config.ansi_c.mode!=configt::ansi_ct::MODE_ARM_C_CPP))
+      symbol.is_file_local=symbol.is_file_local || !get_is_extern();
+    // traditional GCC
+    else
+      symbol.is_file_local=symbol.is_file_local || get_is_extern();
+  }
 }
