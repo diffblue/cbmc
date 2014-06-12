0564a7df90a0e10b45068e264aa36162c466d4fc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-12 17:30:13 +0000

    Some GCC beauties that should be parsed/type checked

---
+++ regression/ansi-c/Struct_Enum_Padding1/main.c
@@ -0,0 +1,30 @@
+#ifdef __GNUC__
+
+#include <inttypes.h>
+
+#define STATIC_ASSERT(condition) \
+  int some_array##__LINE__[(condition) ? 1 : -1]
+
+// Debian package openvswitch
+enum __attribute__((__packed__)) ofpact_type {
+  e_1
+};
+
+enum __attribute__((__packed__)) ofputil_action_code {
+  e_2
+};
+
+struct ofpact {
+    enum ofpact_type type;      /* OFPACT_*. */
+    enum ofputil_action_code compat; /* Original type when added, if any. */
+    uint16_t len;               /* Length of the action, in bytes, including
+                                 * struct ofpact, excluding padding. */
+};
+
+STATIC_ASSERT(sizeof(struct ofpact) == 4);
+#endif
+
+int main()
+{
+}
+
