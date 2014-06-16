c688656f1810d5a73a0abc34a745f8a3b8e72bbd
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-16 12:18:49 +0000

    fix

--- regression/ansi-c/pragma_pack1/main.c
+++ regression/ansi-c/pragma_pack1/main.c
@@ -6,6 +6,7 @@ typedef unsigned __int64 uint64_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
+#define __extension__
 #else
 #include <inttypes.h>
 #endif
