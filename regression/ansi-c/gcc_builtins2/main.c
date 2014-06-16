8fcf18e37f2e2e2fb3d0a70d2bc3b56c1bcc0ce7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-16 09:23:17 +0000

    More from GCC: __typeof__ and __real__ can be used in  type casts...

--- regression/ansi-c/gcc_builtins2/main.c
+++ regression/ansi-c/gcc_builtins2/main.c
@@ -3,8 +3,18 @@
 int main()
 {
   #ifdef __GNUC__
+  // expansion of fabs
   double damp;
   (void)(__extension__(__builtin_classify_type (__real__ ((__typeof__ (damp)) 0)) == 8));
+  float famp;
+  (void)(__extension__(
+      __typeof__(__real__(
+          __typeof__(*(
+              (__typeof__(
+                  (__typeof__(
+                      (__typeof__ (famp)) 0) *) 0 ))
+              0)))
+        0)) famp);
   #endif
 
   return 0;
