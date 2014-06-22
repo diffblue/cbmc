a514256f81f030554ddcdf8d721dfd5e1fbadb9c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-22 14:49:02 +0000

    weaken assertion

--- src/solvers/flattening/boolbv_byte_extract.cpp
+++ src/solvers/flattening/boolbv_byte_extract.cpp
@@ -29,10 +29,7 @@ Function: map_bv
 
 bvt map_bv(const endianness_mapt &map, const bvt &src)
 {
-  // don't do anything for a byte or less
-  if(src.size()<=8) return src;
-
-  assert(map.size()*8==src.size());
+  assert(map.size()*8>=src.size());
 
   bvt result;
   result.resize(src.size(), const_literal(false));
