9e290d0e00498cb16ae777507def541c14dd915a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-23 19:01:36 +0000

    extra check

--- regression/ansi-c/gcc_vector1/main.c
+++ regression/ansi-c/gcc_vector1/main.c
@@ -4,13 +4,15 @@
 
 int main()
 {
-#if defined(__GNUC__) && !defined(__clang__)
+  #if defined(__GNUC__) && !defined(__clang__)
   unsigned int __attribute__((vector_size (4*sizeof(unsigned int)))) v1 = { 1, 1, 1, 1 };
   unsigned int __attribute__((vector_size (4*sizeof(unsigned int)))) v3 = { 3, 3, 3, 3 };
 
+  assert(sizeof(v1==v3)==sizeof(int)*4);
+
   // accepted by GCC, but not Clang
-  assert((v3 == (v1<<1)+v1)[0]);;
-#endif
+  assert((v3 == (v1<<1)+v1)[0]);
+  #endif
 
   return 0;
 }
