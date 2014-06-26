a5f16b25cef3af65ae0c0c60df3893a28b050bfa
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-26 12:01:16 +0000

    More (yet possibly dubious) vector operations

--- regression/ansi-c/gcc_vector1/main.c
+++ regression/ansi-c/gcc_vector1/main.c
@@ -1,6 +1,7 @@
 // Debian package pixman
 
 #include <assert.h>
+#include <stdio.h>
 
 int main()
 {
@@ -12,6 +13,13 @@ int main()
 
   // accepted by GCC, but not Clang
   assert((v3 == (v1<<1)+v1)[0]);
+
+  unsigned int __attribute__((vector_size (4*sizeof(unsigned int)))) b2 = { 3, 3, 3, 3 };
+  unsigned int __attribute__((vector_size (4*sizeof(unsigned int)))) b1 = { 1, 1, 1, 1 };
+  printf("(b2>b1)[0]=%d\n", (b2>b1)[0]); // -1
+  printf("b2[0]>b1[0]=%d\n", b2[0]>b1[0]); // 1
+  b2|=(b2>=b1);
+  assert(b2[0]>=3); // that's really the only guarantee
   #endif
 
   return 0;
