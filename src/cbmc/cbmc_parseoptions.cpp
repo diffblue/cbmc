6ad0c36f03331156f852c74065bf32973698a8ad
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-27 14:24:00 +0000

    added CVC3/4 and OpenSMT
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4176 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/cbmc/cbmc_parseoptions.cpp
+++ src/cbmc/cbmc_parseoptions.cpp
@@ -279,29 +279,103 @@ void cbmc_parseoptionst::get_command_line_options(optionst &options)
   if(cmdline.isset("aig"))
     options.set_option("aig", true);
 
-  if(cmdline.isset("boolector"))
-    options.set_option("boolector", true);
-
-  if(cmdline.isset("mathsat"))
-    options.set_option("mathsat", true);
-
-  if(cmdline.isset("cvc"))
-    options.set_option("cvc", true);
+  // SMT Options
+  bool version_set = false;
 
   if(cmdline.isset("smt1"))
+  {
     options.set_option("smt1", true);
+    options.set_option("smt2", false);
+    version_set = true;
+  }
 
   if(cmdline.isset("smt2"))
+  {
+    options.set_option("smt1", false);// If both are given, smt2 takes precedence
     options.set_option("smt2", true);
+    version_set = true;
+  }
 
   if(cmdline.isset("fpa"))
     options.set_option("fpa", true);
 
+
+  bool solver_set = false;
+
+  if(cmdline.isset("boolector"))
+  {
+    options.set_option("boolector", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt1", true), version_set = true;
+  }
+
+  if(cmdline.isset("mathsat"))
+  {
+    options.set_option("mathsat", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt2", true), version_set = true;
+  }
+
+  if(cmdline.isset("cvc3"))
+  {
+    options.set_option("cvc3", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt1", true), version_set = true;
+  }
+
+  if(cmdline.isset("cvc4"))
+  {
+    options.set_option("cvc4", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt2", true), version_set = true;
+  }
+
   if(cmdline.isset("yices"))
-    options.set_option("yices", true);
+  {
+    options.set_option("yices", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt2", true), version_set = true;
+  }
 
   if(cmdline.isset("z3"))
-    options.set_option("z3", true);
+  {
+    options.set_option("z3", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt2", true), version_set = true;
+  }
+
+  if(cmdline.isset("opensmt"))
+  {
+    options.set_option("opensmt", true), solver_set = true;
+    if (!version_set)
+      options.set_option("smt1", true), version_set = true;
+  }
+
+
+  if (version_set && !solver_set)
+  {
+    if (cmdline.isset("outfile"))
+    {
+      // outfile and no solver should give standard compliant SMT-LIB
+      options.set_option("generic", true), solver_set = true;
+    }
+    else
+    {
+      if (options.get_bool_option("smt1"))
+      {
+	options.set_option("boolector", true), solver_set = true;
+      }
+      else
+      {
+	assert(options.get_bool_option("smt2"));
+	options.set_option("mathsat", true), solver_set = true;
+      }
+    }
+  }
+  // Either have solver and standard version set, or neither.
+  assert(version_set == solver_set);
+
+
 
   if(cmdline.isset("beautify"))
     options.set_option("beautify", true);
@@ -893,9 +967,11 @@ void cbmc_parseoptionst::help()
     " --smt2                       output subgoals in SMT2 syntax (experimental)\n"
     " --boolector                  use Boolector (experimental)\n"
     " --mathsat                    use MathSAT (experimental)\n"
-    " --cvc                        use CVC3 (experimental)\n"
+    " --cvc3                       use CVC3 (experimental)\n"
+    " --cvc4                       use CVC4 (experimental)\n"
     " --yices                      use Yices (experimental)\n"
     " --z3                         use Z3 (experimental)\n"
+    " --opensmt                    use OpenSMT (experimental)\n"
     " --refine                     use refinement procedure (experimental)\n"
     " --outfile filename           output formula to given file\n"
     " --arrays-uf-never            never turn arrays into uninterpreted functions\n"
