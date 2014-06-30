027a1c02250d863629b52f6dd41f72a989d6065d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-06-30 13:37:39 +0000

    decltype tests from Martin

---
+++ regression/cpp/Decltype2/main.cpp
@@ -0,0 +1,11 @@
+// C++11
+// decltype is a C++11 feature to get the "declared type" of an expression.
+// It is similar to 'typeof' but handles reference types "properly".
+
+class base {};
+class derived : public base {};
+
+derived d;
+
+decltype(static_cast<derived *>(&d)) z;
+
