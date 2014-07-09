ec03a3d0d09aa043ea5c37ae66c736ca466c1a8a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-07-09 14:51:10 +0000

    fix parser

--- src/solvers/smt2/parse_smt2.cpp
+++ src/solvers/smt2/parse_smt2.cpp
@@ -25,41 +25,39 @@ Function: parse_smt2
 irept parse_smt2(const std::string &src)
 {
   std::size_t pos=0;
-  irept result;
-  std::stack<irept *> stack;
-  
-  stack.push(&result);
-  
-  while(!stack.empty())
-  {
-    // skip any spaces
-    while(pos<src.size() && src[pos]==' ') pos++;
+  std::stack<irept> stack;
+  stack.push(irept());
 
-    if(pos>=src.size())
+  while(pos<src.size())
+  {
+    if(src[pos]==' ') // skip any spaces
     {
-      // blank
-      stack.pop();
+      pos++;
     }
     else if(src[pos]=='(')
     {
       // produce sub-irep
-      stack.top()->get_sub().push_back(irept());
-      stack.push(&stack.top()->get_sub().front());
+      stack.push(irept());
       pos++;
     }
     else if(src[pos]==')')
     {
       // done with sub-irep
+      if(stack.size()<=1) return irept(); // too many )
+      irept tmp=stack.top();
       stack.pop();
+      stack.top().get_sub().push_back(tmp);
       pos++;
     }
     else if(src[pos]=='|')
     {
       // get token
       std::size_t start=pos;
+      pos++;
       while(pos<src.size() && src[pos]!='|') pos++;
+      if(pos<src.size()) pos++;
       std::string token=src.substr(start, pos-start);
-      stack.top()->get_sub().push_back(irept(token));
+      stack.top().get_sub().push_back(irept(token));
     }
     else
     {
@@ -67,9 +65,14 @@ irept parse_smt2(const std::string &src)
       std::size_t start=pos;
       while(pos<src.size() && src[pos]!=' ' && src[pos]!='(' && src[pos]!=')') pos++;
       std::string token=src.substr(start, pos-start);
-      stack.top()->get_sub().push_back(irept(token));
+      stack.top().get_sub().push_back(irept(token));
     }
   }
-  
-  return result;
+
+  // The stack should really have size 1, or it's a parse error
+
+  if(stack.size()==1 && stack.top().get_sub().size()==1)
+    return stack.top().get_sub().front();
+  else
+    return irept();
 }
