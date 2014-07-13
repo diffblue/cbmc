e83c1bfba51f626ecf1c22a13bace76624675789
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-07-13 19:14:34 +0000

    avoid double linking for library

--- src/ansi-c/cprover_library.cpp
+++ src/ansi-c/cprover_library.cpp
@@ -80,12 +80,8 @@ void add_cprover_library(
     ansi_c_languaget ansi_c_language;
     ansi_c_language.set_message_handler(message_handler);
     ansi_c_language.parse(in, "");
-
-    symbol_tablet new_symbol_table;
-    ansi_c_language.typecheck(
-      new_symbol_table, "<built-in-library>");
-
-    linking(symbol_table, new_symbol_table, message_handler);
+    
+    ansi_c_language.typecheck(symbol_table, "<built-in-library>");
   }
 }
 
