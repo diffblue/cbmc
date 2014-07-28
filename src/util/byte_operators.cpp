60fbe3295cf1fbfaf95656ac2f024cd95931fc87
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-07-28 14:53:00 +0000

    added byte_update_id

--- src/util/byte_operators.cpp
+++ src/util/byte_operators.cpp
@@ -44,6 +44,33 @@ irep_idt byte_extract_id()
 
 /*******************************************************************\
 
+Function: byte_update_id
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+irep_idt byte_update_id()
+{
+  switch(config.ansi_c.endianness)
+  {
+  case configt::ansi_ct::IS_LITTLE_ENDIAN:
+    return ID_byte_update_little_endian;
+
+  case configt::ansi_ct::IS_BIG_ENDIAN:
+    return ID_byte_update_big_endian;
+
+  default:
+    assert(false);
+  }
+}
+
+/*******************************************************************\
+
 Function: endianness_mapt::output
 
   Inputs:
