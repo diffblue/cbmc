635a18dc70fe9b1a0a6e7e57d90893d9479dd28a
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-01 08:50:35 +0000

    more quantifier testing
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4387 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc/Quantifiers1/main.c
+++ regression/cbmc/Quantifiers1/main.c
@@ -7,4 +7,14 @@ int main()
 
   // ACSL style
   assert(\forall int i; ((i>=0 && i<10) ==> (zero_array[i]==0)));
+
+  const unsigned N=10;
+  unsigned i=0;
+  char c[N];
+
+  for(i=0; i<N; ++i)
+    c[i]=i;
+
+  assert(__CPROVER_forall {unsigned i; i>9 || c[i]==i});
+  return 0;                
 }
