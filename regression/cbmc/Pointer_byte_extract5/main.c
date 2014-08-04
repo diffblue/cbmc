9f8d6130d6b86eb68e61d8166b56b01500728828
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-04 12:29:39 +0000

    __attribute__((packed)) is gcc only

--- regression/cbmc/Pointer_byte_extract5/main.c
+++ regression/cbmc/Pointer_byte_extract5/main.c
@@ -6,20 +6,25 @@ typedef union
   int b;
 } Union;
 
+#ifdef __GNUC__
 typedef struct
 {
   int Count;
   Union List[1];
 } __attribute__((packed)) Struct3;
-
-extern size_t __CPROVER_malloc_size;
+#else
+typedef struct
+{
+  int Count;
+  Union List[1];
+} Struct3;
+#endif
 
 int main()
 {
   Struct3 *p = malloc (sizeof (int) + 2 * sizeof(Union));
   p->Count = 3;
   int po=0;
-  size_t m=__CPROVER_malloc_size;
 
   // this should be fine
   p->List[0].a = 555;
