297acfcb4cff569abacefb65e44b6c2cbb5b266d
schram <schram@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-15 14:51:42 +0000

    magic numbers
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/branches/peter-incremental-unwinding@4472 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-symex/symex_target_equation.cpp
+++ src/goto-symex/symex_target_equation.cpp
@@ -863,6 +863,11 @@ void symex_target_equationt::convert_assertions(
         assumption,
         it->cond_expr);
 
+#if 0
+      std::cout << "assertion: " << from_expr(ns,"",it->cond_expr) 
+                << std::endl;
+#endif
+
       // do the conversion
       it->cond_literal=prop_conv.convert(implication);
       // store disjunct
