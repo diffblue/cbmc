32ce1934d829ae9a02aad1edb43f2172dfc52c2d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-23 14:49:32 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4514 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cpp/Trailing_Return_Type1/main.cpp
+++ regression/cpp/Trailing_Return_Type1/main.cpp
@@ -6,6 +6,26 @@ auto f00(int x) -> int
   return x + 1;
 }
 
+class some_class
+{
+  // as a method
+
+  #if 0
+  static auto f002(int x) -> int
+  {
+    return x + 1;
+  }
+  #endif
+  
+  // template function
+  template<typename someT>
+  static auto f003(int x) -> someT
+  {
+    return 0;
+  }
+
+};
+
 int main()
 {
   int i;
