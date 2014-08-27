4d457b5b4510c665342e6ae35a1c8738965b5e3f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-27 13:37:35 +0000

    mildly better typing
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4551 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/c_typecheck_initializer.cpp
+++ src/ansi-c/c_typecheck_initializer.cpp
@@ -292,12 +292,12 @@ void c_typecheck_baset::designator_enter(
   
   if(full_type.id()==ID_struct)
   {
-    const struct_union_typet &struct_type=to_struct_type(full_type);
+    const struct_typet &struct_type=to_struct_type(full_type);
 
     entry.size=struct_type.components().size();
     entry.subtype.make_nil();
 
-    for(struct_union_typet::componentst::const_iterator
+    for(struct_typet::componentst::const_iterator
         it=struct_type.components().begin();
         it!=struct_type.components().end();
         ++it)
@@ -890,11 +890,11 @@ exprt c_typecheck_baset::do_initializer_list(
     assert(result.operands().size()==
            to_struct_type(full_type).components().size());
 
-    const struct_union_typet::componentst &components=
+    const struct_typet::componentst &components=
       to_struct_type(full_type).components();
 
     exprt::operandst::const_iterator o_it=result.operands().begin();
-    for(struct_union_typet::componentst::const_iterator
+    for(struct_typet::componentst::const_iterator
         c_it=components.begin();
         c_it!=components.end();
         c_it++, ++o_it)
