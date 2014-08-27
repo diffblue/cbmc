a98b72342c492ac56e1024123022a47af4df6cbc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-08-27 19:49:31 +0000

    this one should pass
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4573 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/ansi-c/Struct_Padding6/main.c
+++ regression/ansi-c/Struct_Padding6/main.c
@@ -1,11 +1,13 @@
+#define STATIC_ASSERT(condition) \
+  int some_array##__LINE__[(condition) ? 1 : -1]
+
 struct _classinfo {
   char a;
   int *interfaces[];
 };
 
-struct _classinfo nullclass = { 0, 42 };
+struct _classinfo nullclass = { 42, 0, 0 };
 
 int main()
 {
 }
-
