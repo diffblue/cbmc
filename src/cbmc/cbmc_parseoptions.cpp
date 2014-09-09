6c40c03bf921875ad77416e041ac58b84069cfb4
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-09-09 15:29:11 +0000

    use remove_returns in CBMC

--- src/cbmc/cbmc_parseoptions.cpp
+++ src/cbmc/cbmc_parseoptions.cpp
@@ -21,6 +21,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <goto-programs/goto_convert_functions.h>
 #include <goto-programs/remove_function_pointers.h>
+#include <goto-programs/remove_returns.h>
 #include <goto-programs/goto_inline.h>
 #include <goto-programs/show_properties.h>
 #include <goto-programs/set_properties.h>
@@ -764,6 +765,7 @@ bool cbmc_parseoptionst::process_goto_program(
       string_instrumentation(
         symbol_table, get_message_handler(), goto_functions);
 
+    // remove function pointers
     status() << "Function Pointer Removal" << eom;
     remove_function_pointers(symbol_table, goto_functions,
       cmdline.isset("pointer-check"));
@@ -771,6 +773,9 @@ bool cbmc_parseoptionst::process_goto_program(
     // do partial inlining
     status() << "Partial Inlining" << eom;
     goto_partial_inline(goto_functions, ns, ui_message_handler);
+    
+    // remove returns
+    remove_returns(symbol_table, goto_functions);
     
     // add generic checks
     status() << "Generic Property Instrumentation" << eom;
