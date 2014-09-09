0e717c949c60d75e76689ff705c70cd789c6c173
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-09-09 13:56:16 +0000

    comments
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4601 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-programs/remove_returns.h
+++ src/goto-programs/remove_returns.h
@@ -13,6 +13,10 @@ Date:   September 2009
 
 #include <goto-programs/goto_model.h>
 
+// Turns 'return x' into an assignment to fkt#return_value,
+// unless the function returns void,
+// and a 'goto the_end_of_the_function'.
+
 void remove_returns(symbol_tablet &, goto_functionst &);
 
 void remove_returns(goto_modelt &);
