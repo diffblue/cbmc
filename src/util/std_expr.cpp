ff46f32875e8e1dc51240867539dc714401b46d2
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-10-20 08:53:04 +0000

    typing

--- src/util/std_expr.cpp
+++ src/util/std_expr.cpp
@@ -105,37 +105,37 @@ static void build_ssa_identifier_rec(
   const exprt &expr,
   const irep_idt &l0,
   const irep_idt &l1,
-  std::ostringstream &oss)
+  std::ostream &os)
 {
   if(expr.id()==ID_member)
   {
     const member_exprt &member=to_member_expr(expr);
 
-    build_ssa_identifier_rec(member.struct_op(), l0, l1, oss);
+    build_ssa_identifier_rec(member.struct_op(), l0, l1, os);
 
-    oss << '.' << member.get_component_name();
+    os << '.' << member.get_component_name();
   }
   else if(expr.id()==ID_index)
   {
     const index_exprt &index=to_index_expr(expr);
 
-    build_ssa_identifier_rec(index.array(), l0, l1, oss);
+    build_ssa_identifier_rec(index.array(), l0, l1, os);
 
     mp_integer idx;
     if(to_integer(to_constant_expr(index.index()), idx))
       assert(false);
 
-    oss << '[' << idx << ']';
+    os << '[' << idx << ']';
   }
   else if(expr.id()==ID_symbol)
   {
-    oss << to_symbol_expr(expr).get_identifier();
+    os << to_symbol_expr(expr).get_identifier();
 
     if(!l0.empty())
-      oss << '!' << l0;
+      os << '!' << l0;
 
     if(!l1.empty())
-      oss << '@' << l1;
+      os << '@' << l1;
   }
   else
     assert(false);
