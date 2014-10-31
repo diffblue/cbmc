e0df0a818413b0da85a318eb8c140e8142cc9434
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-10-31 16:27:07 +0000

    added algorithm header for std::max

--- src/util/namespace.cpp
+++ src/util/namespace.cpp
@@ -6,6 +6,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <algorithm>
+
 #include <cassert>
 
 #include "string2int.h"
