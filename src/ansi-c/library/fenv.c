c31e9c307c6651af09bf1fdf0815126f424a631c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-11-23 11:14:35 +0000

    fix for fegetround
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4769 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/library/fenv.c
+++ src/ansi-c/library/fenv.c
@@ -10,12 +10,12 @@ __CPROVER_HIDE:;
   // CPROVER uses the x86 numbering of the rounding modes
   return
          #ifdef FE_DOWNWARD
-         __CPROVER_rounding_mode==FE_DOWNWARD?1:
+         __CPROVER_rounding_mode==1?FE_DOWNWARD:
          #endif
-         __CPROVER_rounding_mode==FE_TONEAREST?0:
-         __CPROVER_rounding_mode==FE_TOWARDZERO?3:
+         __CPROVER_rounding_mode==0?FE_TONEAREST:
+         __CPROVER_rounding_mode==3?FE_TOWARDZERO:
          #ifdef FE_UPWARD
-         __CPROVER_rounding_mode==FE_UPWARD?2:
+         __CPROVER_rounding_mode==2?FE_UPWARD:
          #endif
          -1;
 }
