6e4ec12b418fbfb24d8dd2f403953ee4e0737155
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-11-26 10:01:45 +0000

    patch 4 from Vincent
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4782 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/wmm/shared_buffers.cpp
+++ src/goto-instrument/wmm/shared_buffers.cpp
@@ -1081,7 +1081,9 @@ bool shared_bufferst::is_buffered(
      identifier=="c::stderr" ||
      identifier=="c::sys_nerr" ||
      has_prefix(id2string(identifier), "__unbuffered_") ||
-     has_prefix(id2string(identifier), "c::__unbuffered_") )
+     has_prefix(id2string(identifier), "c::__unbuffered_")  ||
+     has_prefix(id2string(identifier), "__CPROVER") ||
+     has_prefix(id2string(identifier), "c::__CPROVER") )
     return false; // not buffered
 
   const symbolt &symbol=ns.lookup(identifier);
