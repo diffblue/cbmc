6ba637fa2d92af1be85a7f3223bcbfd821f92b9b
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-11-30 12:45:38 +0000

    convert more type information to XML

--- src/util/xml_expr.h
+++ src/util/xml_expr.h
@@ -10,12 +10,17 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_XML_EXPR_H
 
 class source_locationt;
+class typet;
 class exprt;
 class xmlt;
 class namespacet;
 
 xmlt xml(
   const exprt &,
+  const namespacet &);
+
+xmlt xml(
+  const typet &,
   const namespacet &);
 
 xmlt xml(const source_locationt &);
