d6bc288fbfe2b654e3b76ca4db427dbe36d63269
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-11-30 14:19:22 +0000

    construct enums from ints

--- regression/cpp/enum1/main.cpp
+++ regression/cpp/enum1/main.cpp
@@ -1,4 +1,4 @@
-enum { E1, E2, E3 } a;
+enum E123_tag { E1, E2, E3 } a;
 
 class Z
 {
@@ -11,8 +11,10 @@ public:
 
 static_assert(Z::E4==4, "value of Z::E4");
 static_assert(sizeof(z.array)==sizeof(int)*5, "size of z.array");
-static_assert(((int)E1)==0, "value of E1");
-static_assert(((int)E2)==1, "value of E2");
+static_assert(E1==0, "value of E1");
+static_assert(E2==1, "value of E2");
+static_assert(E123_tag(0)==E1, "value of E123_tag(0)");
+static_assert((E123_tag)0==E1, "value of E123_tag(0)");
 
 int main()
 {
