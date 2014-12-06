6509ff4f97817c79418bbe82018fe8e86cd9661f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-12-06 20:58:48 +0000

    don't test integers

--- regression/cbmc/Float_lib1/main.c
+++ regression/cbmc/Float_lib1/main.c
@@ -13,7 +13,7 @@ int main() {
   #else
   assert(fpclassify(DBL_MAX+DBL_MAX)==FP_INFINITE);
   assert(fpclassify(0*(DBL_MAX+DBL_MAX))==FP_NAN);
-  assert(fpclassify(1)==FP_NORMAL);
+  assert(fpclassify(1.0)==FP_NORMAL);
   assert(fpclassify(DBL_MIN)==FP_NORMAL);
   assert(fpclassify(DBL_MIN/2)==FP_SUBNORMAL);
   assert(fpclassify(-0.0)==FP_ZERO);
