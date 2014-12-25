772c8b85a6c18034f3aad578bef824e3c0009d12
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-12-25 16:40:32 +0000

    use static_assert

--- regression/cpp/Bit_fields1/main.cpp
+++ regression/cpp/Bit_fields1/main.cpp
@@ -1,5 +1,3 @@
-//#include <cassert>
-
 typedef int INT;
 
 typedef enum { E1=1 } some_enum_type;
@@ -25,14 +23,16 @@ struct some_struct {
   some_enum_type enum_field2 : E1;
 } X;
 
-int func(unsigned int) { return 1; }
-int func(signed int) { return 2; }
-int func(some_enum_type) { return 3; }
- 
+template<typename T> class trait { public: enum { t=0 }; };
+template<> class trait<unsigned> { public: enum { t=1 }; };
+template<> class trait<signed> { public: enum { t=2 }; };
+template<> class trait<some_enum_type> { public: enum { t=3 }; };
+
+//static_assert(trait<decltype(X.a)>::t==1, "X.a");
+//static_assert(trait<decltype(X.c)>::t==2, "X.c");
+//static_assert(trait<decltype(X.x)>::t==2, "X.x");
+//static_assert(trait<decltype(X.enum_field1)>::t==3, "X.enum_field1");
+
 int main()
 {
-  assert(func(X.a)==1);
-  assert(func(X.c)==2);
-  assert(func(X.x)==2);
-  assert(func(X.enum_field1)==3);
 }
