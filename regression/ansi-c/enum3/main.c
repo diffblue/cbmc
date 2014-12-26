d5ced530e33804ff7b911126b44ca86ec2afecd6
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-12-26 14:37:01 +0000

    check operators

--- regression/ansi-c/enum3/main.c
+++ regression/ansi-c/enum3/main.c
@@ -8,6 +8,7 @@ enum __attribute__((packed)) { E2v1=-1, E2v2 } e2;
 enum __attribute__((packed)) { E3v1=1000, E3v2 } e3;
 enum { E4v1=-1, E4v2 } e4; // not packed
 enum { E5v0=10, E5v1=0x100000000ll } e5;
+enum { E6 = E3v1 | 1 };
 
 STATIC_ASSERT(sizeof(e1) == 1);
 STATIC_ASSERT(sizeof(e2) == 1);
@@ -15,9 +16,17 @@ STATIC_ASSERT(sizeof(e3) == 2);
 STATIC_ASSERT(sizeof(E1v1) == sizeof(int));
 STATIC_ASSERT(sizeof(e4) == sizeof(int));
 // STATIC_ASSERT(sizeof(e5) == 8); // todo
+STATIC_ASSERT(E6 == 1001);
 
 #endif
 
 int main()
 {
+  e5+=1;
+  e5|=1;
+  e5|=e5;
+  e5&=1;
+  e5&=(_Bool)1;
+  e5^=(_Bool)1;
+  e5<<=1;
 }
