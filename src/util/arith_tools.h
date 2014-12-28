43c5d1fc1b06b2098c668ac960bcf63db327af7c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-12-28 12:21:23 +0000

    added helpful comments
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4924 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/arith_tools.h
+++ src/util/arith_tools.h
@@ -16,11 +16,13 @@ class constant_exprt;
 class typet;
 
 // this one will go away
+// returns 'true' on error
 bool to_integer(const exprt &expr, mp_integer &int_value);
 
-// use this one
+// returns 'true' on error
 bool to_integer(const constant_exprt &expr, mp_integer &int_value);
 
+// returns 'nil' on error
 constant_exprt from_integer(const mp_integer &int_value, const typet &type);
 
 // ceil(log2(size))
