6c2af90e125d53af7dec1d6a7d95151577d0ce95
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2014-12-31 14:17:39 +0000

    added cpp_namet::namet
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@4963 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/cpp/cpp_name.h
+++ src/cpp/cpp_name.h
@@ -14,18 +14,61 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 class cpp_namet:public irept
 {
 public:
+  // the subs are one of the following:
+  // ID_name (see namet)
+  // ID_operator
+  // ID_template_args
+  // ::
+  // ~
+
+  class namet:public irept
+  {
+  public:
+    inline namet():irept(ID_name)
+    {
+    }
+
+    explicit inline namet(const irep_idt &base_name):irept(ID_name)
+    {
+      set(ID_identifier, base_name);
+    }
+
+    inline namet(
+      const irep_idt &_base_name,
+      const source_locationt &_source_location):irept(ID_name)
+    {
+      set(ID_identifier, _base_name);
+      add_source_location()=_source_location;
+    }
+    
+    inline source_locationt &add_source_location()
+    {
+      return static_cast<source_locationt &>(add(ID_C_source_location));
+    }
+
+    inline const source_locationt &source_location() const
+    {
+      return static_cast<const source_locationt &>(find(ID_C_source_location));
+    }
+  };
+
   inline cpp_namet():irept(ID_cpp_name)
   {
   }
   
-  explicit cpp_namet(const irep_idt &base_name):irept(ID_cpp_name)
+  inline explicit cpp_namet(const irep_idt &base_name):irept(ID_cpp_name)
   {
-    subt &sub=get_sub();
-    sub.push_back(irept(ID_name));
-    sub.front().set(ID_identifier, base_name);
+    get_sub().push_back(namet(base_name));
   }
 
-  const source_locationt &source_location() const
+  inline cpp_namet(
+    const irep_idt &_base_name,
+    const source_locationt &_source_location):irept(ID_cpp_name)
+  {
+    get_sub().push_back(namet(_base_name, _source_location));
+  }
+
+  inline const source_locationt &source_location() const
   {
     if(get_sub().empty())
       return static_cast<const source_locationt &>(get_nil_irep());
