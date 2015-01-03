22344c1860533a825603afe2dd396bafb6eb6a5e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-03 22:04:29 +0000

    more

--- regression/ansi-c/Struct_Initialization1/main.c
+++ regression/ansi-c/Struct_Initialization1/main.c
@@ -6,7 +6,11 @@ struct _classinfo {
   int *interfaces[];
 };
 
-struct _classinfo nullclass = { 42, 0, 0 };
+struct _classinfo nullclass1 = { 42, 0, 0 };
+struct _classinfo nullclass2 = { 42, { 0, 0 } };
+
+STATIC_ASSERT(sizeof(nullclass1)==sizeof(struct _classinfo));
+STATIC_ASSERT(sizeof(nullclass2)==sizeof(struct _classinfo));
 
 int main()
 {
