7d482c47e4a2f22a825f811be4bc7b258aa8b2fd
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-05 13:01:59 +0000

    gcc can stick mode attributes onto enums and enum tags
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5005 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/ansi-c/gcc_attributes4/main.c
+++ regression/ansi-c/gcc_attributes4/main.c
@@ -3,6 +3,7 @@
 
 #ifdef __GNUC__
 
+// gcc mode attribute
 typedef unsigned char uchar;
 
 uchar int8 __attribute__ ((__mode__ (__QI__))),
@@ -14,6 +15,26 @@ STATIC_ASSERT(sizeof(int8)==1);
 STATIC_ASSERT(sizeof(int16)==2);
 STATIC_ASSERT(sizeof(int32)==4);
 STATIC_ASSERT(sizeof(int64)==8);
+
+// gcc allows these on enums, but clang doesn't
+enum E_QI { E_QI } __attribute__ ((__mode__ (__QI__)));
+enum E_HI { E_HI } __attribute__ ((__mode__ (__HI__)));
+enum E_SI { E_SI } __attribute__ ((__mode__ (__SI__)));
+enum E_DI { E_DI } __attribute__ ((__mode__ (__DI__)));
+
+STATIC_ASSERT(sizeof(enum E_QI)==1);
+STATIC_ASSERT(sizeof(enum E_HI)==2);
+STATIC_ASSERT(sizeof(enum E_SI)==4);
+STATIC_ASSERT(sizeof(enum E_DI)==8);
+
+// can be changed afterwards
+
+STATIC_ASSERT(sizeof(enum E_DI __attribute__ ((__mode__ (__QI__))))==1);
+STATIC_ASSERT(sizeof(enum E_HI __attribute__ ((__mode__ (__HI__))))==2);
+STATIC_ASSERT(sizeof(enum E_SI __attribute__ ((__mode__ (__SI__))))==4);
+STATIC_ASSERT(sizeof(enum E_DI __attribute__ ((__mode__ (__DI__))))==8);
+
+// gcc vector attribute
 
 uchar v1 __attribute__ ((vector_size (1))),
       v2 __attribute__ ((vector_size (2))),
