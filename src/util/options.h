47239465d0e8ada7f2baaaafa6ea72e99029d9d1
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-05 12:34:40 +0000

    optionst can now do lists

--- src/util/options.h
+++ src/util/options.h
@@ -11,26 +11,30 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <string>
 #include <map>
+#include <list>
 
 class optionst
 {
 public:
-  typedef std::map<std::string, std::string> option_mapt;
-  
-  option_mapt option_map; // input
+  typedef std::list<std::string> value_listt;
+  typedef std::map<std::string, value_listt> option_mapt;
   
   const std::string get_option(const std::string &option) const;
   bool get_bool_option(const std::string &option) const;
   signed int get_signed_int_option(const std::string &option) const;
   unsigned int get_unsigned_int_option(const std::string &option) const;
-  unsigned int get_list_option(const std::string &option) const;
+  const value_listt &get_list_option(const std::string &option) const;
   void set_option(const std::string &option, const bool value);
   void set_option(const std::string &option, const int value);
   void set_option(const std::string &option, const unsigned value);
   void set_option(const std::string &option, const std::string &value);
   
   optionst() { }
-  virtual ~optionst() { }
+  ~optionst() { }
+
+protected:
+  option_mapt option_map;  
+  const value_listt empty_list;
 };
 
 #endif
