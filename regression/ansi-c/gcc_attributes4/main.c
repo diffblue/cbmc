60de3e1c3f652569d0b16ebe86ebf02201238fb6
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-06 14:10:54 +0000

    check byte, word, pointer modes
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5014 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/ansi-c/gcc_attributes4/main.c
+++ regression/ansi-c/gcc_attributes4/main.c
@@ -11,28 +11,17 @@ uchar int8 __attribute__ ((__mode__ (__QI__))),
       int32 __attribute__ ((__mode__ (__SI__))),
       int64 __attribute__ ((__mode__ (__DI__)));
 
+int byte_int __attribute__ ((__mode__(__byte__)));
+int word_int __attribute__ ((__mode__(__word__)));
+int pointer_int __attribute__ ((__mode__(__pointer__)));
+
 STATIC_ASSERT(sizeof(int8)==1);
 STATIC_ASSERT(sizeof(int16)==2);
 STATIC_ASSERT(sizeof(int32)==4);
 STATIC_ASSERT(sizeof(int64)==8);
-
-// gcc allows these on enums, but clang doesn't
-enum E_QI { E_QI } __attribute__ ((__mode__ (__QI__)));
-enum E_HI { E_HI } __attribute__ ((__mode__ (__HI__)));
-enum E_SI { E_SI } __attribute__ ((__mode__ (__SI__)));
-enum E_DI { E_DI } __attribute__ ((__mode__ (__DI__)));
-
-STATIC_ASSERT(sizeof(enum E_QI)==1);
-STATIC_ASSERT(sizeof(enum E_HI)==2);
-STATIC_ASSERT(sizeof(enum E_SI)==4);
-STATIC_ASSERT(sizeof(enum E_DI)==8);
-
-// can be changed afterwards
-
-STATIC_ASSERT(sizeof(enum E_DI __attribute__ ((__mode__ (__QI__))))==1);
-STATIC_ASSERT(sizeof(enum E_HI __attribute__ ((__mode__ (__HI__))))==2);
-STATIC_ASSERT(sizeof(enum E_SI __attribute__ ((__mode__ (__SI__))))==4);
-STATIC_ASSERT(sizeof(enum E_DI __attribute__ ((__mode__ (__DI__))))==8);
+STATIC_ASSERT(sizeof(byte_int)==1);
+STATIC_ASSERT(sizeof(word_int)==sizeof(long int));
+STATIC_ASSERT(sizeof(pointer_int)==sizeof(void *));
 
 // gcc vector attribute
 
