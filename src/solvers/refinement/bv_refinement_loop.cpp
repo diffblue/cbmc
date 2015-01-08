78f69284fdc5587c70bb924fbf484e6a050f1c62
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-08 13:30:59 +0000

    output the iteration number in XML

--- src/solvers/refinement/bv_refinement_loop.cpp
+++ src/solvers/refinement/bv_refinement_loop.cpp
@@ -6,6 +6,11 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <iostream>
+
+#include <util/i2string.h>
+#include <util/xml.h>
+
 #include "bv_refinement.h"
 
 /*******************************************************************\
@@ -75,7 +80,15 @@ decision_proceduret::resultt bv_refinementt::dec_solve()
     iteration++;
   
     status() << "BV-Refinement: iteration " << iteration << eom;
-  
+
+    // output the very same information in a structured fashion
+    if(ui==ui_message_handlert::XML_UI)
+    {
+      xmlt xml("refinement-iteration");
+      xml.data=i2string(iteration);
+      std::cout << xml << '\n';
+    }
+
     switch(prop_solve())
     {
     case D_SATISFIABLE:
