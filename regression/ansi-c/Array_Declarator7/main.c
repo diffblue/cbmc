9bc56a56f5f20b6bf8122c07d18762a58357fdbf
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-01-25 11:16:52 +0000

    typo

--- regression/ansi-c/Array_Declarator7/main.c
+++ regression/ansi-c/Array_Declarator7/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-  // bar4 should fail, , e.g., gcc says
+  // bar4 should fail, e.g., gcc says
   // error: static or type qualifiers in non-parameter array declarator
   int bar4[static restrict 4U] = {1, 2, 3, 4};
   return 0;
