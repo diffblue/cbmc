8d9fe3dbb046e21cbd3f568b616e58bc649cb481
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-02-01 21:42:16 +0000

    rewrite union constructors into byte_update

--- src/goto-symex/rewrite_union.cpp
+++ src/goto-symex/rewrite_union.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <util/std_expr.h>
+#include <util/std_code.h>
 #include <util/expr_util.h>
 #include <util/byte_operators.h>
 
@@ -22,8 +23,8 @@ Function: rewrite_union
 
  Outputs:
 
- Purpose: We rewrite u.c for unions u into
-          byte_extract(u, 0)
+ Purpose: We rewrite u.c for unions u into byte_extract(u, 0),
+          and { .c = v } into byte_update(NIL, 0, v)
 
 \*******************************************************************/
 
@@ -45,5 +46,14 @@ void rewrite_union(
       byte_extract_exprt tmp(byte_extract_id(), op, offset, expr.type());
       expr=tmp;
     }
+  }
+  else if(expr.id()==ID_union)
+  {
+    const union_exprt &union_expr=to_union_expr(expr);
+    exprt offset=gen_zero(index_type());
+    side_effect_expr_nondett nondet(expr.type());
+    byte_update_exprt tmp(
+      byte_update_id(), nondet, offset, union_expr.op());
+    expr=tmp;
   }
 }
