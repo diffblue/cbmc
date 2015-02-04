c86ca3052bd3eee9688886d89720d92c555f3dc3
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-02-04 19:24:40 +0000

    unicode command-line on windows
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5160 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/goto_instrument_main.cpp
+++ src/goto-instrument/goto_instrument_main.cpp
@@ -20,8 +20,14 @@ Function: main
 
 \*******************************************************************/
 
+#ifdef _MSC_VER
+int wmain(int argc, const wchar_t **argv_wide)
+{
+  const char **argv=narrow_argv(argc, argv_wide);
+#else
 int main(int argc, const char **argv)
 {
+#endif
   goto_instrument_parse_optionst parse_options(argc, argv);
   return parse_options.main();
 }
