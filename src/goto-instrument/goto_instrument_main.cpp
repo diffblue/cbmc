4ed719534c84990b2dff20e916a96c5525bcb1cc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-02-12 13:14:42 +0000

    missing header
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5187 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/goto_instrument_main.cpp
+++ src/goto-instrument/goto_instrument_main.cpp
@@ -6,6 +6,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <util/unicode.h>
+
 #include "goto_instrument_parse_options.h"
 
 /*******************************************************************\
