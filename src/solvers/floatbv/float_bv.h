6f10f35c29e19b23cb66047069294fe9c59403f7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-02-24 18:15:44 +0000

    remove a layer
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5215 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/solvers/floatbv/float_bv.h
+++ src/solvers/floatbv/float_bv.h
@@ -30,30 +30,21 @@ public:
   }
   
   exprt convert(const exprt &);
-  exprt convert_abs(const abs_exprt &);
-  exprt convert_unary_minus(const unary_minus_exprt &);
-  exprt convert_ieee_float_equal(const ieee_float_equal_exprt &);
-  exprt convert_ieee_float_notequal(const ieee_float_notequal_exprt &);
-  exprt convert_floatbv_typecast(const floatbv_typecast_exprt &expr);
-  exprt convert_floatbv_plus(const ieee_float_op_exprt &expr);
-  exprt convert_floatbv_minus(const ieee_float_op_exprt &expr);
-  exprt convert_floatbv_mult(const ieee_float_op_exprt &expr);
-  exprt convert_floatbv_div(const ieee_float_op_exprt &expr);
-  exprt convert_isnan(const isnan_exprt &expr) { return is_NaN(expr.op(), get_spec(expr.op())); }
-  exprt convert_isfinite(const isfinite_exprt &);
-  exprt convert_isinf(const isinf_exprt &);
-  exprt convert_isnormal(const isnormal_exprt &);
-  exprt convert_floatbv_rel(const binary_relation_exprt &);
 
 protected:
   // helpers
   exprt exponent_all_ones(const exprt &, const ieee_float_spect &spec);
   exprt exponent_all_zeros(const exprt &, const ieee_float_spect &spec);
   exprt fraction_all_zeros(const exprt &, const ieee_float_spect &spec);
-  
+
+  exprt negation(const exprt &, const ieee_float_spect &);
+  exprt abs(const exprt &, const ieee_float_spect &);
   exprt is_equal(const exprt &, const exprt &, const ieee_float_spect &);
   exprt is_zero(const exprt &, const ieee_float_spect &);
-  exprt is_NaN(const exprt &, const ieee_float_spect &);
+  exprt isnan(const exprt &, const ieee_float_spect &);
+  exprt isinf(const exprt &, const ieee_float_spect &);
+  exprt isnormal(const exprt &, const ieee_float_spect &);
+  exprt isfinite(const exprt &, const ieee_float_spect &);
 
   ieee_float_spect get_spec(const exprt &);
   
@@ -61,7 +52,24 @@ protected:
   {
     return constant_exprt(i2string(u), typet(ID_integer));
   }
+  
+  // add/sub
+  exprt add_sub(bool subtract, const exprt &, const exprt &, const exprt &rm, const ieee_float_spect &);
+  
+  // mul/div
+  exprt mul(const exprt &, const exprt &, const exprt &rm, const ieee_float_spect &);
+  exprt div(const exprt &, const exprt &, const exprt &rm, const ieee_float_spect &);
 
+  // conversion
+  exprt from_unsigned_integer(const exprt &, const exprt &rm, const ieee_float_spect &);
+  exprt from_signed_integer(const exprt &, const exprt &rm, const ieee_float_spect &);
+  exprt to_signed_integer(const exprt &src, unsigned int_width, const exprt &rm, const ieee_float_spect &);
+  exprt to_unsigned_integer(const exprt &src, unsigned int_width, const exprt &rm, const ieee_float_spect &);
+  exprt conversion(const exprt &src, const exprt &rm, const ieee_float_spect &src_spec, const ieee_float_spect &dest_spec);
+
+  // relations
+  typedef enum { LT, LE, EQ, GT, GE } relt;
+  exprt relation(const exprt &, relt rel, const exprt &, const ieee_float_spect &);
 };
 
 #endif
