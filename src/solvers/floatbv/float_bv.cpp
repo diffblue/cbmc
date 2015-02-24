d7edd0b999e0d06e9fc3692ecd4de8c3c9c009d2
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-02-24 18:15:44 +0000

    remove a layer

--- src/solvers/floatbv/float_bv.cpp
+++ src/solvers/floatbv/float_bv.cpp
@@ -25,34 +25,57 @@ Function: float_bvt::convert
 
 \*******************************************************************/
 
-exprt float_bvt::convert(const exprt &src)
+exprt float_bvt::convert(const exprt &expr)
 {
-  if(src.id()==ID_abs)
-    return convert_abs(to_abs_expr(src));
-  else if(src.id()==ID_unary_minus)
-    return convert_unary_minus(to_unary_minus_expr(src));
-  else if(src.id()==ID_ieee_float_equal)
-    return convert_ieee_float_equal(to_ieee_float_equal_expr(src));
-  else if(src.id()==ID_ieee_float_notequal)
-    return convert_ieee_float_notequal(to_ieee_float_notequal_expr(src));
-  else if(src.id()==ID_floatbv_typecast)
-    return convert_floatbv_typecast(to_floatbv_typecast_expr(src));
-  else if(src.id()==ID_floatbv_plus)
-    return convert_floatbv_plus(to_ieee_float_op_expr(src));
-  else if(src.id()==ID_floatbv_minus)
-    return convert_floatbv_minus(to_ieee_float_op_expr(src));
-  else if(src.id()==ID_floatbv_mult)
-    return convert_floatbv_mult(to_ieee_float_op_expr(src));
-  else if(src.id()==ID_floatbv_div)
-    return convert_floatbv_div(to_ieee_float_op_expr(src));
-  else if(src.id()==ID_isnan)
-    return convert_isnan(to_isnan_expr(src));
-  else if(src.id()==ID_isfinite)
-    return convert_isfinite(to_isfinite_expr(src));
-  else if(src.id()==ID_isinf)
-    return convert_isinf(to_isinf_expr(src));
-  else if(src.id()==ID_isnormal)
-    return convert_isnormal(to_isnormal_expr(src));
+  if(expr.id()==ID_abs)
+    return abs(to_abs_expr(expr).op(), get_spec(expr));
+  else if(expr.id()==ID_unary_minus)
+    return negation(to_unary_minus_expr(expr).op(), get_spec(expr));
+  else if(expr.id()==ID_ieee_float_equal)
+    return is_equal(expr.op0(), expr.op1(), get_spec(expr.op0()));
+  else if(expr.id()==ID_ieee_float_notequal)
+    return not_exprt(is_equal(expr.op0(), expr.op1(), get_spec(expr.op0())));
+  else if(expr.id()==ID_floatbv_typecast)
+  {
+    if(expr.type().id()==ID_signedbv)
+      return to_signed_integer(
+        expr.op0(), to_signedbv_type(expr.type()).get_width(), expr.op1(), get_spec(expr.op0()));
+    else if(expr.type().id()==ID_unsignedbv)
+      return to_unsigned_integer(
+        expr.op0(), to_unsignedbv_type(expr.type()).get_width(), expr.op1(), get_spec(expr.op0()));
+    else if(expr.op0().type().id()==ID_signedbv)
+      return from_signed_integer(
+        expr.op0(), expr.op1(), get_spec(expr));
+    else if(expr.op0().type().id()==ID_unsignedbv)
+      return from_unsigned_integer(
+        expr.op0(), expr.op1(), get_spec(expr));
+    else
+      return conversion(expr.op0(), expr.op1(), get_spec(expr.op0()), get_spec(expr));
+  }
+  else if(expr.id()==ID_floatbv_plus)
+    return add_sub(false, expr.op0(), expr.op1(), expr.op2(), get_spec(expr));
+  else if(expr.id()==ID_floatbv_minus)
+    return add_sub(true, expr.op0(), expr.op1(), expr.op2(), get_spec(expr));
+  else if(expr.id()==ID_floatbv_mult)
+    return mul(expr.op0(), expr.op1(), expr.op2(), get_spec(expr));
+  else if(expr.id()==ID_floatbv_div)
+    return div(expr.op0(), expr.op1(), expr.op2(), get_spec(expr));
+  else if(expr.id()==ID_isnan)
+    return isnan(expr.op0(), get_spec(expr.op0()));
+  else if(expr.id()==ID_isfinite)
+    return isfinite(expr.op0(), get_spec(expr.op0()));
+  else if(expr.id()==ID_isinf)
+    return isinf(expr.op0(), get_spec(expr.op0()));
+  else if(expr.id()==ID_isnormal)
+    return isnormal(expr.op0(), get_spec(expr.op0()));
+  else if(expr.id()==ID_lt)
+    return relation(expr.op0(), LT, expr.op1(), get_spec(expr.op0()));
+  else if(expr.id()==ID_gt)
+    return relation(expr.op0(), GT, expr.op1(), get_spec(expr.op0()));
+  else if(expr.id()==ID_le)
+    return relation(expr.op0(), LE, expr.op1(), get_spec(expr.op0()));
+  else if(expr.id()==ID_ge)
+    return relation(expr.op0(), GE, expr.op1(), get_spec(expr.op0()));
   else
     return nil_exprt();
 }
@@ -69,15 +92,15 @@ Function: float_bvt::get_spec
 
 \*******************************************************************/
 
-ieee_float_spect float_bvt::get_spec(const exprt &src)
+ieee_float_spect float_bvt::get_spec(const exprt &expr)
 {
-  const floatbv_typet &type=to_floatbv_type(src.type());
+  const floatbv_typet &type=to_floatbv_type(expr.type());
   return ieee_float_spect(type);
 }
 
 /*******************************************************************\
 
-Function: float_bvt::convert_abs
+Function: float_bvt::abs
 
   Inputs:
 
@@ -87,21 +110,21 @@ Function: float_bvt::convert_abs
 
 \*******************************************************************/
 
-exprt float_bvt::convert_abs(const abs_exprt &src)
+exprt float_bvt::abs(const exprt &op, const ieee_float_spect &spec)
 {
   // we mask away the sign bit, which is the most significand bit
   std::string mask_str;
-  mask_str.resize(get_spec(src).width(), '1');
+  mask_str.resize(spec.width(), '1');
   mask_str[0]='0';
   
-  constant_exprt mask(mask_str, src.type());
+  constant_exprt mask(mask_str, op.type());
   
-  return bitand_exprt(src.op(), mask);
+  return bitand_exprt(op, mask);
 }
 
 /*******************************************************************\
 
-Function: float_bvt::convert_unary_minus
+Function: float_bvt::negation
 
   Inputs:
 
@@ -111,52 +134,16 @@ Function: float_bvt::convert_unary_minus
 
 \*******************************************************************/
 
-exprt float_bvt::convert_unary_minus(const unary_minus_exprt &src)
+exprt float_bvt::negation(const exprt &op, const ieee_float_spect &spec)
 {
   // we flip the sign bit with an xor
   std::string mask_str;
-  mask_str.resize(get_spec(src).width(), '0');
+  mask_str.resize(spec.width(), '0');
   mask_str[0]='1';
   
-  constant_exprt mask(mask_str, src.type());
+  constant_exprt mask(mask_str, op.type());
   
-  return bitxor_exprt(src.op(), mask);
-}
-
-/*******************************************************************\
-
-Function: float_bvt::convert_ieee_float_equal
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-exprt float_bvt::convert_ieee_float_equal(const ieee_float_equal_exprt &src)
-{
-  return is_equal(src.op0(), src.op1(), get_spec(src.op0()));
-}
-
-/*******************************************************************\
-
-Function: float_bvt::convert_ieee_float_notequal
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-exprt float_bvt::convert_ieee_float_notequal(
-  const ieee_float_notequal_exprt &src)
-{
-  return not_exprt(
-    is_equal(src.op0(), src.op1(), get_spec(src.op0())));
+  return bitxor_exprt(op, mask);
 }
 
 /*******************************************************************\
@@ -182,15 +169,15 @@ exprt float_bvt::is_equal(
   exprt both_zero=and_exprt(is_zero0, is_zero1);
 
   // NaN compares to nothing
-  exprt is_NaN0=is_NaN(src0, spec);
-  exprt is_NaN1=is_NaN(src1, spec);
-  exprt NaN=or_exprt(is_NaN0, is_NaN1);
+  exprt isnan0=isnan(src0, spec);
+  exprt isnan1=isnan(src1, spec);
+  exprt nan=or_exprt(isnan0, isnan1);
 
   exprt bitwise_equal=equal_exprt(src0, src1);
 
   return and_exprt(
     or_exprt(bitwise_equal, both_zero),
-    not_exprt(NaN));
+    not_exprt(nan));
 }
 
 /*******************************************************************\
@@ -255,7 +242,7 @@ Function: float_bvt::exponent_all_zeros
   Inputs:
 
  Outputs:
-
+D
  Purpose:
 
 \*******************************************************************/
@@ -295,5 +282,184 @@ exprt float_bvt::fraction_all_zeros(
     src, uint_const(spec.f-1), uint_const(0), fraction_type);
 
   return equal_exprt(fraction, fraction_type.zero_expr());
+}
+
+/*******************************************************************\
+
+Function: float_bvt::add_sub
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::add_sub(
+  bool subtract,
+  const exprt &op0,
+  const exprt &op1,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::mul
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::mul(
+  const exprt &op0,
+  const exprt &op1,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::div
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::div(
+  const exprt &op0,
+  const exprt &op1,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::from_unsigned_integer
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::from_unsigned_integer(
+  const exprt &src,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::relation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::from_signed_integer(
+  const exprt &src,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::relation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::to_signed_integer(
+  const exprt &src,
+  unsigned int_width,
+  const exprt &rm,
+  const ieee_float_spect &spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::relation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::to_unsigned_integer(
+  const exprt &src,
+  unsigned int_width,
+  const exprt &rm,
+  const ieee_float_spect &)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::relation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::conversion(
+  const exprt &src,
+  const exprt &rm,
+  const ieee_float_spect &src_spec,
+  const ieee_float_spect &dest_spec)
+{
+}
+
+/*******************************************************************\
+
+Function: float_bvt::relation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt float_bvt::relation(
+  const exprt &op0,
+  relt rel,
+  const exprt &op1,
+  const ieee_float_spect &spec)
+{
 }
 
