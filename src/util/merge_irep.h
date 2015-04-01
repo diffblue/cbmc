1ece26118a51f05c39f6b7991280150b7efcd9bb
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-04-01 17:24:29 +0000

    merged_irept added

--- src/util/merge_irep.h
+++ src/util/merge_irep.h
@@ -12,6 +12,79 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "irep.h"
 #include "hash_cont.h"
 
+class merged_irept:public irept
+{
+public:
+  inline bool operator == (const merged_irept &other) const
+  {
+    // we assume that both are in the same container
+    return data==other.data;
+  }
+
+  inline bool operator < (const merged_irept &other) const
+  {
+    // again, assumes that both are in the same container  
+    return ((std::size_t)data) < ((std::size_t)other.data);
+  }
+  
+  inline std::size_t hash() const { return (std::size_t)data; }
+
+protected:
+  // can only be used by merged_irepst
+  inline explicit merged_irept(const irept &src):irept(src)
+  {
+  }
+  
+  friend class merged_irepst;
+  friend class to_be_merged_irept;
+};
+
+struct merged_irep_hash
+{
+  inline std::size_t operator()(const merged_irept &irep) const
+  { return irep.hash(); }
+};
+
+// internal, don't use me
+class to_be_merged_irept:public irept
+{
+public:
+  bool operator == (const to_be_merged_irept &other) const;
+  std::size_t hash() const;
+
+protected:
+  // can only be used by merged_irepst
+  inline explicit to_be_merged_irept(const irept &src):irept(src)
+  {
+  }
+  
+  friend class merged_irepst;
+};
+
+struct to_be_merged_irep_hash
+{
+  inline std::size_t operator()(const to_be_merged_irept &irep) const
+  { return irep.hash(); }
+};
+
+class merged_irepst
+{
+public:
+  inline const merged_irept &operator()(const irept &src)
+  {
+    return merged(src);
+  }
+
+protected:
+  typedef hash_set_cont<merged_irept, merged_irep_hash> merged_irep_storet;
+  merged_irep_storet merged_irep_store;
+  
+  typedef hash_set_cont<to_be_merged_irept, to_be_merged_irep_hash> to_be_merged_irep_storet;
+  to_be_merged_irep_storet to_be_merged_irep_store;
+  
+  const merged_irept &merged(const irept &);
+};
+
 // Warning: the below uses irep_hash, as opposed to irep_full_hash,
 // i.e., any comments will be disregarded during merging. Use
 // merge_full_irept if any comments are of importance.
@@ -25,7 +98,7 @@ protected:
   typedef hash_set_cont<irept, irep_hash> irep_storet;
   irep_storet irep_store;     
 
-  const irept& merged(const irept &irep);
+  const irept & merged(const irept &irep);
 };
 
 class merge_full_irept
