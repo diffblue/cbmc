ce6b0ea3e912ec8e75136a7db85efeb2728e4520
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-04-03 11:20:23 +0000

    more of the grammar
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5307 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/memory-models/mm_parser.h
+++ src/memory-models/mm_parser.h
@@ -10,127 +10,24 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_MM_PARSER_H
 
 #include <util/parser.h>
+#include <util/std_code.h>
 
 int yymmparse();
 
 class mm_parsert:public parsert
 {
 public:
-#if 0
-  ansi_c_parse_treet parse_tree;
-  
-  ansi_c_parsert():
-    cpp98(false), cpp11(false),
-    for_has_scope(false)
-  {
-  }
-  
+  codet parse_tree;
+
   virtual bool parse()
   {
-    return yyansi_cparse()!=0;
+    return yymmparse()!=0;
   }
 
   virtual void clear()
   {
-    parsert::clear();
     parse_tree.clear();
-    
-    // scanner state
-    tag_following=false;
-    asm_block_following=false;
-    parenthesis_counter=0;
-    string_literal.clear();
-    pragma_pack.clear();
-    
-    // setup global scope
-    scopes.clear();
-    scopes.push_back(scopet());
   }
-
-  // internal state of the scanner
-  bool tag_following;
-  bool asm_block_following;
-  unsigned parenthesis_counter;
-  std::string string_literal;
-  std::list<exprt> pragma_pack;
-  
-  typedef enum { ANSI, GCC, MSC, ICC, CW, ARM } modet;
-  modet mode;
-  // ANSI is strict ANSI-C
-  // GCC is, well, gcc
-  // MSC is Microsoft Visual Studio
-  // ICC is Intel's C compiler
-  // CW is CodeWarrior (with GCC extensions enabled)
-  // ARM is ARM's RealView
-
-  // recognize C++98 and C++11 keywords  
-  bool cpp98, cpp11;
-  
-  // in C99 and upwards, for(;;) has a scope
-  bool for_has_scope;
-
-  typedef ansi_c_identifiert identifiert;  
-  typedef ansi_c_scopet scopet;
-
-  typedef std::list<scopet> scopest;
-  scopest scopes;
-  
-  scopet &root_scope()
-  {
-    return scopes.front();
-  }
-  
-  const scopet &root_scope() const
-  {
-    return scopes.front();
-  }
-  
-  void pop_scope()
-  {
-    scopes.pop_back();
-  }
-   
-  scopet &current_scope()
-  {
-    assert(!scopes.empty());
-    return scopes.back();
-  }
-
-  typedef enum { TAG, MEMBER, PARAMETER, OTHER } decl_typet;
-
-  // convert a declarator and then add it to existing an declaration
-  void add_declarator(exprt &declaration, irept &declarator);
-
-  // adds a tag to the current scope
-  void add_tag_with_body(irept &tag);
-
-  void copy_item(const ansi_c_declarationt &declaration)
-  {
-    assert(declaration.id()==ID_declaration);
-    parse_tree.items.push_back(declaration);
-  }
-   
-  void new_scope(const std::string &prefix)
-  {
-    const scopet &current=current_scope();
-    scopes.push_back(scopet());
-    scopes.back().prefix=current.prefix+prefix;
-  }
-
-  ansi_c_id_classt lookup(
-    std::string &name,
-    bool tag,
-    bool label);
-
-  static ansi_c_id_classt get_class(const typet &type);
-  
-  irep_idt lookup_label(const irep_idt id)
-  {
-    std::string name=id2string(id);
-    lookup(name, false, true);
-    return name;
-  }
-#endif
 };
 
 extern mm_parsert mm_parser;
