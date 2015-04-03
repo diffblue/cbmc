76fb84156df2a24baca4aee12cd1fac08623be1e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-04-03 15:06:57 +0000

    mmcc now compiles

---
+++ src/memory-models/mm_parser.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "mm_parser.h"
+
+mm_parsert mm_parser;
+
