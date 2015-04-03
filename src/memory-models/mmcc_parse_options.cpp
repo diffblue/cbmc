20c769a55072dcfe3f542cdfa385fff323dd2dd2
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-04-03 15:18:19 +0000

    convert from stdin

--- src/memory-models/mmcc_parse_options.cpp
+++ src/memory-models/mmcc_parse_options.cpp
@@ -7,50 +7,11 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <iostream>
-
-#if 0
 #include <fstream>
-#include <cstdlib> // exit()
-#include <memory>
-
-#include <util/string2int.h>
-#include <util/config.h>
-#include <util/expr_util.h>
-#include <util/language.h>
-#include <util/unicode.h>
-#include <util/memory_info.h>
-#include <util/i2string.h>
-
-#include <ansi-c/c_preprocess.h>
-
-#include <goto-programs/goto_convert_functions.h>
-#include <goto-programs/remove_function_pointers.h>
-#include <goto-programs/remove_returns.h>
-#include <goto-programs/remove_vector.h>
-#include <goto-programs/remove_complex.h>
-#include <goto-programs/remove_asm.h>
-#include <goto-programs/remove_unused_functions.h>
-#include <goto-programs/goto_inline.h>
-#include <goto-programs/show_properties.h>
-#include <goto-programs/set_properties.h>
-#include <goto-programs/read_goto_binary.h>
-#include <goto-programs/string_abstraction.h>
-#include <goto-programs/string_instrumentation.h>
-#include <goto-programs/loop_ids.h>
-#include <goto-programs/link_to_library.h>
-
-#include <pointer-analysis/add_failed_symbols.h>
-
-#include <analyses/goto_check.h>
-
-#include <langapi/mode.h>
-
-#include "bmc.h"
-#include "xml_interface.h"
-#endif
 
 #include <cbmc/version.h>
 
+#include "mm_parser.h"
 #include "mmcc_parse_options.h"
 
 /*******************************************************************\
@@ -90,6 +51,54 @@ int mmcc_parse_optionst::doit()
     return 0;
   }
   
+  if(cmdline.args.size()==1)
+  {
+    std::ifstream in(cmdline.args[0].c_str());
+    
+    if(!in)
+    {
+      std::cerr << "failed to open `" << cmdline.args[0] << "'\n";
+      return 2;
+    }
+
+    return convert(in);    
+  }
+  else if(cmdline.args.size()==0)
+  {
+    return convert(std::cin);
+  }
+  else
+  {
+    usage_error();
+    return 1;
+  }
+  
+  return 0;
+}
+
+/*******************************************************************\
+
+Function: mmcc_parse_optionst::convert
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+int mmcc_parse_optionst::convert(std::istream &in)
+{
+  mm_parsert mm_parser;
+  mm_parser.in=&in;
+  
+  if(mm_parser.parse())
+  {
+    std::cerr << "parse error, giving up\n";
+    return 3;
+  }
+  
   return 0;
 }
 
@@ -116,6 +125,7 @@ void mmcc_parse_optionst::help()
     "Usage:                       Purpose:\n"
     "\n"
     " mmcc [-?] [-h] [--help]      show help\n"
-    " mmcc file.cat                source file name\n"
+    " mmcc file.cat                convert given source file\n"
+    " mmcc                         convert from stdin\n"
     "\n";
 }
