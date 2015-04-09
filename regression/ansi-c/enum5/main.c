a8c5bc0284013054ffa429741e583f8c4283a04d
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-04-09 20:17:28 +0000

    Permit re-declaring anonymous enum types

---
+++ regression/ansi-c/enum5/main.c
@@ -0,0 +1,30 @@
+static void foo()
+{
+  enum {
+    COL_CUR,
+    COL_CHG,
+    COL_AVG,
+    COL_MA10,
+    COL_MA100,
+    COL_MA1000,
+    COL_LAST
+  } col;
+}
+
+static void bar()
+{
+  enum {
+    COL_CUR,
+    COL_CHG,
+    COL_AVG,
+    COL_MA10,
+    COL_MA100,
+    COL_MA1000,
+    COL_LAST
+  } col;
+}
+
+int main()
+{
+  return 0;
+}
