6502b2efc8d2316a935179d1826777aa9bb1b051
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-05-08 13:25:26 +0000

    Ensure type consistency when applying process_array_expr
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5367 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-symex/symex_clean_expr.cpp
+++ src/goto-symex/symex_clean_expr.cpp
@@ -9,12 +9,71 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/std_expr.h>
 #include <util/expr_util.h>
 #include <util/cprover_prefix.h>
+#include <util/base_type.h>
 
 #include <ansi-c/c_types.h>
 
 #include "adjust_float_expressions.h"
 #include "rewrite_union.h"
 #include "goto_symex.h"
+
+/*******************************************************************\
+
+Function: goto_symext::process_array_expr_rec
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void goto_symext::process_array_expr_rec(
+  exprt &expr,
+  const typet &type) const
+{
+  if(expr.id()==ID_if)
+  {
+    if_exprt &if_expr=to_if_expr(expr);
+    process_array_expr_rec(if_expr.true_case(), type);
+    process_array_expr_rec(if_expr.false_case(), type);
+  }
+  else if(expr.id()==ID_index)
+  {
+    // strip index
+    index_exprt &index_expr=to_index_expr(expr);
+    exprt tmp=index_expr.array();
+    expr.swap(tmp);
+  }
+  else if(expr.id()==ID_typecast)
+  {
+    // strip
+    exprt tmp=to_typecast_expr(expr).op0();
+    expr.swap(tmp);
+    process_array_expr_rec(expr, type);
+  }
+  else if(expr.id()==ID_address_of)
+  {
+    // strip
+    exprt tmp=to_address_of_expr(expr).op0();
+    expr.swap(tmp);
+    process_array_expr_rec(expr, type);
+  }
+  else
+    Forall_operands(it, expr)
+      process_array_expr_rec(*it, it->type());
+
+  if(!base_type_eq(expr.type(), type, ns))
+  {
+    byte_extract_exprt be(byte_extract_id());
+    be.type()=type;
+    be.op()=expr;
+    be.offset()=gen_zero(index_type());
+
+    expr.swap(be);
+  }
+}
 
 /*******************************************************************\
 
@@ -30,11 +89,16 @@ Function: goto_symext::process_array_expr
 
 void goto_symext::process_array_expr(exprt &expr)
 {
+  // This may change the type of the expression!
+
   if(expr.id()==ID_if)
   {
     if_exprt &if_expr=to_if_expr(expr);
     process_array_expr(if_expr.true_case());
-    process_array_expr(if_expr.false_case());
+
+    process_array_expr_rec(if_expr.false_case(),
+                           if_expr.true_case().type());
+
     if_expr.type()=if_expr.true_case().type();
   }
   else if(expr.id()==ID_index)
