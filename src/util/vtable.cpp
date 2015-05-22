f9802d71de88e08c93ff405a3a82f4fe6281ece3
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-05-22 18:00:10 +0000

    vtable helper (likely to move)

---
+++ src/util/vtable.cpp
@@ -0,0 +1,58 @@
+#include <util/std_types.h>
+#include <util/vtable.h>
+
+namespace {
+const char VT_NS[] = "virtual_table::";
+const char VT_SEP = '@';
+const char ID_virtual_name[] = "virtual_name";
+const char NS_SEP[] = "::";
+}
+
+const char vtable_namest::VT_PTR[] = "@vtable_pointer";
+
+std::string vtable_namest::get_type(const std::string &class_name) {
+  return VT_NS + class_name;
+}
+
+std::string vtable_namest::get_table(const std::string &class_name) {
+  return get_type(class_name) + VT_SEP + class_name;
+}
+
+std::string vtable_namest::get_table(const typet &class_type) {
+  const irep_idt &type_id(class_type.id());
+  if (ID_pointer == type_id) return get_table(class_type.subtype());
+  assert(ID_symbol == type_id);
+  const irep_idt &id(to_symbol_type(class_type).get_identifier());
+  return get_table(id2string(id));
+}
+
+std::string vtable_namest::get_table(const exprt &this_obj) {
+  return get_table(this_obj.type());
+}
+
+
+std::string vtable_namest::get_table_base(
+    const std::string &class_base_name) {
+  return get_type_base(class_base_name) + VT_SEP + class_base_name;
+}
+
+std::string vtable_namest::get_type(const typet &class_type) {
+  const irep_idt &type_id(class_type.id());
+  if (ID_pointer == type_id) return get_type(class_type.subtype());
+  assert(ID_symbol == type_id);
+  const irep_idt &id(to_symbol_type(class_type).get_identifier());
+  return get_type(id2string(id));
+}
+
+std::string vtable_namest::get_type(const exprt &this_obj) {
+  return get_type(this_obj.type());
+}
+
+std::string vtable_namest::get_type_base(
+    const std::string &class_base_name) {
+  return VT_NS + class_base_name;
+}
+
+std::string vtable_namest::get_pointer(const std::string &class_name) {
+  return class_name + NS_SEP + std::string(VT_PTR);
+}
