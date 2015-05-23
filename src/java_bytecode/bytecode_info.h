a70f9800b93f39b7d6f81e0f0b8d671e7b36f6c0
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-05-23 13:43:08 +0000

    bytecode_info is now in separate file

---
+++ src/java_bytecode/bytecode_info.h
@@ -0,0 +1,23 @@
+// http://en.wikipedia.org/wiki/Java_bytecode_instruction_listings
+
+// The 'result_type' is one of the following:
+// i  integer
+// l  long
+// s  short
+// b  byte
+// c  character
+// f  float
+// d  double
+// z  boolean
+// a  reference
+
+struct bytecode_infot
+{
+  const char *mnemonic;
+  unsigned char opcode;
+  unsigned pop, push;
+  char result_type;
+};
+
+extern struct bytecode_infot const bytecode_info[];
+
