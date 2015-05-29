27f9407424eeff5bb1f23183e4a62f1d43584c24
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-05-29 15:11:16 +0000

    class loading with dependencies
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5463 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/java_bytecode/java_class_loader.cpp
@@ -0,0 +1,56 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <stack>
+
+#include "java_bytecode_parser.h"
+#include "java_class_loader.h"
+
+/*******************************************************************\
+
+Function:
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void java_class_loadert::operator()(const irep_idt &class_name)
+{
+  std::stack<irep_idt> queue;
+
+  queue.push(class_name);
+
+  while(!queue.empty())
+  {  
+    irep_idt c=queue.top();
+    queue.pop();
+    
+    // do we have the class already?
+    if(class_map.find(c)!=class_map.end())
+      continue; // got it already
+      
+    java_bytecode_parse_treet &parse_tree=class_map[c];
+
+    // parse the class file
+    java_bytecode_parse(
+      id2string(c),
+      parse_tree,
+      get_message_handler());
+
+    // add any dependencies to queue
+    for(java_bytecode_parse_treet::class_refst::const_iterator
+        it=parse_tree.class_refs.begin();
+        it!=parse_tree.class_refs.end();
+        it++)
+      queue.push(*it);
+  }  
+}
