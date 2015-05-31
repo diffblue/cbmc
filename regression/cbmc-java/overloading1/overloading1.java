80c8b2d57f9e6df4554a6454d1349213feb8fc2e
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-05-31 10:51:38 +0000

    check more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5478 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cbmc-java/overloading1/overloading1.java
+++ regression/cbmc-java/overloading1/overloading1.java
@@ -1,9 +1,14 @@
+class other_class
+{
+};
+
 class overloading1
 {
   public static void main(String[] args)
   {
     assert(f(1)==1);
     assert(f(1.0d)==2);
+    assert(f(new other_class())==3);
   }
   
   static int f(int i)
@@ -14,6 +19,11 @@ class overloading1
   static int f(double d)
   {
     return 2;
+  }
+  
+  static int f(other_class o)
+  {
+    return 3;
   }
 }
 
