ee8ed6dec389117b6970a650917d31c6a7299902
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-06-01 14:26:13 +0000

    check a static field

--- regression/cbmc-java/constructor1/constructor1.java
+++ regression/cbmc-java/constructor1/constructor1.java
@@ -10,11 +10,14 @@ class A
   
   A(int param)
   {
+    assert(my_field==0);
     my_field=2;
   }
   
   int my_field;
   sub my_sub;
+  
+  static int something_static = 3;
 };
 
 class constructor1
@@ -24,6 +27,7 @@ class constructor1
     A some_a=new A();
     assert(some_a.my_field==0);
     assert(some_a.my_sub==null);
+    assert(some_a.something_static==3);
     A other_a=new A(1);
     assert(other_a.my_field==2);
     assert(other_a.my_sub==null);
