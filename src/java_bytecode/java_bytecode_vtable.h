8952390ee00d2ee63048d67892fddb8d78f1790f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-06-06 16:07:14 +0000

    formatting

--- src/java_bytecode/java_bytecode_vtable.h
+++ src/java_bytecode/java_bytecode_vtable.h
@@ -1,19 +1,33 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
 #ifndef CPROVER_JAVA_BYTECODE_VTABLE_H
 #define CPROVER_JAVA_BYTECODE_VTABLE_H
 
 #include <util/std_types.h>
 
-bool java_bytecode_vtable(class symbol_tablet &symbol_table,
-    const std::string &module);
+bool java_bytecode_vtable(
+  class symbol_tablet &symbol_table,
+  const std::string &module);
 
-void create_vtable_type_and_pointer(symbol_tablet &symbol_table,
-    class symbolt &class_symbol);
+void create_vtable_type_and_pointer(
+  symbol_tablet &symbol_table,
+  class symbolt &class_symbol);
 
-code_assignt make_vtable_assignment(const symbol_tablet &symbol_table,
-    const exprt &func);
+code_assignt make_vtable_assignment(
+  const symbol_tablet &symbol_table,
+  const exprt &func);
 
-exprt make_vtable_function(const exprt &function, const exprt &this_obj);
+exprt make_vtable_function(
+  const exprt &function,
+  const exprt &this_obj);
 
-void set_virtual_name(class_typet::methodt &method);
+void set_virtual_name(
+  class_typet::methodt &method);
 
 #endif /* CPROVER_JAVA_BYTECODE_VTABLE_H */
