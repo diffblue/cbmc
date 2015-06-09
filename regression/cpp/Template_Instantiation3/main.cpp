478eb752b06044c948222170dd9102682bbf376f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-06-09 19:45:25 +0000

    another template instantiation bug

---
+++ regression/cpp/Template_Instantiation3/main.cpp
@@ -0,0 +1,19 @@
+template <unsigned int X, unsigned int Y>
+struct sc_ufixed {
+  unsigned int value;
+
+  sc_ufixed (const unsigned int x) : value(x) { }
+
+  sc_ufixed<X*2,Y*2> multiply (const sc_ufixed<X,Y> &op) const {
+    unsigned int result = this->value * op.value;
+    return sc_ufixed<X*2,Y*2>(result);
+  }
+
+};
+
+const sc_ufixed<1, 1> one(1U);
+
+int main (void) {
+  //const sc_ufixed<1, 1> one(1U);
+  return 0;
+}
