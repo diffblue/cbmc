a94c4cbe880c59b8799b373d8a5c769b9316d7d5
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-06-12 08:39:30 +0000

    CEGIS with GA patch
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5562 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

---
+++ src/cegis/cegis.h
@@ -0,0 +1,44 @@
+/*******************************************************************
+ Module:  JAVA Bytecode Language Conversion
+
+ Authors: Daniel Kroening, kroening@kroening.com
+ Pascal Kesseli, pascal.kesseil@cs.ox.ac.uk
+
+ \*******************************************************************/
+
+#ifndef CPROVER_CEGIS_H
+#define CPROVER_CEGIS_H
+
+#include <iosfwd>
+
+/**
+ * @brief CEGIS common template algorithm.
+ *
+ * @details Counterexample-guided inductive synthesis implementation
+ * relying on generic learning algorithms and verification oracles.
+ *
+ * @param learn The learning algorithm to use.
+ * @param oracle The verification oracle to use.
+ * @param os Stream for solution output.
+ *
+ * @tparam learnt CEGIS learning algorithm type (e.g. GA).
+ * @tparam oraclet CEGIS verification oracle type (e.g BMC).
+ */
+template<class learnt, class oraclet>
+int run_cegis(learnt &learn, oraclet &oracle, std::ostream &os)
+{
+  do
+  {
+    const typename learnt::candidatet candidate(learn.next_candidate());
+    oracle.verify(candidate);
+  } while (oracle.has_counterexample()
+      && learn.learn(oracle.get_counterexample()));
+  if (oracle.success())
+  {
+    learn.show_candidate(os);
+    return 0;
+  }
+  return 10;
+}
+
+#endif
