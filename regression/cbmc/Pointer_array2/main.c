47f14e2b832202f41a9d9b466bba6e4283d54ce5
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-06-23 07:13:15 +0000

    test now works

--- regression/cbmc/Pointer_array2/main.c
+++ regression/cbmc/Pointer_array2/main.c
@@ -1,13 +1,16 @@
+#include <assert.h>
+
 struct gendisk {
   int major;
   char disk_name[32U];
 } ;
 
-void main() {
+int main() {
   struct gendisk a;
   struct gendisk *disk=&a;
   char (*__cil_tmp24)[32U];
   __cil_tmp24 = &(disk->disk_name);
   assert(__cil_tmp24!=0);
+  return 0;
 }
 
