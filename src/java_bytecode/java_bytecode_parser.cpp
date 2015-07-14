e3e9cce1496e912694ff01ccc1dd78778d1de679
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-07-14 21:03:18 +0000

    Missing #include <map>
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5684 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/java_bytecode/java_bytecode_parser.cpp
+++ src/java_bytecode/java_bytecode_parser.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <fstream>
+#include <map>
 
 #include <util/i2string.h>
 #include <util/parser.h>
--- src/java_bytecode/java_class_loader.cpp
+++ src/java_bytecode/java_class_loader.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <stack>
+#include <map>
 
 #include "java_bytecode_parser.h"
 #include "java_class_loader.h"
