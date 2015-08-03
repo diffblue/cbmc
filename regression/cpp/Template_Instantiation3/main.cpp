358c2bebe522e7e0d417fa53950a2f195dd5cd51
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-08-03 09:00:29 +0000

    nudges
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5705 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cpp/Template_Instantiation3/main.cpp
+++ regression/cpp/Template_Instantiation3/main.cpp
@@ -1,10 +1,14 @@
 template <unsigned int X, unsigned int Y>
-struct sc_ufixed {
+struct sc_ufixed
+{
   unsigned int value;
 
-  sc_ufixed (const unsigned int x) : value(x) { }
+  sc_ufixed (const unsigned int x) : value(x)
+  {
+  }
 
-  sc_ufixed<X*2,Y*2> multiply (const sc_ufixed<X,Y> &op) const {
+  sc_ufixed<X*2,Y*2> multiply (const sc_ufixed<X,Y> &op) const
+  {
     unsigned int result = this->value * op.value;
     return sc_ufixed<X*2,Y*2>(result);
   }
@@ -14,6 +18,7 @@ struct sc_ufixed {
 const sc_ufixed<1, 1> one(1U);
 
 int main (void) {
-  //const sc_ufixed<1, 1> one(1U);
+  //const sc_ufixed<1, 1> one(1U);  
+  one.multiply(one);
   return 0;
 }
