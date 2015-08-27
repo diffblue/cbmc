1b38f16417ae6fdd3d578ee20d4bb935a0c8bff7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-08-27 18:10:54 +0000

    added to_expr

--- src/langapi/language_util.cpp
+++ src/langapi/language_util.cpp
@@ -11,9 +11,49 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 #include <util/symbol_table.h>
 #include <util/namespace.h>
 #include <util/language.h>
+#include <util/std_expr.h>
 
 #include "language_util.h"
 #include "mode.h"
+
+/*******************************************************************\
+
+Function: get_language
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+std::unique_ptr<languaget> get_language(
+  const namespacet &ns,
+  const irep_idt &identifier)
+{
+  if(identifier=="")
+    return std::unique_ptr<languaget>(get_default_language());
+  else
+  {
+    const symbolt *symbol;
+    
+    if(ns.lookup(identifier, symbol))
+      return std::unique_ptr<languaget>(get_default_language());
+    else if(symbol->mode=="")
+      return std::unique_ptr<languaget>(get_default_language());
+    else
+    {
+      languaget *ptr=get_language_from_mode(symbol->mode);
+
+      if(ptr==NULL)
+        throw "symbol `"+id2string(symbol->name)+
+              "' has unknown mode '"+id2string(symbol->mode)+"'";
+
+      return std::unique_ptr<languaget>(ptr);
+    }
+  }
+}
 
 /*******************************************************************\
 
@@ -32,29 +72,7 @@ std::string from_expr(
   const irep_idt &identifier,
   const exprt &expr)
 {
-  std::unique_ptr<languaget> p;
-
-  if(identifier=="")
-    p=std::unique_ptr<languaget>(get_default_language());
-  else
-  {
-    const symbolt *symbol;
-    
-    if(ns.lookup(identifier, symbol))
-      p=std::unique_ptr<languaget>(get_default_language());
-    else if(symbol->mode=="")
-      p=std::unique_ptr<languaget>(get_default_language());
-    else
-    {
-      languaget *ptr=get_language_from_mode(symbol->mode);
-
-      if(ptr==NULL)
-        throw "symbol `"+id2string(symbol->name)+
-              "' has unknown mode '"+id2string(symbol->mode)+"'";
-
-      p=std::unique_ptr<languaget>(ptr);
-    }
-  }
+  std::unique_ptr<languaget> p=get_language(ns, identifier);
 
   std::string result;
   p->from_expr(expr, result, ns);
@@ -79,29 +97,7 @@ std::string from_type(
   const irep_idt &identifier,
   const typet &type)
 {
-  std::unique_ptr<languaget> p;
-
-  if(identifier=="")
-    p=std::unique_ptr<languaget>(get_default_language());
-  else
-  {
-    const symbolt *symbol;
-    
-    if(ns.lookup(identifier, symbol))
-      p=std::unique_ptr<languaget>(get_default_language());
-    else if(symbol->mode=="")
-      p=std::unique_ptr<languaget>(get_default_language());
-    else
-    {
-      languaget *ptr=get_language_from_mode(symbol->mode);
-
-      if(ptr==NULL)
-        throw "symbol `"+id2string(symbol->name)+
-              "' has unknown mode '"+id2string(symbol->mode)+"'";
-
-      p=std::unique_ptr<languaget>(ptr);
-    }
-  }
+  std::unique_ptr<languaget> p=get_language(ns, identifier);
 
   std::string result;
   p->from_type(type, result, ns);
@@ -145,3 +141,31 @@ std::string from_type(const typet &type)
   return from_type(namespacet(symbol_table), "", type);
 }
 
+/*******************************************************************\
+
+Function: to_expr
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt to_expr(
+  const namespacet &ns,
+  const irep_idt &identifier,
+  const std::string &src)
+{
+  std::unique_ptr<languaget> p=get_language(ns, identifier);
+  
+  const symbolt &symbol=ns.lookup(identifier);
+
+  exprt expr;
+
+  if(p->to_expr(src, id2string(symbol.module), expr, ns))
+    return nil_exprt();
+  
+  return expr;
+}
