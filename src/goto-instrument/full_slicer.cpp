80900df05c801ed16e4075065f11ba20ea0fa288
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-08-28 09:53:21 +0000

    Implement slicing via dependence graph
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@5788 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-instrument/full_slicer.cpp
+++ src/goto-instrument/full_slicer.cpp
@@ -6,11 +6,245 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <util/find_symbols.h>
+#include <util/cprover_prefix.h>
+
 #include <goto-programs/remove_skip.h>
+
+#include <analyses/dependence_graph.h>
 
 #include "full_slicer.h"
 #include "full_slicer_class.h"
-#include "object_id.h"
+
+/*******************************************************************\
+
+Function: full_slicert::add_dependencies
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void full_slicert::add_dependencies(
+  const cfgt::nodet &node,
+  queuet &queue,
+  const dependence_grapht &dep_graph,
+  const dep_node_to_cfgt &dep_node_to_cfg)
+{
+  const dependence_grapht::nodet &d_node=
+    dep_graph[dep_graph[node.PC].get_node_id()];
+
+  for(dependence_grapht::edgest::const_iterator
+      it=d_node.in.begin();
+      it!=d_node.in.end();
+      ++it)
+    add_to_queue(queue, dep_node_to_cfg[it->first], node.PC);
+}
+
+/*******************************************************************\
+
+Function: full_slicert::add_dependencies
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void full_slicert::add_function_calls(
+  const cfgt::nodet &node,
+  queuet &queue,
+  const goto_functionst &goto_functions)
+{
+  goto_functionst::function_mapt::const_iterator f_it=
+    goto_functions.function_map.find(node.PC->function);
+  assert(f_it!=goto_functions.function_map.end());
+
+  assert(!f_it->second.body.instructions.empty());
+  goto_programt::const_targett begin_function=
+    f_it->second.body.instructions.begin();
+
+  cfgt::entry_mapt::const_iterator entry=
+    cfg.entry_map.find(begin_function);
+  assert(entry!=cfg.entry_map.end());
+
+  for(cfgt::edgest::const_iterator
+      it=cfg[entry->second].in.begin();
+      it!=cfg[entry->second].in.end();
+      ++it)
+    add_to_queue(queue, it->first, node.PC);
+}
+
+/*******************************************************************\
+
+Function: full_slicert::add_decl_dead
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void full_slicert::add_decl_dead(
+  const cfgt::nodet &node,
+  queuet &queue,
+  decl_deadt &decl_dead)
+{
+  if(decl_dead.empty()) return;
+
+  find_symbols_sett syms;
+  find_symbols(node.PC->code, syms);
+  find_symbols(node.PC->guard, syms);
+
+  for(find_symbols_sett::const_iterator
+      it=syms.begin();
+      it!=syms.end();
+      ++it)
+  {
+    decl_deadt::iterator entry=decl_dead.find(*it);
+    if(entry==decl_dead.end()) continue;
+
+    while(!entry->second.empty())
+    {
+      add_to_queue(queue, entry->second.top(), node.PC);
+      entry->second.pop();
+    }
+
+    decl_dead.erase(entry);
+  }
+}
+
+/*******************************************************************\
+
+Function: full_slicert::add_jumps
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void full_slicert::add_jumps(
+  queuet &queue,
+  jumpst &jumps,
+  const cfg_post_dominatorst &cfg_post_dominators)
+{
+  // Based on:
+  // On slicing programs with jump statements
+  // Hiralal Agrawal, PLDI'94
+  // Figure 7:
+  // Slice = the slice obtained using the conventional slicing algorithm;
+  // do {
+  //   Traverse the postdominator tree using the preorder traversal,
+  //   and for each jump statement, J, encountered that is
+  //     (i) not in Slice and
+  //     (ii) whose nearest postdominator in Slice is different from
+  //          the nearest lexical successor in Slice) do {
+  //            Add J to Slice;
+  //            Add the transitive closure of the dependences of J to Slice;
+  //          }
+  // } until no new jump statements may be added to Slice;
+  // For each goto statement, Goto L, in Slice, if the statement
+  // labeled L is not in Slice then associate the label L with its
+  // nearest postdominator in Slice;
+  // return (Slice);
+
+  for(jumpst::iterator
+      it=jumps.begin();
+      it!=jumps.end();
+      ) // no ++it
+  {
+    jumpst::iterator next=it;
+    ++next;
+
+    const cfgt::nodet &j=cfg[*it];
+
+    // is j in the slice already?
+    if(j.node_required)
+    {
+      jumps.erase(it);
+      it=next;
+      continue;
+    }
+
+    // check nearest lexical successor in slice
+    goto_programt::const_targett lex_succ=j.PC;
+    for( ; !lex_succ->is_end_function(); ++lex_succ)
+    {
+      cfgt::entry_mapt::const_iterator entry=
+        cfg.entry_map.find(lex_succ);
+      assert(entry!=cfg.entry_map.end());
+
+      if(cfg[entry->second].node_required)
+        break;
+    }
+    if(lex_succ->is_end_function())
+    {
+      it=next;
+      continue;
+    }
+
+    cfg_post_dominatorst::cfgt::entry_mapt::const_iterator e=
+      cfg_post_dominators.cfg.entry_map.find(j.PC);
+    assert(e!=cfg_post_dominators.cfg.entry_map.end());
+    const cfg_post_dominatorst::cfgt::nodet &n=
+      cfg_post_dominators.cfg[e->second];
+
+    // find the nearest post-dominator in slice
+    if(n.dominators.find(lex_succ)==n.dominators.end())
+    {
+      add_to_queue(queue, *it, lex_succ);
+      jumps.erase(it);
+    }
+    else
+    {
+      // check whether the nearest post-dominator is different from
+      // lex_succ
+      goto_programt::const_targett nearest=lex_succ;
+      std::size_t post_dom_size=0;
+      for(cfg_dominatorst::target_sett::const_iterator
+          d_it=n.dominators.begin();
+          d_it!=n.dominators.end();
+          ++d_it)
+      {
+        cfgt::entry_mapt::const_iterator entry=
+          cfg.entry_map.find(*d_it);
+        assert(entry!=cfg.entry_map.end());
+
+        if(cfg[entry->second].node_required)
+        {
+          cfg_post_dominatorst::cfgt::entry_mapt::const_iterator e2=
+            cfg_post_dominators.cfg.entry_map.find(*d_it);
+          assert(e2!=cfg_post_dominators.cfg.entry_map.end());
+          const cfg_post_dominatorst::cfgt::nodet &n2=
+            cfg_post_dominators.cfg[e2->second];
+
+          if(n2.dominators.size()>post_dom_size)
+          {
+            nearest=*d_it;
+            post_dom_size=n2.dominators.size();
+          }
+        }
+      }
+      if(nearest!=lex_succ)
+      {
+        add_to_queue(queue, *it, nearest);
+        jumps.erase(it);
+      }
+    }
+
+    it=next;
+  }
+}
 
 /*******************************************************************\
 
@@ -24,61 +258,58 @@ Function: full_slicert::fixedpoint
 
 \*******************************************************************/
 
-void full_slicert::fixedpoint()
+void full_slicert::fixedpoint(
+  goto_functionst &goto_functions,
+  queuet &queue,
+  jumpst &jumps,
+  decl_deadt &decl_dead,
+  const dependence_grapht &dep_graph)
 {
-  queuet queue;
-
-  // fill queue with asserts
-  for(cfgt::entry_mapt::iterator
-      e_it=cfg.entry_map.begin();
-      e_it!=cfg.entry_map.end();
-      e_it++)
+  std::vector<cfgt::entryt> dep_node_to_cfg;
+  dep_node_to_cfg.reserve(dep_graph.size());
+  for(unsigned i=0; i<dep_graph.size(); ++i)
   {
-    cfgt::nodet &node=cfg[e_it->second];
+    cfgt::entry_mapt::const_iterator entry=
+      cfg.entry_map.find(dep_graph[i].PC);
+    assert(entry!=cfg.entry_map.end());
 
-    if(e_it->first->is_assert())
-    {
-      get_objects(e_it->first->guard, node.required_objects);
-      node.node_required=true;
-      queue.push(e_it->second);
-    }
-    else if(e_it->first->is_end_function())
-      node.node_required=true; // always retained
+    dep_node_to_cfg.push_back(entry->second);
   }
 
   // process queue until empty
   while(!queue.empty())
   {
-    cfgt::entryt e=queue.top();
-    cfgt::nodet &node=cfg[e];
-    queue.pop();
-    
-    // look at required_objects
-    object_id_sett required_objects=transform(node);
-
-    for(cfgt::edgest::const_iterator
-        p_it=node.in.begin();
-        p_it!=node.in.end();
-        p_it++)
+    while(!queue.empty())
     {
-      object_id_sett::size_type old_size = cfg[p_it->first].required_objects.size();
+      cfgt::entryt e=queue.top();
+      cfgt::nodet &node=cfg[e];
+      queue.pop();
 
-      cfg[p_it->first].required_objects.insert(
-        required_objects.begin(), required_objects.end());
+      // already done by some earlier iteration?
+      if(node.node_required)
+        continue;
 
-      object_id_sett::size_type new_size = cfg[p_it->first].required_objects.size();
+      // node is required
+      node.node_required=true;
 
-      assert(new_size >= old_size);
+      // add data and control dependencies of node
+      add_dependencies(node, queue, dep_graph, dep_node_to_cfg);
 
-      if (new_size > old_size)
-        queue.push(p_it->first);
+      // retain all calls of the containing function
+      add_function_calls(node, queue, goto_functions);
+
+      // find all the symbols it uses to add declarations
+      add_decl_dead(node, queue, decl_dead);
     }
+
+    // add any required jumps
+    add_jumps(queue, jumps, dep_graph.cfg_post_dominators());
   }
 }
 
 /*******************************************************************\
 
-Function: full_slicert::slice
+Function: implicit
 
   Inputs:
 
@@ -88,13 +319,78 @@ Function: full_slicert::slice
 
 \*******************************************************************/
 
-void full_slicert::slice(goto_functionst &goto_functions)
+static bool implicit(goto_programt::const_targett target)
+{
+  // some variables are used during symbolic execution only
+
+  if(!target->is_assign()) return false;
+
+  const code_assignt &a=to_code_assign(target->code);
+  if(a.lhs().id()!=ID_symbol) return false;
+
+  const symbol_exprt &s=to_symbol_expr(a.lhs());
+
+  return s.get_identifier()==CPROVER_PREFIX "rounding_mode";
+}
+
+/*******************************************************************\
+
+Function: full_slicert::operator()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void full_slicert::operator()(
+  goto_functionst &goto_functions,
+  const namespacet &ns,
+  slicing_criteriont &criterion)
 {
   // build the CFG data structure
   cfg(goto_functions);
-  
+
+  // fill queue with according to slicing criterion
+  queuet queue;
+  // gather all unconditional jumps as they may need to be included
+  jumpst jumps;
+  // declarations or dead instructions may be necessary as well
+  decl_deadt decl_dead;
+
+  for(cfgt::entry_mapt::iterator
+      e_it=cfg.entry_map.begin();
+      e_it!=cfg.entry_map.end();
+      e_it++)
+  {
+    if(criterion(e_it->first))
+      add_to_queue(queue, e_it->second, e_it->first);
+    else if(implicit(e_it->first))
+      add_to_queue(queue, e_it->second, e_it->first);
+    else if((e_it->first->is_goto() && e_it->first->guard.is_true()) ||
+            e_it->first->is_throw() ||
+            e_it->first->is_return())
+      jumps.push_back(e_it->second);
+    else if(e_it->first->is_decl())
+    {
+      const exprt &s=to_code_decl(e_it->first->code).symbol();
+      decl_dead[to_symbol_expr(s).get_identifier()].push(e_it->second);
+    }
+    else if(e_it->first->is_dead())
+    {
+      const exprt &s=to_code_dead(e_it->first->code).symbol();
+      decl_dead[to_symbol_expr(s).get_identifier()].push(e_it->second);
+    }
+  }
+
+  // compute program dependence graph (and post-dominators)
+  dependence_grapht dep_graph(ns);
+  dep_graph(goto_functions, ns);
+
   // compute the fixedpoint
-  fixedpoint();
+  fixedpoint(goto_functions, queue, jumps, decl_dead, dep_graph);
 
   // now replace those instructions that are not needed
   // by skips
@@ -105,11 +401,12 @@ void full_slicert::slice(goto_functionst &goto_functions)
       Forall_goto_program_instructions(i_it, f_it->second.body)
       {
         const cfgt::entryt &e=cfg.entry_map[i_it];
-        if(!cfg[e].node_required)
+        if(!i_it->is_end_function() && // always retained
+           !cfg[e].node_required)
           i_it->make_skip();
       }
     }
-  
+
   // remove the skips
   remove_skip(goto_functions);
   goto_functions.update();
@@ -117,7 +414,7 @@ void full_slicert::slice(goto_functionst &goto_functions)
 
 /*******************************************************************\
 
-Function: full_slicert::transform
+Function: full_slicer
 
   Inputs:
 
@@ -127,146 +424,17 @@ Function: full_slicert::transform
 
 \*******************************************************************/
 
-object_id_sett full_slicert::transform(cfgt::nodet& e)
+void full_slicer(
+  goto_functionst &goto_functions,
+  const namespacet &ns,
+  slicing_criteriont &criterion)
 {
-  const object_id_sett &old_set=e.required_objects;
-  object_id_sett new_set=e.required_objects; // copy!
-  
-  const goto_programt::instructiont &instruction=*(e.PC);
-
-  switch(instruction.type)
-  {
-  // these introduce control-flow dependencies
-  case ASSUME:
-    {
-      object_id_sett w;
-      get_objects(instruction.guard, w);
-      
-      bool found=false;
-
-      // does it constrain something we need?
-      for(object_id_sett::const_iterator
-          o_it1=w.begin(); o_it1!=w.end(); o_it1++)
-        if(old_set.find(*o_it1)!=old_set.end())
-          found=true;
-
-      if(found)
-      {
-        e.node_required=true;
-        new_set.insert(w.begin(), w.end());
-      }
-    }    
-    break;
-    
-  // these introduce control-flow dependencies
-  case GOTO:
-    {
-      object_id_sett w;
-      get_objects(instruction.guard, w);
-      
-      bool found=false;
-
-      // does it constrain something we need?
-      for(object_id_sett::const_iterator
-          o_it1=w.begin(); o_it1!=w.end(); o_it1++)
-        if(old_set.find(*o_it1)!=old_set.end())
-          found=true;
-
-      if(found)
-      {
-        e.node_required=true;
-        new_set.insert(w.begin(), w.end());
-      }
-    }
-    break;
-  
-  case OTHER:
-    break;
-
-  case RETURN:
-    // TODO
-    break;
-    
-  case ASSIGN:
-    {
-      const code_assignt &code_assign=to_code_assign(instruction.code);
-      
-      object_id_sett w;
-      get_objects_w(code_assign, w);
-      
-      bool found=false;
-
-      // does it write something we need?
-      for(object_id_sett::const_iterator
-          o_it1=w.begin(); o_it1!=w.end(); o_it1++)
-      {
-        object_id_sett::iterator o_it2=old_set.find(*o_it1);
-        if(o_it2!=old_set.end())
-        {
-          found=true;
-          // remove what is written
-          new_set.erase(*o_it2);
-        }
-      }
-
-      if(found)
-      {
-        e.node_required=true;
-        // add what it reads
-        get_objects_r(code_assign, new_set);
-      }
-    }    
-    break;
-  
-  case DECL: // this cuts off dependencies
-    {
-      object_idt object_id(to_symbol_expr(instruction.code.op0()));
-      if(old_set.find(object_id)!=old_set.end())
-      {
-        e.node_required=true;
-        new_set.erase(object_id);
-      }
-    }
-    break;
-  
-  case FUNCTION_CALL:
-    {
-      // These are like assignments for the arguments.
-      // The LHS is dealt with on return.
-      const code_function_callt &code_function_call=
-        to_code_function_call(instruction.code);
-
-      assert(code_function_call.function().type().id()==ID_code);
-      
-      const code_typet &code_type=
-        to_code_type(code_function_call.function().type());
-
-      assert(code_function_call.arguments().size()>=
-             code_type.parameters().size());
-
-      for(unsigned i=0; i<code_type.parameters().size(); i++)
-      {
-        object_idt lhs_id=object_idt(code_type.parameters()[i].get_identifier());
-
-        if(old_set.find(lhs_id)!=old_set.end())
-        {
-          new_set.erase(lhs_id);
-          e.node_required=true;
-          get_objects(code_function_call.arguments()[i], new_set);
-        }
-      }
-    }    
-    break;
-  
-  default:;
-  }
-  
-  return new_set;
+  full_slicert()(goto_functions, ns, criterion);
 }
 
 /*******************************************************************\
 
-Function: slicer
+Function: full_slicer
 
   Inputs:
 
@@ -280,5 +448,23 @@ void full_slicer(
   goto_functionst &goto_functions,
   const namespacet &ns)
 {
-  full_slicert()(goto_functions);
+  assert_criteriont a;
+  full_slicert()(goto_functions, ns, a);
 }
+
+/*******************************************************************\
+
+Function: slicing_criteriont::~slicing_criteriont
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+slicing_criteriont::~slicing_criteriont()
+{
+}
+
--- src/goto-instrument/full_slicer_class.h
+++ src/goto-instrument/full_slicer_class.h
@@ -10,11 +10,15 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_GOTO_PROGRAM_FULL_SLICER_CLASS_H
 
 #include <stack>
+#include <vector>
+#include <list>
 
 #include <goto-programs/goto_functions.h>
 #include <goto-programs/cfg.h>
 
-#include "object_id.h"
+#include <analyses/cfg_dominators.h>
+
+class dependence_grapht;
 
 /*******************************************************************\
 
@@ -27,10 +31,10 @@ Author: Daniel Kroening, kroening@kroening.com
 class full_slicert
 {
 public:
-  void operator()(goto_functionst &goto_functions)
-  {
-    slice(goto_functions);
-  }
+  void operator()(
+    goto_functionst &goto_functions,
+    const namespacet &ns,
+    slicing_criteriont &criterion);
 
 protected:
   struct cfg_nodet
@@ -40,17 +44,60 @@ protected:
     }
 
     bool node_required;
-    object_id_sett required_objects;
   };
 
   typedef cfg_baset<cfg_nodet> cfgt;
   cfgt cfg;
 
+  typedef std::vector<cfgt::entryt> dep_node_to_cfgt;
   typedef std::stack<cfgt::entryt> queuet;
+  typedef std::list<cfgt::entryt> jumpst;
+  typedef hash_map_cont<irep_idt, queuet, irep_id_hash> decl_deadt;
 
-  void fixedpoint();
-  void slice(goto_functionst &goto_functions);
-  object_id_sett transform(cfgt::nodet&);
+  void fixedpoint(
+    goto_functionst &goto_functions,
+    queuet &queue,
+    jumpst &jumps,
+    decl_deadt &decl_dead,
+    const dependence_grapht &dep_graph);
+
+  void add_dependencies(
+    const cfgt::nodet &node,
+    queuet &queue,
+    const dependence_grapht &dep_graph,
+    const dep_node_to_cfgt &dep_node_to_cfg);
+
+  void add_function_calls(
+    const cfgt::nodet &node,
+    queuet &queue,
+    const goto_functionst &goto_functions);
+
+  void add_decl_dead(
+    const cfgt::nodet &node,
+    queuet &queue,
+    decl_deadt &decl_dead);
+
+  void add_jumps(
+    queuet &queue,
+    jumpst &jumps,
+    const cfg_post_dominatorst &cfg_post_dominators);
+
+  inline void add_to_queue(
+    queuet &queue,
+    const cfgt::entryt &entry,
+    goto_programt::const_targett reason)
+  {
+    queue.push(entry);
+  }
+};
+
+class assert_criteriont:public slicing_criteriont
+{
+public:
+  virtual bool operator()(goto_programt::const_targett target)
+  {
+    return target->is_assert();
+  }
 };
 
 #endif
