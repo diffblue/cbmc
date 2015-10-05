32548fa251fd9078dbe4b47eb95e5e5534e4cf30
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-10-05 15:13:30 +0000

    beautify

--- regression/cbmc/Initialization7/main.c
+++ regression/cbmc/Initialization7/main.c
@@ -3,16 +3,21 @@ struct S
   int *s;
 };
 
+int nondet_int();
+
 int main()
 {
   int x=nondet_int();
+
   struct S p;
   if(p.s!=0)
-  {
     p.s=&x;
-  }
+
   *(p.s)=42;
+
+  // should fail, as p.s may be null
   __CPROVER_assert(x==42, "");
+
   return 0;
 }
 
