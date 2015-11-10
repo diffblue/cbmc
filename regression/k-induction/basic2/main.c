a9c06d9cb4db75f297d3b5ca3bc9b13a69cd2f97
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-11-10 13:39:15 +0000

    more tests

---
+++ regression/k-induction/basic2/main.c
@@ -0,0 +1,15 @@
+int main()
+{
+  int a=0, b=0;
+  
+  while(a<=10 && b<=10)
+  {
+    int choice;
+
+    b++;    
+    a=b;
+
+    // this is 2-inductive, but not 1-inductive    
+    __CPROVER_assert(a>=0, "property");
+  }
+}
