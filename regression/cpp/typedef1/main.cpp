c2b0ec3aef55729908e2f0a6bb7cc4e0dee05164
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-11-24 12:36:02 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6119 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- regression/cpp/typedef1/main.cpp
+++ regression/cpp/typedef1/main.cpp
@@ -2,4 +2,5 @@ typedef void (*func1)(int asd[]);
 typedef void (*func2)(int asd);
 typedef void (*func3)(int);
 typedef void (*func4)(int ...);
+typedef void (*func4)(int ... asd);
 typedef void (*func4)(int, ...); // same, how mean!
