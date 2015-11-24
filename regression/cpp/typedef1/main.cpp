fce868e5ac1e852236a034a6fdccc67e5bea64ec
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-11-24 12:35:33 +0000

    more

--- regression/cpp/typedef1/main.cpp
+++ regression/cpp/typedef1/main.cpp
@@ -1,3 +1,5 @@
-typedef void (*func)(int asd[]);
-typedef void (*func)(int asd);
-typedef void (*func)(int);
+typedef void (*func1)(int asd[]);
+typedef void (*func2)(int asd);
+typedef void (*func3)(int);
+typedef void (*func4)(int ...);
+typedef void (*func4)(int, ...); // same, how mean!
