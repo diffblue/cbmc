b52381b4da38f8ccd6e1625fb796cabe79f641bc
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-12-01 08:30:55 +0000

    new cegis options

--- src/cbmc/cbmc_parse_options.cpp
+++ src/cbmc/cbmc_parse_options.cpp
@@ -37,13 +37,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <goto-programs/loop_ids.h>
 #include <goto-programs/link_to_library.h>
 
-#include <cegis/symex/cegis_symex_verify.h>
-#include <cegis/symex/cegis_symex_learn.h>
-#include <cegis/danger/constant/default_constant_strategy.h>
-#include <cegis/danger/preprocess/danger_preprocessing.h>
-#include <cegis/danger/symex/verify/danger_verify_config.h>
-#include <cegis/danger/symex/learn/danger_learn_config.h>
-#include <cegis/facade/cegis.h>
+#include <cegis/danger/facade/danger_runner.h>
 
 #include <goto-instrument/full_slicer.h>
 
@@ -439,6 +433,43 @@ void cbmc_parse_optionst::get_command_line_options(optionst &options)
 
   if(cmdline.isset("graphml-cex"))
     options.set_option("graphml-cex", cmdline.get_value("graphml-cex"));
+
+  if(cmdline.isset("danger"))
+  {
+    unsigned int min_prog_size=1u;
+    if (cmdline.isset("cegis-min-size"))
+      min_prog_size=string2integer(cmdline.get_value("cegis-min-size")).to_ulong();
+    options.set_option("cegis-min-size", min_prog_size);
+    unsigned int max_prog_size=5u;
+    if (cmdline.isset("cegis-max-size"))
+      max_prog_size=string2integer(cmdline.get_value("cegis-max-size")).to_ulong();
+    options.set_option("cegis-max-size", max_prog_size);
+    options.set_option("cegis-parallel-verify", cmdline.isset("cegis-parallel-verify"));
+    options.set_option("cegis-limit-wordsize", cmdline.isset("cegis-limit-wordsize"));
+    options.set_option("cegis-match-select", !cmdline.isset("cegis-tournament-select"));
+    options.set_option("cegis-statistics", cmdline.isset("cegis-statistics"));
+    options.set_option("cegis-genetic", cmdline.isset("cegis-genetic"));
+    unsigned int genetic_rounds=10u;
+    if (cmdline.isset("cegis-genetic-rounds"))
+      genetic_rounds=string2integer(cmdline.get_value("cegis-genetic-rounds")).to_ulong();
+    options.set_option("cegis-genetic-rounds", genetic_rounds);
+    unsigned int seed=747864937u;
+    if (cmdline.isset("cegis-seed"))
+      seed=string2integer(cmdline.get_value("cegis-seed")).to_ulong();
+    options.set_option("cegis-seed", seed);
+    unsigned int pop_size=2000u;
+    if (cmdline.isset("cegis-genetic-popsize"))
+      pop_size=string2integer(cmdline.get_value("cegis-genetic-popsize")).to_ulong();
+    options.set_option("cegis-genetic-popsize", pop_size);
+    unsigned int mutation_rate=1u;
+    if (cmdline.isset("cegis-genetic-mutation-rate"))
+      mutation_rate=string2integer(cmdline.get_value("cegis-genetic-mutation-rate")).to_ulong();
+    options.set_option("cegis-genetic-mutation-rate", mutation_rate);
+    unsigned int replace_rate=15u;
+    if (cmdline.isset("cegis-genetic-replace-rate"))
+      replace_rate=string2integer(cmdline.get_value("cegis-genetic-replace-rate")).to_ulong();
+    options.set_option("cegis-genetic-replace-rate", replace_rate);
+  }
 }
 
 /*******************************************************************\
@@ -556,20 +587,7 @@ int cbmc_parse_optionst::doit()
     return 7;
 
   if(cmdline.isset("danger"))
-  {
-    size_t max_prog_size=100u;
-    if (cmdline.isset("danger-max-size"))
-      max_prog_size=string2integer(cmdline.get_value("function")).to_ulong();
-
-    const constant_strategyt strategy=default_constant_strategy;
-    danger_preprocessingt preproc(symbol_table, goto_functions, strategy);
-    const danger_programt &prog=preproc.get_danger_program();
-    danger_verify_configt verify_config(prog);
-    cegis_symex_verifyt<danger_verify_configt> verify(options, verify_config);
-    danger_learn_configt learn_config(prog);
-    cegis_symex_learnt<danger_learn_configt> learn(options, learn_config);
-    return run_cegis(learn, verify, preproc, max_prog_size, result());
-  }
+    return run_danger(options, result(), symbol_table, goto_functions);
 
   // do actual BMC
   return do_bmc(bmc, goto_functions);
