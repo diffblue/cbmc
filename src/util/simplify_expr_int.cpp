074f6efc101159f2b21c1a82cd73b9ab0cc807b7
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-12-29 16:18:04 +0000

    further splitup
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6207 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/simplify_expr_int.cpp
+++ src/util/simplify_expr_int.cpp
@@ -11,6 +11,9 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "simplify_expr_class.h"
 #include "expr.h"
 #include "namespace.h"
+#include "namespace_utils.h"
+#include "config.h"
+#include "bv_arithmetic.h"
 #include "std_expr.h"
 #include "expr_util.h"
 #include "arith_tools.h"
@@ -1379,6 +1382,745 @@ bool simplify_exprt::simplify_bitnot(exprt &expr)
     }
   }
 
+  return true;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_inequality
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: simplifies inequalities !=, <=, <, >=, >, and also ==
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_inequality(exprt &expr)
+{
+  exprt::operandst &operands=expr.operands();
+
+  if(expr.type().id()!=ID_bool) return true;
+
+  if(operands.size()!=2) return true;
+
+  exprt tmp0=expr.op0();
+  exprt tmp1=expr.op1();
+  
+  // types must match
+  if(!base_type_eq(tmp0.type(), tmp1.type(), ns))
+    return true;
+    
+  // if rhs is ID_if (and lhs is not), swap operands for == and !=
+  if((expr.id()==ID_equal || expr.id()==ID_notequal) &&
+     tmp0.id()!=ID_if &&
+     tmp1.id()==ID_if)
+  {
+    expr.op0().swap(expr.op1());
+    return simplify_inequality(expr);
+  }
+
+  if(tmp0.id()==ID_if && tmp0.operands().size()==3)
+  {
+    const if_exprt &if_expr=to_if_expr(tmp0);
+
+    exprt tmp_op1=expr;
+    tmp_op1.op0()=if_expr.true_case();
+    simplify_inequality(tmp_op1);
+    exprt tmp_op2=expr;
+    tmp_op2.op0()=if_expr.false_case();
+    simplify_inequality(tmp_op2);
+
+    expr=if_exprt(if_expr.cond(), tmp_op1, tmp_op2);
+
+    simplify_if(expr);
+
+    return false;
+  }
+
+  // see if we are comparing pointers that are address_of
+  if((tmp0.id()==ID_address_of ||
+        (tmp0.id()==ID_typecast && tmp0.op0().id()==ID_address_of)) &&
+      (tmp1.id()==ID_address_of ||
+       (tmp1.id()==ID_typecast && tmp1.op0().id()==ID_address_of)) &&
+      (expr.id()==ID_equal || expr.id()==ID_notequal))
+    return simplify_inequality_address_of(expr);
+
+  if(tmp0.id()==ID_pointer_object &&
+     tmp1.id()==ID_pointer_object &&
+     (expr.id()==ID_equal || expr.id()==ID_notequal))
+    return simplify_inequality_pointer_object(expr);
+
+  // first see if we compare to a constant
+  
+  bool op0_is_const=tmp0.is_constant();
+  bool op1_is_const=tmp1.is_constant();
+  
+  ns.follow_symbol(tmp0.type());
+  ns.follow_symbol(tmp1.type());
+  
+  if(tmp0.type().id()==ID_c_enum_tag)
+    tmp0.type()=ns.follow_tag(to_c_enum_tag_type(tmp0.type()));
+
+  if(tmp1.type().id()==ID_c_enum_tag)
+    tmp1.type()=ns.follow_tag(to_c_enum_tag_type(tmp1.type()));
+
+  // are _both_ constant?  
+  if(op0_is_const && op1_is_const)
+  {
+    if(tmp0.type().id()==ID_bool)
+    {
+      bool v0=tmp0.is_true();
+      bool v1=tmp1.is_true();
+
+      if(expr.id()==ID_equal)
+      {
+        expr.make_bool(v0==v1);
+        return false;
+      }
+      else if(expr.id()==ID_notequal)
+      {
+        expr.make_bool(v0!=v1);
+        return false;
+      }
+    }
+    else if(tmp0.type().id()==ID_fixedbv)
+    {
+      fixedbvt f0(to_constant_expr(tmp0));
+      fixedbvt f1(to_constant_expr(tmp1));
+
+      if(expr.id()==ID_notequal)
+        expr.make_bool(f0!=f1);
+      else if(expr.id()==ID_equal)
+        expr.make_bool(f0==f1);
+      else if(expr.id()==ID_ge)
+        expr.make_bool(f0>=f1);
+      else if(expr.id()==ID_le)
+        expr.make_bool(f0<=f1);
+      else if(expr.id()==ID_gt)
+        expr.make_bool(f0>f1);
+      else if(expr.id()==ID_lt)
+        expr.make_bool(f0<f1);
+      else
+        assert(false);
+    
+      return false;
+    }
+    else if(tmp0.type().id()==ID_floatbv)
+    {
+      ieee_floatt f0(to_constant_expr(tmp0));
+      ieee_floatt f1(to_constant_expr(tmp1));
+
+      if(expr.id()==ID_notequal)
+        expr.make_bool(f0!=f1);
+      else if(expr.id()==ID_equal)
+        expr.make_bool(f0==f1);
+      else if(expr.id()==ID_ge)
+        expr.make_bool(f0>=f1);
+      else if(expr.id()==ID_le)
+        expr.make_bool(f0<=f1);
+      else if(expr.id()==ID_gt)
+        expr.make_bool(f0>f1);
+      else if(expr.id()==ID_lt)
+        expr.make_bool(f0<f1);
+      else
+        assert(false);
+    
+      return false;
+    }
+    else if(tmp0.type().id()==ID_rational)
+    {
+      rationalt r0, r1;
+
+      if(to_rational(tmp0, r0))
+        return true;
+
+      if(to_rational(tmp1, r1))
+        return true;
+
+      if(expr.id()==ID_notequal)
+        expr.make_bool(r0!=r1);
+      else if(expr.id()==ID_equal)
+        expr.make_bool(r0==r1);
+      else if(expr.id()==ID_ge)
+        expr.make_bool(r0>=r1);
+      else if(expr.id()==ID_le)
+        expr.make_bool(r0<=r1);
+      else if(expr.id()==ID_gt)
+        expr.make_bool(r0>r1);
+      else if(expr.id()==ID_lt)
+        expr.make_bool(r0<r1);
+      else
+        assert(false);
+
+      return false;
+    }
+    else
+    {
+      mp_integer v0, v1;
+      
+      if(to_integer(tmp0, v0))
+        return true;
+
+      if(to_integer(tmp1, v1))
+        return true;
+      
+      if(expr.id()==ID_notequal)
+        expr.make_bool(v0!=v1);
+      else if(expr.id()==ID_equal)
+        expr.make_bool(v0==v1);
+      else if(expr.id()==ID_ge)
+        expr.make_bool(v0>=v1);
+      else if(expr.id()==ID_le)
+        expr.make_bool(v0<=v1);
+      else if(expr.id()==ID_gt)
+        expr.make_bool(v0>v1);
+      else if(expr.id()==ID_lt)
+        expr.make_bool(v0<v1);
+      else
+        assert(false);
+
+      return false;
+    }
+  }
+  else if(op0_is_const)
+  {
+    // we want the constant on the RHS
+
+    if(expr.id()==ID_ge)
+      expr.id(ID_le);
+    else if(expr.id()==ID_le)
+      expr.id(ID_ge);
+    else if(expr.id()==ID_gt)
+      expr.id(ID_lt);
+    else if(expr.id()==ID_lt)
+      expr.id(ID_gt);
+    
+    expr.op0().swap(expr.op1());
+    
+    // one is constant
+    simplify_inequality_constant(expr);
+    return false;
+  }
+  else if(op1_is_const)
+  {
+    // one is constant
+    return simplify_inequality_constant(expr); 
+  }
+  else
+  {
+    // both are not constant
+    return simplify_inequality_not_constant(expr);
+  }
+    
+  assert(false);
+  return false;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::eliminate_common_addends
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::eliminate_common_addends(
+  exprt &op0,
+  exprt &op1)
+{
+  // we can't eliminate zeros
+  if(op0.is_zero() ||
+     op1.is_zero() ||
+     (op0.is_constant() &&
+      to_constant_expr(op0).get_value()==ID_NULL) ||
+     (op1.is_constant() &&
+      to_constant_expr(op1).get_value()==ID_NULL))
+    return true;
+  
+  if(op0.id()==ID_plus)
+  {
+    bool result=true;
+
+    Forall_operands(it, op0)
+      if(!eliminate_common_addends(*it, op1))
+        result=false;
+
+    return result;
+  }
+  else if(op1.id()==ID_plus)
+  {
+    bool result=true;
+
+    Forall_operands(it, op1)
+      if(!eliminate_common_addends(op0, *it))
+        result=false;
+      
+    return result;
+  }
+  else if(op0==op1)
+  {
+    if(!op0.is_zero() &&
+       op0.type().id()!=ID_complex)
+    {
+      // elimination!
+      op0=gen_zero(op0.type());
+      op1=gen_zero(op1.type());
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_inequality_not_constant
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_inequality_not_constant(exprt &expr)
+{
+  exprt::operandst &operands=expr.operands();
+  
+  // pretty much all of the simplifications below are unsound
+  // for IEEE float because of NaN!
+  
+  if(ns.follow(expr.op0().type()).id()==ID_floatbv)
+    return true;
+  
+  // eliminate strict inequalities
+  if(expr.id()==ID_notequal)
+  {
+    expr.id(ID_equal);
+    simplify_inequality_not_constant(expr);
+    expr.make_not();
+    simplify_node(expr);
+    return false;
+  }
+  else if(expr.id()==ID_gt)
+  {
+    expr.id(ID_ge);
+    // swap operands
+    expr.op0().swap(expr.op1());
+    simplify_inequality_not_constant(expr);
+    expr.make_not();
+    simplify_node(expr);
+    return false;
+  }
+  else if(expr.id()==ID_lt)
+  {
+    expr.id(ID_ge);
+    simplify_inequality_not_constant(expr);
+    expr.make_not();
+    simplify_node(expr);
+    return false;
+  }
+  else if(expr.id()==ID_le)
+  {
+    expr.id(ID_ge);
+    // swap operands
+    expr.op0().swap(expr.op1());
+    simplify_inequality_not_constant(expr);
+    return false;
+  }
+
+  // now we only have >=, =
+
+  assert(expr.id()==ID_ge || expr.id()==ID_equal);
+
+  // syntactically equal?
+
+  if(operands.front()==operands.back())
+  {
+    expr=true_exprt();
+    return false;
+  }
+
+  // try constants
+
+  value_listt values0, values1;
+
+  bool ok0=!get_values(expr.op0(), values0);
+  bool ok1=!get_values(expr.op1(), values1);
+
+  if(ok0 && ok1)
+  {
+    bool first=true;
+    bool result=false; // dummy initialization to prevent warning
+    bool ok=true;
+
+    // compare possible values
+
+    forall_value_list(it0, values0)
+      forall_value_list(it1, values1)
+      {
+        bool tmp;
+        const mp_integer &int_value0=*it0;
+        const mp_integer &int_value1=*it1;
+
+        if(expr.id()==ID_ge)
+          tmp=(int_value0 >= int_value1);
+        else if(expr.id()==ID_equal)
+          tmp=(int_value0 == int_value1);
+        else
+          assert(0);
+
+        if(first)
+        {
+          result=tmp;
+          first=false;
+        }
+        else if(result!=tmp)
+        {
+          ok=false;
+          break;
+        }
+      }
+
+    if(ok)
+    {
+      expr.make_bool(result);
+      return false;
+    }
+  }
+  
+  // See if we can eliminate common addends on both sides.
+  // On bit-vectors, this is only sound on '='.
+  if(expr.id()==ID_equal)
+  {
+    if(!eliminate_common_addends(expr.op0(), expr.op1()))
+    {
+      // remove zeros
+      simplify_node(expr.op0());
+      simplify_node(expr.op1());
+      simplify_inequality(expr); // recursive call
+      return false;
+    }
+  }
+  
+  return true;
+}  
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_inequality_constant
+
+  Inputs: an inequality with a constant on the RHS
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_inequality_constant(exprt &expr)
+{
+  // the constant is always on the RHS
+  assert(expr.op1().is_constant());
+
+  if(expr.op0().id()==ID_if && expr.op0().operands().size()==3)
+  {
+    const if_exprt &if_expr=to_if_expr(expr.op0());
+
+    exprt tmp_op1=expr;
+    tmp_op1.op0()=if_expr.true_case();
+    simplify_inequality_constant(tmp_op1);
+    exprt tmp_op2=expr;
+    tmp_op2.op0()=if_expr.false_case();
+    simplify_inequality_constant(tmp_op2);
+
+    expr=if_exprt(if_expr.cond(), tmp_op1, tmp_op2);
+
+    simplify_if(expr);
+
+    return false;
+  }
+
+  // do we deal with pointers?
+  if(expr.op1().type().id()==ID_pointer)
+  {
+    if(expr.id()==ID_notequal)
+    {
+      expr.id(ID_equal);
+      simplify_inequality_constant(expr);
+      expr.make_not();
+      simplify_node(expr);
+      return false;
+    }
+  
+    // very special case for pointers
+    if(expr.id()==ID_equal &&
+       expr.op1().is_constant() &&
+       expr.op1().get(ID_value)==ID_NULL)
+    {
+      // the address of an object is never NULL
+    
+      if(expr.op0().id()==ID_address_of && 
+         expr.op0().operands().size()==1)
+      {
+        if(expr.op0().op0().id()==ID_symbol ||
+           expr.op0().op0().id()==ID_dynamic_object ||
+           expr.op0().op0().id()==ID_member ||
+           expr.op0().op0().id()==ID_index ||
+           expr.op0().op0().id()==ID_string_constant)
+        {
+          expr=false_exprt();
+          return false;
+        }
+      }
+      else if(expr.op0().id()==ID_typecast &&
+              expr.op0().operands().size()==1 &&
+              expr.op0().type().id()==ID_pointer &&
+              expr.op0().op0().id()==ID_address_of &&
+              expr.op0().op0().operands().size()==1)
+      {
+        if(expr.op0().op0().op0().id()==ID_symbol ||
+           expr.op0().op0().op0().id()==ID_dynamic_object ||
+           expr.op0().op0().op0().id()==ID_member ||
+           expr.op0().op0().op0().id()==ID_index ||
+           expr.op0().op0().op0().id()==ID_string_constant)
+        {
+          expr=false_exprt();
+          return false;
+        }
+      }
+      else if(expr.op0().id()==ID_typecast &&
+              expr.op0().operands().size()==1 &&
+              expr.op0().type().id()==ID_pointer &&
+              (expr.op0().op0().type().id()==ID_pointer ||
+               config.ansi_c.NULL_is_zero))
+      {
+        // (type)ptr == NULL -> ptr == NULL
+        // note that 'ptr' may be an integer
+        exprt op=expr.op0().op0();
+        expr.op0().swap(op);
+        if(expr.op0().type().id()!=ID_pointer)
+          expr.op1()=gen_zero(expr.op0().type());
+        else
+          expr.op1().type()=expr.op0().type();
+        simplify_inequality(expr); // do again!
+        return false;
+      }
+    }
+
+    // all we are doing with pointers
+    return true;
+  }
+  
+  // is it a separation predicate?
+  
+  if(expr.op0().id()==ID_plus)
+  {
+    // see if there is a constant in the sum
+    
+    if(expr.id()==ID_equal || expr.id()==ID_notequal)
+    {
+      mp_integer constant=0;
+      bool changed=false;
+      
+      Forall_operands(it, expr.op0())
+      {
+        if(it->is_constant())
+        {
+          mp_integer i;
+          if(!to_integer(*it, i))
+          {
+            constant+=i;
+            *it=gen_zero(it->type());
+            changed=true;
+          }
+        }
+      }
+      
+      if(changed)
+      {
+        // adjust constant
+        mp_integer i;
+        to_integer(expr.op1(), i);
+        i-=constant;
+        expr.op1()=from_integer(i, expr.op1().type());
+
+        simplify_plus(expr.op0());
+        simplify_inequality(expr);
+        return false;
+      }
+    }
+  }
+  
+  #if 1
+  // (double)value REL const ---> value rel const
+  // if 'const' can be represented exactly.
+  
+  if(expr.op0().id()==ID_typecast &&
+     expr.op0().operands().size()==1 &&
+     ns.follow(expr.op0().type()).id()==ID_floatbv &&
+     ns.follow(expr.op0().op0().type()).id()==ID_floatbv)
+  {
+    ieee_floatt const_val(to_constant_expr(expr.op1()));
+    ieee_floatt const_val_converted=const_val;
+    const_val_converted.change_spec(to_floatbv_type(ns.follow(expr.op0().op0().type())));
+    ieee_floatt const_val_converted_back=const_val_converted;
+    const_val_converted_back.change_spec(to_floatbv_type(ns.follow(expr.op0().type())));
+    if(const_val_converted_back==const_val)
+    {
+      exprt result=expr;
+      result.op0()=expr.op0().op0();
+      result.op1()=const_val_converted.to_expr();
+      expr.swap(result);
+      return false;
+    }
+  }
+  #endif
+
+  // is the constant zero?
+
+  if(expr.op1().is_zero())
+  {
+    if(expr.id()==ID_ge &&
+       expr.op0().type().id()==ID_unsignedbv)
+    {
+      // zero is always smaller or equal something unsigned
+      expr=true_exprt();
+      return false;
+    }
+
+    exprt &operand=expr.op0();
+
+    if(expr.id()==ID_equal)
+    {
+      // rules below do not hold for >=
+      if(operand.id()==ID_unary_minus)
+      {
+        if(operand.operands().size()!=1) return true;
+        exprt tmp;
+        tmp.swap(operand.op0());
+        operand.swap(tmp);
+        return false;
+      }
+      else if(operand.id()==ID_plus)
+      {
+        // simplify a+-b=0 to a=b
+
+        if(operand.operands().size()==2)
+        {
+          // if we have -b+a=0, make that a+(-b)=0
+
+          if(operand.op0().id()==ID_unary_minus)
+            operand.op0().swap(operand.op1());
+
+          if(operand.op1().id()==ID_unary_minus &&
+             operand.op1().operands().size()==1)
+          {
+            exprt tmp(expr.id(), expr.type());
+            tmp.operands().resize(2);
+            tmp.op0().swap(operand.op0());
+            tmp.op1().swap(operand.op1().op0());
+            expr.swap(tmp);
+            return false;
+          }
+        }
+      }
+    }
+  }
+
+  // are we comparing with a typecast from bool?
+  if(expr.op0().id()==ID_typecast &&
+     expr.op0().operands().size()==1 &&
+     expr.op0().op0().type().id()==ID_bool)
+  {
+    // we re-write (TYPE)boolean == 0 -> !boolean
+    if(expr.op1().is_zero() && expr.id()==ID_equal)
+    {
+      expr=expr.op0().op0();
+      expr.make_not();
+      return false;
+    }
+
+    // we re-write (TYPE)boolean != 0 -> boolean
+    if(expr.op1().is_zero() && expr.id()==ID_notequal)
+    {
+      expr=expr.op0().op0();
+      return false;
+    }
+  }
+
+  #define NORMALISE_CONSTANT_TESTS
+  #ifdef NORMALISE_CONSTANT_TESTS
+  // Normalise to >= and = to improve caching and term sharing
+  if (expr.op0().type().id()==ID_unsignedbv ||
+      expr.op0().type().id()==ID_signedbv)
+  {
+    bv_spect spec(expr.op0().type());
+    mp_integer max(spec.max_value());
+    
+    if(expr.id()==ID_notequal)
+    {
+      expr.id(ID_equal);
+      simplify_inequality_constant(expr);
+      expr.make_not();
+      simplify_node(expr);
+      return false;
+    }
+    else if(expr.id()==ID_gt)
+    {
+      mp_integer i;
+      if(to_integer(expr.op1(), i))
+	throw "Bit-vector constant unexpectedly non-integer";
+      
+      if (i == max)
+      {
+	expr=false_exprt();
+	return false;
+      }
+
+      expr.id(ID_ge);
+      ++i;
+      expr.op1()=from_integer(i, expr.op1().type());
+      simplify_inequality_constant(expr);
+      return false;
+    }
+    else if(expr.id()==ID_lt)
+    {
+      expr.id(ID_ge);
+      simplify_inequality_constant(expr);
+      expr.make_not();
+      simplify_node(expr);
+      return false;
+    }
+    else if(expr.id()==ID_le)
+    {
+      mp_integer i;
+      if(to_integer(expr.op1(), i))
+	throw "Bit-vector constant unexpectedly non-integer";
+
+      if (i == max)
+      {
+	expr=true_exprt();
+	return false;
+      }
+
+      expr.id(ID_ge);
+      ++i;
+      expr.op1()=from_integer(i, expr.op1().type());
+      simplify_inequality_constant(expr);
+      expr.make_not();
+      simplify_node(expr);
+      return false;
+    }
+  }
+#endif
   return true;
 }
 
