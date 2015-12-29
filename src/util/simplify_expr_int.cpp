14b46fad23d8ea6582f09c49de4d48d24b3cfa50
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-12-29 16:11:39 +0000

    further split up of simplify_expr.cpp
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6204 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/simplify_expr_int.cpp
+++ src/util/simplify_expr_int.cpp
@@ -16,6 +16,8 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "arith_tools.h"
 #include "fixedbv.h"
 #include "rational_tools.h"
+#include "simplify_utils.h"
+#include "ieee_float.h"
 
 /*******************************************************************\
 
@@ -1004,6 +1006,328 @@ bool simplify_exprt::simplify_shifts(exprt &expr)
         expr=from_integer(value, expr.type());
         return false;
       }
+    }
+  }
+
+  return true;
+}
+
+/*******************************************************************\
+
+Function: sort_and_join
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: produce canonical ordering for associative and commutative
+          binary operators
+
+\*******************************************************************/
+
+// The entries
+//  { ID_plus,   ID_floatbv  },
+//  { ID_mult,   ID_floatbv  },
+//  { ID_plus,   ID_pointer  },
+// are deliberately missing, as FP-addition and multiplication
+// aren't associative. Addition to pointers isn't really
+// associative.
+
+struct saj_tablet
+{
+  const irep_idt id;
+  const irep_idt type_ids[10];
+} const saj_table[]=
+{
+  { ID_plus,  {ID_integer    ,
+               ID_natural    ,
+               ID_real       ,
+               ID_complex    ,
+               ID_rational   ,
+               ID_unsignedbv ,
+               ID_signedbv   ,
+               ID_fixedbv    ,
+               irep_idt()  }},
+  { ID_mult,  {ID_integer    ,
+               ID_natural    ,
+               ID_real       ,
+               ID_complex    ,
+               ID_rational   ,
+               ID_unsignedbv ,
+               ID_signedbv   ,
+               ID_fixedbv    ,
+               irep_idt()  }},
+  { ID_and,   {ID_bool       ,
+               irep_idt()  }},
+  { ID_or,    {ID_bool       ,
+               irep_idt()  }},
+  { ID_xor,   {ID_bool       ,
+               irep_idt()  }},
+  { ID_bitand,{ID_unsignedbv ,
+               ID_signedbv   ,
+               ID_floatbv    ,
+               ID_fixedbv    ,
+               irep_idt()  }},
+  { ID_bitor, {ID_unsignedbv ,
+               ID_signedbv   ,
+               ID_floatbv    ,
+               ID_fixedbv    ,
+               irep_idt()  }},
+  { ID_bitxor,{ID_unsignedbv ,
+               ID_signedbv   ,
+               ID_floatbv    ,
+               ID_fixedbv    ,
+               irep_idt()  }},
+  { irep_idt(),{ irep_idt() }}
+};
+
+static bool sort_and_join(
+  const struct saj_tablet &saj_entry,
+  const irep_idt &type_id)
+{
+  for(unsigned i=0; saj_entry.type_ids[i]!=irep_idt(); i++)
+    if(type_id==saj_entry.type_ids[i])
+      return true;
+
+  return false;
+}
+
+static const struct saj_tablet &sort_and_join(
+  const irep_idt &id,
+  const irep_idt &type_id)
+{
+  unsigned i=0;
+
+  for( ; saj_table[i].id!=irep_idt(); i++)
+    if(id==saj_table[i].id &&
+       sort_and_join(saj_table[i], type_id))
+      return saj_table[i];
+
+  return saj_table[i];
+}
+
+/*******************************************************************\
+
+Function: sort_and_join
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::sort_and_join(exprt &expr)
+{
+  bool result=true;
+
+  if(!expr.has_operands()) return true;
+
+  const struct saj_tablet &saj_entry=
+    ::sort_and_join(expr.id(), expr.type().id());
+  if(saj_entry.id==irep_idt())
+    return true;
+
+  // check operand types
+
+  forall_operands(it, expr)
+    if(!::sort_and_join(saj_entry, it->type().id()))
+      return true;
+
+  // join expressions
+
+  exprt::operandst new_ops;
+  new_ops.reserve(expr.operands().size());
+
+  forall_operands(it, expr)
+  {
+    if(it->id()==expr.id())
+    {
+      new_ops.reserve(new_ops.capacity()+it->operands().size()-1);
+
+      forall_operands(it2, *it)
+        new_ops.push_back(*it2);
+
+      result=false;
+    }
+    else
+      new_ops.push_back(*it);
+  }
+
+  // sort it
+
+  result=sort_operands(new_ops) && result;
+  expr.operands().swap(new_ops);
+
+  return result;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_extractbits
+
+  Inputs:
+
+ Outputs:
+
+ Purpose: Simplifies extracting of bits from a constant. 
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_extractbits(exprt &expr)
+{
+  assert(expr.operands().size()==3);
+
+  const typet &op0_type=expr.op0().type();
+
+  if(!is_bitvector_type(op0_type) &&
+     !is_bitvector_type(expr.type()))
+    return true;
+
+  if(expr.op0().is_constant())
+  {
+    unsigned width=to_bitvector_type(op0_type).get_width();
+    mp_integer start, end;
+    
+    if(to_integer(expr.op1(), start))
+      return true;
+
+    if(to_integer(expr.op2(), end))
+      return true;
+
+    if(start<0 || start>=width ||
+       end  <0 || end  >=width)
+      return true;
+
+    assert(start>=end); //is this always the case??
+
+    const irep_idt &value=expr.op0().get(ID_value);
+
+    if(value.size()!=width)
+      return true;
+
+    std::string svalue=id2string(value);
+
+    std::string extracted_value=
+      svalue.substr(width-integer2long(start)-1,
+                    integer2long(start-end+1));
+    
+    exprt tmp(ID_constant, expr.type());
+    tmp.set(ID_value, extracted_value);
+    expr.swap(tmp);
+
+    return false;
+  }
+
+  return true;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_unary_plus
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_unary_plus(exprt &expr)
+{
+  if(expr.operands().size()!=1)
+    return true;
+
+  // simply remove, this is always 'nop'
+  expr=expr.op0();
+  return false;
+}
+
+/*******************************************************************\
+
+Function: simplify_exprt::simplify_unary_minus
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+bool simplify_exprt::simplify_unary_minus(exprt &expr)
+{
+  if(expr.operands().size()!=1)
+    return true;
+
+  if(!is_number(expr.type()))
+    return true;
+
+  exprt &operand=expr.op0();
+
+  if(expr.type()!=operand.type())
+    return true;
+
+  if(operand.id()==ID_unary_minus)
+  {
+    // cancel out "-(-x)" to "x"
+    if(operand.operands().size()!=1)
+      return true;
+
+    if(!is_number(operand.op0().type()))
+      return true;
+
+    exprt tmp;
+    tmp.swap(expr.op0().op0());
+    expr.swap(tmp);
+    return false;
+  }
+  else if(operand.id()==ID_constant)
+  {
+    const irep_idt &type_id=expr.type().id();
+
+    if(type_id==ID_integer ||
+       type_id==ID_signedbv ||
+       type_id==ID_unsignedbv)
+    {
+      mp_integer int_value;
+
+      if(to_integer(expr.op0(), int_value))
+        return true;
+
+      exprt tmp=from_integer(-int_value, expr.type());
+
+      if(tmp.is_nil())
+        return true;
+
+      expr.swap(tmp);
+
+      return false;
+    }
+    else if(type_id==ID_rational)
+    {
+      rationalt r;
+      if(to_rational(expr.op0(), r))
+        return true;
+
+      expr=from_rational(-r);
+      return false;
+    }
+    else if(type_id==ID_fixedbv)
+    {
+      fixedbvt f(to_constant_expr(expr.op0()));
+      f.negate();
+      expr=f.to_expr();
+      return false;
+    }
+    else if(type_id==ID_floatbv)
+    {
+      ieee_floatt f(to_constant_expr(expr.op0()));
+      f.negate();
+      expr=f.to_expr();
+      return false;
     }
   }
 
