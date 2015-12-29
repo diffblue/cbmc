0255e2a9b2cc9c8b53590200976bb9c3d968a2bd
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-12-29 16:00:00 +0000

    further prepare further split of simplify_expr.cpp
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6201 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/simplify_expr_struct.cpp
+++ src/util/simplify_expr_struct.cpp
@@ -1,1 +1,15 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <cassert>
+
+#include "simplify_expr_class.h"
+#include "expr.h"
+#include "namespace.h"
+#include "std_expr.h"
 
