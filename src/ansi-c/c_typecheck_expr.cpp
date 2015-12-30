0f395131438cf6913736e0ddac80cee16fed5573
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2015-12-30 12:29:24 +0000

    Merged code clones
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6208 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/c_typecheck_expr.cpp
+++ src/ansi-c/c_typecheck_expr.cpp
@@ -2533,28 +2533,15 @@ exprt c_typecheck_baset::do_special_functions(
   }
   else if(identifier=="__builtin_popcount" ||
           identifier=="__builtin_popcountl" ||
-          identifier=="__builtin_popcountll")
-  {
-    if(expr.arguments().size()!=1)
-    {
-      err_location(f_op);
-      throw "__builtin_popcount expects one operand";
-    }
-
-    exprt popcount_expr(ID_popcount, expr.type());
-    popcount_expr.operands()=expr.arguments();
-    popcount_expr.add_source_location()=source_location;
-
-    return popcount_expr;
-  }
-  else if(identifier=="__popcnt16" ||
+          identifier=="__builtin_popcountll" ||
+          identifier=="__popcnt16" ||
           identifier=="__popcnt" ||
           identifier=="__popcnt64")
   {
     if(expr.arguments().size()!=1)
     {
       err_location(f_op);
-      throw "__popcnt expects one operand";
+      throw id2string(identifier)+" expects one operand";
     }
 
     exprt popcount_expr(ID_popcount, expr.type());
