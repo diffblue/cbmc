b373a561351d97ac77c84983036fce1e164b981c
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2016-01-14 13:01:54 +0000

    test list initialisation

---
+++ regression/cpp/List_initialization1/main.cpp
@@ -0,0 +1,28 @@
+class some_class
+{
+//  int member1 { 1 };
+  int member2 = { 2 };
+};
+
+class other_class
+{
+  other_class():member { 1 }
+  {
+  }
+
+  int member;
+};
+
+int some_function(int)
+{
+//  return { 1 };
+}
+
+int main()
+{
+  int x, *p;
+  x={ 1 };
+//  x=int { 1 };
+//  p=new int { 1 };
+  some_function({1});
+}
