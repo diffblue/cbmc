0bd76fa194209743bc9a4a7228fa4a42be56b2a6
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2016-01-20 17:17:18 +0000

    Moved read_object&linking code from goto-cc to  goto-programs as it is of general use

--- src/goto-programs/read_goto_binary.cpp
+++ src/goto-programs/read_goto_binary.cpp
@@ -20,6 +20,12 @@ Author:
 #include <util/message.h>
 #include <util/unicode.h>
 #include <util/tempfile.h>
+#include <util/rename_symbol.h>
+#include <util/base_type.h>
+
+#include <langapi/language_ui.h>
+
+#include <linking/linking_class.h>
 
 #include "goto_model.h"
 #include "read_goto_binary.h"
@@ -233,3 +239,183 @@ bool is_goto_binary(const std::string &filename)
   
   return false;
 }
+
+/*******************************************************************\
+
+Function: rename_symbols_in_function
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+static void rename_symbols_in_function(
+  goto_functionst::goto_functiont &function,
+  const rename_symbolt &rename_symbol)
+{
+  goto_programt &program=function.body;
+  rename_symbol(function.type);
+
+  Forall_goto_program_instructions(iit, program)
+  {
+    rename_symbol(iit->code);
+    rename_symbol(iit->guard);
+  }
+}
+
+/*******************************************************************\
+
+Function: link_functions
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+static bool link_functions(
+  symbol_tablet &dest_symbol_table,
+  goto_functionst &dest_functions,
+  symbol_tablet &src_symbol_table,
+  goto_functionst &src_functions,
+  const rename_symbolt &rename_symbol)
+{
+  namespacet ns(dest_symbol_table);
+
+  // merge functions
+  Forall_goto_functions(src_it, src_functions)
+  {
+    // the function might have been renamed
+    rename_symbolt::expr_mapt::const_iterator e_it=
+      rename_symbol.expr_map.find(src_it->first);
+
+    irep_idt final_id=src_it->first;
+
+    if(e_it!=rename_symbol.expr_map.end())
+      final_id=e_it->second;
+
+    // already there?
+    goto_functionst::function_mapt::iterator dest_f_it=
+      dest_functions.function_map.find(final_id);
+
+    if(dest_f_it==dest_functions.function_map.end()) // not there yet
+    {
+      rename_symbols_in_function(src_it->second, rename_symbol);
+
+      goto_functionst::goto_functiont &in_dest_symbol_table=
+        dest_functions.function_map[final_id];
+
+      in_dest_symbol_table.body.swap(src_it->second.body);
+      in_dest_symbol_table.body_available=src_it->second.body_available;
+      in_dest_symbol_table.type=src_it->second.type;
+    }
+    else // collision!
+    {
+      goto_functionst::goto_functiont &in_dest_symbol_table=
+        dest_functions.function_map[final_id];
+
+      goto_functionst::goto_functiont &src_func=src_it->second;
+
+      if(in_dest_symbol_table.body.instructions.empty())
+      {
+        // the one with body wins!
+        rename_symbols_in_function(src_func, rename_symbol);
+
+        in_dest_symbol_table.body.swap(src_func.body);
+        in_dest_symbol_table.body_available=src_func.body_available;
+        in_dest_symbol_table.type=src_func.type;
+      }
+      else if(src_func.body.instructions.empty())
+      {
+        // just keep the old one in dest
+      }
+      else if(in_dest_symbol_table.type.get_bool(ID_C_inlined))
+      {
+        // ok, we silently ignore
+      }
+      else
+      {
+        // the linking code will have ensured that types match
+        rename_symbol(src_func.type);
+        assert(base_type_eq(in_dest_symbol_table.type, src_func.type, ns));
+      }
+    }
+  }
+
+  return false;
+}
+
+/*******************************************************************\
+
+Function: read_object_and_link
+
+  Inputs: a file_name
+
+ Outputs: true on error, false otherwise
+
+ Purpose: reads an object file
+
+\*******************************************************************/
+
+bool read_object_and_link(
+  const std::string &file_name,
+  symbol_tablet &symbol_table,
+  goto_functionst &functions,
+  language_uit &language_ui)
+{
+  language_ui.print(8, "Reading: " + file_name);
+
+  // we read into a temporary symbol_table
+  symbol_tablet temp_symbol_table;
+  goto_functionst temp_functions;
+
+  if(read_goto_binary(
+      file_name,
+      temp_symbol_table,
+      temp_functions,
+      language_ui.get_message_handler()))
+    return true;
+
+  std::set<irep_idt> seen_modes;
+
+  for(symbol_tablet::symbolst::const_iterator
+      it=temp_symbol_table.symbols.begin();
+      it!=temp_symbol_table.symbols.end();
+      it++)
+  {
+    if(it->second.mode!="")
+      seen_modes.insert(it->second.mode);
+  }
+
+  seen_modes.erase(ID_cpp);
+  seen_modes.erase(ID_C);
+
+  if(!seen_modes.empty())
+  {
+    language_ui.error() << "Multi-language linking not supported"
+                        << messaget::eom;
+    return true;
+  }
+
+  // hardwired to C-style linking
+
+  linkingt linking(symbol_table,
+                   temp_symbol_table,
+                   language_ui.get_message_handler());
+
+  if(linking.typecheck_main())
+    return true;
+
+  if(link_functions(symbol_table, functions,
+                    temp_symbol_table, temp_functions,
+                    linking.rename_symbol))
+    return true;
+
+  return false;
+}
+
