153aa6332d87ad7b3d259360dbbca04a2143f6b0
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2016-02-06 18:58:44 +0000

    fix
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6299 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/ansi-c/ansi_c_internal_additions.cpp
+++ src/ansi-c/ansi_c_internal_additions.cpp
@@ -339,7 +339,7 @@ void ansi_c_architecture_strings(std::string &code)
   code+=architecture_string(config.ansi_c.alignment, "alignment");
   code+=architecture_string(config.ansi_c.memory_operand_size, "memory_operand_size");
   code+=architecture_string(int(config.ansi_c.endianness), "endianness");
-  code+=architecture_string(config.ansi_c.arch, "architecture");
+  code+=architecture_string(id2string(config.ansi_c.arch), "arch");
   code+=architecture_string(configt::ansi_ct::os_to_string(config.ansi_c.os), "os");
   code+=architecture_string(config.ansi_c.NULL_is_zero, "NULL_is_zero");
 }
