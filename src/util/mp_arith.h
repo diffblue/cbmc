b04f6ec824d9c76f47f8dc6444c9c594aafbcc0f
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2016-02-22 10:10:14 +0000

    use std::size_t for widths
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6412 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/util/mp_arith.h
+++ src/util/mp_arith.h
@@ -22,7 +22,7 @@ mp_integer operator<<(const mp_integer &a, const mp_integer &b);
 
 const std::string integer2string(const mp_integer &n, unsigned base=10);
 const mp_integer string2integer(const std::string &n, unsigned base=10);
-const std::string integer2binary(const mp_integer &n, unsigned width);
+const std::string integer2binary(const mp_integer &n, std::size_t width);
 const mp_integer binary2integer(const std::string &n, bool is_signed);
 mp_integer::ullong_t integer2long(const mp_integer &n);
 unsigned integer2unsigned(const mp_integer &n);
