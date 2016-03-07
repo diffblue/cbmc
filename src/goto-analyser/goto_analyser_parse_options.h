8c01b42c7698815f76c28072ebf7175214031c26
kroening <kroening@6afb6bc1-c8e4-404c-8f48-9ae832c5b171>
2016-03-07 19:16:01 +0000

    clean up options
    git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6457 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

--- src/goto-analyser/goto_analyser_parse_options.h
+++ src/goto-analyser/goto_analyser_parse_options.h
@@ -19,39 +19,17 @@ class goto_functionst;
 class optionst;
 
 #define GOTO_ANALYSER_OPTIONS \
-  "(program-only)(function):(preprocess)(slice-by-trace):" \
-  "(no-simplify)(unwind):(unwindset):(slice-formula)(full-slice)" \
-  "(debug-level):(no-propagation)(no-simplify-if)" \
-  "(document-subgoals)(outfile):(test-preprocessor)" \
+  "(function):" \
   "D:I:(std89)(std99)(std11)" \
   "(classpath):" \
-  "(depth):(partial-loops)(no-unwinding-assertions)(unwinding-assertions)" \
-  "(bounds-check)(pointer-check)(div-by-zero-check)(memory-leak-check)" \
-  "(signed-overflow-check)(unsigned-overflow-check)(float-overflow-check)(nan-check)" \
-  "(no-assertions)(no-assumptions)" \
-  "(xml-ui)(xml-interface)(vcd):" \
-  "(smt1)(smt2)(fpa)(cvc3)(cvc4)(boolector)(yices)(z3)(opensmt)(mathsat)" \
-  "(no-sat-preprocessor)" \
-  "(no-pretty-names)(beautify)" \
-  "(floatbv)(fixedbv)" \
-  "(dimacs)(refine)(max-node-refinement):(refine-arrays)(refine-arithmetic)(aig)" \
   "(16)(32)(64)(LP64)(ILP64)(LLP64)(ILP32)(LP32)" \
   "(little-endian)(big-endian)" \
   "(show-goto-functions)(show-loops)" \
-  "(show-symbol-table)(show-parse-tree)(show-vcc)" \
-  "(show-claims)(claim):(show-properties)(show-reachable-properties)(property):" \
-  "(all-claims)(all-properties)" \
-  "(error-label):(verbosity):(no-library)" \
-  "(version)" \
-  "(cover):" \
-  "(mm):" \
-  "(i386-linux)(i386-macos)(i386-win32)(win32)(winx64)(gcc)" \
-  "(ppc-macos)(unsigned-char)" \
-  "(arrays-uf-always)(arrays-uf-never)" \
-  "(string-abstraction)(no-arch)(arch):" \
-  "(round-to-nearest)(round-to-plus-inf)(round-to-minus-inf)(round-to-zero)" \
-  "(graphml-cex):(json-cex):" \
-  "(decide)" // legacy, and will eventually disappear
+  "(show-symbol-table)(show-parse-tree)" \
+  "(show-properties)(show-reachable-properties)(property):" \
+  "(verbosity):(version)" \
+  "(gcc)(arch):" \
+  "(taint):"
 
 class goto_analyser_parse_optionst:
   public parse_options_baset,
@@ -62,16 +40,11 @@ public:
   virtual void help();
 
   goto_analyser_parse_optionst(int argc, const char **argv);
-  goto_analyser_parse_optionst(
-    int argc,
-    const char **argv,
-    const std::string &extra_options);
 
 protected:
   virtual void register_languages();
 
   virtual void get_command_line_options(optionst &options);
-  virtual int do_bmc(bmct &bmc, const goto_functionst &goto_functions);
 
   virtual int get_goto_program(
     const optionst &options,
@@ -85,14 +58,6 @@ protected:
   bool set_properties(goto_functionst &goto_functions);
   
   void eval_verbosity();
-  
-  // get any additional stuff before finalizing
-  virtual int get_modules(bmct &bmc)
-  {
-    return -1; // continue
-  }
-  
-  void preprocessing();
 };
 
 #endif
