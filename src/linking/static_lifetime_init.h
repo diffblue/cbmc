87395fbe2265bdbcef0d938b23ae63b79feaa3cc
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-04-07 22:11:19 +0100

    split up static_lifetime_init and entry_point

--- src/linking/static_lifetime_init.h
+++ src/linking/static_lifetime_init.h
@@ -6,15 +6,19 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
-#ifndef CPROVER_LINKING_ENTRY_POINT_H
-#define CPROVER_LINKING_ENTRY_POINT_H
+#ifndef CPROVER_LINKING_STATIC_LIFETIME_INIT_H
+#define CPROVER_LINKING_STATIC_LIFETIME_INIT_H
 
 #include <util/symbol_table.h>
 #include <util/message.h>
+#include <util/source_location.h>
+#include <util/cprover_prefix.h>
 
-bool entry_point(
+bool static_lifetime_init(
   symbol_tablet &symbol_table,
-  const std::string &standard_main,
+  const source_locationt &source_location,
   message_handlert &message_handler);
+
+#define INITIALIZE_FUNCTION CPROVER_PREFIX "initialize"
 
 #endif
