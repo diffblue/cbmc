e4ca48088826cf18ad5f2695a819a29719067725
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-04-10 09:54:52 +0100

    check __MMX__ define

--- regression/ansi-c/MMX1/main.c
+++ regression/ansi-c/MMX1/main.c
@@ -6,11 +6,13 @@ int main()
 {
   // This is a gcc extension
   #ifdef __GNUC__  
+  #ifdef __MMX__
   __m64 x;
   
   long long unsigned di;
   
   x=(__m64)di;  
+  #endif
   #endif
 
   return 0;
