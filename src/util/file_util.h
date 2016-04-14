a210c2d98454564edc0561eb8fc30b9226cf116f
Michael Tautschnig <tautschn@amazon.com>
2016-04-14 15:06:54 +0000

    Refactor OS-specific path concatenation into utility function

--- src/util/file_util.h
+++ src/util/file_util.h
@@ -15,4 +15,7 @@ void delete_directory(const std::string &path);
 
 std::string get_current_working_directory();
 
+std::string concat_dir_file(const std::string &directory,
+                            const std::string &file_name);
+
 #endif
