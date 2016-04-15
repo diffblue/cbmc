bffb07264a2928aad4f473be0660acc8f46a09b5
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-04-15 16:47:55 +0100

    update to locale.c

--- src/ansi-c/library/locale.c
+++ src/ansi-c/library/locale.c
@@ -6,13 +6,14 @@
 #define __CPROVER_LOCALE_H_INCLUDED
 #endif
 
-char *setlocale(int category, const char *locale)
+inline char *setlocale(int category, const char *locale)
 {
+  __CPROVER_HIDE:;
   (void)*locale;
   #ifdef __CPROVER_CUSTOM_BITVECTOR_ANALYSIS
   __CPROVER_event("invalidate_pointer", "setlocale_result");
   char *setlocale_result;
-  __CPROVER_set_must(setlocale_result, "setlocale_result");
+  __CPROVER_set_may(setlocale_result, "setlocale_result");
   return setlocale_result;
   #endif
 }
@@ -24,12 +25,13 @@ char *setlocale(int category, const char *locale)
 #define __CPROVER_LOCALE_H_INCLUDED
 #endif
 
-struct lconv *localeconv(void)
+inline struct lconv *localeconv(void)
 {
+  __CPROVER_HIDE:;
   #ifdef __CPROVER_CUSTOM_BITVECTOR_ANALYSIS
   __CPROVER_event("invalidate_pointer", "localeconv_result");
   struct lconv *localeconv_result;
-  __CPROVER_set_must(localeconv_result, "localeconv_result");
+  __CPROVER_set_may(localeconv_result, "localeconv_result");
   return localeconv_result;
   #endif
 }
