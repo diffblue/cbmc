8416727b272cc3db5a2dec26e3a3a84a9ac7a942
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-05-03 14:37:12 +0100

    allow values for -m gcc options

--- src/goto-cc/gcc_cmdline.cpp
+++ src/goto-cc/gcc_cmdline.cpp
@@ -241,7 +241,15 @@ bool gcc_cmdlinet::parse(int argc, const char **argv)
     }
     else if(has_prefix(argv_i, "-m")) // m-options
     {
-      set(argv_i);
+      // these sometimes come with a value separated by '=', e.g., -march=cpu_type
+    
+      std::size_t equal_pos=argv_i.find('=');
+      
+      if(equal_pos==std::string::npos)
+        set(argv_i); // no value
+      else
+        set(std::string(argv_i, 0, equal_pos),
+            std::string(argv_i, equal_pos+1, std::string::npos));
     }
     else if(in_list(argv[i], gcc_options_without_argument)) // without argument
     {
