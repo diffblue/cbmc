6ccc3536c7b7cc7bcf9d3b06e1cbd40f896b2fd5
Cristina <cristina.david@gmail.com>
2016-05-13 12:55:41 +0100

    + regression test for NULL object dereferencing in Java

---
+++ regression/cbmc-java/pointer_check1/pointer_check1.java
@@ -0,0 +1,21 @@
+class A 
+{
+  int val;
+}
+
+class B 
+{
+  int getVal(A a) 
+  {
+    return a.val;
+  }
+}
+
+class pointer_check1 
+{
+  public static void main(String[] args) 
+  {
+    B b = new B();
+    int myval = b.getVal(null);
+  }
+}
