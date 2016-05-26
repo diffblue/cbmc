707e70f2d726934e975b5b736f12eded17e4f89d
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-05-26 10:45:30 +0100

    another test for branch coverage

---
+++ regression/cbmc-cover/branch3/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+
+int main()
+{
+  char ch;
+  unsigned state=0;
+  while((ch=getc(stdin))!=-1)
+  {
+    switch(state)
+    {
+    case 0: if(ch=='<') state=1; else state=0; break;
+    case 1: if(ch=='h') state=2; else state=0; break;
+    case 2: if(ch=='t') state=3; else state=0; break;
+    case 3: if(ch=='m') state=4; else state=0; break;
+    case 4: if(ch=='l') state=5; else state=0; break;
+    default:;
+    }
+  }
+}
