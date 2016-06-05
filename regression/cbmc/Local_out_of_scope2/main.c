diffblue/cbmc

commit a5bc493713a2c744fd55e1572adf6adffd0c8433

use ssa_exprt
git-svn-id: svn+ssh://svn.cprover.org/srv/svn/cbmc/trunk@6381 6afb6bc1-c8e4-404c-8f48-9ae832c5b171

src/goto-symex/symex_function_call.cpp
@@ -136,15 +136,7 @@ void goto_symext::parameter_assignments(
         }
       }
       
-      guardt guard;
-      state.rename(lhs, ns, goto_symex_statet::L1);
-      
-      symex_targett::assignment_typet assignment_type=
-        goto_function.is_hidden()?
-        symex_targett::HIDDEN_ACTUAL_PARAMETER:
-        symex_targett::VISIBLE_ACTUAL_PARAMETER;
-        
-      symex_assign_symbol(state, lhs, nil_exprt(), rhs, guard, assignment_type);
+      symex_assign_rec(state, code_assignt(lhs, rhs));
     }
 
     it1++;
@@ -173,15 +165,7 @@ void goto_symext::parameter_assignments(
       
       symbol_exprt lhs=symbol_exprt(id, it1->type());
 
-      guardt guard;
-      state.rename(lhs, ns, goto_symex_statet::L1);
-
-      symex_targett::assignment_typet assignment_type=
-        goto_function.is_hidden()?
-        symex_targett::HIDDEN_ACTUAL_PARAMETER:
-        symex_targett::VISIBLE_ACTUAL_PARAMETER;
-        
-      symex_assign_symbol(state, lhs, nil_exprt(), *it1, guard, assignment_type);
+      symex_assign_rec(state, code_assignt(lhs, *it1));
     }
   }
   else if(it1!=arguments.end())
@@ -328,9 +312,8 @@ void goto_symext::symex_function_call_code(
     {
       side_effect_expr_nondett rhs(call.lhs().type());
       rhs.add_source_location()=call.source_location();
-      state.rename(rhs, ns, goto_symex_statet::L1);
       code_assignt code(call.lhs(), rhs);
-      symex_assign(state, to_code_assign(code)); /* TODO: clean_expr? */
+      symex_assign_rec(state, code);
     }
 
     state.source.pc++;
@@ -400,11 +383,23 @@ void goto_symext::pop_frame(statet &state)
     state.level1.restore_from(frame.old_level1);
   
     // clear function-locals from L2 renaming
-    for(statet::framet::local_variablest::const_iterator
-        it=frame.local_variables.begin();
-        it!=frame.local_variables.end();
-        it++)
-      state.level2.remove(*it);
+    for(goto_symex_statet::renaming_levelt::current_namest::iterator
+        c_it=state.level2.current_names.begin();
+        c_it!=state.level2.current_names.end();
+       ) // no ++c_it
+    {
+      const irep_idt l1_o_id=c_it->second.first.get_l1_object_identifier();
+      // could use iteration over local_objects as l1_o_id is prefix
+      if(frame.local_objects.find(l1_o_id)==frame.local_objects.end())
+      {
+        ++c_it;
+        continue;
+      }
+      goto_symex_statet::renaming_levelt::current_namest::iterator
+        cur=c_it;
+      ++c_it;
+      state.level2.current_names.erase(cur);
+    }
   }
   
   state.pop_frame();
@@ -467,7 +462,9 @@ void goto_symext::locality(
       it++)
   {
     // get L0 name
-    irep_idt l0_name=state.rename_identifier(*it, ns, goto_symex_statet::L0);
+    ssa_exprt ssa(ns.lookup(*it).symbol_expr());
+    state.rename(ssa, ns, goto_symex_statet::L0);
+    const irep_idt l0_name=ssa.get_identifier();
 
     // save old L1 name for popping the frame
     statet::level1t::current_namest::const_iterator c_it=
@@ -480,19 +477,23 @@ void goto_symext::locality(
     // identifiers may be shared among functions
     // (e.g., due to inlining or other code restructuring)
     
-    irep_idt l1_name;
+    state.level1.current_names[l0_name]=
+      std::make_pair(ssa, frame_nr);
+    state.rename(ssa, ns, goto_symex_statet::L1);
+
+    irep_idt l1_name=ssa.get_identifier();
     unsigned offset=0;
     
-    do
+    while(state.l1_history.find(l1_name)!=state.l1_history.end())
     {
-      state.level1.rename_identifier(l0_name, frame_nr+offset);
-      l1_name=state.level1(l0_name);
-      offset++;
+      state.level1.increase_counter(l0_name);
+      ssa.set_level_1(frame_nr+offset);
+      l1_name=ssa.get_identifier();
+      ++offset;
     }
-    while(state.l1_history.find(l1_name)!=state.l1_history.end());
     
     // now unique -- store
-    frame.local_variables.insert(l1_name);
+    frame.local_objects.insert(l1_name);
     state.l1_history.insert(l1_name);
   }
 }
@@ -522,8 +523,6 @@ void goto_symext::return_assignment(statet &state)
   if(code.operands().size()==1)
   {
     exprt value=code.op0();
-
-    clean_expr(value, state, false);
   
     if(frame.return_value.is_not_nil())
     {
@@ -536,7 +535,7 @@ void goto_symext::return_assignment(statet &state)
               "assignment.lhs().type():\n"+assignment.lhs().type().pretty()+"\n"+
               "assignment.rhs().type():\n"+assignment.rhs().type().pretty();
 
-      symex_assign(state, assignment);
+      symex_assign_rec(state, assignment);
     }
   }
   else

regression/cbmc/Malloc18/test.desc
@@ -1,4 +1,4 @@
-KNOWNBUG
+CORE
 main.c
 
 ^EXIT=0$

regression/cbmc/Malloc21/test.desc
@@ -1,4 +1,4 @@
-KNOWNBUG
+CORE
 main.c
 
 ^EXIT=0$

src/analyses/goto_check.cpp
@@ -1540,6 +1540,8 @@ void goto_checkt::goto_check(goto_functiont &goto_function)
           goto_programt::targett t=new_code.add_instruction(ASSIGN);
           exprt address_of_expr=address_of_exprt(variable);
           exprt lhs=ns.lookup(CPROVER_PREFIX "dead_object").symbol_expr();
+          if(!base_type_eq(lhs.type(), address_of_expr.type(), ns))
+            address_of_expr.make_typecast(lhs.type());
           exprt rhs=if_exprt(
             side_effect_expr_nondett(bool_typet()), address_of_expr, lhs, lhs.type());
           t->source_location=i.source_location;

src/cbmc/counterexample_beautification.cpp
@@ -45,7 +45,7 @@ void counterexample_beautificationt::get_minimization_list(
     {
       if(!prop_conv.l_get(it->guard_literal).is_false())
       {
-        const typet &type=it->original_lhs_object.type();
+        const typet &type=it->ssa_lhs.type();
       
         if(type!=bool_typet())
         {

src/goto-programs/goto_trace.cpp
@@ -112,8 +112,8 @@ void goto_trace_stept::output(
 
   if(pc->is_other() || pc->is_assign())
   {
-    irep_idt identifier=lhs_object.get_identifier();
-    out << "  " << identifier
+    irep_idt identifier=lhs_object.get_object_name();
+    out << "  " << from_expr(ns, identifier, lhs_object.get_original_expr())
         << " = " << from_expr(ns, identifier, lhs_object_value)
         << "\n";
   }
@@ -221,11 +221,11 @@ Function: trace_value
 void trace_value(
   std::ostream &out,
   const namespacet &ns,
-  const symbol_exprt &lhs_object,
+  const ssa_exprt &lhs_object,
   const exprt &full_lhs,
   const exprt &value)
 {
-  const irep_idt &identifier=lhs_object.get_identifier();
+  const irep_idt &identifier=lhs_object.get_object_name();
   std::string value_string;
   
   if(value.is_nil())

src/goto-programs/goto_trace.h
@@ -21,7 +21,7 @@ Date: July 2005
 #include <iosfwd>
 #include <vector>
 
-#include <util/std_expr.h>
+#include <util/ssa_expr.h>
 
 #include <goto-programs/goto_program.h>
 
@@ -80,7 +80,7 @@ public:
   std::string comment;
 
   // the object being assigned
-  symbol_exprt lhs_object;
+  ssa_exprt lhs_object;
   
   // the full, original lhs expression
   exprt full_lhs;
@@ -173,7 +173,7 @@ void show_goto_trace(
 void trace_value(
   std::ostream &out,
   const namespacet &ns,
-  const symbol_exprt &lhs_object,
+  const ssa_exprt &lhs_object,
   const exprt &full_lhs,
   const exprt &value);
 

src/goto-symex/auto_objects.cpp
@@ -101,7 +101,7 @@ void goto_symext::initialize_auto_object(
         address_of_expr);
       
       code_assignt assignment(expr, rhs);
-      symex_assign(state, assignment); /* TODO: needs clean */
+      symex_assign_rec(state, assignment);
     }
   }
 }
@@ -122,21 +122,25 @@ void goto_symext::trigger_auto_object(
   const exprt &expr,
   statet &state)
 {
-  if(expr.id()==ID_symbol)
+  if(expr.id()==ID_symbol &&
+     expr.get_bool(ID_C_SSA_symbol))
   {
-    const symbol_exprt &symbol_expr=to_symbol_expr(expr);
-    const irep_idt &identifier=symbol_expr.get_identifier();
+    const ssa_exprt &ssa_expr=to_ssa_expr(expr);
+    const irep_idt &obj_identifier=ssa_expr.get_object_name();
     
-    const symbolt &symbol=
-      ns.lookup(state.get_original_name(identifier));
-      
-    if(has_prefix(id2string(symbol.base_name), "auto_object"))
+    if(obj_identifier!="goto_symex::\\guard")
     {
-      // done already?
-      if(state.level2.current_names.find(identifier)==
-         state.level2.current_names.end())
+      const symbolt &symbol=
+        ns.lookup(obj_identifier);
+        
+      if(has_prefix(id2string(symbol.base_name), "auto_object"))
       {
-        initialize_auto_object(expr, state);
+        // done already?
+        if(state.level2.current_names.find(ssa_expr.get_identifier())==
+           state.level2.current_names.end())
+        {
+          initialize_auto_object(expr, state);
+        }
       }
     }
   }

commit 5a775c15c2e64e7639ef7611d4c64a6fda779bdd

Fix consistency error in SSA level 1 renaming
a5bc493713 introduced cases where information in the frame may become
inconsistent with the actual renaming being used, as shown in the regression
test.

src/goto-symex/symex_function_call.cpp
@@ -501,9 +501,9 @@ void goto_symext::locality(
     while(state.l1_history.find(l1_name)!=state.l1_history.end())
     {
       state.level1.increase_counter(l0_name);
+      ++offset;
       ssa.set_level_1(frame_nr+offset);
       l1_name=ssa.get_identifier();
-      ++offset;
     }
     
     // now unique -- store

regression/cbmc/Local_out_of_scope2/main.c
@@ -1,0 +1,25 @@
+inline void foo(int x)
+{
+}
+
+void bar()
+{
+  foo(0);
+}
+
+void foobar()
+{
+  // different argument values must not cause an inconsistency in the
+  // equation system
+  foo(0);
+  bar();
+  foo(1);
+}
+
+int main()
+{
+  foobar();
+  foobar();
+  __CPROVER_assert(0, "");
+  return 0;
+}

regression/cbmc/Local_out_of_scope2/test.desc
@@ -1,0 +1,8 @@
+CORE
+main.c
+
+^EXIT=10$
+^SIGNAL=0$
+^VERIFICATION FAILED$
+--
+^warning: ignoring