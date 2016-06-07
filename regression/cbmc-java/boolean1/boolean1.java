2675a17d4013044ac31150f3838d4d9098bbc336
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-06-07 17:58:22 +0100

    added test for Boolean parameters

---
+++ regression/cbmc-java/boolean1/boolean1.java
@@ -0,0 +1,12 @@
+class boolean1 {
+
+  static public void doit(boolean a, int div)
+  {
+    // Boolean shall be either true or false.
+    if (a == true) return;
+    if (a == false) return;
+
+    assert false;
+  }
+
+};
