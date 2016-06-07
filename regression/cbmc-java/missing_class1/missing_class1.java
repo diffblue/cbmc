bb6dfa02879fe2112ab1b54df77e9ba438291eaa
Cristina <cristina.david@gmail.com>
2016-06-07 18:12:38 +0100

    + test failing to find a null pointer dereferencing when one of the classes is missing (pointer related assertions missing from the goto program)

---
+++ regression/cbmc-java/missing_class1/missing_class1.java
@@ -0,0 +1,16 @@
+class B {
+  int j;
+}
+ 
+class A {
+  int i;
+  B b;
+}
+
+public class missing_class1 {
+  public static void main(String[] args) {
+    A a = new A();
+    B b = a.b;
+    int j = b.j; // NULL pointer dereference
+  }
+}
