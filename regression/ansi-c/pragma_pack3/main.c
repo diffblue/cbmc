377a73b345035eb2a8b62b6408f078fb58840eaa
Michael Tautschnig <tautschn@amazon.com>
2016-06-16 13:44:45 +0000

    pragma pack(n) still permits more dense alignment than n
    Quoting the documentation: "The alignment of a member will be on a
    boundary that is either a multiple of n or a multiple of the size of the
    member, whichever is smaller."

--- regression/ansi-c/pragma_pack3/main.c
+++ regression/ansi-c/pragma_pack3/main.c
@@ -13,6 +13,29 @@ struct S
 
 STATIC_ASSERT(sizeof(struct S)==sizeof(int)+2);
 
+// "The alignment of a member will be on a boundary that is either a multiple of
+// n or a multiple of the size of the member, whichever is smaller."
+#pragma pack(4)
+
+struct S2
+{
+  short s1;
+  short s2;
+  int i;
+};
+
+STATIC_ASSERT(sizeof(struct S2)==sizeof(short)+sizeof(short)+sizeof(int));
+
+struct S3
+{
+  short s1;
+  short s2;
+  short s3;
+  int i;
+};
+
+STATIC_ASSERT(sizeof(struct S3)==sizeof(short)+sizeof(short)+sizeof(int)+4);
+
 int main()
 {
   return 0;
