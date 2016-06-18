059da82b7d570b3a29633f0f014f212921b68efb
Peter Schrammel <peter.schrammel@cs.ox.ac.uk>
2016-06-18 13:15:35 +0100

    goto-diff initial

---
+++ src/goto-diff/goto_diff_main.cpp
@@ -0,0 +1,54 @@
+/*******************************************************************\
+
+Module: GOTO-DIFF Main Module 
+
+Author: Peter Schrammel
+
+\*******************************************************************/
+
+#include <util/unicode.h>
+
+#ifdef IREP_HASH_STATS
+#include <iostream>
+#endif
+
+#include "goto_diff_parse_options.h"
+
+/*******************************************************************\
+
+Function: main / wmain
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+#ifdef IREP_HASH_STATS
+extern unsigned long long irep_hash_cnt;
+extern unsigned long long irep_cmp_cnt;
+extern unsigned long long irep_cmp_ne_cnt;
+#endif
+
+#ifdef _MSC_VER
+int wmain(int argc, const wchar_t **argv_wide)
+{
+  const char **argv=narrow_argv(argc, argv_wide);
+#else
+int main(int argc, const char **argv)
+{
+#endif
+  goto_diff_parse_optionst parse_options(argc, argv);
+
+  int res=parse_options.main();
+
+  #ifdef IREP_HASH_STATS
+  std::cout << "IREP_HASH_CNT=" << irep_hash_cnt << std::endl;
+  std::cout << "IREP_CMP_CNT=" << irep_cmp_cnt << std::endl;
+  std::cout << "IREP_CMP_NE_CNT=" << irep_cmp_ne_cnt << std::endl;
+  #endif
+
+  return res;
+}
