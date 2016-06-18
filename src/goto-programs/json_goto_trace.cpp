c34d725c7c4f12d3a59646ba078a208f50c653d2
Peter Schrammel <peter.schrammel@cs.ox.ac.uk>
2016-06-18 23:37:11 +0100

    camlCase goto trace JSON property names

--- src/goto-programs/json_goto_trace.cpp
+++ src/goto-programs/json_goto_trace.cpp
@@ -100,14 +100,14 @@ void convert(
       
         json_objectt &json_failure=dest_array.push_back().make_object();
         
-        json_failure["step_type"]=json_stringt("failure");
+        json_failure["stepType"]=json_stringt("failure");
         json_failure["hidden"]=jsont::json_boolean(it->hidden);
         json_failure["thread"]=json_numbert(i2string(it->thread_nr));
         json_failure["reason"]=json_stringt(id2string(it->comment));
         json_failure["property"]=json_stringt(id2string(property_id));
 
         if(!json_location.is_null())
-          json_failure["source_location"]=json_location;
+          json_failure["sourceLocation"]=json_location;
       }
       break;
       
@@ -117,10 +117,10 @@ void convert(
         irep_idt identifier=it->lhs_object.get_identifier();
         json_objectt &json_assignment=dest_array.push_back().make_object();
         
-        json_assignment["step_type"]=json_stringt("assignment");
+        json_assignment["stepType"]=json_stringt("assignment");
 
         if(!json_location.is_null())
-          json_assignment["source_location"]=json_location;
+          json_assignment["sourceLocation"]=json_location;
 
         std::string value_string, binary_string, type_string,
                     full_lhs_string, full_lhs_value_string;
@@ -155,9 +155,9 @@ void convert(
         json_assignment["hidden"]=jsont::json_boolean(it->hidden);
         json_assignment["thread"]=json_numbert(i2string(it->thread_nr));
 
-        json_assignment["assignment_type"]=
-          json_stringt(it->assignment_type==goto_trace_stept::ACTUAL_PARAMETER?"actual_parameter":
-                       "variable");
+        json_assignment["assignmentType"]=
+          json_stringt(it->assignment_type==goto_trace_stept::ACTUAL_PARAMETER?
+                       "actual-parameter":"variable");
       }
       break;
       
@@ -173,21 +173,23 @@ void convert(
     case goto_trace_stept::FUNCTION_RETURN:
       {
         std::string tag=
-          (it->type==goto_trace_stept::FUNCTION_CALL)?"function_call":"function_return";
+          (it->type==goto_trace_stept::FUNCTION_CALL)?
+            "function-call":"function-return";
         json_objectt &json_call_return=dest_array.push_back().make_object();
         
-        json_call_return["step_type"]=json_stringt(tag);
+        json_call_return["stepType"]=json_stringt(tag);
         json_call_return["hidden"]=jsont::json_boolean(it->hidden);
         json_call_return["thread"]=json_numbert(i2string(it->thread_nr));
 
         const symbolt &symbol=ns.lookup(it->identifier);
         json_objectt &json_function=json_call_return["function"].make_object();
-        json_function["display_name"]=json_stringt(id2string(symbol.display_name()));
+        json_function["displayName"]=
+          json_stringt(id2string(symbol.display_name()));
         json_function["identifier"]=json_stringt(id2string(it->identifier));
-        json_function["source_location"]=json(symbol.location);
+        json_function["sourceLocation"]=json(symbol.location);
 
         if(!json_location.is_null())
-          json_call_return["source_location"]=json_location;
+          json_call_return["sourceLocation"]=json_location;
       }
       break;
       
@@ -198,10 +200,10 @@ void convert(
         if(!json_location.is_null())
         {
           json_objectt &json_location_only=dest_array.push_back().make_object();
-          json_location_only["step_type"]=json_stringt("location-only");
+          json_location_only["stepType"]=json_stringt("location-only");
           json_location_only["hidden"]=jsont::json_boolean(it->hidden);
           json_location_only["thread"]=json_numbert(i2string(it->thread_nr));
-          json_location_only["source_location"]=json_location;
+          json_location_only["sourceLocation"]=json_location;
         }
       }
     }
