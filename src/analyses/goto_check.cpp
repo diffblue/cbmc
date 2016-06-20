b19c55f684d8ef747424d7f067fd4561c892295c
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-06-20 13:42:04 +0100

    check whether 'this' is null on method invokation

--- src/analyses/goto_check.cpp
+++ src/analyses/goto_check.cpp
@@ -54,6 +54,8 @@ public:
 
   void goto_check(goto_functiont &goto_function);
     
+  irep_idt mode;
+
 protected:
   const namespacet &ns;
   local_bitvector_analysist *local_bitvector_analysis;
@@ -905,14 +907,6 @@ void goto_checkt::pointer_validity_check(
     
   const typet &dereference_type=pointer_type.subtype();
 
-  irep_idt mode;
-
-  {
-    const symbolt *init_symbol;
-    if(!ns.lookup(CPROVER_PREFIX "initialize", init_symbol))
-      mode=init_symbol->mode;
-  }
-
   // For Java, we only need to check for null
   if(mode==ID_java)
   {
@@ -1446,6 +1440,12 @@ Function: goto_checkt::goto_check
 
 void goto_checkt::goto_check(goto_functiont &goto_function)
 {
+  {
+    const symbolt *init_symbol;
+    if(!ns.lookup(CPROVER_PREFIX "initialize", init_symbol))
+      mode=init_symbol->mode;
+  }
+
   assertions.clear();
   
   local_bitvector_analysist local_bitvector_analysis_obj(goto_function);
@@ -1518,6 +1518,33 @@ void goto_checkt::goto_check(goto_functiont &goto_function)
     {
       const code_function_callt &code_function_call=
         to_code_function_call(i.code);
+        
+      // for Java, need to check whether 'this' is null
+      // on non-static method invocations
+      if(mode==ID_java &&
+         enable_pointer_check &&
+         !code_function_call.arguments().empty() &&
+         code_function_call.function().type().id()==ID_code &&
+         to_code_type(code_function_call.function().type()).has_this())
+      {
+        exprt pointer=code_function_call.arguments()[0];
+      
+        local_bitvector_analysist::flagst flags=
+          local_bitvector_analysis->get(t, pointer);
+          
+        if(flags.is_unknown() || flags.is_null())
+        {
+          notequal_exprt not_eq_null(pointer, gen_zero(pointer.type()));
+
+          add_guarded_claim(
+            not_eq_null,
+            "this is null on method invokation",
+            "pointer dereference",
+            i.source_location,
+            pointer,
+            guardt());
+        }
+      }
     
       forall_operands(it, code_function_call)
         check(*it);
