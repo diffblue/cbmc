a7e932b0bd30b3f58829fbfd50dc7087f8c908b5
Daniel Kroening <dankro@amazon.com>
2016-06-20 15:23:29 -0400

    properly test byte_update flattening

---
+++ regression/cbmc/Pointer_byte_extract9/main.c
@@ -0,0 +1,18 @@
+int main()
+{
+  int N;
+
+  char a1[N];
+  *((char *)a1)=1;
+  *((int *)a1)=1;
+
+  short a2[N];
+  *((char *)a2)=1;
+  *((int *)a2)=1;
+
+  int a3[N];
+  *((char *)a3)=1;
+  *((int *)a3)=1;
+
+  assert(0);
+}
