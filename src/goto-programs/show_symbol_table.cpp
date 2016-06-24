b9cbd9a46613b2b377f27be1712313b18c6880d8
Michael Tautschnig <tautschn@amazon.com>
2016-06-24 02:29:16 +0000

    std::unique_ptr requires #include <memory>

--- src/goto-programs/show_symbol_table.cpp
+++ src/goto-programs/show_symbol_table.cpp
@@ -7,6 +7,7 @@ Author: Daniel Kroening, kroening@kroening.com
 \*******************************************************************/
 
 #include <iostream>
+#include <memory>
 
 #include <util/language.h>
 #include <langapi/mode.h>
