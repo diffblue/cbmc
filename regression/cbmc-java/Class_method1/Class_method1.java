614775c953a5fca4a633836f57240967ac643799
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-06-24 14:53:27 +0100

    added test for .class

---
+++ regression/cbmc-java/Class_method1/Class_method1.java
@@ -0,0 +1,12 @@
+public class Class_method1
+{
+  public static void main(String[] args)
+  {
+    assert(f(String.class, true));
+  }
+
+  public static boolean f(Class c, boolean b)
+  {
+    return b;
+  }
+}
