ca35de79d2ff64e721146cf13e176145c89d9fa0
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-07-20 17:02:33 +0100

    separate out conversion of methods

---
+++ src/java_bytecode/java_bytecode_convert_method.h
@@ -0,0 +1,24 @@
+/*******************************************************************\
+
+Module: JAVA Bytecode Language Conversion
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_JAVA_BYTECODE_CONVERT_METHOD_H
+#define CPROVER_JAVA_BYTECODE_CONVERT_METHOD_H
+
+#include <util/symbol_table.h>
+#include <util/message.h>
+
+#include "java_bytecode_parse_tree.h"
+
+void java_bytecode_convert_method(
+  const symbolt &class_symbol,
+  const java_bytecode_parse_treet::methodt &,
+  symbol_tablet &symbol_table,
+  message_handlert &message_handler);
+
+#endif
+
