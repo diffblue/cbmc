8d1ecc79aea336fbea3cd0eaad62ccc7220c7790
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-07-20 11:02:17 +0100

    extra code for bad compilers

--- src/java_bytecode/java_types.cpp
+++ src/java_bytecode/java_types.cpp
@@ -271,7 +271,7 @@ typet java_type_from_char(char t)
   case 'd': return java_double_type();
   case 'z': return java_boolean_type();
   case 'a': return java_reference_type(void_typet());
-  default: assert(false);
+  default: assert(false); return nil_typet();
   }
 }
 
