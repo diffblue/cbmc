11a4d3d7d843ccbd7be7e6ea6ad49a4c23557ee2
Chris Smowton <chris@smowton.net>
2016-07-25 13:14:19 +0100

    Factor struct member offset routine to enable iterating over members with offsets.

--- src/util/pointer_offset_size.h
+++ src/util/pointer_offset_size.h
@@ -21,6 +21,20 @@ class constant_exprt;
 
 // these return -1 on failure
 
+class member_offset_iterator {
+  typedef std::pair<size_t,mp_integer> refst;
+  refst current;
+  const struct_typet &type;
+  const namespacet &ns;
+  size_t bit_field_bits;
+ public:
+  member_offset_iterator(const struct_typet _type,
+                         const namespacet _ns);
+  member_offset_iterator& operator++();
+  const refst& operator*() const { return current; }
+  const refst* operator->() const { return &current; }
+};
+
 mp_integer member_offset(
   const struct_typet &type,
   const irep_idt &member,
