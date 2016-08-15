888e37462b87e122b7f87a67a2c6a137e7e6b870
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-08-15 12:03:47 +0100

    Corrected the order in the arguments of issuffix

--- regression/strings/test3.2/test.c
+++ regression/strings/test3.2/test.c
@@ -7,7 +7,7 @@ int main()
     __CPROVER_string s, s2, s3;
     int i;
 
-    __CPROVER_assume(i < 10);
+    //__CPROVER_assume(i < 10);
 
     s = __CPROVER_string_concat(s2, s3);
     __CPROVER_assume(__CPROVER_string_length(s2) == i);
