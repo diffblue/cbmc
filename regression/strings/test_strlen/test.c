7bf66a16d9ad337114105dc2af5f84c9cade6807
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-08-15 09:47:37 +0100

    Adding new tests for strings

---
+++ regression/strings/test_strlen/test.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include "cprover-string-hack.h"
+
+
+int main()
+{
+  __CPROVER_string s,t;
+  unsigned len_s, len_t;
+  s = __CPROVER_string_literal("abc");
+  t = __CPROVER_string_literal("xyz");
+  len_s = __CPROVER_string_length(s);
+  len_t = __CPROVER_string_length(t);
+  int b = ( len_s == len_t );
+  assert(b);
+  return 0;
+}
