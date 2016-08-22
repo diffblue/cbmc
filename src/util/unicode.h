75718fcc9b65b1182e80a8885afa868279972109
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-08-22 12:58:59 +0100

    added utf16_to_utf8

--- src/util/unicode.h
+++ src/util/unicode.h
@@ -20,6 +20,7 @@ std::string narrow(const std::wstring &s);
 std::wstring widen(const std::string &s);
 
 std::string utf32_to_utf8(const std::basic_string<unsigned int> &s);
+std::string utf16_to_utf8(const std::basic_string<unsigned short int> &s);
 
 const char **narrow_argv(int argc, const wchar_t **argv_wide);
 
