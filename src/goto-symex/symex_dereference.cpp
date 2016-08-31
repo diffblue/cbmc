1b2e321dab7e493a3df6639a49f77b940eb77e1f
Chris Smowton <chris@smowton.net>
2016-08-31 09:24:54 +0100

    Don't reference CPROVER_memory from Java code. Fixes #197.
    goto-symex's value-set analysis can reach the conclusion that an integer may be cast
    to a pointer, and so generate a reference to the __CPROVER_memory symbol. This is
    invalid in Java however, since the symbol is not defined and of course Java cannot
    express dereferencing a non-pointer type.
    
    This change disables generation of __CPROVER_memory references when a Java-typed
    entry point is found. I couldn't provide a test case against master at this time however
    because the examples in both of the above bugs now succeed against master, presumably
    because value-set analysis has been improved and is harder to confuse into introducing
    a pointer-to-int cast.

--- src/goto-symex/symex_dereference.cpp
+++ src/goto-symex/symex_dereference.cpp
@@ -295,8 +295,9 @@ void goto_symext::dereference_rec(
       ns,
       new_symbol_table,
       options,
-      symex_dereference_state);
-
+      symex_dereference_state,
+      mode);      
+    
     // std::cout << "**** " << from_expr(ns, "", tmp1) << std::endl;
     exprt tmp2=dereference.dereference(
       tmp1, guard, write?value_set_dereferencet::WRITE:value_set_dereferencet::READ);
