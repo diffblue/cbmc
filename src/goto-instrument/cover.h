5e1ffe0cac42a0f12212b420fd44ca0c81c7c1ce
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-09-02 16:28:21 +0100

    generate goals only if they do not exist in the json file

--- src/goto-instrument/cover.h
+++ src/goto-instrument/cover.h
@@ -19,6 +19,16 @@ Date: May 2016
 
 class message_handlert;
 
+class coverage_goals
+{
+public:
+	void set_goals(std::string goal);
+	const bool get_goals(const char* goal);
+
+private:
+	std::vector<std::string> existing_goals;
+};
+
 enum class coverage_criteriont
 {
   LOCATION, BRANCH, DECISION, CONDITION,
@@ -27,12 +37,14 @@ enum class coverage_criteriont
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
   goto_programt &goto_program,
-  coverage_criteriont);
+  coverage_criteriont,
+  coverage_goals &goals);
 
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
   goto_functionst &goto_functions,
-  coverage_criteriont);
+  coverage_criteriont,
+  coverage_goals &goals);
 
 bool instrument_cover_goals(
   const cmdlinet &cmdline,
