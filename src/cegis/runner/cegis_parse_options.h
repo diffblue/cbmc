b29de6248f8c48e8823d91b9d5e6fb6fc2430069
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-09-08 20:36:40 +0100

    line width

--- src/cegis/runner/cegis_parse_options.h
+++ src/cegis/runner/cegis_parse_options.h
@@ -12,9 +12,13 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <cbmc/cbmc_parse_options.h>
 
 #define CEGIS_OPTIONS \
-  "(cegis)(cegis-seed):(cegis-root):(cegis-targets):(cegis-min-prog-size):(cegis-max-prog-size):(cegis-skolem):(cegis-ranking):" \
-  "(cegis-max-size):(cegis-statistics)(cegis-show-iterations)(cegis-keep-goto-programs)(cegis-genetic)(cegis-genetic-rounds):(cegis-genetic-popsize):(cegis-tournament-select)" \
-  "(cegis-genetic-mutation-rate):(cegis-genetic-replace-rate):(cegis-limit-wordsize)(cegis-parallel-verify)(cegis-symex-head-start):" \
+  "(cegis)(cegis-seed):(cegis-root):(cegis-targets):" \
+  "(cegis-min-prog-size):(cegis-max-prog-size):(cegis-skolem):(cegis-ranking):" \
+  "(cegis-max-size):(cegis-statistics)(cegis-show-iterations)" \
+  "(cegis-keep-goto-programs)(cegis-genetic)(cegis-genetic-rounds):" \
+  "(cegis-genetic-popsize):(cegis-tournament-select)" \
+  "(cegis-genetic-mutation-rate):(cegis-genetic-replace-rate):" \
+  "(cegis-limit-wordsize)(cegis-parallel-verify)(cegis-symex-head-start):" \
   "(safety)(danger)(jsa)(danger-max-size):(danger-no-ranking)"
 
 class cegis_parse_optionst: public cbmc_parse_optionst
@@ -23,6 +27,7 @@ public:
   cegis_parse_optionst(int argc, const char **argv);
   virtual ~cegis_parse_optionst()=default;
   virtual void help();
+
 protected:
   virtual void get_command_line_options(optionst &options);
   virtual int do_bmc(bmct &bmc, const goto_functionst &goto_functions);
