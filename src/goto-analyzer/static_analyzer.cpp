01e744b2ada3bea73dd97e959ca1a207fd324d2d
Martin Brain <martin@debian8564>
2016-09-09 08:07:36 +0100

    Remove the special case handling of --show-intervals.  --show --intervals does more and --show-intervals now invokes it.

--- src/goto-analyzer/static_analyzer.cpp
+++ src/goto-analyzer/static_analyzer.cpp
@@ -292,24 +292,3 @@ bool static_analyzer(
   else
     return true;
 }
-
-/*******************************************************************\
-
-Function: show_intervals
-
-  Inputs:
-
- Outputs:
-
- Purpose: 
-
-\*******************************************************************/
-
-void show_intervals(
-  const goto_modelt &goto_model,
-  std::ostream &out)
-{
-  ait<interval_domaint> interval_analysis;
-  interval_analysis(goto_model);
-  interval_analysis.output(goto_model, out);
-}
