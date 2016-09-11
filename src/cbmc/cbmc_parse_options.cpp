cddbb6cb606529d396cf1e635d1e257b233c3681
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-09-11 17:33:08 +0100

    removed unecessary debugging information

--- src/cbmc/cbmc_parse_options.cpp
+++ src/cbmc/cbmc_parse_options.cpp
@@ -888,10 +888,6 @@ bool cbmc_parse_optionst::process_goto_program(
              << config.ansi_c.arch << ")" << eom;
     link_to_library(symbol_table, goto_functions, ui_message_handler);
 
-    debug () << "beggining" << eom;
-    show_goto_functions(ns, get_ui(), goto_functions);
-
-
     if(cmdline.isset("string-abstraction"))
       string_instrumentation(
         symbol_table, get_message_handler(), goto_functions);
@@ -901,8 +897,6 @@ bool cbmc_parse_optionst::process_goto_program(
     remove_function_pointers(symbol_table, goto_functions,
       cmdline.isset("pointer-check"));
     remove_virtual_functions(symbol_table, goto_functions);
-    show_goto_functions(ns, get_ui(), goto_functions);
-
 
     // full slice?
     if(cmdline.isset("full-slice"))
@@ -946,8 +940,6 @@ bool cbmc_parse_optionst::process_goto_program(
       string_abstraction(symbol_table,
         get_message_handler(), goto_functions);
     }
-
-    // for debuggin: show_goto_functions(ns, get_ui(), goto_functions);
 
     // add failed symbols
     // needs to be done before pointer analysis
@@ -998,14 +990,6 @@ bool cbmc_parse_optionst::process_goto_program(
     remove_skip(goto_functions);
     goto_functions.update();
 
-    // for debuggin: 
-    show_goto_functions(ns, get_ui(), goto_functions);
-
-    /*    debug() << "DETAILS:" << eom;
-    Forall_goto_functions(it,goto_functions) 
-      Forall_goto_program_instructions(it2,it->second.body){
-      debug() << "program instruction: " << it2->code.pretty() << eom;
-      }*/
   }
 
   catch(const char *e)
