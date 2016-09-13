6661386403c345d91b285ad36b6506c5bd0a3431
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-09-13 16:20:59 +0100

    substring with only one argument (we should add tests for this also)

--- src/goto-programs/pass_preprocess.cpp
+++ src/goto-programs/pass_preprocess.cpp
@@ -169,6 +169,11 @@ void replace_string_calls(symbol_tablet & symbol_table,goto_functionst & goto_fu
 	  make_string_function(symbol_table, goto_functions, i_it,"__CPROVER_uninterpreted_string_endswith");
 	} else if(function_id == irep_idt("java::java.lang.String.substring:(II)Ljava/lang/String;")) {
 	  make_string_function(symbol_table, goto_functions, i_it,"__CPROVER_uninterpreted_substring");
+	} else if(function_id == irep_idt("java::java.lang.String.substring:(II)Ljava/lang/String;")
+		  || function_id == irep_idt("java::java.lang.String.substring:(I)Ljava/lang/String;")
+		  || function_id == irep_idt("java::java.lang.StringBuilder.substring:(II)Ljava/lang/String;")
+		  || function_id == irep_idt("java::java.lang.StringBuilder.substring:(I)Ljava/lang/String;")) {
+	  make_string_function(symbol_table, goto_functions, i_it,"__CPROVER_uninterpreted_substring");
 	} else if(function_id == irep_idt("java::java.lang.String.contains:(Ljava/lang/CharSequence;)Z")) {
 	  make_string_function(symbol_table, goto_functions, i_it,"__CPROVER_uninterpreted_strcontains");
 	} else if(function_id == irep_idt("java::java.lang.StringBuilder.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")) {
