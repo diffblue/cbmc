9017a18d4c0afdbdec34fe8e18b37b00a9d0c5eb
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-09-14 16:38:49 +0100

    add the --no-trivial-tests option to CBMC
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/goto-instrument/cover.h
+++ src/goto-instrument/cover.h
@@ -22,13 +22,17 @@ class message_handlert;
 class coverage_goalst
 {
 public:
+  coverage_goalst();
   static coverage_goalst get_coverage_goals(const std::string &coverage,
                                       message_handlert &message_handler);
   void set_goals(source_locationt goal);
   bool is_existing_goal(source_locationt source_location);
+  void set_no_trivial_tests(const bool trivial);
+  const bool get_no_trivial_tests();
 
 private:
   std::vector<source_locationt> existing_goals;
+  bool no_trivial_tests;
 };
 
 enum class coverage_criteriont
@@ -37,7 +41,8 @@ enum class coverage_criteriont
   PATH, MCDC, ASSERTION, COVER };
 
 bool consider_goals(
-  const goto_programt &goto_program);
+  const goto_programt &goto_program,
+  coverage_goalst &goals);
 
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
