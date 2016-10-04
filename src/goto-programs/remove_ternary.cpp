fc743bb08d91a308ad9cb27038736f7c74e4619c
Daniel Neville <daniel.neville@cs.ox.ac.uk>
2016-10-04 12:49:23 +0100

    Comments.

--- src/goto-programs/remove_ternary.cpp
+++ src/goto-programs/remove_ternary.cpp
@@ -1,12 +1,13 @@
-/*******************************************************************\
+/*******************************************************************
 
-Module: Remove ternaries by rewritting into IFs.
+ Module: Remove ternaries by rewritting into IFs.
 
-Author: Daniel Neville
+ Author: Daniel Neville.
+         John Galea.
 
-Date:   September 2016
+ Date:   September 2016
 
-\*******************************************************************/
+ \*******************************************************************/
 
 #include <util/std_expr.h>
 #include <util/symbol_table.h>
@@ -17,54 +18,56 @@ Date:   September 2016
 
 #include "remove_ternary.h"
 
+/*******************************************************************
+ Function:  Replace ternary
 
-/*******************************************************************\
+ Inputs:  Takes:
+ - A Goto Program, used to insert new instructions.
+ - A specific instruction, used to know the current location
+ in the program.  This is handled using pointer introduction.
+ - Ternaries on the LHS are generally of poor readability and
+ are avoided by many developers.
+ - The current expr, analysed recursively.
 
-Function:  Replace ternary
+ Outputs:  Modified instruction.
 
-Inputs:  Takes:
-  - A Goto Program, used to insert new instructions.
-  - A specific instruction, used to know the current location
-  in the program.
-  - The current expr, analysed recursively.
+ Purpose:
 
-Outputs:  Modified instruction.
+ \*******************************************************************/
 
-Purpose:
-
-\*******************************************************************/
-
-void remove_ternaryt::replace_ternary(
-    goto_programt &goto_program,
-    goto_programt::instructionst::iterator &i_it,
-    exprt &expr,
-    bool lhs)
+void remove_ternaryt::replace_ternary(goto_programt &goto_program,
+    goto_programt::instructionst::iterator &i_it, exprt &expr, bool lhs)
 {
 
-  // Ternary MUST have operands by definition.
+  // Ternary must have operands by definition.
   if(!expr.has_operands())
     return;
 
   /* LHS handles ternary when used on the LHS of an assignment
-   * operator.  LHS = false is used whenever the ternary is simply
-   * part of an arbitrary condition.
+   * operator.
+   * LHS = false is used whenever the ternary is simply
+   * part of an arbitrary condition (including all RHS)
    */
 
-  if(expr.id() == ID_code) {
-    codet &code = to_code(expr);
-    if(to_code(expr).get_statement()==ID_assign) {
-      code_assignt &code_assign = to_code_assign(code);
+  if(expr.id() == ID_code)
+  {
+    codet &code=to_code(expr);
+    if(to_code(expr).get_statement() == ID_assign)
+    {
+      code_assignt &code_assign=to_code_assign(code);
 
       replace_ternary(goto_program, i_it, code_assign.lhs(), true);
       replace_ternary(goto_program, i_it, code_assign.rhs(), false);
+
       return;
     }
   }
 
   Forall_operands(it, expr)
   {
-      replace_ternary(goto_program, i_it, *it, lhs);
-      // Go through sub-tree first.
+    // Go through sub-tree first.
+    // This allows parsing the expr-tree leaf first.
+    replace_ternary(goto_program, i_it, *it, lhs);
   }
 
   // If we are on an IF statement.
@@ -73,13 +76,13 @@ void remove_ternaryt::replace_ternary(
     if_exprt &if_expr=to_if_expr(expr); // Some short hands.
 
     /* 1  (...) cond : T : F (...)
-     ->
+     -> (where x is some integer)
      1  decl tmp_x;
      2  if !cond GOTO false_case_x
      3  tmp_x = T
      4  GOTO end_x
      6  false_case_x:  tmp_x = F
-     7  end_x: (...) tmp_x (...)
+     7  end_x: (...) tmp_x (...) // replace ternary with tmp_x
      */
 
     auxiliary_symbolt new_symbol;
@@ -136,7 +139,8 @@ void remove_ternaryt::replace_ternary(
 
     expr=symbol_ptr->symbol_expr();
 
-    if(lhs) {
+    if(lhs)
+    {
       expr=dereference_exprt(expr);
       assign_true.rhs()=address_of_exprt(assign_true.rhs());
       assign_false.rhs()=address_of_exprt(assign_false.rhs());
@@ -147,62 +151,58 @@ void remove_ternaryt::replace_ternary(
   }
 }
 
-/*******************************************************************\
+/*******************************************************************
+ Function: remove_ternaryt::operator()(goto_functionst &goto_functions)
 
-Function: remove_ternaryt::operator()(goto_functionst &goto_functions)
+ Inputs:
 
-Inputs:
+ Outputs:
 
-Outputs:
+ Purpose:
 
-Purpose:
-
-\*******************************************************************/
+ \*******************************************************************/
 
 void remove_ternaryt::operator()(goto_functionst &goto_functions)
 {
-  Forall_goto_functions(f_it, goto_functions) {
-   goto_programt &goto_program=f_it->second.body;
+  Forall_goto_functions(f_it, goto_functions){
+  goto_programt &goto_program=f_it->second.body;
 
-    Forall_goto_program_instructions(i_it, goto_program)
-        replace_ternary(goto_program, i_it, i_it->code, false);
-  }
-
-  goto_functions.update();
+  Forall_goto_program_instructions(i_it, goto_program)
+  replace_ternary(goto_program, i_it, i_it->code, false);
 }
 
-/*******************************************************************\
+goto_functions.update();
+}
 
-Function: remove_ternary(symbol_tablet &symbol_table,
-goto_functionst &goto_functions)
+/*******************************************************************
+ Function: remove_ternary(symbol_tablet &symbol_table,
+ goto_functionst &goto_functions)
 
-Inputs:
+ Inputs:
 
-Outputs:
+ Outputs:
 
-Purpose:
+ Purpose:
 
-\*******************************************************************/
+ \*******************************************************************/
 
-void remove_ternary(
-  symbol_tablet &symbol_table,
-  goto_functionst &goto_functions)
+void remove_ternary(symbol_tablet &symbol_table,
+    goto_functionst &goto_functions)
 {
   remove_ternaryt rt(symbol_table);
   rt(goto_functions);
 }
 
-/*******************************************************************\
+/*******************************************************************
+ Function: remove_ternary(goto_modelt &goto_model)
 
-Function: remove_ternary(goto_modelt &goto_model)
+ Inputs:
 
-Inputs:
+ Outputs:
 
-Outputs:
+ Purpose:
 
-Purpose:
-
-\*******************************************************************/
+ \*******************************************************************/
 
 void remove_ternary(goto_modelt &goto_model)
 {
