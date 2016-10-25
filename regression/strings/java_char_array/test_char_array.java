dca43ef2e5324c22e6439a410cf1f198e4345cba
Romain Brenguier <romain.brenguier@cs.ox.ac.uk>
2016-10-25 17:39:47 +0100

    regression test for toCharArray

--- regression/strings/java_char_array/test_char_array.java
+++ regression/strings/java_char_array/test_char_array.java
@@ -2,13 +2,13 @@ public class test_char_array {
 
     public static void main(String[] argv) {
 	String s = "abc";
-	//char[] str = new char[12];
 	char [] str = s.toCharArray();
 	char c = str[2];
 	String t = argv[0];
-	//str[3]='0';
-	//assert(str.length == 3);
 	char d = t.charAt(0);
+
+
+	assert(str.length == 3);
 	assert(c == 'c');
 	assert(c == d || d < 'a' || d > 'z' );
     }
