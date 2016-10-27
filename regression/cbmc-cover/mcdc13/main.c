91642a922430a130c1028e27e0b3021598bc41dc
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-10-27 14:30:55 +0100

    fix test desc of mcdc13
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/cbmc-cover/mcdc13/main.c
+++ regression/cbmc-cover/mcdc13/main.c
@@ -1,11 +1,12 @@
 int main()
 {
-  _Bool A, B;
+  _Bool A, B, C;
 
   __CPROVER_input("A", A);
   __CPROVER_input("B", B);
+  __CPROVER_input("C", C);
 
-  if (A && B)
+  if (A && B && C)
   {
     /* instructions */
   }
