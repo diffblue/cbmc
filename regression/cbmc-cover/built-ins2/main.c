c3931974d7a021deb1066ff84ba4f1cfdf442790
Peter Schrammel <peter.schrammel@diffblue.com>
2016-10-27 22:22:41 +0100

    Regression tests for not instrumenting built-ins

---
+++ regression/cbmc-cover/built-ins2/main.c
@@ -0,0 +1,19 @@
+struct mystruct {
+  int x;
+  char y;
+};
+
+int main()
+{
+  struct mystruct s;
+  char c;
+  __CPROVER_input("c", c);
+
+  memset(&s,c,sizeof(struct mystruct));
+
+  if(s.y=='A')
+  {
+    return 0;
+  }
+  return 1;
+}
