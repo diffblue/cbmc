190e93e5dc962cf808affd954043bc489e2412a4
Chris Smowton <chris@smowton.net>
2016-10-28 16:15:54 +0100

    Check static array bounds, even when the underlying object is accessed via a pointer.

--- src/analyses/goto_check.cpp
+++ src/analyses/goto_check.cpp
@@ -1185,13 +1185,11 @@ void goto_checkt::bounds_check(
 
     add_guarded_claim(
       precond,
-      name+" upper bound",
+      name+" dynamic object upper bound",
       "array bounds",
       expr.find_source_location(),
       expr,
       guard);
-
-    return;
   }
 
   const exprt &size=array_type.id()==ID_array ?
