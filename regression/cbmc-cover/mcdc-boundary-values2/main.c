3fee77e659c77e4b3e4c7f2bf142f5257b75fea4
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-11-07 12:25:59 +0000

    add test cases related to boundary values by Youcheng
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/cbmc-cover/mcdc-boundary-values2/main.c
@@ -0,0 +1,12 @@
+int main()
+{
+  unsigned x, y;
+  
+  __CPROVER_input("x", x);
+  __CPROVER_input("y", y);
+  
+  if (!(x>3) && y<5) 
+    ;
+
+  return 1;
+}
---
+++ regression/cbmc-cover/mcdc-boundary-values4/main.c
@@ -0,0 +1,18 @@
+int main()
+{
+  _Bool A, B, C;
+
+
+  __CPROVER_input("cold", A);
+
+  if (A)
+  {
+  /* instructions */
+  }
+  else
+  {
+  /* instructions */
+  }
+
+  return 1;
+}
