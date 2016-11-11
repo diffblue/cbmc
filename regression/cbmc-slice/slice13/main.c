b53ac9127b561cbf5f9cb7310545002b12ffabb6
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-11-11 14:21:49 +0000

    add test case slice13
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/cbmc-slice/slice13/main.c
@@ -0,0 +1,23 @@
+#ifdef __GNUC__
+#include <assert.h>
+#include <math.h>
+#include <fenv.h>
+
+float setRoundingModeAndCast(int mode, double d) {
+  fesetround(mode);
+  return (float)d;
+}
+
+void test (int mode, double d, float result) {
+  float f = setRoundingModeAndCast(mode,d);
+  assert(f == result);
+  return;
+}
+#endif
+
+int main (void)
+{
+  // Nearer to 0x1.fffffep+127 than to 0x1.000000p+128
+  test(FE_UPWARD, 0x1.fffffe0000001p+127, +INFINITY);
+  return 1;
+}
