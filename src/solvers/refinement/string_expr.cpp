e35d93af30d8c36eec75168f76c62dfb951582c0
Romain Brenguier <romain.brenguier@gmail.com>
2016-11-21 14:56:26 +0000

    small cleaning

--- src/solvers/refinement/string_expr.cpp
+++ src/solvers/refinement/string_expr.cpp
@@ -8,14 +8,11 @@ Author: Romain Brenguier, romain.brenguier@diffblue.com
 \*******************************************************************/
 
 #include <solvers/refinement/string_expr.h>
-//#include <ansi-c/string_constant.h>
-//#include <java_bytecode/java_types.h>
 
-exprt index_zero = refined_string_typet::index_zero();
 unsigned string_exprt::next_symbol_id = 1;
 
-symbol_exprt string_exprt::fresh_symbol(const irep_idt &prefix,
-					  const typet &tp)
+symbol_exprt string_exprt::fresh_symbol
+(const irep_idt &prefix, const typet &tp)
 {
   std::ostringstream buf;
   buf << "string_refinement#" << prefix << "#" << (next_symbol_id++);
@@ -24,11 +21,8 @@ symbol_exprt string_exprt::fresh_symbol(const irep_idt &prefix,
   return symbol_exprt(name, tp);
 }
 
-constant_exprt constant_of_nat(int i,int width, typet t) {
-  return constant_exprt(integer2binary(i,width), t);
-}
-
-string_exprt::string_exprt(unsignedbv_typet char_type) : struct_exprt(refined_string_typet(char_type))
+string_exprt::string_exprt(unsignedbv_typet char_type)
+  : struct_exprt(refined_string_typet(char_type))
 {
   refined_string_typet t(char_type);
   symbol_exprt length = fresh_symbol("string_length",refined_string_typet::index_type());
