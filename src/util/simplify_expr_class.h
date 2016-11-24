b40dc2b213a940f61784aa878c2531e35476f6fc
Chris Smowton <chris@smowton.net>
2016-11-24 14:38:17 +0000

    Add option for simplify-expr to keep identical structs
    Normally it will eliminate casts from one struct type to another
    if they have like members, but this leads to type errors in
    languages like Java where they cannot be used interchangeably
    unless they also have a subtype relationship.

--- src/util/simplify_expr_class.h
+++ src/util/simplify_expr_class.h
@@ -38,6 +38,7 @@ class simplify_exprt
 public:
   explicit simplify_exprt(const namespacet &_ns):
     do_simplify_if(true),
+    keep_identical_structs(false),
     ns(_ns)
 #ifdef DEBUG_ON_DEMAND
     , debug_on(false)
@@ -54,6 +55,7 @@ public:
   }
 
   bool do_simplify_if;
+  bool keep_identical_structs;
 
   // These below all return 'true' if the simplification wasn't applicable.
   // If false is returned, the expression has changed.
