b85e29fee64cc36041c19f071bf269e7c849f709
Daniel Kroening <kroening@cs.ox.ac.uk>
2016-11-27 12:39:17 +0000

    added the usual header guard

--- src/util/signal_catcher.h
+++ src/util/signal_catcher.h
@@ -1,2 +1,15 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_SIGNAL_CATCHER_H
+#define CPROVER_SIGNAL_CATCHER_H
+
 void install_signal_catcher();
 void signal_catcher(int sig);
+
+#endif
