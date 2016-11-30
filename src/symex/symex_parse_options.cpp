edc223694a6cc9edeb190bd1ebc37e068868d533
Chris Smowton <chris@smowton.net>
2016-11-30 11:01:25 +0000

    Call get_language_options from get_goto_model

--- src/symex/symex_parse_options.cpp
+++ src/symex/symex_parse_options.cpp
@@ -177,7 +177,7 @@ int symex_parse_optionst::doit()
 
   goto_model.set_message_handler(get_message_handler());
 
-  if(goto_model(cmdline.args))
+  if(goto_model(cmdline))
     return 6;
 
   if(process_goto_program(options))
