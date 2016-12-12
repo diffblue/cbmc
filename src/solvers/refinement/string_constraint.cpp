1035bd246f2ee26bbcd1b2fdd7fffe2a41fd53a9
Romain Brenguier <romain.brenguier@gmail.com>
2016-12-12 17:01:04 +0000

    Removed previously commented code

--- src/solvers/refinement/string_constraint.cpp
+++ src/solvers/refinement/string_constraint.cpp
@@ -9,23 +9,3 @@ Author: Romain Brenguier, romain.brenguier@diffblue.com
 
 #include <solvers/refinement/string_constraint.h>
 
-
-// string_constraintt::string_constraintt(const symbol_exprt & univ, const exprt & bound_inf, const exprt & bound_sup, const exprt &prem, const exprt &body)
-//   : string_constraintt(prem,body)
-// {
-//   copy_to_operands(univ, bound_sup, bound_inf);
-// }
-
-// string_constraintt string_constraintt::with_exists(const symbol_exprt & exist, const exprt & bound_inf, const exprt & bound_sup)
-// {
-//   assert(!is_univ_quant());
-//   return string_constraintt
-//     (and_exprt(*this,
-// 	       and_exprt(binary_relation_exprt(exist, ID_ge, bound_inf),
-// 			 binary_relation_exprt(exist, ID_lt, bound_sup))));
-// }
-
-// string_constraintt string_constraintt::with_exists(const symbol_exprt & univ, const exprt & bound_sup)
-// {
-//   return with_exists(univ,refined_string_typet::index_zero(),bound_sup);
-// }
