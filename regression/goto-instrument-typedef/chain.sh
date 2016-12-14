08cf4ee55227be3959f5c34a1ba7a4c143df0d82
thk123 <thomas@diffblue.com>
2016-12-14 17:10:50 +0000

    Added initial collection of tests for typedef'd types
    Covers the basic cases of typedef-ing a normal type (int)

---
+++ regression/goto-instrument-typedef/chain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SRC=../../../src
+
+GC=$SRC/goto-cc/goto-cc
+GI=$SRC/goto-instrument/goto-instrument
+
+OPTS=$1
+NAME=${2%.c}
+
+$GC $NAME.c --function fun -o $NAME.gb
+echo $GI $OPTS $NAME.gb
+$GI $OPTS $NAME.gb
