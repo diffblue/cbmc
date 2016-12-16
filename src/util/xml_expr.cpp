298dbfb08cc57c3d554b8c34f3fae6c0ff58440b
Peter Schrammel <peter.schrammel@diffblue.com>
2016-12-16 13:57:53 +0000

    Fix _Bool XML output

--- src/util/xml_expr.cpp
+++ src/util/xml_expr.cpp
@@ -263,6 +263,15 @@ xmlt xml(
       result.set_attribute("binary", expr.is_true()?"1":"0");
       result.data=expr.is_true()?"TRUE":"FALSE";
     }
+    else if(type.id()==ID_c_bool)
+    {
+      result.name="integer";
+      result.set_attribute("c_type", "_Bool");
+      result.set_attribute("binary", expr.get_string(ID_value));
+      mp_integer b;
+      to_integer(to_constant_expr(expr), b);
+      result.data=integer2string(b);
+    }
     else
     {
       result.name="unknown";
