4f803c604e42b3b8e2285b962a37b388eaa65355
thk123 <thomas@diffblue.com>
2016-12-20 17:58:07 +0000

    Added test for spacing error that wasn't being triggered

---
+++ regression/cpp-linter/operator-spacing1/main.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************\
+
+Module: Lint Examples
+
+Author: Thomas Kiley, thomas@diffblue.com
+
+\*******************************************************************/
+
+static void fun()
+{
+  version_set = true;
+}
