6cc846a46bcee8318a29e5603f0d70fd2522526f
Chris Smowton <chris@smowton.net>
2017-01-09 12:27:58 +0000

    Add basic test for lexical scope inference
    This simply checks that a scoped block got created at PC index 7
    (which will enclose a variable scope, but this is harder to spot
    with a regex) where previously the variable would have been global.

---
+++ regression/cbmc-java/inferlexicalscope1/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+  public static void main() {
+
+    int x = 5;
+    if(x == 4) {
+      int y = x + 1;
+      ++y;
+    }
+    else {
+      int z = x + 1;
+      ++z;
+    }
+
+  }
+
+}
