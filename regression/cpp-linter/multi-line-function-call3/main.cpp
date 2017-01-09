eb4597d04a8d5719b19ac66f13fd032b6715875f
thk123 <thomas@diffblue.com>
2017-01-09 17:55:26 +0000

    Replaced regex for matching multi-line function calls
    Instead use a more complex system that finds the matching closing
    bracket and checks all the lines in between to check one parameter only
    on each line.
    
    It handles nested function calls by skipping over their whole parameter
    list (since we don't require nested functions be separated onto
    individual lines just because the outer call is. Instead these get
    checked when we get to this function call separately.

---
+++ regression/cpp-linter/multi-line-function-call3/main.cpp
@@ -0,0 +1,47 @@
+/*******************************************************************\
+
+Module: Lint Examples
+
+Author: Thomas Kiley, thomas@diffblue.com
+
+\*******************************************************************/
+
+/*******************************************************************\
+
+Function: fun
+
+ Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+static void fun()
+{
+  // Correct as for loop not function
+  for(int x=0;
+    x<10;
+    ++x)
+  {}
+
+  // Correct as an if statement not a function
+  if(a==b ||
+    c==d)
+  {
+    do_something();
+  }
+
+  // Correct as ranged based for loop not function
+  for(x:
+    list)
+  {}
+
+  // Correct since if statement
+  if(some_check(with, params)==
+    some_value)
+  {
+    do_something();
+  }
+}
