af23573fe3d2f8b51beeedc0148cf987f5018112
Matthias Güdemann <matthias.guedemann@diffblue.com>
2017-01-11 15:48:22 +0100

    Clean up Java pointer cast handling

---
+++ src/java_bytecode/java_pointer_casts.h
@@ -0,0 +1,24 @@
+/*******************************************************************\
+
+Module: JAVA Pointer Casts
+
+Author: DiffBlue
+
+\*******************************************************************/
+
+#ifndef CPROVER_JAVA_BYTECODE_JAVA_POINTER_CASTS_H
+#define CPROVER_JAVA_BYTECODE_JAVA_POINTER_CASTS_H
+
+exprt clean_deref(const exprt ptr);
+
+bool find_superclass_with_type(
+  exprt &ptr,
+  const typet &target_type,
+  const namespacet &ns);
+
+exprt make_clean_pointer_cast(
+  const exprt &ptr,
+  const typet &target_type,
+  const namespacet &ns);
+
+#endif // CPROVER_JAVA_BYTECODE_JAVA_POINTER_CASTS_H
