73a3bd0be5270deefcad3f0e41c81915a7db6802
thk123 <thomas@diffblue.com>
2017-01-13 11:32:34 +0000

    Adding (failing) tests for function pointer removal
    These tests demonstrate the different ways in which we could easily do
    better in function pointer resolution. At the moment, each example
    thinks the function pointer could be any of the 9 functions but in all
    cases can do much better (either know exactly the function or subset of
    functions)

---
+++ regression/goto-analyzer/fp-removal4/main.c
@@ -0,0 +1,30 @@
+void f1 (void) { int tk = 1; }
+void f2 (void) { int tk = 2; }
+void f3 (void) { int tk = 3; }
+void f4 (void) { int tk = 4; }
+void f5 (void) { int tk = 5; }
+void f6 (void) { int tk = 6; }
+void f7 (void) { int tk = 7; }
+void f8 (void) { int tk = 8; }
+void f9 (void) { int tk = 9; }
+
+typedef void(*void_fp)(void);
+
+void_fp fp_tbl[] = {f1, f2 ,f3};
+
+// There is a basic check that excludes all functions that aren't used anywhere
+// This ensures that check can't work in this example
+void_fp fp_tbl2[] = {f4, f5 ,f6, f7, f8, f9};
+
+void func4()
+{
+  int x = 1;
+  void_fp fp = fp_tbl[x];
+  fp();
+}
+
+void main(){
+   for(int i=0;i<3;i++){
+     func4(i,0);
+   }
+}
