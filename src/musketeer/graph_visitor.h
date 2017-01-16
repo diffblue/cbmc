6fb9cf20402c579eddbe154d536c48c3fc802791
Michael Tautschnig <mt@debian.org>
2017-01-16 08:19:26 -0500

    fixup! wmm/musketeer: Use wmm_grapht::node_indext instead of unsigned

--- src/musketeer/graph_visitor.h
+++ src/musketeer/graph_visitor.h
@@ -26,26 +26,36 @@ protected:
 
 public:
   /* computes the PT (btwn), CT (cml) and IT (cntrl) */
-  void PT(const edget &e, std::set<event_idt> &edges);
-  void CT(const edget &e, std::set<event_idt> &edges);
-  void CT_not_powr(const edget &e, std::set<event_idt> &edges);
-  void IT(const edget &e, std::set<event_idt> &edges);
+  void PT(const edget& e, std::set<unsigned>& edges);
+  void CT(const edget& e, std::set<unsigned>& edges);
+  void CT_not_powr(const edget& e, std::set<unsigned> &edges);
+  void IT(const edget& e, std::set<unsigned>& edges);
 
   void const_graph_explore(
-    event_grapht &graph, event_idt next, event_idt end,
-    std::list<event_idt> &old_path);
+    event_grapht& graph,
+    event_idt next,
+    event_idt end,
+    std::list<event_idt>& old_path);
   void graph_explore(event_grapht& graph, event_idt next, event_idt end,
-    std::list<event_idt> &old_path, std::set<event_idt> &edges);
+    std::list<event_idt>& old_path, std::set<unsigned>& edges);
   void graph_explore_BC(event_grapht& egraph, event_idt next,
-    std::list<event_idt>& old_path, std::set<event_idt>& edges, bool porw);
+    std::list<event_idt>& old_path, std::set<unsigned>& edges, bool porw);
   void graph_explore_AC(event_grapht& egraph, event_idt next,
-    std::list<event_idt>& old_path, std::set<event_idt>& edges, bool porw);
-  void graph_explore_BC(event_grapht& egraph, event_idt next,
-    std::list<event_idt>& old_path, std::set<event_idt>& edges) {
+    std::list<event_idt>& old_path, std::set<unsigned>& edges, bool porw);
+  void graph_explore_BC(
+    event_grapht& egraph, event_idt next,
+    std::list<event_idt>& old_path,
+    std::set<unsigned>& edges)
+  {
     graph_explore_BC(egraph, next, old_path, edges, false);
   }
-  void graph_explore_AC(event_grapht& egraph, event_idt next,
-    std::list<event_idt>& old_path, std::set<event_idt>& edges) {
+
+  void graph_explore_AC(
+    event_grapht& egraph,
+    event_idt next,
+    std::list<event_idt>& old_path,
+    std::set<unsigned>& edges)
+  {
     graph_explore_AC(egraph, next, old_path, edges, false);
   }
 
