0887a19a8809e7e6e3acca535a52adf85da6eba9
Chris Smowton <chris@smowton.net>
2017-02-03 09:44:44 +0000

    Implement function-only coverage
    This adds support for this-function-only coverage instrumentation.
    Some function renaming and interface improvement happens at the
    same time, in particular altering how no-trivial-tests is applied.
    Adapted from merge commit e7061a0208cde20ec1fe5b582594532e01ce11ab
    and subsequent fixup d462a7076f2d46bcc1bcee6e70186c76de5e36a8 which
    appear to have introduced this reworking as part of a merge.
    
    Original commit message was "consolidate integration with cover",
    committed by @cristina-david

--- src/goto-instrument/cover.h
+++ src/goto-instrument/cover.h
@@ -22,11 +22,10 @@ class message_handlert;
 class coverage_goalst
 {
 public:
-  coverage_goalst();
   static coverage_goalst get_coverage_goals(const std::string &coverage,
                                       message_handlert &message_handler);
-  void set_goals(source_locationt goal);
-  bool is_existing_goal(source_locationt source_location);
+  void add_goal(source_locationt goal);
+  bool is_existing_goal(source_locationt source_location) const;
   void set_no_trivial_tests(const bool trivial);
   const bool get_no_trivial_tests();
 
@@ -46,19 +45,30 @@ bool consider_goals(
 
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
+  goto_functionst &goto_functions,
+  coverage_criteriont,
+  bool function_only=false);
+
+void instrument_cover_goals(
+  const symbol_tablet &symbol_table,
   goto_programt &goto_program,
   coverage_criteriont,
-  coverage_goalst &goals);
+  bool function_only=false);
 
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
   goto_functionst &goto_functions,
   coverage_criteriont,
-  coverage_goalst &goals);
+  const coverage_goalst &goals,
+  bool function_only=false,
+  bool ignore_trivial=false);
 
 void instrument_cover_goals(
   const symbol_tablet &symbol_table,
-  goto_functionst &goto_functions,
-  coverage_criteriont);
+  goto_programt &goto_program,
+  coverage_criteriont,
+  const coverage_goalst &goals,
+  bool function_only=false,
+  bool ignore_trivial=false);
 
 #endif // CPROVER_GOTO_INSTRUMENT_COVER_H
