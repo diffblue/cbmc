diffblue/cbmc

commit c1a34930effd3c9886ab20055787fe75d44d6d3f

Correct java type width handling
Use `.get_width()` instead of `ID_width` and split a complex statement in Java
typecheck.

src/java_bytecode/java_bytecode_typecheck_expr.cpp
@@ -127,7 +127,8 @@ void java_bytecode_typecheckt::typecheck_expr_java_string_literal(exprt &expr)
   escape_non_alnum(escaped);
   identifier_str << "java::java.lang.String.Literal." << escaped;
   // Avoid naming clashes by virtue of escaping:
-  size_t unique_num=++(escaped_string_literal_count[identifier_str.str()]);
+  size_t unique_num=escaped_string_literal_count[identifier_str.str()];
+  unique_num++;
   if(unique_num!=1)
     identifier_str << unique_num;
 

src/java_bytecode/java_types.cpp
@@ -448,24 +448,24 @@ char java_char_from_type(const typet &type)
 
   if(id==ID_signedbv)
   {
-    const size_t width(type.get_unsigned_int(ID_width));
-    if(java_int_type().get_unsigned_int(ID_width) == width)
+    const size_t width=to_signedbv_type(type).get_width();
+    if(to_signedbv_type(java_int_type()).get_width()==width)
       return 'i';
-    else if(java_long_type().get_unsigned_int(ID_width) == width)
+    else if(to_signedbv_type(java_long_type()).get_width()==width)
       return 'l';
-    else if(java_short_type().get_unsigned_int(ID_width) == width)
+    else if(to_signedbv_type(java_short_type()).get_width()==width)
       return 's';
-    else if(java_byte_type().get_unsigned_int(ID_width) == width)
+    else if(to_signedbv_type(java_byte_type()).get_width()==width)
       return 'b';
   }
   else if(id==ID_unsignedbv)
     return 'c';
   else if(id==ID_floatbv)
   {
-    const size_t width(type.get_unsigned_int(ID_width));
-    if(java_float_type().get_unsigned_int(ID_width) == width)
+    const size_t width(to_floatbv_type(type).get_width());
+    if(to_floatbv_type(java_float_type()).get_width()==width)
       return 'f';
-    else if(java_double_type().get_unsigned_int(ID_width) == width)
+    else if(to_floatbv_type(java_double_type()).get_width()==width)
       return 'd';
   }
   else if(id==ID_c_bool)

commit 245b6ee245e842873f2377c5dce6f0bd8c630e14

Restore string literal symbol name escaping
c1a34930effd3c9886ab20055787fe75d44d6d3f accidentally disabled use
of the escaped_string_literal_count map. This restores the original
behaviour and adds a comment to dissuade repeating the same mistake.

src/java_bytecode/java_bytecode_typecheck_expr.cpp
@@ -128,8 +128,8 @@ void java_bytecode_typecheckt::typecheck_expr_java_string_literal(exprt &expr)
   escape_non_alnum(escaped);
   identifier_str << "java::java.lang.String.Literal." << escaped;
   // Avoid naming clashes by virtue of escaping:
-  size_t unique_num=escaped_string_literal_count[identifier_str.str()];
-  unique_num++;
+  // NOTE this increments the count stored in the map.
+  size_t unique_num=++(escaped_string_literal_count[identifier_str.str()]);
   if(unique_num!=1)
     identifier_str << unique_num;
 