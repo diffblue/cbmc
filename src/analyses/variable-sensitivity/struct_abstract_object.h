984e706e9ecd2f15a47ab8007d2783093ce34267
thk123 <thomas.kiley@diffblue.com>
2017-02-21 18:04:14 +0000

    Adding a 2 element implemention of sturct abstract object

--- src/analyses/variable-sensitivity/struct_abstract_object.h
+++ src/analyses/variable-sensitivity/struct_abstract_object.h
@@ -9,7 +9,11 @@
 #ifndef CPROVER_ANALYSES_VARIABLE_SENSITIVITY_STRUCT_ABSTRACT_OBJECT_H
 #define CPROVER_ANALYSES_VARIABLE_SENSITIVITY_STRUCT_ABSTRACT_OBJECT_H
 
+#include <stack>
 #include <analyses/variable-sensitivity/abstract_object.h>
+
+class abstract_environmentt;
+class member_exprt;
 
 class struct_abstract_objectt:public abstract_objectt
 {
@@ -21,6 +25,15 @@ public:
 
   CLONE
   MERGE(abstract_objectt)
+
+  // struct interface
+  virtual abstract_object_pointert read_component(
+    const abstract_environmentt &environment, const member_exprt &member_expr);
+
+  virtual sharing_ptrt<struct_abstract_objectt> write_component(
+    const abstract_environmentt &environment,
+    const std::stack<exprt> stack,
+    const member_exprt &member_expr);
 };
 
 #endif // CPROVER_ANALYSES_VARIABLE_SENSITIVITY_STRUCT_ABSTRACT_OBJECT_H
