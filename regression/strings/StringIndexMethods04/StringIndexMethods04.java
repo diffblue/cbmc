c94a19350a49b4aea3fb618890adc489c932e86c
Lucas Cordeiro <lucasccordeiro@gmail.com>
2017-02-22 14:23:22 +0000

     added string searching methods indexOf and lastIndexOf
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/strings/StringIndexMethods04/StringIndexMethods04.java
@@ -0,0 +1,8 @@
+public class StringIndexMethods04
+{
+   public static void main(String[] args)
+   {
+      String letters = "automatictestcasegenerationatdiffblue";
+      assert letters.indexOf("diffblue")==28;
+   }
+}
