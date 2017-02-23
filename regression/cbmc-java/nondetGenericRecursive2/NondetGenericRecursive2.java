3cde10264871561a168d0484a87b6d6067666adb
reuk <reuben.thomas@me.com>
2017-02-23 16:32:22 +0000

    Add nondetGenericRecursive2 test

---
+++ regression/cbmc-java/nondetGenericRecursive2/NondetGenericRecursive2.java
@@ -0,0 +1,24 @@
+import org.cprover.CProver;
+
+class A
+{
+}
+
+class B
+{
+  A a;
+}
+
+class C
+{
+  B b;
+}
+
+class NondetGenericRecursive2
+{
+  static void foo()
+  {
+    C c = CProver.nondetWithoutNull();
+    assert c != null;
+  }
+}
