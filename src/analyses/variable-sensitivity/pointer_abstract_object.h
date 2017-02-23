962378e745b4bae29d0580e9ad849d6adc83701b
thk123 <thomas.kiley@diffblue.com>
2017-02-23 14:00:28 +0000

    Basic implementations for abstract pointer and abstract array

--- src/analyses/variable-sensitivity/pointer_abstract_object.h
+++ src/analyses/variable-sensitivity/pointer_abstract_object.h
@@ -8,10 +8,14 @@
 #ifndef CPROVER_ANALYSES_VARIABLE_SENSITIVITY_POINTER_ABSTRACT_OBJECT_H
 #define CPROVER_ANALYSES_VARIABLE_SENSITIVITY_POINTER_ABSTRACT_OBJECT_H
 
+
+#include <stack>
+
 #include <analyses/variable-sensitivity/abstract_object.h>
 
 class typet;
 class constant_exprt;
+class abstract_environmentt;
 
 class pointer_abstract_objectt:public abstract_objectt
 {
@@ -23,6 +27,16 @@ public:
 
   CLONE
   MERGE(abstract_objectt)
+
+  // pointer interface
+  virtual abstract_object_pointert read_dereference(
+    const abstract_environmentt &env) const;
+
+  virtual sharing_ptrt<pointer_abstract_objectt> write_dereference(
+    abstract_environmentt &environment,
+    const std::stack<exprt> stack,
+    const abstract_object_pointert value,
+    bool merging_write);
 };
 
 #endif // CPROVER_ANALYSES_VARIABLE_SENSITIVITY_POINTER_ABSTRACT_OBJECT_H
