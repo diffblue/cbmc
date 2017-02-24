591a8db43bd15a96fcb6a6a5c8df4fd0fbac9eb6
Joel Allred <joel.allred@diffblue.com>
2017-02-24 17:36:40 +0000

    Restructuration of tests on strings
    Add test java_intern
    Adapt test.desc to new output format
    Make more robust java tests.
    Add test for parseint.
    
    Correction in the hacks to use refined strings in C programs
    
    Add smoke tests for java string support
    Quick set of tests.
    Created by modifying the existing development tests.
    Longer tests are run when using 'make testall'.
    Format of java files was adapted.
    No change to the validation tests (written by Lucas Cordeiro).
    
    Update smoke tests
    
    Add a series of performance tests that check that the negation of the
    assertions found in the smoke tests indeed fail.
    
    Update java_char_array_init/test_init.class
    
    Add first fixed_bugs test
    For bug #95
    
    Update test description with new option name
    String refinement option name has changed to --refine-strings
    
    Formatting in cprover-string-hack.h
    
    Move smoke tests to regression folder.
    
    Move fixed bugs tests to strings folder
    
    Move string perfomance tests to strings directory
    
    These are not really performance tests, only tests that are longer than
    the smoke tests.
    
    Adapt Makefile for new location of smoke tests

---
+++ regression/strings-smoke-tests/java_char_array/test_char_array.java
@@ -0,0 +1,13 @@
+public class test_char_array
+{
+   public static void main(/*String[] argv*/)
+   {
+      String s = "abc";
+      char [] str = s.toCharArray();
+      char c = str[2];
+      char a = s.charAt(0);
+      assert(str.length == 3);
+      assert(a == 'a');
+      assert(c == 'c');
+   }
+}
