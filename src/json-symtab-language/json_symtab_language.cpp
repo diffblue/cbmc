b4d4a0f556a9aa16c4e490082d899766d5c0211b
Chris Smowton <chris@smowton.net>
2017-02-24 16:20:53 +0000

    Implement external symbol table language

--- src/json-symtab-language/json_symtab_language.cpp
+++ src/json-symtab-language/json_symtab_language.cpp
@@ -9,21 +9,37 @@ Author: Chris Smowton, chris.smowton@diffblue.com
 \*******************************************************************/
 
 #include "json_symtab_language.h"
+#include <json/json_parser.h>
+#include <util/json_symbol_table.h>
 
 bool json_symtab_languaget::parse(
   std::istream &instream,
   const std::string &path)
 {
-  return true;
+  return parse_json(
+    instream,
+    path,
+    get_message_handler(),
+    parsed_json_file);
 }
 
 bool json_symtab_languaget::typecheck(
   symbol_tablet &symbol_table,
   const std::string &module)
 {
-  return true;
+  try
+  {
+    symbol_table_from_json(parsed_json_file, symbol_table);
+    return false;
+  }
+  catch(const std::string &str)
+  {
+    error() << "json_symtab_languaget::typecheck: " << str << eom;
+    return true;
+  }
 }
 
 void json_symtab_languaget::show_parse(std::ostream &out)
 {
+  parsed_json_file.output(out);
 }
