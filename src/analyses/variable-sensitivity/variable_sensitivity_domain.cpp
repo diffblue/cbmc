753832933c47d3e9fccbcbe972aa3e426be72f89
thk123 <thomas.kiley@diffblue.com>
2017-02-28 14:18:35 +0000

    Changed the pointer abstract object to store an expression
    Before there was a problem where if we stored an abstract object that
    get modified, as this would cause a copy, the pointer would point at the
    old value. To resolve, just store an expression representing what it is
    pointing to. This can then be re-evaluated by the enviroment to find its
    current possible values.

--- src/analyses/variable-sensitivity/variable_sensitivity_domain.cpp
+++ src/analyses/variable-sensitivity/variable_sensitivity_domain.cpp
@@ -57,7 +57,8 @@ void variable_sensitivity_domaint::transform(
     abstract_object_pointert top_object=
       abstract_state.abstract_object_factory(
         to_code_dead(instruction.code).symbol().type(), true);
-    abstract_state.assign(to_code_dead(instruction.code).symbol(), top_object);
+    abstract_state.assign(
+      to_code_dead(instruction.code).symbol(), top_object, ns);
     }
     break;
 
@@ -67,7 +68,7 @@ void variable_sensitivity_domaint::transform(
 
       // TODO : check return values
       abstract_object_pointert r = abstract_state.eval(inst.rhs(), ns);
-      abstract_state.assign(inst.lhs(), r);
+      abstract_state.assign(inst.lhs(), r, ns);
     }
     break;
 
