5150ecae39053328aba8756907fda08f02947c2b
Chris Smowton <chris@smowton.net>
2017-03-01 15:52:54 +0000

    Add parameter-indices to util Makefile

--- src/util/parameter_indices.h
+++ src/util/parameter_indices.h
@@ -1,9 +1,18 @@
-#ifndef PARAMETER_INDICES_H
-#define PARAMETER_INDICES_H
+/*******************************************************************\
+
+Module: Parameter indexing
+
+Author: Chris Smowton, chris.smowton@diffblue.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_UTIL_PARAMETER_INDICES_H
+#define CPROVER_UTIL_PARAMETER_INDICES_H
 
 #include <unordered_map>
 #include "std_types.h"
 
-std::map<irep_idt,std::size_t> get_parameter_indices(const code_typet& fn_type);
+std::map<irep_idt, std::size_t>
+  get_parameter_indices(const code_typet &fn_type);
 
 #endif
