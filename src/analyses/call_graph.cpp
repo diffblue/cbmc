0601811af2692c52933ec9b285054eaa14b5c34a
Marek Trtik <trtik@diffblue.com>
2017-03-03 11:27:47 +0000

    Extensions of call-graph: recording call-sites for edges; added algorithms operating on it, like inversion, inverted topological order, search for leaf callees, basic caller/caller queries.

--- src/analyses/call_graph.cpp
+++ src/analyses/call_graph.cpp
@@ -6,10 +6,12 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include "call_graph.h"
 #include <util/std_expr.h>
 #include <util/xml.h>
+#include <algorithm>
+#include <iterator>
 
-#include "call_graph.h"
 
 /*******************************************************************\
 
@@ -70,10 +72,18 @@ void call_grapht::add(
     {
       const exprt &function_expr=to_code_function_call(i_it->code).function();
       if(function_expr.id()==ID_symbol)
-        add(function, to_symbol_expr(function_expr).get_identifier());
+        add(function, to_symbol_expr(function_expr).get_identifier(), {i_it});
     }
   }
 }
+
+void call_grapht::swap(call_grapht &other)
+{
+  std::swap(graph,other.graph);
+  std::swap(map_from_edges_to_call_locations,
+            other.map_from_edges_to_call_locations);
+}
+
 
 /*******************************************************************\
 
@@ -94,6 +104,16 @@ void call_grapht::add(
   graph.insert(std::pair<irep_idt, irep_idt>(caller, callee));
 }
 
+void call_grapht::add(const irep_idt &caller, const irep_idt &callee,
+  const map_from_edges_to_call_locationst::mapped_type &call_sites)
+{
+  add(caller,callee);
+  std::copy(
+    call_sites.cbegin(),call_sites.cend(),
+    std::back_inserter(map_from_edges_to_call_locations[{caller,callee}]));
+}
+
+
 /*******************************************************************\
 
 Function: call_grapht::output_dot
@@ -108,18 +128,27 @@ Function: call_grapht::output_dot
 
 void call_grapht::output_dot(std::ostream &out) const
 {
-  out << "digraph call_graph {\n";
-
+  out << "digraph call_graph {\n"
+      << "  node [fontsize=12 shape=box];\n";
   for(const auto &edge : graph)
   {
     out << "  \"" << edge.first << "\" -> "
         << "\"" << edge.second << "\" "
-        << " [arrowhead=\"vee\"];"
-        << "\n";
+        << " [label=\"{";
+    bool first=true;
+    for(const auto instr_it :
+        get_map_from_edges_to_call_locations().at({edge.first,edge.second}))
+    {
+      if (!first)
+        out << ",";
+      out << instr_it->location_number;
+      first=false;
+    }
+    out << "}\"];\n";
   }
-
   out << "}\n";
 }
+
 
 /*******************************************************************\
 
@@ -163,4 +192,139 @@ void call_grapht::output_xml(std::ostream &out) const
     xmlt::escape_attribute(id2string(edge.second), out);
     out << "\">\n";
   }
+}
+
+
+call_grapht::call_edges_ranget
+call_grapht::out_edges(irep_idt const&  caller) const
+{
+  return graph.equal_range(caller);
+}
+
+
+void  inverted_partial_topological_order(
+    call_grapht const&  call_graph,
+    irep_idt const&  start_function,
+    std::unordered_set<irep_idt,dstring_hash>&  processed_functions,
+    std::vector<irep_idt>&  output
+    )
+{
+  if (processed_functions.count(start_function) != 0ULL)
+    return;
+  processed_functions.insert(start_function);
+  call_grapht::call_edges_ranget const  range =
+      call_graph.out_edges(start_function);
+  for (auto  it = range.first; it != range.second; ++it)
+    inverted_partial_topological_order(
+          call_graph,
+          it->second,
+          processed_functions,
+          output
+          );
+  output.push_back(start_function);
+}
+
+void get_inverted_topological_order(
+  call_grapht const& call_graph,
+  goto_functionst const& functions,
+  std::vector<irep_idt>& output)
+{
+  std::unordered_set<irep_idt,dstring_hash>  processed;
+  for (auto const&  elem : functions.function_map)
+    inverted_partial_topological_order(
+      call_graph,
+      elem.first,
+      processed,
+      output);
+}
+
+bool  exists_direct_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee
+    )
+{
+  call_grapht::call_edges_ranget const  range =
+      call_graph.out_edges(caller);
+  for (auto  it = range.first; it != range.second; ++it)
+    if (callee == it->second)
+      return true;
+  return false;
+}
+
+bool  exists_direct_or_indirect_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee,
+    std::unordered_set<irep_idt,dstring_hash>&  ignored_functions
+    )
+{
+  if (ignored_functions.count(caller) != 0UL)
+    return false;
+  ignored_functions.insert(caller);
+  if (exists_direct_call(call_graph,caller,callee))
+    return ignored_functions.count(callee) == 0UL;
+  call_grapht::call_edges_ranget const  range =
+      call_graph.out_edges(caller);
+  for (auto  it = range.first; it != range.second; ++it)
+    if (exists_direct_or_indirect_call(
+          call_graph,
+          it->second,
+          callee,
+          ignored_functions
+          ))
+      return true;
+  return false;
+}
+
+bool  exists_direct_or_indirect_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee
+    )
+{
+  std::unordered_set<irep_idt,dstring_hash>  ignored;
+  return exists_direct_or_indirect_call(call_graph,caller,callee,ignored);
+}
+
+void compute_inverted_call_graph(
+    call_grapht const&  original_call_graph,
+    call_grapht&  output_inverted_call_graph
+    )
+{
+  assert(output_inverted_call_graph.graph.empty());
+  for (auto const&  elem : original_call_graph.graph)
+    output_inverted_call_graph.add(
+      elem.second,elem.first,
+      original_call_graph.get_map_from_edges_to_call_locations().at(
+        {elem.first,elem.second}));
+}
+
+void find_leaves_bellow_function(
+    call_grapht const&  call_graph,
+    irep_idt const&  function,
+    std::unordered_set<irep_idt,dstring_hash>&  to_avoid,
+    std::unordered_set<irep_idt,dstring_hash>&  output
+    )
+{
+  if (to_avoid.count(function) != 0UL)
+    return;
+  to_avoid.insert(function);
+  call_grapht::call_edges_ranget const  range =
+      call_graph.out_edges(function);
+  if (range.first == range.second)
+    output.insert(function);
+  else
+    for (auto  it = range.first; it != range.second; ++it)
+      find_leaves_bellow_function(call_graph,it->second,to_avoid,output);
+}
+
+void find_leaves_bellow_function(
+    call_grapht const&  call_graph,
+    irep_idt const&  function,
+    std::unordered_set<irep_idt,dstring_hash>&  output
+    )
+{
+  std::unordered_set<irep_idt,dstring_hash>  to_avoid;
+  find_leaves_bellow_function(call_graph,function,to_avoid,output);
 }
--- src/analyses/call_graph.h
+++ src/analyses/call_graph.h
@@ -11,6 +11,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <iosfwd>
 #include <map>
+#include <vector>
+#include <unordered_set>
 
 #include <goto-programs/goto_functions.h>
 
@@ -27,11 +29,164 @@ public:
   typedef std::multimap<irep_idt, irep_idt> grapht;
   grapht graph;
 
+  void swap(call_grapht &other);
+
   void add(const irep_idt &caller, const irep_idt &callee);
 
+  /**
+   * The type provides a mapping from edges of the call-graph to particular
+   * instructions in the caller GOTO programs, where the calls are performed.
+   */
+  typedef std::map<
+            std::pair<irep_idt, irep_idt>,
+            std::vector<goto_programt::instructionst::const_iterator> >
+          map_from_edges_to_call_locationst;
+
+  const map_from_edges_to_call_locationst &
+  get_map_from_edges_to_call_locations() const
+  { return map_from_edges_to_call_locations; }
+
+  void add(const irep_idt &caller, const irep_idt &callee,
+           const map_from_edges_to_call_locationst::mapped_type &call_sites);
 protected:
   void add(const irep_idt &function,
            const goto_programt &body);
+
+public:
+  /**
+   * Each element of the call graph is a pair where the first element is a
+   * name of a caller function and the second element is the name of a called
+   * function. So, an iterator to any such element is actually an iterator to
+   * an edge of the call graph.
+   */
+  typedef grapht::const_iterator  call_edge_iteratort;
+
+  /**
+   * Since a call graph is implemented as a multimap, we use ranges
+   * of call graph edges to represent out-edges from a node (a function)
+   */
+  typedef std::pair<call_edge_iteratort,call_edge_iteratort>  call_edges_ranget;
+
+  /**
+   * It returns a range of edges represented by a pair of iterators. The
+   * first iterator refers to the first edge in the range and the second
+   * iterator the end of the range (that edge does NOT belong to the range).
+   *
+   * Usage:
+   *  irep_idt const  caller_name = "foo";
+   *  call_grapht::call_edges_ranget const  range =
+   *      my_call_graph.out_edges(caller_name);
+   *  std::cout << "Callees of " << caller_name << " are: ";
+   *  for (auto  it = range.first; it != range.second; ++it)
+   *    std::cout << it->second << ", ";
+   */
+  call_edges_ranget  out_edges(irep_idt const&  caller) const;
+
+private:
+  map_from_edges_to_call_locationst map_from_edges_to_call_locations;
 };
+
+/*******************************************************************\
+
+Function: inverted_partial_topological_order
+
+  Inputs: See purpose
+
+ Outputs: See purpose
+
+ Purpose:
+
+For DAG call graphs it computes an inverted topological order of all
+functions in the call graph. Otherwise, it computes only a partial
+inverted topological order (all loops are broken at some (randomly)
+chosen edge to get a DAG). The topolocical order is stored in the
+'output' vector.
+
+Since the algorithm is implemented using DFS, those 'breaks' are
+implemented naturally by a set of processed (vidited) functions.
+
+The function actually performs only one DFS from a passed 'start_function'.
+So, to get whole inverted (partial) topological order of all functions in
+the call graph, this function has to be called for all functions in the
+program.
+
+NOTE: The order is 'inverted'. It means that
+
+Typical usage:
+ // Let's assume there is 'goto_modelt GM' and 'call_grapht CG'
+ std::vector<irep_idt>  result; // Here we will store the topological order.
+ {
+   std::unordered_set<irep_idt,dstring_hash>  processed;
+   for (auto const&  elem : GM.goto_functions.function_map)
+     partial_topological_order(CG,elem.first,processed,result);
+   // Now we reverse the result to get the classic (partial)
+   // topological order instead of the inverted one.
+   std::reverse(result.begin(),result.end());
+ }
+ std::cout << "A (partial) topological order of my call graph is: ";
+ for (irep_idt const&  fn_name : result)
+   std::cout << fn_name << ", ";
+
+\*******************************************************************/
+void  inverted_partial_topological_order(
+    call_grapht const&  call_graph,
+    irep_idt const&  start_function,
+    std::unordered_set<irep_idt,dstring_hash>&  processed_functions,
+    std::vector<irep_idt>&  output
+    );
+
+void get_inverted_topological_order(
+  call_grapht const& call_graph,
+  goto_functionst const& functions,
+  std::vector<irep_idt>& output);
+
+bool  exists_direct_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee
+    );
+
+bool  exists_direct_or_indirect_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee,
+    std::unordered_set<irep_idt,dstring_hash>&  ignored_functions
+    );
+
+bool  exists_direct_or_indirect_call(
+    call_grapht const&  call_graph,
+    irep_idt const&  caller,
+    irep_idt const&  callee
+    );
+
+void compute_inverted_call_graph(
+    call_grapht const&  original_call_graph,
+    call_grapht&  output_inverted_call_graph
+    );
+
+void find_leaves_bellow_function(
+    call_grapht const&  call_graph,
+    irep_idt const&  function,
+    std::unordered_set<irep_idt,dstring_hash>&  to_avoid,
+    std::unordered_set<irep_idt,dstring_hash>&  output
+    );
+
+void find_leaves_bellow_function(
+    call_grapht const&  call_graph,
+    irep_idt const&  function,
+    std::unordered_set<irep_idt,dstring_hash>&  output
+    );
+
+/**
+ * The "callee" must be a DIRECT callee of the "caller" in the "call_graph".
+ */
+inline const std::vector<goto_programt::instructionst::const_iterator> &
+get_call_sites(
+  call_grapht const&  call_graph,
+  irep_idt const&  caller,
+  irep_idt const&  callee)
+{
+  return call_graph.get_map_from_edges_to_call_locations().at({caller,callee});
+}
 
 #endif // CPROVER_ANALYSES_CALL_GRAPH_H
