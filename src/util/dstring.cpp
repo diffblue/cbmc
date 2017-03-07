8b1b97bb3ebb7ac0263b6621915cab6727d31490
reuk <reuben.thomas@me.com>
2017-03-07 22:21:54 +0000

    Fix build against master, remove unnecessary files
    CMake can generate Visual Studio projects by default, and CPack can
    create distributable packages.

--- src/util/dstring.cpp
+++
@@ -1,9 +0,0 @@
-/*******************************************************************\
-
-Module: Container for C-Strings
-
-Author: Daniel Kroening, kroening@kroening.com
-
-\*******************************************************************/
-
-#include "dstring.h"
--- src/util/irep_hash.cpp
+++
@@ -1,9 +0,0 @@
-/*******************************************************************\
-
-Module: irep hash functions
-
-Author: Michael Tautschnig, mt@eecs.qmul.ac.uk
-
-\*******************************************************************/
-
-#include "irep_hash.h"
