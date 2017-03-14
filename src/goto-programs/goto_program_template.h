731f5317a073894c48cc36b192ca27cf56b34f04
reuk <reuben.thomas@me.com>
2017-03-14 13:52:29 +0000

    Reduce repetition of goto-program instructions

--- src/goto-programs/goto_program_template.h
+++ src/goto-programs/goto_program_template.h
@@ -23,28 +23,11 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/source_location.h>
 #include <util/std_expr.h>
 
-typedef enum
+enum goto_program_instruction_typet
 {
-  NO_INSTRUCTION_TYPE=0,
-  GOTO=1,           // branch, possibly guarded
-  ASSUME=2,         // non-failing guarded self loop
-  ASSERT=3,         // assertions
-  OTHER=4,          // anything else
-  SKIP=5,           // just advance the PC
-  START_THREAD=6,   // spawns an asynchronous thread
-  END_THREAD=7,     // end the current thread
-  LOCATION=8,       // semantically like SKIP
-  END_FUNCTION=9,   // exit point of a function
-  ATOMIC_BEGIN=10,  // marks a block without interleavings
-  ATOMIC_END=11,    // end of a block without interleavings
-  RETURN=12,        // set function return value (no control-flow change)
-  ASSIGN=13,        // assignment lhs:=rhs
-  DECL=14,          // declare a local variable
-  DEAD=15,          // marks the end-of-live of a local variable
-  FUNCTION_CALL=16, // call a function
-  THROW=17,         // throw an exception
-  CATCH=18          // catch an exception
-} goto_program_instruction_typet;
+#define GOTO_PROG_INSTR(instr) instr,
+#include "goto_program_instructions.def"
+};
 
 std::ostream &operator<<(std::ostream &, goto_program_instruction_typet);
 
