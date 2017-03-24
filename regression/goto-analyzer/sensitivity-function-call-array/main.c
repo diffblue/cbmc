e71a2e7199c3394e4d3596a3ce5b85216112fcb9
thk123 <thomas@diffblue.com>
2017-03-24 14:16:00 +0000

    Adding tests for function calls
    Some of the code is commented out to disable unsupported features from the tests
    
    Disabling parts of the test dependent on pointer arithmetic

---
+++ regression/goto-analyzer/sensitivity-function-call-array/main.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+
+int *bar(int *arr_unmodified, int *arr_modified);
+
+int main(int argc, char *argv[])
+{
+  int arr_x[] = {1, 2, 3, 4};
+  int arr_y[] = {1, 2, 3, 4};
+  int *p2arr = arr_x;
+
+  p2arr = bar(arr_x, arr_y);
+  assert(*arr_y==2);
+  // assert(arr_y[1]==3);
+
+  assert(p2arr==arr_y);
+  assert(*p2arr==2);
+  // assert(p2arr[1]==3);
+}
+
+int *bar(int *arr_unmodified, int *arr_modified)
+{
+  assert(*arr_unmodified==1);
+  // assert(arr_unmodified[1]==2);
+
+  (*arr_modified) += 1;
+  // arr_modified[1] = 3;
+
+  assert(*arr_modified==2);
+  // assert(arr_modified[1]==3);
+
+  return arr_modified;
+}
---
+++ regression/goto-analyzer/sensitivity-function-call-primitive/main.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+int bar(int other)
+{
+  assert(other==4);
+  return other + 1;
+}
+
+int main(int argc, char *argv[])
+{
+  int x=3;
+  int y=bar(x+1);
+  assert(y==5);
+}
