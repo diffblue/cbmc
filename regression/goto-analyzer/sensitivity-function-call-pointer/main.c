82f3e275559f59c2b71bdcfe894800c36426dd8a
thk123 <thomas@diffblue.com>
2017-03-24 14:16:00 +0000

    Adding tests for function calls
    Some of the code is commented out to disable unsupported features from the tests
    
    Disabling parts of the test dependent on pointer arithmetic

---
+++ regression/goto-analyzer/sensitivity-function-call-pointer/main.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+
+int *bar(int *unmodified, int *modifed);
+
+int main(int argc, char *argv[])
+{
+  int x=3;
+  int y=4;
+  int *p2x=&x;
+  p2x = bar(&x, &y);
+  assert(y==5);
+  assert(p2x==&y);
+  assert(*p2x==5);
+}
+
+int *bar(int *unmodified, int *modifed)
+{
+  assert(*unmodified==3);
+
+  (*modifed) += 1;
+
+  assert(*modifed==5);
+
+  return modifed;
+}
