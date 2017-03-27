3050c53e86d5243406029a175dc19f280d7ba872
thk123 <thomas@diffblue.com>
2017-03-27 15:17:38 +0100

    Don't stop when recursion found
    Since we are finding a fixed point we do the same things we do for loops
    when we find a recurssive function - we descend into it and carry on.
    
    Previously we were never doing the transforms from the recurssive
    function call to the start of the function meaning we would not realise
    it can be called with multiple parameters. Further, we weren't doing the
    transform from after the recursive function call onwards, meaning for
    some inputs it would incorrectly conclude the function never returned.
    
    Now we just do the transforms ignoring the fact it could be recursive.
    For the constants domain this is OK as a fixed point will be found
    quickly. For other domains we will need to implement some sort of
    widening.
    
    Included a test for recursion.

---
+++ regression/goto-analyzer/sensitivity-function-call-recursive/main.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+
+int bar(int other)
+{
+  if(other > 0)
+  {
+    int value = bar(other - 1);
+    return value + 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int bar_clean(int other)
+{
+  if(other > 0)
+  {
+    int value = bar_clean(other - 1);
+    return value + 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int fun(int changing, int constant)
+{
+  if(changing > 0)
+  {
+    int value = fun(changing - 1, constant);
+    return value;
+  }
+  else
+  {
+    return constant;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int x=3;
+  int y=bar(x+1);
+  assert(y==4); // Unknown in the constants domain
+
+  int y2 = bar(0);
+  assert(y2==0); // Unknown since we are not sensitive to call domain
+
+  int z = bar_clean(0);
+  assert(z==0); // Unknown as the function has parameter as top
+
+  int w = fun(5, 18);
+  assert(w==18);
+}
