353d7d06cd72aacc4fb6e22419d6d18421f169b0
Romain Brenguier <romain.brenguier@gmail.com>
2017-04-20 13:10:38 +0100

    Merging string and character preprocess as one argument of bytecode conversion
    Removing add_string_type declaration, not used anymore

--- src/java_bytecode/java_bytecode_convert_method.h
+++ src/java_bytecode/java_bytecode_convert_method.h
@@ -12,6 +12,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/symbol_table.h>
 #include <util/message.h>
 #include <util/safe_pointer.h>
+#include "java_string_library_preprocess.h"
 
 #include "java_bytecode_parse_tree.h"
 #include "ci_lazy_methods.h"
@@ -24,14 +25,16 @@ void java_bytecode_convert_method(
   symbol_tablet &symbol_table,
   message_handlert &message_handler,
   size_t max_array_length,
-  safe_pointer<ci_lazy_methodst> lazy_methods);
+  safe_pointer<ci_lazy_methodst> lazy_methods,
+  const java_string_library_preprocesst &string_preprocess);
 
 inline void java_bytecode_convert_method(
   const symbolt &class_symbol,
   const java_bytecode_parse_treet::methodt &method,
   symbol_tablet &symbol_table,
   message_handlert &message_handler,
-  size_t max_array_length)
+  size_t max_array_length,
+  const java_string_library_preprocesst &string_preprocess)
 {
   java_bytecode_convert_method(
     class_symbol,
@@ -39,7 +42,8 @@ inline void java_bytecode_convert_method(
     symbol_table,
     message_handler,
     max_array_length,
-    safe_pointer<ci_lazy_methodst>::create_null());
+    safe_pointer<ci_lazy_methodst>::create_null(),
+    string_preprocess);
 }
 
 void java_bytecode_convert_method_lazy(
