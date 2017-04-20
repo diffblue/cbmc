32a61d9a787fa829c3d860ccf1c413752af200d9
Cristina <cristina.david@gmail.com>
2017-04-20 11:00:46 +0100

    Set the bytecode index for the instrumentation in goto_check.cpp

--- src/analyses/goto_check.cpp
+++ src/analyses/goto_check.cpp
@@ -1702,6 +1702,11 @@ void goto_checkt::goto_check(
 
         if(!it->source_location.get_column().empty())
           i_it->source_location.set_column(it->source_location.get_column());
+
+        if(mode==ID_java &&
+           it->source_location.get_java_bytecode_index()!=irep_idt())
+          i_it->source_location.set_java_bytecode_index(
+            it->source_location.get_java_bytecode_index());
       }
 
       if(i_it->function.empty())
