3391d459c064c23ba0a1e6d5d2570ebc92d51377
thk123 <thomas@diffblue.com>
2017-04-25 14:08:49 +0100

    Basic harness for unit tests
    Adds catch.hpp - a test harness for writing and running unit tests.
    Updated the make file so that running `make test` runs all the tests. I
    removed all the tests that either didn't compile, failing on running or
    didn't terminate without some input.
    
    Added an example catch test to demonstrate it working.

---
+++ unit/catch_entry_point.cpp
@@ -0,0 +1,15 @@
+/*******************************************************************\
+
+ Module: Catch Tests
+
+ Author: DiffBlue Limited. All rights reserved.
+
+\*******************************************************************/
+#ifndef CATCH_ENTRY_POINT_H
+#define CATCH_ENTRY_POINT_H
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+#endif // CATCH_ENTRY_POINT_H
