760e1fc09fda10a855f7c0a40b7ae9d942d9d094
thk123 <thomas@diffblue.com>
2017-04-28 17:48:37 +0100

    Renamed exe and added exes to gitignore

--- unit/unit_tests.cpp
+++ unit/unit_tests.cpp
@@ -5,11 +5,6 @@
  Author: DiffBlue Limited. All rights reserved.
 
 \*******************************************************************/
-#ifndef CATCH_ENTRY_POINT_H
-#define CATCH_ENTRY_POINT_H
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-
-#endif // CATCH_ENTRY_POINT_H
