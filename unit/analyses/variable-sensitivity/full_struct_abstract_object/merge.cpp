2f5660962af0233420f33ddb8371dd748e2c1dc3
thk123 <thomas@diffblue.com>
2017-05-05 18:30:19 +0100

    Completed test suite for full struct and constant array AOs

--- unit/analyses/variable-sensitivity/full_struct_abstract_object/merge.cpp
+++ unit/analyses/variable-sensitivity/full_struct_abstract_object/merge.cpp
@@ -24,7 +24,7 @@
 
 #include <iostream>
 
-typedef constant_array_abstract_objectt::constant_array_abstract_object_pointert
+typedef constant_array_abstract_objectt::constant_array_pointert
   constant_array_abstract_object_pointert;
 
 // Util
@@ -75,6 +75,20 @@ public:
     }
 
     return result;
+  }
+
+  full_struct_abstract_objectt::constant_struct_pointert build_top_struct(
+    const typet &struct_type)
+  {
+    return std::make_shared<full_struct_abstract_objectt>(
+        struct_type, true, false);
+  }
+
+  full_struct_abstract_objectt::constant_struct_pointert build_bottom_struct(
+    const typet &struct_type)
+  {
+    return std::make_shared<full_struct_abstract_objectt>(
+        struct_type, false, true);
   }
 
 private:
@@ -136,8 +150,7 @@ SCENARIO("merge_full_struct_abstract_object",
       result=abstract_objectt::merge(op1, op2, modified);
 
       const auto &cast_result=
-        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(
-          result);
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
       THEN("The original struct AO should be returned")
       {
         // Though we may become top or bottom, the type should be unchanged
@@ -163,8 +176,7 @@ SCENARIO("merge_full_struct_abstract_object",
       result=abstract_objectt::merge(op1, op2, modified);
 
       const auto &cast_result=
-        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(
-          result);
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
       THEN("A new constant struct AO whose a component is the same and the "
         "b and c are set to top")
       {
@@ -182,6 +194,514 @@ SCENARIO("merge_full_struct_abstract_object",
 
         // Since it has modified, we definitely shouldn't be reusing the pointer
         REQUIRE_FALSE(result==op1);
+      }
+    }
+    WHEN("Merging a constant struct AO with a value "
+      "with a constant struct AO set to top")
+    {
+      auto op1=util.build_struct(val1);
+      auto op2=util.build_top_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("A new constant struct AO set to top should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, b)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, c)==nil_exprt());
+
+        // We can't reuse the abstract object as the value has changed
+        REQUIRE(result!=op1);
+      }
+    }
+    WHEN("Merging a constant struct AO with a value "
+      "with a constant struct AO set to bottom")
+    {
+      auto op1=util.build_struct(val1);
+      auto op2=util.build_bottom_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("The original const AO should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE_FALSE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==val1.op0());
+        REQUIRE(util.read_component(cast_result, b)==val1.op1());
+        REQUIRE(util.read_component(cast_result, c)==val1.op2());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to top "
+      "with a constant struct AO with a value")
+    {
+      auto op1=util.build_top_struct(struct_type);
+      auto op2=util.build_struct(val1);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("The original constant struct AO should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, b)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, c)==nil_exprt());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to top "
+      "with a constant struct AO set to top")
+    {
+      auto op1=util.build_top_struct(struct_type);
+      auto op2=util.build_top_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("The original constant struct AO should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, b)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, c)==nil_exprt());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to top "
+      "with a constant struct AO set to bottom")
+    {
+      auto op1=util.build_top_struct(struct_type);
+      auto op2=util.build_bottom_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("The original constant struct AO should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, b)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, c)==nil_exprt());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to bottom "
+      "with a constant struct AO with a value")
+    {
+      auto op1=util.build_bottom_struct(struct_type);
+      auto op2=util.build_struct(val1);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("A new AO should be returned with op2s valuee")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE(modified);
+        REQUIRE_FALSE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==val1.op0());
+        REQUIRE(util.read_component(cast_result, b)==val1.op1());
+        REQUIRE(util.read_component(cast_result, c)==val1.op2());
+
+        // Is optimal
+        REQUIRE(result!=op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to bottom "
+      "with a constant struct AO set to top")
+    {
+      auto op1=util.build_bottom_struct(struct_type);
+      auto op2=util.build_top_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("A new constant struct AO should be returned set to top ")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, b)==nil_exprt());
+        REQUIRE(util.read_component(cast_result, c)==nil_exprt());
+
+        // Is optimal
+        REQUIRE(result!=op1);
+      }
+    }
+    WHEN("Merging a constant struct AO set to bottom "
+      "with a constant struct AO set to bottom")
+    {
+      auto op1=util.build_bottom_struct(struct_type);
+      auto op2=util.build_bottom_struct(struct_type);
+
+      result=abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("The original bottom AO should be returned")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE_FALSE(cast_result->is_top());
+        REQUIRE(cast_result->is_bottom());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging constant struct AO with value "
+      "with a abstract object set to top")
+    {
+      const auto &op1=util.build_struct(val1);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+
+      THEN("We should get a new AO of the same type but set to top")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE(modified);
+        REQUIRE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+
+        // Since it has modified, we definitely shouldn't be reusing the pointer
+        REQUIRE_FALSE(result==op1);
+      }
+    }
+    WHEN("Merging constant struct AO with value "
+      "with a abstract object set to bottom")
+    {
+      const auto &op1=util.build_struct(val1);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      const auto &cast_result=
+        std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+      THEN("We should get the same constant struct AO back")
+      {
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+
+        // Correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE_FALSE(cast_result->is_top());
+        REQUIRE_FALSE(cast_result->is_bottom());
+        REQUIRE(util.read_component(cast_result, a)==val1.op0());
+        REQUIRE(util.read_component(cast_result, b)==val1.op1());
+        REQUIRE(util.read_component(cast_result, c)==val1.op2());
+
+        // Is optimal
+        REQUIRE(result==op1);
+      }
+    }
+    WHEN("Merging constant struct AO set to top "
+      "with a abstract object set to top")
+    {
+      const auto &op1=
+        util.build_top_struct(struct_type);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      THEN("We should get the same abstract object back")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // Is optimal
+        REQUIRE(op1==result);
+
+        // Is type still correct
+        const auto &cast_result=
+          std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+      }
+    }
+    WHEN("Merging constant struct AO set to top "
+      "with a abstract object set to bottom")
+    {
+      const auto &op1=
+        util.build_top_struct(struct_type);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("Should get the same abstract object back")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // Is optimal
+        REQUIRE(op1==result);
+
+        // Is type still correct
+        const auto &cast_result=
+          std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+      }
+    }
+    WHEN("Merging constant struct AO set to bottom "
+    " with a abstract object set to top")
+    {
+      const auto &op1=
+        util.build_bottom_struct(struct_type);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("Return a new top abstract object of the same type")
+      {
+        // Simple correctness of merge
+        REQUIRE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // We don't optimize for returning the second parameter if we can
+
+        // Is type still correct
+        const auto &cast_result=
+          std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+      }
+    }
+    WHEN("Merging constant struct AO set to bottom with a AO set to bottom")
+    {
+      const auto &op1=
+        util.build_bottom_struct(struct_type);
+      const auto &op2=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("Return the original abstract object")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE_FALSE(result->is_top());
+        REQUIRE(result->is_bottom());
+
+        // Optimization check
+        REQUIRE(result==op1);
+
+        // Is type still correct
+        const auto &cast_result=
+          std::dynamic_pointer_cast<const full_struct_abstract_objectt>(result);
+        // Though we may become top or bottom, the type should be unchanged
+        REQUIRE(cast_result);
+      }
+    }
+    WHEN("Merging AO set to top with a constant struct AO with a value")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+      const auto &op2=util.build_struct(val1);
+
+      bool modified;
+
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("The original AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // Is optimal
+        REQUIRE(op1==result);
+      }
+    }
+    WHEN("Merging AO set to top with a constant struct AO set to top")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+      const auto &op2=
+        util.build_top_struct(struct_type);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("The original AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // Is optimal
+        REQUIRE(op1==result);
+      }
+    }
+    WHEN("Merging AO set to top with a constant struct AO set to bottom")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), true, false);
+      const auto &op2=
+        util.build_bottom_struct(struct_type);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+      THEN("The original AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // Is optimal
+        REQUIRE(op1==result);
+      }
+    }
+    WHEN("Merging AO set to bottom with a constant struct AO with a value")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+      const auto &op2=util.build_struct(val1);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      THEN("The a new top AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE(modified);
+        REQUIRE(typeid(result.get())==typeid(const abstract_objectt *));
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+      }
+
+      // We don't optimize for returning the second parameter if we can
+    }
+    WHEN("Merging AO set to bottom with a constant struct AO set to top")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+      const auto &op2=
+        util.build_top_struct(struct_type);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      THEN("The a new top AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE(modified);
+        REQUIRE(result->is_top());
+        REQUIRE_FALSE(result->is_bottom());
+
+        // We don't optimize for returning the second parameter if we can
+      }
+    }
+    WHEN("Merging AO set to bottom with a constant struct AO set to bottom")
+    {
+      const auto &op1=
+        std::make_shared<abstract_objectt>(val1.type(), false, true);
+      const auto &op2=
+        util.build_bottom_struct(struct_type);
+
+      bool modified;
+      abstract_object_pointert result=
+        abstract_objectt::merge(op1, op2, modified);
+
+      THEN("The original AO should be returned")
+      {
+        // Simple correctness of merge
+        REQUIRE_FALSE(modified);
+        REQUIRE_FALSE(result->is_top());
+        REQUIRE(result->is_bottom());
+
+        REQUIRE(result==op1);
       }
     }
   }
