dc809b73d9821c0b9b241d414419259338c6a8bd
reuk <reuk@users.noreply.github.com>
2017-05-07 14:07:13 +0100

    Fix include order in memory-models

--- src/memory-models/mmcc_main.cpp
+++ src/memory-models/mmcc_main.cpp
@@ -9,9 +9,9 @@ Author: Daniel Kroening, kroening@kroening.com
 /// \file
 /// mmcc Main Module
 
-#include <util/unicode.h>
-
 #include "mmcc_parse_options.h"
+
+#include <util/unicode.h>
 
 #ifdef _MSC_VER
 int wmain(int argc, const wchar_t **argv_wide)
