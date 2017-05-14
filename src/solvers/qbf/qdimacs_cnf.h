d9ba0e4c5b4f38faa17a779fb6ef5e3daefc5e4d
reuk <reuk@users.noreply.github.com>
2017-05-14 20:47:59 +0100

    Use strong enum in qbf_cnf

--- src/solvers/qbf/qdimacs_cnf.h
+++ src/solvers/qbf/qdimacs_cnf.h
@@ -32,11 +32,11 @@ public:
   class quantifiert
   {
   public:
-    typedef enum { NONE, EXISTENTIAL, UNIVERSAL } typet;
+    enum class typet { NONE, EXISTENTIAL, UNIVERSAL };
     typet type;
     unsigned var_no;
 
-    quantifiert():type(NONE), var_no(0)
+    quantifiert():type(typet::NONE), var_no(0)
     {
     }
 
@@ -51,7 +51,7 @@ public:
 
     size_t hash() const
     {
-      return var_no^(type<<24);
+      return var_no^(static_cast<int>(type)<<24);
     }
   };
 
@@ -71,12 +71,12 @@ public:
 
   void add_existential_quantifier(const literalt l)
   {
-    add_quantifier(quantifiert(quantifiert::EXISTENTIAL, l));
+    add_quantifier(quantifiert(quantifiert::typet::EXISTENTIAL, l));
   }
 
   void add_universal_quantifier(const literalt l)
   {
-    add_quantifier(quantifiert(quantifiert::UNIVERSAL, l));
+    add_quantifier(quantifiert(quantifiert::typet::UNIVERSAL, l));
   }
 
   bool is_quantified(const literalt l) const;
