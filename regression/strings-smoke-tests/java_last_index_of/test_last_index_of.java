5f4e3e7fb8623bd04af991188410dbed40065e72
Joel Allred <joel.allred@diffblue.com>
2017-06-06 17:33:05 +0100

    Update regressions tests for indexOf and lastIndexOf

--- regression/strings-smoke-tests/java_last_index_of/test_last_index_of.java
+++ regression/strings-smoke-tests/java_last_index_of/test_last_index_of.java
@@ -1,10 +1,11 @@
 public class test_last_index_of
 {
-   public static void main(/*String[] argv*/)
-   {
-      String s = "abcab";
-      String ab = "ab";
-      int i = s.lastIndexOf(ab);
-      assert(i == 3);
-   }
+    public static void main(/*String[] argv*/)
+    {
+        String s = "abcab";
+        String ab = "ab";
+        int i = s.lastIndexOf(ab);
+        assert(i == 3);
+        assert(i == 1);
+    }
 }
