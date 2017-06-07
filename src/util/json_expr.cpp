a9a98431f2ee5ebb143876edcf1de765d477350f
Romain Brenguier <romain.brenguier@gmail.com>
2017-06-07 09:48:28 +0100

    Adding assertions to prevent the "data" field from not being set

--- src/util/json_expr.cpp
+++ src/util/json_expr.cpp
@@ -250,6 +250,8 @@ json_objectt json(
       mp_integer i;
       if(!to_integer(expr, i))
         result["data"]=json_stringt(integer2string(i));
+      else
+        assert(false && "could not convert data to integer");
     }
     else if(type.id()==ID_c_enum)
     {
@@ -261,6 +263,8 @@ json_objectt json(
       mp_integer i;
       if(!to_integer(expr, i))
         result["data"]=json_stringt(integer2string(i));
+      else
+        assert(false && "could not convert data to integer");
     }
     else if(type.id()==ID_c_enum_tag)
     {
