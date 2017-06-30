8dcff58e8413dad898433cc8a616b48ccbf4b2fd
thk123 <thomas@diffblue.com>
2017-06-30 12:22:58 +0100

    Code tidy changes
    Clarifying comment in symbol_table and adding required braces
    
    Correcting an include postion
    
    Removed unusued method declaration

--- src/java_bytecode/java_object_factory.cpp
+++ src/java_bytecode/java_object_factory.cpp
@@ -6,6 +6,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include "java_object_factory.h"
 
 #include <unordered_set>
 #include <sstream>
@@ -26,7 +27,6 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <goto-programs/goto_functions.h>
 
-#include "java_object_factory.h"
 #include "java_types.h"
 #include "java_utils.h"
 
@@ -59,11 +59,6 @@ class java_object_factoryt
     const exprt &expr,
     const pointer_typet &ptr_type);
 
-  void gen_pointer_target_init(
-    const exprt &expr,
-    const typet &target_type,
-    bool create_dynamic_objects,
-    update_in_placet update_in_place);
 
   code_assignt get_null_assignment(
     const exprt &expr,
@@ -356,8 +351,10 @@ void java_object_factoryt::gen_pointer_target_init(
     if(target.id()==ID_address_of)
       init_expr=target.op0();
     else
+    {
       init_expr=
         dereference_exprt(target, target.type().subtype());
+    }
     gen_nondet_init(
       assignments,
       init_expr,
