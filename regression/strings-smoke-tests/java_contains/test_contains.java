939fb876622b320ef4ae2d6a0239912fee4ecc96
Jesse Sigal <jesse.sigal@gmail.com>
2017-07-03 11:44:12 +0100

    Added regression tests based on originating test-gen tests
    The relevant tests implemented in diffblue/test-gen/pull/479 (StringContains3 and StringContains4) for test-gen have been adapted and added as StringContains01 and StringContains02 resp. They have been marked as future in accordance to the standard for other tests. A contains smoke test already exists and is active, which was extended.

--- regression/strings-smoke-tests/java_contains/test_contains.java
+++ regression/strings-smoke-tests/java_contains/test_contains.java
@@ -1,12 +1,15 @@
 public class test_contains
 {
-   public static void main(/*String[] argv*/)
+   public static void main(String x)
    {
       String s = new String("Abc");
       String u = "bc";
       String t = "ab";
       assert(s.contains(u));
-      // Long version:
-      // assert(s.contains(t));
+      assert(!s.contains(t));
+
+      String z = new String(x);
+      if (z.length() > 3) assert(t.contains(z));
+      else assert(z.contains(u));
    }
 }
