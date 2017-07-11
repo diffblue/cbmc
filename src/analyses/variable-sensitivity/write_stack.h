ca6c22cf4ec42b305cc9105994e09881e3eb6a8a
thk123 <thomas@diffblue.com>
2017-07-11 14:54:59 +0100

    Implementing a write_stack to represent writing to a addressable location
    Deals with storing a stack to write to things like:
    
    &a[1]
    &s.comp
    &a[x] + 1
    
    By building a stack as parsing the expression tree.
    
    It does not support pointer arithmetic outside the scope of an array
    (e.g. within a struct)
    
    This will be used to store pointer values.
    
    Disabled unit tests since don't have the prerequistes in this branch

---
+++ src/analyses/variable-sensitivity/write_stack.h
@@ -0,0 +1,66 @@
+/*******************************************************************\
+
+ Module: Variable Sensitivity Domain
+
+ Author: DiffBlue Limited. All rights reserved.
+
+\*******************************************************************/
+
+/// \file
+/// Represents a stack of write operations to an addressable memory
+/// location
+
+#ifndef CPROVER_ANALYSES_VARIABLE_SENSITIVITY_WRITE_STACK_H
+#define CPROVER_ANALYSES_VARIABLE_SENSITIVITY_WRITE_STACK_H
+
+#include <analyses/variable-sensitivity/write_stack_entry.h>
+
+class write_stackt
+{
+public:
+  typedef std::vector<std::shared_ptr<write_stack_entryt>>
+    continuation_stack_storet;
+
+  write_stackt();
+
+  write_stackt(
+    const exprt &expr,
+    const abstract_environmentt &environment,
+    const namespacet &ns);
+
+  exprt to_expression() const;
+  bool is_top_value() const;
+
+private:
+  void construct_stack_to_pointer(
+    const exprt &expr,
+    const abstract_environmentt &environment,
+    const namespacet &ns);
+
+  void construct_stack_to_lvalue(
+    const exprt &expr,
+    const abstract_environmentt &environment,
+    const namespacet &ns);
+
+  void construct_stack_to_array_index(
+    const index_exprt &expr,
+    const abstract_environmentt &environment,
+    const namespacet &ns);
+
+  void add_to_stack(
+    std::shared_ptr<write_stack_entryt> entry_pointer,
+    const abstract_environmentt environment,
+    const namespacet &ns);
+
+  enum class integral_resultt { LHS_INTEGRAL, RHS_INTEGRAL, NEITHER_INTEGRAL };
+
+  static integral_resultt get_which_side_integral(
+    const exprt &expr,
+    exprt &out_base_expr,
+    exprt &out_integral_expr);
+
+  continuation_stack_storet stack;
+  bool top_stack;
+};
+
+#endif // CPROVER_ANALYSES_VARIABLE_SENSITIVITY_WRITE_STACK_H
