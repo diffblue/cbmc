9f8e1555b896c8c7411e2ea4d478c78aac359f60
thk123 <thomas@diffblue.com>
2017-07-11 14:54:59 +0100

    Implementing a write_stack to represent writing to a addressable location
    Deals with storing a stack to write to things like:
    
    &a[1]
    &s.comp
    &a[x] + 1
    
    By building a stack as parsing the expression tree.
    
    It does not support pointer arithmetic outside the scope of an array
    (e.g. within a struct)
    
    This will be used to store pointer values.
    
    Disabled unit tests since don't have the prerequistes in this branch

---
+++ src/analyses/variable-sensitivity/write_stack_entry.cpp
@@ -0,0 +1,79 @@
+#include <unordered_set>
+
+
+#include <util/std_expr.h>
+
+#include "write_stack_entry.h"
+
+/// Try to combine a new stack element with the current top of the stack
+/// \param new_entry: The new entry to combine with
+/// \param enviroment: The enviroment to evalaute things in
+/// \param ns: The global namespace
+/// \return True if this stack entry and thew new entry were combined
+bool write_stack_entryt::try_squash_in(
+  std::shared_ptr<const write_stack_entryt> new_entry,
+  const abstract_environmentt &enviroment,
+  const namespacet &ns)
+{
+  return false;
+}
+
+/// Build a simple entry based off a single expression
+/// \param expr: The expression being represented
+simple_entryt::simple_entryt(exprt expr):
+  simple_entry(expr)
+{
+  // Invalid simple expression added to the stack
+  PRECONDITION(expr.id()==ID_member || expr.id()==ID_symbol);
+}
+
+/// Get the expression part needed to read this stack entry. For simple
+/// expressions this is just the expression itself.
+/// \return The expression to read this part of the stack
+exprt simple_entryt::get_access_expr() const
+{
+  return simple_entry;
+}
+
+offset_entryt::offset_entryt(abstract_object_pointert offset_value):
+  offset(offset_value)
+{
+  // The type of the abstract object should be an integral number
+  static const std::unordered_set<irep_idt, irep_id_hash> integral_types=
+    { ID_signedbv, ID_unsignedbv, ID_integer };
+  PRECONDITION(
+    integral_types.find(offset_value->type().id())!=integral_types.cend());
+}
+
+/// Get the expression part needed to read this stack entry. For offset entries
+/// this is an index expression with the index() part the offset
+/// \return The expression to read this part of the stack
+exprt offset_entryt::get_access_expr() const
+{
+  return index_exprt(exprt(), offset->to_constant());
+}
+
+/// Try to combine a new stack element with the current top of the stack. This
+/// will succeed if they are both offsets as we can combine these offsets into
+/// the sum of the offsets
+/// \param new_entry: The new entry to combine with
+/// \param enviroment: The enviroment to evalaute things in
+/// \param ns: The global namespace
+/// \return True if this stack entry and thew new entry were combined
+bool offset_entryt::try_squash_in(
+  std::shared_ptr<const write_stack_entryt> new_entry,
+  const abstract_environmentt &enviroment,
+  const namespacet &ns)
+{
+  std::shared_ptr<const offset_entryt> cast_entry=
+    std::dynamic_pointer_cast<const offset_entryt>(new_entry);
+  if(cast_entry)
+  {
+    plus_exprt result_offset(
+      cast_entry->offset->to_constant(), offset->to_constant());
+    offset=enviroment.eval(result_offset, ns);
+    return true;
+  }
+  return false;
+}
+
