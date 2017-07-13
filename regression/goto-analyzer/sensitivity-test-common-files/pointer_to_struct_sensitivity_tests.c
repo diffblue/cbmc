fb63f24734610e56ee353800f62ae01cf79c7db5
thk123 <thomas@diffblue.com>
2017-07-13 09:47:52 +0100

    Enable the write stack for pointers to structs
    This works correctly so we don't need to cautiously throw away stacks of
    pointers to structs. The original concern was pointers to structs being
    used as pointers to the first component, but the goto program introduces
    a cast which means we don't create the pointer.
    
    Added a unit test to document this behavior.

--- regression/goto-analyzer/sensitivity-test-common-files/pointer_to_struct_sensitivity_tests.c
+++ regression/goto-analyzer/sensitivity-test-common-files/pointer_to_struct_sensitivity_tests.c
@@ -24,5 +24,30 @@ int main(int argc, char *argv[])
   __CPROVER_assert(p->b==2.0, "p->b==2.0");
   __CPROVER_assert(p->b==1.0, "p->b==1.0");
 
+  // pointers to components
+  int * comp_p = &x.a;
+  __CPROVER_assert(comp_p==&x.a, "comp_p==&x.a");
+  __CPROVER_assert(comp_p==&x.b, "comp_p==&x.b");
+  __CPROVER_assert(*comp_p==0, "*comp_p==0");
+  __CPROVER_assert(*comp_p==1, "*comp_p==1");
+
+  float * compb_p = &x.b;
+  __CPROVER_assert(compb_p==&x.a, "compb_p==&x.a");
+  __CPROVER_assert(compb_p==&x.b, "compb_p==&x.b");
+  __CPROVER_assert(*compb_p==2.0, "*compb_p==2.0");
+  __CPROVER_assert(*compb_p==1.0, "*compb_p==1.0");
+
+  // Use pointer implicitly pointing at the first component
+  int * implicit_p = &x;
+  __CPROVER_assert(implicit_p==&x.a, "implicit_p==&x.a");
+  __CPROVER_assert(implicit_p==&x, "implicit_p==&x");
+  __CPROVER_assert(*implicit_p==0, "*implicit_p==0");
+  __CPROVER_assert(*implicit_p==1, "*implicit_p==1");
+
+  // Write through pointer implicitly pointing at the first component
+  *implicit_p=5;
+  __CPROVER_assert(x.a==5, "x.a==5");
+  __CPROVER_assert(x.a==1, "x.a==1");
+
   return 0;
 }
