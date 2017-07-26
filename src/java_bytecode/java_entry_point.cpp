63e61deed73d8ead15845f8be38bf118340cb0ea
Chris Smowton <chris@smowton.net>
2017-07-26 17:20:17 +0100

    Mark well-known globals System.out and System.err non-null
    This is a precursor for the forthcoming exceptions improvements, which
    among other things will throw a NullPointerException on attempting a
    virtual call against a null pointer (e.g. System.out.println, if
    System.out may be null). Test basic1 would then fail. This therefore
    augments basic1 to explicitly check that .out and.err are non-null,
    and marks them out as special in java_entry_point, similar to the existing
    handling of Class and String literals.
    
    Longer-term the modelling library should determine that these are always
    expected to be non-null, but this serves as a stopgap in the meantime.

--- src/java_bytecode/java_entry_point.cpp
+++ src/java_bytecode/java_entry_point.cpp
@@ -10,6 +10,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <algorithm>
 #include <set>
+#include <unordered_set>
 #include <iostream>
 
 #include <util/arith_tools.h>
@@ -61,7 +62,6 @@ static void create_initialize(symbol_tablet &symbol_table)
     throw "failed to add "+std::string(INITIALIZE);
 }
 
-
 static bool should_init_symbol(const symbolt &sym)
 {
   if(sym.type.id()!=ID_code &&
@@ -72,6 +72,17 @@ static bool should_init_symbol(const symbolt &sym)
     return true;
 
   return has_prefix(id2string(sym.name), "java::java.lang.String.Literal");
+}
+
+static bool is_non_null_library_global(const irep_idt &symbolid)
+{
+  static const std::unordered_set<irep_idt, irep_id_hash> non_null_globals=
+  {
+    "java::java.lang.System.out",
+    "java::java.lang.System.err",
+    "java::java.lang.System.in"
+  };
+  return non_null_globals.count(symbolid);
 }
 
 void java_static_lifetime_init(
@@ -110,6 +121,8 @@ void java_static_lifetime_init(
           if(allow_null && has_prefix(
                namestr,
                "java::java.lang.String.Literal"))
+            allow_null=false;
+          if(allow_null && is_non_null_library_global(nameid))
             allow_null=false;
         }
         auto newsym=object_factory(
