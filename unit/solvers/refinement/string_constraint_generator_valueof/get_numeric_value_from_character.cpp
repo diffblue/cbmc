a58d65a7fedd3dcfc77600930a100984f1416fc8
Owen Jones <owen.jones@diffblue.com>
2017-07-27 11:40:54 +0100

    Updated two unit tests

--- unit/solvers/refinement/string_constraint_generator_valueof/get_numeric_value_from_character.cpp
+++ unit/solvers/refinement/string_constraint_generator_valueof/get_numeric_value_from_character.cpp
@@ -15,27 +15,97 @@
 #include <util/simplify_expr.h>
 #include <util/std_types.h>
 
+
+
 SCENARIO("get_numeric_value_from_character",
   "[core][solvers][refinement][string_constraint_generator_valueof]")
 {
-  typet char_type=unsignedbv_typet(16);
-  typet int_type=signedbv_typet(32);
+  const typet char_type_1=unsignedbv_typet(8);
+  const typet char_type_2=unsignedbv_typet(16);
+  const typet char_type_3=unsignedbv_typet(32);
+  const typet int_type_1=signedbv_typet(8);
+  const typet int_type_2=signedbv_typet(16);
+  const typet int_type_3=signedbv_typet(32);
+  const typet int_type_4=signedbv_typet(64);
   symbol_tablet symtab;
-  namespacet ns(symtab);
+  const namespacet ns(symtab);
 
-  REQUIRE(from_integer(0, int_type)==simplify_expr(
-    get_numeric_value_from_character(
-      from_integer('0', char_type), char_type, int_type), ns));
-  REQUIRE(from_integer(9, int_type)==simplify_expr(
-    get_numeric_value_from_character(
-      from_integer('9', char_type),
-      char_type, int_type), ns));
-  REQUIRE(from_integer(10, int_type)==simplify_expr(
-    get_numeric_value_from_character(
-      from_integer('A', char_type),
-      char_type, int_type), ns));
-  REQUIRE(from_integer(35, int_type)==simplify_expr(
-    get_numeric_value_from_character(
-      from_integer('z', char_type),
-      char_type, int_type), ns));
+  WHEN("0")
+  {
+    mp_integer character='0';
+    mp_integer expected_value=0;
+    const typet& char_type=char_type_1;
+    const typet& int_type=int_type_1;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
+  WHEN("9")
+  {
+    mp_integer character='9';
+    mp_integer expected_value=9;
+    const typet& char_type=char_type_2;
+    const typet& int_type=int_type_2;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
+  WHEN("A")
+  {
+    mp_integer character='A';
+    mp_integer expected_value=10;
+    const typet& char_type=char_type_3;
+    const typet& int_type=int_type_3;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
+  WHEN("z")
+  {
+    mp_integer character='z';
+    mp_integer expected_value=35;
+    const typet& char_type=char_type_1;
+    const typet& int_type=int_type_4;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
+  WHEN("+")
+  {
+    mp_integer character='+';
+    mp_integer expected_value=0;
+    const typet& char_type=char_type_2;
+    const typet& int_type=int_type_1;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
+  WHEN("-")
+  {
+    mp_integer character='-';
+    mp_integer expected_value=0;
+    const typet& char_type=char_type_3;
+    const typet& int_type=int_type_2;
+    constant_exprt expected_value_exprt=from_integer(expected_value, int_type);
+    exprt actual_value_exprt=simplify_expr(
+      get_numeric_value_from_character(
+        from_integer(character, char_type), char_type, int_type),
+      ns);
+    REQUIRE(expected_value_exprt==actual_value_exprt);
+  }
 }
