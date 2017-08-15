c16e8c2d7c2f2d5e31bf6e09d1464a54b0d2dd2e
Daniel Kroening <kroening@cs.ox.ac.uk>
2017-08-15 16:52:05 +0100

    move include

--- src/java_bytecode/java_utils.h
+++ src/java_bytecode/java_utils.h
@@ -6,11 +6,10 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
-
-#include <util/type.h>
-
 #ifndef CPROVER_JAVA_BYTECODE_JAVA_UTILS_H
 #define CPROVER_JAVA_BYTECODE_JAVA_UTILS_H
+
+#include <util/type.h>
 
 bool java_is_array_type(const typet &type);
 
