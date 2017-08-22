eac3fe18d5f9065e5b7c041727dedc1f7d00ef8a
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2017-08-22 15:23:58 +0100

    Added a basic file for array tests. Need thomas to config its validity before I author the test.desc

---
+++ regression/goto-analyzer/variable-sensitivity-assign-aware-merge-array/main.c
@@ -0,0 +1,35 @@
+int a[10];
+
+int c[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+void param_test(int array[])
+{
+    array[1] = 5;
+    // TODO: This is marked as "unknown" when running analysis of param_test and global_test. Doesn't seem correct
+    // as array[1] is being assigned one line above?
+    __CPROVER_assert(a[1]==5,  "a[1]==5");
+}
+
+void global_test()
+{
+    a[2] = 6;
+    __CPROVER_assert(a[2]==6,  "a[2]==6");
+    c[8] = 15;
+    __CPROVER_assert(c[8]==15,  "c[8]==15");
+    // TODO: This is marked as "unknown" when running analysis of global_test. CORRECT?
+    __CPROVER_assert(c[9]==9,  "c[9]==9");
+}
+
+void pass_param()
+{
+    int b[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    __CPROVER_assert(b[0]==0,  "b[0]==0");
+    __CPROVER_assert(b[4]==4,  "b[4]==4");
+    __CPROVER_assert(b[8]==8,  "b[8]==8");
+    // TODO: This is marked as "unknown" when running analysis of pass_param. CORRECT?
+    // The theme here is that global array initialisation assertions are marked as "unknown". Why?
+    __CPROVER_assert(c[8]==8,  "c[8]==8");
+    
+    param_test(&a);
+}
