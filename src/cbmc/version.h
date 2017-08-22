bb88fd2942346b9c4870395ab23807b2205e1720
reuk <reuk@users.noreply.github.com>
2017-08-22 16:43:12 +0100

    Add author string to version.h

--- src/cbmc/version.h
+++ src/cbmc/version.h
@@ -1,3 +1,11 @@
+/*******************************************************************\
+
+Module: Bounded Model Checking for ANSI-C + HDL
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
 #ifndef CPROVER_CBMC_VERSION_H
 #define CPROVER_CBMC_VERSION_H
 
