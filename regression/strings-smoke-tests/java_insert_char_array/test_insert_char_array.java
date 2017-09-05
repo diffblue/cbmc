c71c64b962dea08775eeebad0a0789baaea75f1f
Romain Brenguier <romain.brenguier@gmail.com>
2017-09-05 14:56:36 +0100

    Regression: include model for string to char array
    Using model of char array functions in tests.
    Also correct java_if test description file.
    Remove java_object_allocation test, this should be replaced by unit tests.
    Removing java_append_char test duplicate of the one in
    string-smoke-test.
    Including models of char array functions for some string
    regression tests.

--- regression/strings-smoke-tests/java_insert_char_array/test_insert_char_array.java
+++ regression/strings-smoke-tests/java_insert_char_array/test_insert_char_array.java
@@ -1,14 +1,24 @@
 public class test_insert_char_array
 {
-   public static void main(/*String[] argv*/)
-   {
-      StringBuilder sb = new StringBuilder("ad");
-      char[] array = new char[2];
-      array[0] = 'b';
-      array[1] = 'c';
-      sb.insert(1, array);
-      String s = sb.toString();
-      System.out.println(s);
-      assert(s.equals("abcd"));
-   }
+    public static void insert(StringBuilder sb, int offset, char[] arr)
+    {
+        assert(arr.length<5);
+        for(int i=0; i<arr.length && i<5; i++)
+            sb.insert(offset+i, arr[i]);
+    }
+
+    public static void main(int i)
+    {
+        StringBuilder sb = new StringBuilder("ad");
+        char[] array = new char[2];
+        array[0] = 'b';
+        array[1] = 'c';
+        insert(sb, 1, array);
+        String s = sb.toString();
+        System.out.println(s);
+        if(i==0)
+            assert(s.equals("abcd"));
+        else
+            assert(!s.equals("abcd"));
+    }
 }
