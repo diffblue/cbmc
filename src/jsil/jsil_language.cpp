acdd8b41d6551367fc8d10fcbe07fad11c3d3192
thk123 <thomas@diffblue.com>
2017-09-08 16:55:24 +0100

    Corrected doxygen errors

--- src/jsil/jsil_language.cpp
+++ src/jsil/jsil_language.cpp
@@ -42,7 +42,7 @@ bool jsil_languaget::interfaces(symbol_tablet &symbol_table)
 }
 
 /// Generate a _start function for a specific function
-/// \param entry_function_symbol: The symbol for the function that should be
+/// \param entry_function_symbol_id: The symbol for the function that should be
 ///   used as the entry point
 /// \param symbol_table: The symbol table for the program. The new _start
 ///   function symbol will be added to this table
