632ae4ba7931214462e07da60741bb3e33eb1c7f
Nathan Phillips <nathan.phillips@diffblue.com>
2017-10-03 10:32:45 +0100

    Updates to recording_symbol_tablet
    Use explicit static wrap method to create wrapping recording_symbol_tablet so that users don't confuse copy construction and wrapping
    Track inserted symbols so that we know not to mark them removed if they are subsequently removed (but do mark them removed if they are removed, inserted and then removed again)
    Added unit tests

--- src/util/recording_symbol_table.h
+++ src/util/recording_symbol_table.h
@@ -20,12 +20,16 @@ public:
   typedef std::unordered_set<irep_idt, irep_id_hash> changesett;
 private:
   symbol_tablet &base_symbol_table;
+  // Symbols originally in the table will never be marked inserted
+  changesett inserted;
+  // get_writeable marks an existing symbol updated
+  // Inserted symbols are marked updated, removed ones aren't
   changesett updated;
+  // Symbols not originally in the table will never be marked removed
   changesett removed;
 
-public:
-  // NOLINTNEXTLINE(runtime/explicit) - No information is lost in conversion
-  recording_symbol_tablet(symbol_tablet &base_symbol_table)
+private:
+  explicit recording_symbol_tablet(symbol_tablet &base_symbol_table)
     : symbol_tablet(
         base_symbol_table.symbols,
         base_symbol_table.symbol_base_map,
@@ -34,6 +38,7 @@ public:
   {
   }
 
+public:
   recording_symbol_tablet(const recording_symbol_tablet &other)
     : symbol_tablet(
         other.base_symbol_table.symbols,
@@ -41,6 +46,11 @@ public:
         other.base_symbol_table.symbol_module_map),
       base_symbol_table(other.base_symbol_table)
   {
+  }
+
+  static recording_symbol_tablet wrap(symbol_tablet &base_symbol_table)
+  {
+    return recording_symbol_tablet(base_symbol_table);
   }
 
   virtual opt_symbol_reft get_writeable(const irep_idt &identifier) override
@@ -56,7 +66,7 @@ public:
     std::pair<symbolt &, bool> result=
       base_symbol_table.insert(std::move(symbol));
     if(result.second)
-      on_update(result.first.name);
+      on_insert(result.first.name);
     return result;
   }
 
@@ -73,19 +83,27 @@ public:
     base_symbol_table.clear();
   }
 
+  const changesett &get_inserted() const { return inserted; }
   const changesett &get_updated() const { return updated; }
   const changesett &get_removed() const { return removed; }
 
 private:
+  void on_insert(const irep_idt &id)
+  {
+    if(removed.erase(id)==0)
+      inserted.insert(id);
+    updated.insert(id);
+  }
+
   void on_update(const irep_idt &id)
   {
     updated.insert(id);
-    removed.erase(id);
   }
 
   void on_remove(const irep_idt &id)
   {
-    removed.insert(id);
+    if(inserted.erase(id)==0)
+      removed.insert(id);
     updated.erase(id);
   }
 };
