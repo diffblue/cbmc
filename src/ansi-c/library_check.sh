31970458116edc0b45fda69fef5e9ef643370a22
Michael Tautschnig <tautschn@amazon.com>
2017-10-04 11:04:54 +0100

    library_check: rename _mm_*fence functions
    These functions are built-ins as of LLVM>=9. Treat them the same way as is done
    for __builtin_* functions.
    
    Fixes: #1415

--- src/ansi-c/library_check.sh
+++ src/ansi-c/library_check.sh
@@ -4,6 +4,7 @@ for f in "$@"; do
     echo "Checking ${f}"
     cp "${f}" __libcheck.c
     perl -p -i -e 's/(__builtin_[^v])/s$1/' __libcheck.c
+    perl -p -i -e 's/(_mm_.fence)/s$1/' __libcheck.c
     perl -p -i -e 's/(__sync_)/s$1/' __libcheck.c
     perl -p -i -e 's/(__noop)/s$1/' __libcheck.c
     cc -std=gnu99 -E -include library/cprover.h -D__CPROVER_bool=_Bool -D__CPROVER_thread_local=__thread -DLIBRARY_CHECK -o __libcheck.i __libcheck.c
