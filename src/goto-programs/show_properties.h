134c77d69517c4af59f8d6a0dc31bc398e6a03ff
polgreen <polgreen@amazon.com>
2017-10-11 15:09:50 +0200

    Find source location from a property irep_idt
    returns the source_locationt for a property irep_idt

--- src/goto-programs/show_properties.h
+++ src/goto-programs/show_properties.h
@@ -13,6 +13,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #define CPROVER_GOTO_PROGRAMS_SHOW_PROPERTIES_H
 
 #include <util/ui_message.h>
+#include <util/optional.h>
 
 class namespacet;
 class goto_modelt;
@@ -27,5 +28,16 @@ void show_properties(
   const namespacet &ns,
   ui_message_handlert::uit ui,
   const goto_functionst &goto_functions);
+
+/// \brief Returns a source_locationt that corresponds
+/// to the property given by an irep_idt.
+/// \param property irep_idt that identifies property
+/// \param goto_functions  program in which to search for
+///   the property
+/// \return optional<source_locationt> the location of the
+/// property, if found.
+optionalt<source_locationt> find_property(
+    const irep_idt &property,
+    const goto_functionst &goto_functions);
 
 #endif // CPROVER_GOTO_PROGRAMS_SHOW_PROPERTIES_H
