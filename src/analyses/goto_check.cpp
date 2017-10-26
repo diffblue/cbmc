1e3712c33b0946beb0b6e59f5a5435d906c745bc
Michael Tautschnig <tautschn@amazon.com>
2017-10-26 17:05:45 +0100

    Shifts of non-integers and left shifts of negative integers are undefined
    6.5.7 of C11: "Each of the operands shall have integer type."
    
    Paragraph 4: "If E1 has a signed type and nonnegative value, and E1 x 2^E2 is
    representable in the result type, then that is the resulting value; otherwise,
    the behavior is undefined."

--- src/analyses/goto_check.cpp
+++ src/analyses/goto_check.cpp
@@ -286,6 +286,30 @@ void goto_checkt::undefined_shift_check(
       expr.find_source_location(),
       expr,
       guard);
+
+    if(op_type.id()==ID_signedbv && expr.id()==ID_shl)
+    {
+      binary_relation_exprt inequality(
+        expr.op(), ID_ge, from_integer(0, op_type));
+
+      add_guarded_claim(
+        inequality,
+        "shift operand is negative",
+        "undefined-shift",
+        expr.find_source_location(),
+        expr,
+        guard);
+    }
+  }
+  else
+  {
+    add_guarded_claim(
+      false_exprt(),
+      "shift of non-integer type",
+      "undefined-shift",
+      expr.find_source_location(),
+      expr,
+      guard);
   }
 }
 
