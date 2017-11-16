ff31a193b3b11c6a9138afee5769b60507ced4a2
Dario Cattaruzza <dario.cattaruzza@hotmail.com>
2017-11-16 13:07:41 +0000

    Add constructors for having memory-loaded jar files

--- src/java_bytecode/jar_file.h
+++ src/java_bytecode/jar_file.h
@@ -21,16 +21,29 @@ class java_class_loader_limitt;
 class jar_filet final
 {
 public:
-  /// Open java file for reading
+  /// Open java file for reading.
   /// \param limit Object limiting number of loaded .class files
   /// \param filename Name of the file
   /// \throw Throws std::runtime_error if file cannot be opened
   jar_filet(java_class_loader_limitt &limit, const std::string &filename);
+
+  /// Open a JAR file of size \p size loaded in memory at address \p pMem.
+  /// \param limit Object limiting number of loaded .class files
+  /// \param pMem memory buffer with the contents of the jar file
+  /// \param size size  of the memory buffer
+  /// \throw Throws std::runtime_error if file cannot be opened
+  jar_filet(java_class_loader_limitt &limit, const void *pMem, size_t size);
+
   jar_filet(const jar_filet &)=delete;
   jar_filet &operator=(const jar_filet &)=delete;
   jar_filet(jar_filet &&);
   jar_filet &operator=(jar_filet &&);
   ~jar_filet()=default;
+
+  /// Loads the fileindex (m_name_to_index) with a map of loaded files to
+  /// indices.
+  void initialize_file_index(java_class_loader_limitt &limit);
+
   /// Get contents of a file in the jar archive.
   /// Terminates the program if file doesn't exist
   /// \param filename Name of the file in the archive
@@ -41,7 +54,7 @@ public:
   std::vector<std::string> filenames() const;
 private:
   mz_zip_archivet m_zip_archive;
-  /// Map of filename to the file index in the zip archive
+  /// Map of filename to the file index in the zip archive.
   std::unordered_map<std::string, size_t> m_name_to_index;
 };
 
