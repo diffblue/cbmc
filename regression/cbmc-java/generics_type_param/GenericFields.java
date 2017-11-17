e8f5e0817b0e2de165faab6fd0941d72dea2676a
Matthias Güdemann <matthias.guedemann@diffblue.com>
2017-11-17 08:56:30 +0100

    Add regression tests for generic type arg dependencies

---
+++ regression/cbmc-java/generics_type_param/GenericFields.java
@@ -0,0 +1,25 @@
+class IWrapper {
+    public int i;
+}
+
+class FWrapper {
+}
+
+class AWrapper {
+}
+
+class SimpleWrapper<T> {
+}
+
+public class GenericFields
+{
+  class SimpleGenericField  {
+    // IWrapper field; // for this to work, uncomment this line
+    SimpleWrapper<IWrapper> field_input;
+    public void foo() {
+    }
+    SimpleWrapper<IWrapper> f(SimpleWrapper<FWrapper> s, SimpleWrapper<AWrapper []> a) {
+      return new SimpleWrapper<>();
+    }
+  }
+}
