ebd8248b59e84b63a7c088e41660583dd02ff7ae
Peter Schrammel <peter.schrammel@diffblue.com>
2017-11-28 11:55:50 +0000

    Add list-goto-functions command line option

--- src/goto-programs/show_goto_functions.cpp
+++ src/goto-programs/show_goto_functions.cpp
@@ -30,34 +30,52 @@ Author: Peter Schrammel
 void show_goto_functions(
   const namespacet &ns,
   ui_message_handlert::uit ui,
-  const goto_functionst &goto_functions)
+  const goto_functionst &goto_functions,
+  bool list_only)
 {
   switch(ui)
   {
   case ui_message_handlert::uit::XML_UI:
   {
-    show_goto_functions_xmlt xml_show_functions(ns);
+    show_goto_functions_xmlt xml_show_functions(ns, list_only);
     xml_show_functions(goto_functions, std::cout);
   }
   break;
 
   case ui_message_handlert::uit::JSON_UI:
   {
-    show_goto_functions_jsont json_show_functions(ns);
+    show_goto_functions_jsont json_show_functions(ns, list_only);
     json_show_functions(goto_functions, std::cout);
   }
   break;
 
   case ui_message_handlert::uit::PLAIN:
-    goto_functions.output(ns, std::cout);
+    if(list_only)
+    {
+      for(const auto &fun : goto_functions.function_map)
+      {
+        const symbolt &symbol = ns.lookup(fun.first);
+        std::cout << '\n'
+                  << symbol.display_name() << " /* " << symbol.name
+                  << (fun.second.body_available() ? ""
+                                                  : ", body not available")
+                  << " */";
+      }
+
+      std::cout << std::endl;
+    }
+    else
+      goto_functions.output(ns, std::cout);
+
     break;
   }
 }
 
 void show_goto_functions(
   const goto_modelt &goto_model,
-  ui_message_handlert::uit ui)
+  ui_message_handlert::uit ui,
+  bool list_only)
 {
   const namespacet ns(goto_model.symbol_table);
-  show_goto_functions(ns, ui, goto_model.goto_functions);
+  show_goto_functions(ns, ui, goto_model.goto_functions, list_only);
 }
