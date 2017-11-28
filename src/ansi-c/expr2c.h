9fb735b9a102e7dc6810b29ca0f11f88c06dbd0c
Michael Tautschnig <mt@debian.org>
2017-11-28 09:14:21 +0000

    expr2ct/clean configuration: Do not output C library macros
    NULL, NAN, INFINITY are not a keywords, just macros. Do not use them for
    output under the "clean" configuration.

--- src/ansi-c/expr2c.h
+++ src/ansi-c/expr2c.h
@@ -36,17 +36,22 @@ struct expr2c_configurationt final
   /// This is the string that will be printed for the false boolean expression
   std::string false_string;
 
+  /// This is the string that will be printed for null pointers
+  bool use_library_macros;
+
   expr2c_configurationt(
     const bool include_struct_padding_components,
     const bool print_struct_body_in_type,
     const bool include_array_size,
     const std::string &true_string,
-    const std::string &false_string)
+    const std::string &false_string,
+    const bool use_library_macros)
     : include_struct_padding_components(include_struct_padding_components),
       print_struct_body_in_type(print_struct_body_in_type),
       include_array_size(include_array_size),
       true_string(true_string),
-      false_string(false_string)
+      false_string(false_string),
+      use_library_macros(use_library_macros)
   {
   }
 
