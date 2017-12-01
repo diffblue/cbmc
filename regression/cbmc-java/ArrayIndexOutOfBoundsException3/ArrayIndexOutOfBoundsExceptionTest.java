200017ad86c4449d31d3ff5be03062f263c395bb
Antonia Lechner <antonia.lechner@diffblue.com>
2017-12-01 21:37:11 +0000

    Add test for ArrayIndexOutOfBoundsException
    The new test is similar to ArrayIndexOutOfBoundsException1. It checks
    that an ArrayIndexOutOfBoundsException is correctly caught if the catch
    block specifies one of its superclasses.

---
+++ regression/cbmc-java/ArrayIndexOutOfBoundsException3/ArrayIndexOutOfBoundsExceptionTest.java
@@ -0,0 +1,11 @@
+public class ArrayIndexOutOfBoundsExceptionTest {
+  public static void main(String args[]) {
+      try {
+          int[] a=new int[4];
+          a[4]=0;
+      }
+      catch (Exception exc) {
+          assert false;
+      }
+  }
+}
