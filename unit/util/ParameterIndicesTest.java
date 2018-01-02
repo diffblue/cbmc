3f1fd645b44bbd0c397147e2f726dc9d016d0b3b
Chris Smowton <chris@smowton.net>
2018-01-02 13:40:34 +0000

    Add code_typet::get_parameter_indices
    This is simply the opposite of the existing get_parameter_indentifiers,
    useful for mapping from parameter identifier to its argument or parameter
    index.

---
+++ unit/util/ParameterIndicesTest.java
@@ -0,0 +1,8 @@
+
+public class ParameterIndicesTest {
+
+  public void f(ParameterIndicesTest param1, int param2) {}
+
+  public static void g(float param1, ParameterIndicesTest param2) {}
+
+}
---
+++ unit/util/parameter_indices.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************\
+
+ Module: Parameter indices test
+
+ Author: Diffblue Limited. All rights reserved.
+
+\*******************************************************************/
+
+#include <testing-utils/catch.hpp>
+#include <testing-utils/load_java_class.h>
+#include <util/std_types.h>
+
+void check_consistency(const symbolt &symbol)
+{
+  const auto &code_type = to_code_type(symbol.type);
+  auto parameter_ids = code_type.parameter_identifiers();
+  auto parameter_indices = code_type.parameter_indices();
+
+  REQUIRE(parameter_ids.size() == parameter_indices.size());
+  for(std::size_t i = 0; i < parameter_ids.size(); ++i)
+    REQUIRE(parameter_indices.at(parameter_ids.at(i)) == i);
+}
+
+TEST_CASE("Parmeter indices consistency", "[core][util][parameter_indices]")
+{
+  symbol_tablet symbol_table = load_java_class("ParameterIndicesTest", "util/");
+  check_consistency(
+    symbol_table.lookup_ref(
+      "java::ParameterIndicesTest.f:(LParameterIndicesTest;I)V"));
+  check_consistency(
+    symbol_table.lookup_ref(
+      "java::ParameterIndicesTest.g:(FLParameterIndicesTest;)V"));
+}
