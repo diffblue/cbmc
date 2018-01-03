3365054dbe8d9e5c6c4311e51e4da5a9a0a36328
Matthias Güdemann <matthias.guedemann@diffblue.com>
2018-01-03 18:53:47 +0100

    Add regression test

---
+++ regression/cbmc-java/removed_virtual_functions/ArrayListEquals.java
@@ -0,0 +1,11 @@
+public class ArrayListEquals {
+    
+    public int check2(ArrayList<Integer> l1) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        if(l1.equals(al))
+            return 1;
+        else
+            return 0;
+    }
+
+}
---
+++ regression/cbmc-java/removed_virtual_functions/Iterator.java
@@ -0,0 +1,4 @@
+public interface Iterator<E> {
+    boolean hasNext();
+    E next();
+}
