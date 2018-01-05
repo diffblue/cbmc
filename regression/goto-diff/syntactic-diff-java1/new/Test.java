2811363409dd41364e01fdfc03aaa911ec8f021c
Peter Schrammel <peter.schrammel@diffblue.com>
2018-01-05 23:32:12 +0100

    Java regression test for goto-diff

---
+++ regression/goto-diff/syntactic-diff-java1/new/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+  public int run() {
+    int x = 42;
+
+    return x;
+  }
+
+  public void bar() {
+    int y = 0;
+  }
+  
+  public void unchanged() {
+    int z = 0;
+  }
+}
