bc539b5614d55eb6150a70238252b54a6c84bc28
Kurt Degiorgio <degiorgiokurt@outlook.com>
2018-01-23 17:05:55 +0000

    Adds support for concurrency in java programs
    This commit enables basic support (creation of threads) for
    concurrency in java programs using the CAV 13 encoding. To this end,
    new functions haven been introduced to find and appropriately
    instrument thread blocks.
    
    A thread block is a sequence of codet's surrounded with calls to
    CProver.startThread:(I)V and CProver.endThread:(I)V. The
    instrumentation process transforms both functions into the
    appropriate codet's. This mandates the creation of a new variable per
    thread, '__CPROVER__thread_id' which is used to store the thread ID. A
    global counter, '__CPROVER__next_thread_id' is used to keep track of
    thread id's.
    
    Calls to 'CProver.getCurrentThreadID:()I' are also instrumented such
    that the thread ID of the current thread is returned.
    
    Note: instrumentation of thread block's is only enabled when the
    '--java-threading' command line option is specified.

---
+++ src/java_bytecode/java_bytecode_convert_threadblock.h
@@ -0,0 +1,15 @@
+/*******************************************************************\
+
+Module: Java Convert Thread blocks
+
+Author: Kurt Degiogrio, kurt.degiorgio@diffblue.com
+
+\*******************************************************************/
+#ifndef CPROVER_JAVA_BYTECODE_JAVA_BYTECODE_CONVERT_THREADBLOCK_H
+#define CPROVER_JAVA_BYTECODE_JAVA_BYTECODE_CONVERT_THREADBLOCK_H
+
+#include <util/symbol_table.h>
+
+void convert_threadblock(symbol_tablet &symbol_table);
+
+#endif
