ac036fd4ebbdd8b6aff2dacf55d29d5a21752d70
martin <martin@raphael>
2018-02-09 00:01:52 +0000

    Revert "Fix iterator equality check bug in dependence_graph.cpp"
    This reverts commit e0605b70c359832fa1c274eb6f8d9242dff52953.

--- src/analyses/dependence_graph.cpp
+++ src/analyses/dependence_graph.cpp
@@ -188,7 +188,7 @@ void dep_graph_domaint::transform(
   goto_programt::const_targett to,
   ai_baset &ai,
   const namespacet &ns,
-  ai_domain_baset::edge_typet edge_type)
+  ai_domain_baset::edge_typet /*edge_type*/)
 {
   dependence_grapht *dep_graph=dynamic_cast<dependence_grapht*>(&ai);
   assert(dep_graph!=nullptr);
@@ -196,9 +196,10 @@ void dep_graph_domaint::transform(
   // propagate control dependencies across function calls
   if(from->is_function_call())
   {
-    const goto_programt::const_targett next = std::next(from);
+    goto_programt::const_targett next=from;
+    ++next;
 
-    if(edge_type == ai_domain_baset::edge_typet::FUNCTION_LOCAL)
+    if(next==to)
     {
       control_dependencies(from, to, *dep_graph);
     }
