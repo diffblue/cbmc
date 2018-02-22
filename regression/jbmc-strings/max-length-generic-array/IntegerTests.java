985ac776e5bf400df391668093b9df277c43854d
Romain Brenguier <romain.brenguier@gmail.com>
2018-02-22 12:13:48 +0000

    Add test for generics array and strings
    This adds an int greater than string-max-length in a generic array.
    This is to check that TG-2138 is fixed.
    Before the issue was solved setting the array to 101 was in
    contradiction with default axioms added by the string solver.

---
+++ regression/jbmc-strings/max-length-generic-array/IntegerTests.java
@@ -0,0 +1,44 @@
+public class IntegerTests {
+
+    public static Boolean testMyGenSet(Integer key) {
+        if (key == null) return null;
+        MyGenSet<Integer> ms = new MyGenSet<>();
+        ms.array[0] = 101;
+        if (ms.contains(key)) return true;
+        return false;
+    }
+
+    public static Boolean testMySet(Integer key) {
+        if (key == null) return null;
+        MySet ms = new MySet();
+        ms.array[0] = 101;
+        if (ms.contains(key)) return true;
+        return false;
+    }
+
+}
+
+class MyGenSet<E> {
+    E[] array;
+    @SuppressWarnings("unchecked")
+    MyGenSet() {
+        array = (E[]) new Object[1];
+    }
+    boolean contains(E o) {
+        if (o.equals(array[0]))
+            return true;
+        return false;
+    }
+}
+
+class MySet {
+    Integer[] array;
+    MySet() {
+        array = new Integer[1];
+    }
+    boolean contains(Integer o) {
+        if (o.equals(array[0]))
+            return true;
+        return false;
+    }
+}
