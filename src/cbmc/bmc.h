978ca5bf0f66d4a176c990d3a5b5fa475e988e0d
Chris Smowton <chris@smowton.net>
2018-03-02 10:48:17 +0000

    Add callback after symex to bmct
    This permits frontend programs to intervene after symex executes, either in addition to
    or instead of running bmc. Its current use case is to implement show-goto-functions and
    similar debug output options when symex-driven lazy loading means that function bodies are
    not populated until symex has run.
    
    When --paths is in use, the callback will be made every time symex finishes a path.

--- src/cbmc/bmc.h
+++ src/cbmc/bmc.h
@@ -74,7 +74,8 @@ public:
     const symbol_tablet &outer_symbol_table,
     message_handlert &_message_handler,
     prop_convt &_prop_conv,
-    goto_symext::branch_worklistt &_branch_worklist)
+    goto_symext::branch_worklistt &_branch_worklist,
+    std::function<bool(void)> callback_after_symex)
     : safety_checkert(ns, _message_handler),
       options(_options),
       outer_symbol_table(outer_symbol_table),
@@ -83,7 +84,8 @@ public:
       branch_worklist(_branch_worklist),
       symex(_message_handler, outer_symbol_table, equation, branch_worklist),
       prop_conv(_prop_conv),
-      ui(ui_message_handlert::uit::PLAIN)
+      ui(ui_message_handlert::uit::PLAIN),
+      driver_callback_after_symex(callback_after_symex)
   {
     symex.constant_propagation=options.get_bool_option("propagation");
     symex.record_coverage=
@@ -128,10 +130,9 @@ public:
     abstract_goto_modelt &goto_model,
     const ui_message_handlert::uit &ui,
     messaget &message,
-    std::function<void(bmct &, const symbol_tablet &)> frontend_configure_bmc =
-      [](bmct &_bmc, const symbol_tablet &) { // NOLINT (*)
-        // Empty default implementation
-      });
+    std::function<void(bmct &, const symbol_tablet &)>
+      driver_configure_bmc = nullptr,
+    std::function<bool(void)> callback_after_symex = nullptr);
 
 protected:
   /// \brief Constructor for path exploration from saved state
@@ -147,7 +148,8 @@ protected:
     message_handlert &_message_handler,
     prop_convt &_prop_conv,
     symex_target_equationt &_equation,
-    goto_symext::branch_worklistt &_branch_worklist)
+    goto_symext::branch_worklistt &_branch_worklist,
+    std::function<bool(void)> callback_after_symex)
     : safety_checkert(ns, _message_handler),
       options(_options),
       outer_symbol_table(outer_symbol_table),
@@ -156,7 +158,8 @@ protected:
       branch_worklist(_branch_worklist),
       symex(_message_handler, outer_symbol_table, equation, branch_worklist),
       prop_conv(_prop_conv),
-      ui(ui_message_handlert::uit::PLAIN)
+      ui(ui_message_handlert::uit::PLAIN),
+      driver_callback_after_symex(callback_after_symex)
   {
     symex.constant_propagation = options.get_bool_option("propagation");
     symex.record_coverage =
@@ -238,6 +241,12 @@ private:
   /// full-program model-checking from the entry point of the program.
   virtual void perform_symbolic_execution(
     goto_symext::get_goto_functiont get_goto_function);
+
+  /// Optional callback, to be run after symex but before handing the resulting
+  /// equation to the solver. If it returns true then we will skip the solver
+  /// stage and return "safe" (no assertions violated / coverage goals reached),
+  /// similar to the behaviour when 'show-vcc' or 'program-only' are specified.
+  std::function<bool(void)> driver_callback_after_symex;
 };
 
 /// \brief Symbolic execution from a saved branch point
@@ -260,14 +269,16 @@ public:
     prop_convt &_prop_conv,
     symex_target_equationt &saved_equation,
     const goto_symex_statet &saved_state,
-    goto_symext::branch_worklistt &branch_worklist)
+    goto_symext::branch_worklistt &branch_worklist,
+    std::function<bool(void)> callback_after_symex)
     : bmct(
         _options,
         outer_symbol_table,
         _message_handler,
         _prop_conv,
         saved_equation,
-        branch_worklist),
+        branch_worklist,
+        callback_after_symex),
       saved_state(saved_state)
   {
   }
