28c6477e0e7d8409dcf065c13c1ecf05276a0c95
thk123 <thomas@diffblue.com>
2018-03-08 14:04:00 +0000

    Adding utility for getting symbols out of the symbol table

---
+++ unit/testing-utils/require_symbol.h
@@ -0,0 +1,26 @@
+/*******************************************************************\
+
+ Module: Unit test utilities
+
+ Author: DiffBlue Limited. All rights reserved.
+
+\*******************************************************************/
+
+#ifndef CPROVER_TESTING_UTILS_REQUIRE_SYMBOL_H
+#define CPROVER_TESTING_UTILS_REQUIRE_SYMBOL_H
+
+#include <util/symbol.h>
+#include <util/symbol_table.h>
+
+/// \file
+/// Helper functions for getting symbols from the symbol table during unit tests
+
+// NOLINTNEXTLINE(readability/namespace)
+namespace require_symbol
+{
+const symbolt &require_symbol_exists(
+  const symbol_tablet &symbol_table,
+  const irep_idt &symbol_identifier);
+}
+
+#endif // CPROVER_TESTING_UTILS_REQUIRE_SYMBOL_H
