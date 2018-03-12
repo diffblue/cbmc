dcd680f61b97ac3e805076b040ca663c54515434
thk123 <thomas@diffblue.com>
2018-03-12 16:31:41 +0000

    Correcting formating on java file

--- regression/cbmc-java/lambda2/StaticMethodRef.java
+++ regression/cbmc-java/lambda2/StaticMethodRef.java
@@ -3,22 +3,22 @@ import java.util.function.BiFunction;
 
 public class StaticMethodRef{
  public Integer Smr(Integer ctr) {
-   Function<Integer, Integer> func1  = Integer::valueOf;
+  Function<Integer, Integer> func1 = Integer::valueOf;
 
-   // Uses  Integer.valueOf(String)
-   Function<String, Integer> func2  = Integer::valueOf;
+    // Uses  Integer.valueOf(String)
+  Function<String, Integer> func2 = Integer::valueOf;
 
-   BiFunction<String, Integer,  Integer> func3  = Integer::valueOf;
+  BiFunction<String, Integer, Integer> func3 = Integer::valueOf;
 
-   if (ctr.equals(func1.apply(9)))
-       return func1.apply(9);
+  if (ctr.equals(func1.apply(9)))
+    return func1.apply(9);
 
-   if(ctr.equals(func2.apply("8")))
-       return func2.apply("8");
+  if (ctr.equals(func2.apply("8")))
+    return func2.apply("8");
 
-   if(ctr.equals(func3.apply("0111", 2)))
-       return func3.apply("0111", 2);
+  if (ctr.equals(func3.apply("0111", 2)))
+    return func3.apply("0111", 2);
 
-   return ctr;
- }
+  return ctr;
+}
 }
