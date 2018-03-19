46fa1764d84a1936b98669ae4e5f77a25ae59fd7
thk123 <thomas@diffblue.com>
2018-03-19 12:27:24 +0000

    Extending require utilities to be used in test

--- unit/testing-utils/require_expr.cpp
+++ unit/testing-utils/require_expr.cpp
@@ -16,6 +16,7 @@
 
 #include <testing-utils/catch.hpp>
 #include <util/arith_tools.h>
+#include <util/std_code.h>
 
 /// Verify a given exprt is an index_exprt with a a constant value equal to the
 /// expected value
@@ -69,8 +70,39 @@ member_exprt require_expr::require_member(
 symbol_exprt require_expr::require_symbol(
   const exprt &expr, const irep_idt &symbol_name)
 {
-  REQUIRE(expr.id()==ID_symbol);
-  const symbol_exprt &symbol_expr=to_symbol_expr(expr);
+  const symbol_exprt &symbol_expr = require_symbol(expr);
   REQUIRE(symbol_expr.get_identifier()==symbol_name);
   return symbol_expr;
+}
+
+/// Verify a given exprt is a symbol_exprt.
+/// \param expr: The expression.
+/// \return The expr cast to a symbol_exprt
+symbol_exprt require_expr::require_symbol(const exprt &expr)
+{
+  REQUIRE(expr.id() == ID_symbol);
+  return to_symbol_expr(expr);
+}
+
+/// Verify a given exprt is a typecast_expr.
+/// \param expr: The expression.
+/// \return The expr cast to a typecast_exprt
+typecast_exprt require_expr::require_typecast(const exprt &expr)
+{
+  REQUIRE(expr.id() == ID_typecast);
+  return to_typecast_expr(expr);
+}
+
+/// Verify a given exprt is a side_effect_exprt with appropriate statement.
+/// \param expr: The expression.
+/// \param symbol_name: The intended identifier of statement
+/// \return The expr cast to a side_effect_exprt
+side_effect_exprt require_expr::require_side_effect_expr(
+  const exprt &expr,
+  const irep_idt &side_effect_statement)
+{
+  REQUIRE(expr.id() == ID_side_effect);
+  const side_effect_exprt &side_effect_expr = to_side_effect_expr(expr);
+  REQUIRE(side_effect_expr.get_statement() == side_effect_statement);
+  return side_effect_expr;
 }
