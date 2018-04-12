a97bc2809d2fa05bc357c5be5593e06a24b172b3
thk123 <thomas@diffblue.com>
2018-04-12 14:27:25 +0100

    Introduce throwing a guard conversion exception
    Throw the unwrapped exception as a string

---
+++ src/goto-symex/equation_conversion_exceptions.h
@@ -0,0 +1,50 @@
+/*******************************************************************\
+
+Module: Symbolic Execution
+
+Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+/// \file
+/// Exceptions that can be raised during the equation conversion phase
+
+#ifndef CPROVER_GOTO_SYMEX_EQUATION_CONVERSION_EXCEPTIONS_H
+#define CPROVER_GOTO_SYMEX_EQUATION_CONVERSION_EXCEPTIONS_H
+
+#include <ostream>
+#include <util/namespace.h>
+#include "symex_target_equation.h"
+
+class guard_conversion_exceptiont : public std::runtime_error
+{
+public:
+  guard_conversion_exceptiont(
+    const symex_target_equationt::SSA_stept &step,
+    const namespacet &ns)
+    : runtime_error("Error converting guard for step"), step(step)
+  {
+    std::ostringstream error_msg;
+    error_msg << runtime_error::what();
+    error_msg << "\nStep:\n";
+    step.output(ns, error_msg);
+    error_message = error_msg.str();
+  }
+
+  explicit guard_conversion_exceptiont(
+    const symex_target_equationt::SSA_stept &step)
+    : guard_conversion_exceptiont(step, namespacet{symbol_tablet{}})
+  {
+  }
+
+  const char *what() const noexcept override
+  {
+    return error_message.c_str();
+  }
+
+private:
+  symex_target_equationt::SSA_stept step;
+  std::string error_message;
+};
+
+#endif // CPROVER_GOTO_SYMEX_EQUATION_CONVERSION_EXCEPTIONS_H
