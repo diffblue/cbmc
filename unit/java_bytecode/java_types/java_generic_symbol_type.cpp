8f0f780ce9bae42a7634f44184770c15d71e0bdb
Romain Brenguier <romain.brenguier@gmail.com>
2018-05-02 09:27:29 +0100

    Unit test for java_generic_symbol_type

---
+++ unit/java_bytecode/java_types/java_generic_symbol_type.cpp
@@ -0,0 +1,31 @@
+/*******************************************************************\
+
+ Module: Unit tests for java_types
+
+ Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#include <testing-utils/catch.hpp>
+#include <java_bytecode/java_types.h>
+
+SCENARIO("java_generic_symbol_type", "[core][java_types]")
+{
+  WHEN("MyType is LGenericClass<TX;TY;>;")
+  {
+    auto symbol_type = symbol_typet("MyType");
+    const auto generic_symbol_type = java_generic_symbol_typet(
+      symbol_type, "LGenericClass<TX;TY;>;", "PrefixClassName");
+
+    REQUIRE(generic_symbol_type.get_identifier() == "MyType");
+
+    auto types = generic_symbol_type.generic_types();
+    REQUIRE(types.size() == 2);
+
+    auto generic_var0 = to_java_generic_parameter(types[0]).type_variable();
+    REQUIRE(generic_var0.get_identifier() == "PrefixClassName::X");
+
+    auto generic_var1 = to_java_generic_parameter(types[1]).type_variable();
+    REQUIRE(generic_var1.get_identifier() == "PrefixClassName::Y");
+  }
+}
