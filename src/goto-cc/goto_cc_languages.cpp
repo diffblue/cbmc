01685b651ac605665629f3549f4434733a78480b
Peter Schrammel <peter.schrammel@diffblue.com>
2018-05-07 10:48:20 +0200

    Clean up references to SpecC frontend

--- src/goto-cc/goto_cc_languages.cpp
+++ src/goto-cc/goto_cc_languages.cpp
@@ -17,17 +17,9 @@ Author: CM Wintersteiger
 #include <cpp/cpp_language.h>
 #include <jsil/jsil_language.h>
 
-#ifdef HAVE_SPECC
-#include <specc/specc_language.h>
-#endif
-
 void goto_cc_modet::register_languages()
 {
   register_language(new_ansi_c_language);
   register_language(new_cpp_language);
   register_language(new_jsil_language);
-
-  #ifdef HAVE_SPECC
-  register_language(new_specc_language);
-  #endif
 }
