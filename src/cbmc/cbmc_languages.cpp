17e13259a180cf0247cbe39f1b443638ab5cf1dc
Peter Schrammel <peter.schrammel@diffblue.com>
2018-05-07 16:48:38 +0200

    Remove conditional inclusion of JSIL

--- src/cbmc/cbmc_languages.cpp
+++ src/cbmc/cbmc_languages.cpp
@@ -15,17 +15,11 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <ansi-c/ansi_c_language_info.h>
 #include <cpp/cpp_language_info.h>
-
-#ifdef HAVE_JSIL
 #include <jsil/jsil_language_info.h>
-#endif
 
 void cbmc_parse_optionst::register_languages()
 {
   register_language(new_ansi_c_language_info);
   register_language(new_cpp_language_info);
-
-  #ifdef HAVE_JSIL
   register_language(new_jsil_language_info);
-  #endif
 }
