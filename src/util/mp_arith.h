29763da4a7b5aaf94dc3393a14b1f1f45648321b
Michael Tautschnig <tautschn@amazon.com>
2018-05-07 11:10:48 +0000

    Fix misleading comments: the required variant of numeric_cast does not exist

--- src/util/mp_arith.h
+++ src/util/mp_arith.h
@@ -52,16 +52,16 @@ const mp_integer string2integer(const std::string &, unsigned base=10);
 const std::string integer2binary(const mp_integer &, std::size_t width);
 const mp_integer binary2integer(const std::string &, bool is_signed);
 
-/// \deprecated use numeric_cast<unsigned long long> instead
-DEPRECATED("Use numeric_cast<unsigned long long> instead")
+/// \deprecated use numeric_cast_v<unsigned long long> instead
+DEPRECATED("Use numeric_cast_v<unsigned long long> instead")
 mp_integer::ullong_t integer2ulong(const mp_integer &);
 
-/// \deprecated use numeric_cast<std::size_t> instead
-DEPRECATED("Use numeric_cast<std::size_t> instead")
+/// \deprecated use numeric_cast_v<std::size_t> instead
+DEPRECATED("Use numeric_cast_v<std::size_t> instead")
 std::size_t integer2size_t(const mp_integer &);
 
-/// \deprecated use numeric_cast<unsigned> instead
-DEPRECATED("Use numeric_cast<unsigned> instead")
+/// \deprecated use numeric_cast_v<unsigned> instead
+DEPRECATED("Use numeric_cast_v<unsigned> instead")
 unsigned integer2unsigned(const mp_integer &);
 
 const mp_integer mp_zero=string2integer("0");
