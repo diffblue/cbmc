dab421c800f8aefbbebe3619aa32bb367a00c017
Thomas Spriggs <thomas.spriggs@diffblue.com>
2018-05-23 17:08:15 +0100

    Add global instance of `null_message_handlert`

---
+++ unit/testing-utils/message.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************\
+
+ Module: Unit test utilities
+
+ Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+/// \file
+/// Global instance of `null_message_handlert`.
+
+#include "message.h"
+
+null_message_handlert null_message_handler;
---
+++ unit/testing-utils/message.h
@@ -0,0 +1,16 @@
+/*******************************************************************\
+
+ Module: Unit test utilities
+
+ Author: DiffBlue Limited. All rights reserved.
+
+\*******************************************************************/
+
+#ifndef CPROVER_TESTING_UTILS_MESSAGE_H
+#define CPROVER_TESTING_UTILS_MESSAGE_H
+
+#include <util/message.h>
+
+extern null_message_handlert null_message_handler;
+
+#endif // CPROVER_TESTING_UTILS_MESSAGE_H
