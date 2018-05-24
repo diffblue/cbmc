f154840d3a95ec13ec9f37053a6e10856b28ad79
Thomas Spriggs <thomas.spriggs@diffblue.com>
2018-05-24 19:00:21 +0100

    Delete copy constructor of `class java_bytecode_parse_treet`.
    This deletes the default copy constructor of
    `java_bytecode_parse_treet`, but allows move construction.
    This enforces allowing move semantics only and prevents accidental
    copying.

--- jbmc/src/java_bytecode/java_bytecode_parse_tree.h
+++ jbmc/src/java_bytecode/java_bytecode_parse_tree.h
@@ -22,6 +22,16 @@ Author: Daniel Kroening, kroening@kroening.com
 class java_bytecode_parse_treet
 {
 public:
+  // Disallow copy construction and copy assignment, but allow move construction
+  // and move assignment.
+  #ifndef _MSC_VER // Ommit this on MS VC2013 as move is not supported.
+  java_bytecode_parse_treet(const java_bytecode_parse_treet &) = delete;
+  java_bytecode_parse_treet &
+  operator=(const java_bytecode_parse_treet &) = delete;
+  java_bytecode_parse_treet(java_bytecode_parse_treet &&) = default;
+  java_bytecode_parse_treet &operator=(java_bytecode_parse_treet &&) = default;
+  #endif
+
   virtual ~java_bytecode_parse_treet() = default;
   class annotationt
   {
@@ -178,6 +188,17 @@ public:
   class classt
   {
   public:
+    classt() = default;
+
+    // Disallow copy construction and copy assignment, but allow move
+    // construction and move assignment.
+    #ifndef _MSC_VER // Ommit this on MS VC2013 as move is not supported.
+    classt(const classt &) = delete;
+    classt &operator=(const classt &) = delete;
+    classt(classt &&) = default;
+    classt &operator=(classt &&) = default;
+    #endif
+
     irep_idt name, extends;
     bool is_abstract=false;
     bool is_enum=false;
