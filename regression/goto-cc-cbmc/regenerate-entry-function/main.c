46f62314e6d48089fb646fac3a776d45f27f865e
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-05-26 09:46:44 +0100

    cbmc test no longer uses --cover

--- regression/goto-cc-cbmc/regenerate-entry-function/main.c
+++ regression/goto-cc-cbmc/regenerate-entry-function/main.c
@@ -1,23 +1,11 @@
+#include <assert.h>
+
 int fun(int x)
 {
-	if(x > 0)
-	{
-		return x * x;
-	}
-	else
-	{
-		return x;
-	}
+  assert(0);
 }
 
 int main(int argc, char** argv)
 {
-	if(argc>4)
-	{
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+  return 0;
 }
