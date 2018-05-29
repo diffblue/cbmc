5c5e10b6d5a3c665b887572020b9bf6d0bfbca83
Romain Brenguier <romain.brenguier@gmail.com>
2018-05-29 16:19:26 +0100

    fixup! fixup! Test for CProver.nondetWithoutNull

--- regression/jbmc-cover/nondetWithoutNull/Test.java
+++ regression/jbmc-cover/nondetWithoutNull/Test.java
@@ -6,27 +6,27 @@ public class Test {
     public final static Object stat = CProver.nondetWithoutNull();
     
     public static int check() {
-	Object o = CProver.nondetWithoutNull();
+        Object o = CProver.nondetWithoutNull();
 
-	if(stat == null)
-	    return -1;
+        if(stat == null)
+            return -1;
 
-	if(o == null)
-	    return 0;
+        if(o == null)
+            return 0;
 
-	return 1;
+        return 1;
     }
 
     public static int check2() {
-	Integer i = CProver.nondetWithNull();
+        Integer i = CProver.nondetWithNull();
 
-	if(i == null)
-	    return -1;
-	
-	if(i instanceof Integer)
-	    return 0;
+        if(i == null)
+            return -1;
+        
+        if(i instanceof Integer)
+            return 0;
 
-	return 1;
+        return 1;
     }
 
 }
