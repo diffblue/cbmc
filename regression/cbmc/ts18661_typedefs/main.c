7b6148279371942a95a93884fbbdeba81c2e8218
Chris Smowton <chris@smowton.net>
2018-06-07 10:32:47 +0100

    Extend cbmc ts18661 test
    This now expects to find _Float128 when gcc-4.3+ (x86-64) for -4.5+ (other targets) is in use.

--- regression/cbmc/ts18661_typedefs/main.c
+++ regression/cbmc/ts18661_typedefs/main.c
@@ -1,8 +1,19 @@
-#if defined(__clang__)
-#elif defined(__GNUC__)
+#if defined(__GNUC__) && !defined(__clang__)
+
+#include <features.h> // For __GNUC_PREREQ
+
+#ifdef __x86_64__
+#define FLOAT128_MINOR_VERSION 3
+#else
+#define FLOAT128_MINOR_VERSION 5
+#endif
+
 #if __GNUC__ >= 7
 #define HAS_FLOATN
+#elif __GNUC_PREREQ(4, FLOAT128_MINOR_VERSION)
+#define HAS_FLOAT128
 #endif
+
 #endif
 
 #ifndef HAS_FLOATN
@@ -10,8 +21,11 @@ typedef float _Float32;
 typedef double _Float32x;
 typedef double _Float64;
 typedef long double _Float64x;
-typedef long double _Float128;
 typedef long double _Float128x;
+#endif
+
+#if !defined(HAS_FLOATN) && !defined(HAS_FLOAT128)
+typedef long double _Float128;
 #endif
 
 int main(int argc, char** argv) {
