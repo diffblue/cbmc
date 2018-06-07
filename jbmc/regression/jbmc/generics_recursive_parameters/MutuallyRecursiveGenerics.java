d196cf78a7832810667a39ef452bcd4ea5ecbdb8
Matthias Güdemann <matthias.guedemann@diffblue.com>
2018-06-07 16:23:15 +0200

    Add regression test for recursive generic-parameters

---
+++ jbmc/regression/jbmc/generics_recursive_parameters/MutuallyRecursiveGenerics.java
@@ -0,0 +1,11 @@
+class KeyValue<K, V> {
+    KeyValue<K, V> next;
+    KeyValue<V, K> reverse;
+    K key;
+    V value;
+}
+class MutuallyRecursiveGenerics {
+  void f() {
+    KeyValue<String, Integer> example1;
+  }
+}
