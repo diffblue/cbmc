4180e654f2671aea3489253b7e36a212c7afd1a5
Chris Smowton <chris@smowton.net>
2018-06-07 10:32:16 +0100

    Add test for gcc-5
    This version should expose _Float128, but not other ts18661 types

---
+++ regression/ansi-c/gcc_version1/gcc-5.c
@@ -0,0 +1,9 @@
+// These types should *not* be provided when emulating gcc-5:
+typedef float _Float32;
+typedef double _Float32x;
+typedef double _Float64;
+typedef long double _Float64x;
+typedef long double _Float128x;
+
+// But this type should:
+_Float128 f128;
