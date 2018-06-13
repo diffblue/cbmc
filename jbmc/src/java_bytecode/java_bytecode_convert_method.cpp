07acde40b451e434402ecf1e411bc69fe8bd933c
Peter Schrammel <peter.schrammel@diffblue.com>
2018-06-13 13:18:33 +0100

    Refactor user-defined assertion translation for Java
    Assertions in Java are "throw a;" statements where
    a is of type java.lang.AssertionError (an exception,
    or Throwable, to be precise). Sometimes we want to
    translate it into an ASSERT instruction in the goto
    program. Special-casing in order to  handle that
    was scattered across multiple classes. In this commit
    we special-case it only once in the Java frontend
    and translate it into assert(false); assume(false);
    which is then correctly handled by later stages of the
    translation.

--- jbmc/src/java_bytecode/java_bytecode_convert_method.cpp
+++ jbmc/src/java_bytecode/java_bytecode_convert_method.cpp
@@ -32,11 +32,14 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/simplify_expr.h>
 #include <util/std_expr.h>
 #include <util/string2int.h>
+#include <util/string_constant.h>
 
 #include <goto-programs/cfg.h>
 #include <goto-programs/class_hierarchy.h>
 #include <goto-programs/resolve_inherited_component.h>
+
 #include <analyses/cfg_dominators.h>
+#include <analyses/uncaught_exceptions_analysis.h>
 
 #include <limits>
 #include <algorithm>
@@ -2248,10 +2251,39 @@ void java_bytecode_convert_methodt::convert_athrow(
   codet &c,
   exprt::operandst &results) const
 {
-  side_effect_expr_throwt throw_expr;
-  throw_expr.add_source_location() = location;
-  throw_expr.copy_to_operands(op[0]);
-  c = code_expressiont(throw_expr);
+  if(
+    uncaught_exceptions_domaint::get_exception_type(op[0].type()) ==
+      "java::java.lang.AssertionError")
+  {
+    // we translate athrow into
+    // ASSERT false;
+    // ASSUME false:
+    code_assertt assert_code;
+    assert_code.assertion() = false_exprt();
+    source_locationt assert_location = location; // copy
+    assert_location.set_comment("assertion at " + location.as_string());
+    assert_location.set("user-provided", true);
+    assert_location.set_property_class(ID_assertion);
+    assert_code.add_source_location() = assert_location;
+
+    code_assumet assume_code;
+    assume_code.assumption() = false_exprt();
+    source_locationt assume_location = location; // copy
+    assume_location.set("user-provided", true);
+    assume_code.add_source_location() = assume_location;
+
+    code_blockt ret_block;
+    ret_block.move_to_operands(assert_code);
+    ret_block.move_to_operands(assume_code);
+    c = ret_block;
+  }
+  else
+  {
+    side_effect_expr_throwt throw_expr;
+    throw_expr.add_source_location() = location;
+    throw_expr.copy_to_operands(op[0]);
+    c = code_expressiont(throw_expr);
+  }
   results[0] = op[0];
 }
 
