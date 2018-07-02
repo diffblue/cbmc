15d88d95f4673a7bed732b04114a3930044c3e05
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-07-02 14:32:00 +0100

    Add helper class for displaying memory quantities

---
+++ src/util/memory_units.h
@@ -0,0 +1,37 @@
+#ifndef CPROVER_UTIL_MEMORY_UNITS_H
+#define CPROVER_UTIL_MEMORY_UNITS_H
+
+#include <cstddef>
+#include <string>
+
+struct memory_sizet
+{
+  static memory_sizet from_bytes(std::size_t bytes);
+
+  memory_sizet();
+  memory_sizet(const memory_sizet &);
+  memory_sizet(memory_sizet &&);
+
+  memory_sizet &operator=(const memory_sizet &);
+  memory_sizet &operator=(memory_sizet &&) noexcept;
+
+  memory_sizet &operator+=(const memory_sizet &);
+  memory_sizet operator+(const memory_sizet &) const;
+
+  std::size_t get_bytes() const;
+  std::size_t get_kibibytes() const;
+  std::size_t get_mebibytes() const;
+  std::size_t get_gibibytes() const;
+  std::string to_string() const;
+
+  static const char *si_byte_symbol;
+  static const char *si_kibitye_symbol;
+  static const char *si_mebibyte_symbol;
+  static const char *si_gibibyte_symbol;
+
+private:
+  std::size_t bytes;
+  memory_sizet(std::size_t bytes);
+};
+
+#endif // CPROVER_UTIL_MEMORY_UNITS_H
