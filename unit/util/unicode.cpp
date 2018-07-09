8d93087aa44a0a1ddaaf132b19d72a43d1162b80
Michael Tautschnig <tautschn@amazon.com>
2018-07-09 08:40:53 +0100

    Move unicode unit test to CATCH and enable it

--- unit/util/unicode.cpp
+++ unit/util/unicode.cpp
@@ -6,20 +6,22 @@ Author: Vojtech Forejt, forejtv@diffblue.com
 
 \*******************************************************************/
 
-#include <cassert>
+#include <testing-utils/catch.hpp>
+
 #include <vector>
 #include <string>
 #include <codecvt>
-#include <iomanip>
-#include <iostream>
 #include <locale>
 
 #include <util/unicode.h>
 
+// the u8 prefix is only available from VS 2015 onwards
+#if !defined(_MSC_VER) || _MSC_VER >= 1900
+
 // This unit test compares our implementation with codecvt implementation,
 // checking bit-by-bit equivalence of results.
 
-bool paranoid_wstr_equals(const std::wstring &a, const std::wstring &b)
+static bool paranoid_wstr_equals(const std::wstring &a, const std::wstring &b)
 {
   if(a.size() != b.size())
     return false;
@@ -35,7 +37,10 @@ bool paranoid_wstr_equals(const std::wstring &a, const std::wstring &b)
 }
 
 // helper print function, can be called for debugging problem
-void wstr_print(const std::wstring &a, const std::wstring &b)
+#if 0
+#include <iostream>
+
+static void wstr_print(const std::wstring &a, const std::wstring &b)
 {
   int endi=(a.size()>b.size())?a.size():b.size();
   const unsigned char
@@ -49,8 +54,11 @@ void wstr_print(const std::wstring &a, const std::wstring &b)
   }
   std::cout << '\n';
 }
+#endif
 
-void compare_utf8_to_utf16_big_endian(std::string& in)
+#if 0
+// big-endian test is broken, will be fixed in subsequent commit
+static bool compare_utf8_to_utf16_big_endian(const std::string &in)
 {
   std::wstring s1=utf8_to_utf16_big_endian(in);
 
@@ -58,10 +66,11 @@ void compare_utf8_to_utf16_big_endian(std::string& in)
   std::wstring_convert<codecvt_utf8_utf16t> converter;
   std::wstring s2=converter.from_bytes(in);
 
-  assert(paranoid_wstr_equals(s1, s2));
+  return paranoid_wstr_equals(s1, s2);
 }
+#endif
 
-void compare_utf8_to_utf16_little_endian(std::string& in)
+static bool compare_utf8_to_utf16_little_endian(const std::string &in)
 {
   std::wstring s1=utf8_to_utf16_little_endian(in);
 
@@ -72,23 +81,41 @@ void compare_utf8_to_utf16_little_endian(std::string& in)
   std::wstring_convert<codecvt_utf8_utf16t> converter;
   std::wstring s2=converter.from_bytes(in);
 
-  assert(paranoid_wstr_equals(s1, s2));
+  return paranoid_wstr_equals(s1, s2);
 }
 
-int main()
+TEST_CASE("unicode0", "[core][util][unicode]")
 {
-  std::string s;
-  s=u8"\u0070\u00DF\u00E0\u00EF\u00F0\u00F7\u00F8";
-  compare_utf8_to_utf16_big_endian(s);
-  compare_utf8_to_utf16_little_endian(s);
-  s=u8"$¢€𐍈";
-  compare_utf8_to_utf16_big_endian(s);
-  compare_utf8_to_utf16_little_endian(s);
-  s=u8"𐐏𤭢";
-  compare_utf8_to_utf16_big_endian(s);
-  compare_utf8_to_utf16_little_endian(s);
-  s=u8"дȚȨɌṡʒʸͼἨѶݔݺ→⅒⅀▤▞╢◍⛳⻥龍ンㄗㄸ";
-  compare_utf8_to_utf16_big_endian(s);
-  compare_utf8_to_utf16_little_endian(s);
+  const std::string s = u8"abc";
+  // REQUIRE(compare_utf8_to_utf16_big_endian(s));
+  REQUIRE(compare_utf8_to_utf16_little_endian(s));
 }
 
+TEST_CASE("unicode1", "[core][util][unicode]")
+{
+  const std::string s = u8"\u0070\u00DF\u00E0\u00EF\u00F0\u00F7\u00F8";
+  // REQUIRE(compare_utf8_to_utf16_big_endian(s));
+  REQUIRE(compare_utf8_to_utf16_little_endian(s));
+}
+
+TEST_CASE("unicode2", "[core][util][unicode]")
+{
+  const std::string s = u8"$¢€𐍈";
+  // REQUIRE(compare_utf8_to_utf16_big_endian(s));
+  REQUIRE(compare_utf8_to_utf16_little_endian(s));
+}
+
+TEST_CASE("unicode3", "[core][util][unicode]")
+{
+  const std::string s = u8"𐐏𤭢";
+  // REQUIRE(compare_utf8_to_utf16_big_endian(s));
+  REQUIRE(compare_utf8_to_utf16_little_endian(s));
+}
+
+TEST_CASE("unicode4", "[core][util][unicode]")
+{
+  const std::string s = u8"дȚȨɌṡʒʸͼἨѶݔݺ→⅒⅀▤▞╢◍⛳⻥龍ンㄗㄸ";
+  // REQUIRE(compare_utf8_to_utf16_big_endian(s));
+  REQUIRE(compare_utf8_to_utf16_little_endian(s));
+}
+#endif
