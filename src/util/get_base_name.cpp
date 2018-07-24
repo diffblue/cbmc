4995b103c6eb6c956da8a8cba3e4ace2a5e3b598
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-07-24 21:56:42 +0100

    fix get_base_name

--- src/util/get_base_name.cpp
+++ src/util/get_base_name.cpp
@@ -8,7 +8,6 @@ Date:
 
 \*******************************************************************/
 
-
 #include "get_base_name.h"
 
 /// cleans a filename from path and extension
@@ -16,23 +15,32 @@ Date:
 /// \return a new string
 std::string get_base_name(const std::string &in, bool strip_suffix)
 {
-  size_t r=std::string::npos;
+#ifdef _WIN32
+  // Windows now allows both '/' and '\\'
+  const std::size_t slash_pos = in.find_last_of("\\/");
+#else
+  const std::size_t slash_pos = in.rfind('/');
+#endif
+
+  std::size_t start_pos;
+
+  if(slash_pos == std::string::npos)
+    start_pos = 0;
+  else
+    start_pos = slash_pos + 1;
+
+  std::size_t char_count;
+
   if(strip_suffix)
-    r=in.rfind('.', in.length()-1);
-  if(r==std::string::npos)
-    r=in.length();
+  {
+    std::size_t dot_pos = in.rfind('.');
+    if(dot_pos == std::string::npos || dot_pos < start_pos)
+      char_count = std::string::npos;
+    else
+      char_count = dot_pos - start_pos;
+  }
+  else
+    char_count = std::string::npos;
 
-  size_t f=in.rfind('/', in.length()-1);
-  if(f==std::string::npos)
-    f=0;
-
-  size_t fw=in.rfind('\\', in.length()-1);
-  if(fw==std::string::npos)
-    fw=0;
-
-  f = (fw>f)?fw:f;
-
-  if(in[f]=='/' || in[f]=='\\')
-    f++;
-  return in.substr(f, r-f);
+  return std::string(in, start_pos, char_count);
 }
