fbcd383e2ba0a71ddd409d9121523f49bab47657
Daniel Poetzl <daniel.poetzl@diffblue.com>
2018-07-26 17:38:21 +0100

    Add control dependencies from function call to function entry point for new dependency graph

--- src/analyses/variable-sensitivity/variable_sensitivity_dependence_graph.cpp
+++ src/analyses/variable-sensitivity/variable_sensitivity_dependence_graph.cpp
@@ -70,6 +70,25 @@ void variable_sensitivity_dependence_domaint::transform(
     dynamic_cast<variable_sensitivity_dependence_grapht*>(&ai);
   assert(dep_graph!=nullptr);
 
+  // We do not propagate control dependencies on function calls, i.e., only the
+  // entry point of a function should have a control dependency on the call
+  if(!control_deps.empty())
+  {
+    const goto_programt::const_targett &dep = control_deps.begin()->first;
+    if(dep->is_function_call())
+    {
+      INVARIANT(
+        std::all_of(
+          std::next(control_deps.begin()),
+          control_deps.end(),
+          [](const std::pair<const goto_programt::const_targett, tvt> &d)
+            { return d.first->is_function_call(); }),
+        "All entries must be function calls");
+
+      control_deps.clear();
+    }
+  }
+
   // propagate control dependencies across function calls
   if(from->is_function_call())
   {
@@ -100,6 +119,8 @@ void variable_sensitivity_dependence_domaint::transform(
         s->has_changed=true;
 
       control_deps.clear();
+      control_deps.insert(std::make_pair(from, tvt::unknown()));
+
       control_dep_candidates.clear();
     }
   }
