58307a04e035d1aaf92e3a5f934cf06099200d85
Romain Brenguier <romain.brenguier@gmail.com>
2018-08-02 16:06:20 +0100

    Tests for String.toUpperCase

---
+++ jbmc/regression/jbmc-strings/StringToUpperCase/Test.java
@@ -0,0 +1,50 @@
+public class Test {
+    public String det()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase());
+        builder.append("abcdeghijklmnopfrstuqwxyzABCDvFGHIJKLMENOPQRSTUVWXYZ".toUpperCase());
+        builder.append("abcdeghijlmnopqrstvwxyzABCDEFGHIJuKLMNOPQRSfkTUVWXYZ".toUpperCase());
+        builder.append("acdefghijklmnopqrsuvwxyzABCDEFbGHIJKLMNOPtQRSTUVWXYZ".toUpperCase());
+        builder.append("abcdfghijklmnopqrstuvwxyzABCDEFGHIJeKLMNOPQRSTUVWXYZ".toUpperCase());
+        return builder.toString();
+    }
+
+    public String nonDet(String s)
+    {
+        if(s.length() < 20)
+            return "Short string";
+        if(!s.startsWith("a"))
+            return "String not starting with a";
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(s.toUpperCase());
+        builder.append(s.toUpperCase());
+        builder.append(":");
+        builder.append(s);
+        builder.append(":");
+        builder.append(s.toUpperCase());
+        builder.append(s.toUpperCase());
+        return builder.toString();
+    }
+
+    public String withDependency(String s, boolean b)
+    {
+        // Filter
+        if(s == null || s.length() < 20)
+            return "Short string";
+        if(!s.endsWith("a"))
+            return "String not ending with a";
+
+        // Act
+        String result = s + s.toUpperCase();
+
+        // Assert
+        if(b) {
+            assert(result.endsWith("A"));
+        } else {
+            assert(!result.endsWith("A"));
+        }
+        return result;
+    }
+}
