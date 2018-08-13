4b9df3be640ae19c6a5a71612d1d1653be1387c4
Michael Tautschnig <tautschn@amazon.com>
2018-08-13 09:05:31 +0000

    Revert "Ignore return value"
    This reverts commit e74c000a75be4b6152119689c01c2d7f56a67d24.

--- src/util/replace_symbol.cpp
+++ src/util/replace_symbol.cpp
@@ -177,7 +177,7 @@ bool replace_symbolt::replace(typet &dest) const
   else if(dest.id()==ID_code)
   {
     code_typet &code_type=to_code_type(dest);
-    (void)replace(code_type.return_type());
+    replace(code_type.return_type());
     code_typet::parameterst &parameters=code_type.parameters();
     for(code_typet::parameterst::iterator it = parameters.begin();
         it!=parameters.end();
