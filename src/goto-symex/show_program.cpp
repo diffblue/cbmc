a889a166e6593c0a4281c276194581c325f946e6
Peter Schrammel <peter.schrammel@diffblue.com>
2018-09-02 21:59:20 +0100

    Clang-format

--- src/goto-symex/show_program.cpp
+++ src/goto-symex/show_program.cpp
@@ -17,12 +17,13 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <langapi/language_util.h>
 
-
 void show_program(const namespacet &ns, const symex_target_equationt &equation)
 {
-  unsigned count=1;
+  unsigned count = 1;
 
-  std::cout << "\n" << "Program constraints:" << "\n";
+  std::cout << "\n"
+            << "Program constraints:"
+            << "\n";
 
   for(const auto &step : equation.SSA_steps)
   {
@@ -38,7 +39,7 @@ void show_program(const namespacet &ns, const symex_target_equationt &equation)
       if(!step.guard.is_true())
       {
         std::string string_value = from_expr(ns, function, step.guard);
-        std::cout << std::string(std::to_string(count).size()+3, ' ');
+        std::cout << std::string(std::to_string(count).size() + 3, ' ');
         std::cout << "guard: " << string_value << "\n";
       }
 
@@ -47,13 +48,13 @@ void show_program(const namespacet &ns, const symex_target_equationt &equation)
     else if(step.is_assert())
     {
       std::string string_value = from_expr(ns, function, step.cond_expr);
-      std::cout << "(" << count << ") ASSERT("
-                << string_value <<") " << "\n";
+      std::cout << "(" << count << ") ASSERT(" << string_value << ") "
+                << "\n";
 
       if(!step.guard.is_true())
       {
         std::string string_value = from_expr(ns, function, step.guard);
-        std::cout << std::string(std::to_string(count).size()+3, ' ');
+        std::cout << std::string(std::to_string(count).size() + 3, ' ');
         std::cout << "guard: " << string_value << "\n";
       }
 
@@ -62,13 +63,13 @@ void show_program(const namespacet &ns, const symex_target_equationt &equation)
     else if(step.is_assume())
     {
       std::string string_value = from_expr(ns, function, step.cond_expr);
-      std::cout << "(" << count << ") ASSUME("
-                << string_value <<") " << "\n";
+      std::cout << "(" << count << ") ASSUME(" << string_value << ") "
+                << "\n";
 
       if(!step.guard.is_true())
       {
         std::string string_value = from_expr(ns, function, step.guard);
-        std::cout << std::string(std::to_string(count).size()+3, ' ');
+        std::cout << std::string(std::to_string(count).size() + 3, ' ');
         std::cout << "guard: " << string_value << "\n";
       }
 
@@ -77,8 +78,8 @@ void show_program(const namespacet &ns, const symex_target_equationt &equation)
     else if(step.is_constraint())
     {
       std::string string_value = from_expr(ns, function, step.cond_expr);
-      std::cout << "(" << count << ") CONSTRAINT("
-                << string_value <<") " << "\n";
+      std::cout << "(" << count << ") CONSTRAINT(" << string_value << ") "
+                << "\n";
 
       count++;
     }
@@ -86,13 +87,13 @@ void show_program(const namespacet &ns, const symex_target_equationt &equation)
     {
       std::string string_value = from_expr(ns, function, step.ssa_lhs);
       std::cout << "(" << count << ") SHARED_"
-                << (step.is_shared_write()?"WRITE":"READ")
-                << "(" << string_value <<")\n";
+                << (step.is_shared_write() ? "WRITE" : "READ") << "("
+                << string_value << ")\n";
 
       if(!step.guard.is_true())
       {
         std::string string_value = from_expr(ns, function, step.guard);
-        std::cout << std::string(std::to_string(count).size()+3, ' ');
+        std::cout << std::string(std::to_string(count).size() + 3, ' ');
         std::cout << "guard: " << string_value << "\n";
       }
 
