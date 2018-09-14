c5f34881061a170d763779ebdd6e499f706addbe
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-09-14 17:51:24 +0100

    Cleanup invariants in link_goto_model

--- src/goto-programs/link_goto_model.cpp
+++ src/goto-programs/link_goto_model.cpp
@@ -18,6 +18,7 @@ Author: Michael Tautschnig, Daniel Kroening
 #include <util/rename_symbol.h>
 
 #include <linking/linking_class.h>
+#include <util/exception_utils.h>
 
 #include "goto_model.h"
 
@@ -171,15 +172,18 @@ void link_goto_model(
                    message_handler);
 
   if(linking.typecheck_main())
-    throw 0;
-
+  {
+    throw invalid_source_file_exceptiont("typechecking main failed");
+  }
   if(link_functions(
-      dest.symbol_table,
-      dest.goto_functions,
-      src.symbol_table,
-      src.goto_functions,
-      linking.rename_symbol,
-      weak_symbols,
-      linking.object_type_updates))
-    throw 0;
+       dest.symbol_table,
+       dest.goto_functions,
+       src.symbol_table,
+       src.goto_functions,
+       linking.rename_symbol,
+       weak_symbols,
+       linking.object_type_updates))
+  {
+    throw invalid_source_file_exceptiont("linking failed");
+  }
 }
