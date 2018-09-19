a23f4555975e8fa876350e87f3469e4a39fa1f79
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-09-19 22:16:30 +0100

    format(type) now prints unions and struct/union/c_enum tags

--- src/util/format_type.cpp
+++ src/util/format_type.cpp
@@ -32,6 +32,26 @@ static std::ostream &format_rec(std::ostream &os, const struct_typet &src)
   return os << " }";
 }
 
+/// format a \ref union_typet
+static std::ostream &format_rec(std::ostream &os, const union_typet &src)
+{
+  os << "union"
+     << " {";
+  bool first = true;
+
+  for(const auto &c : src.components())
+  {
+    if(first)
+      first = false;
+    else
+      os << ',';
+
+    os << ' ' << format(c.type()) << ' ' << c.get_name();
+  }
+
+  return os << " }";
+}
+
 // The below generates a string in a generic syntax
 // that is inspired by C/C++/Java, and is meant for debugging
 // purposes.
@@ -51,6 +71,14 @@ std::ostream &format_rec(std::ostream &os, const typet &type)
   }
   else if(id == ID_struct)
     return format_rec(os, to_struct_type(type));
+  else if(id == ID_union)
+    return format_rec(os, to_union_type(type));
+  else if(id == ID_union_tag)
+    return os << "union " << to_union_tag_type(type).get_identifier();
+  else if(id == ID_struct_tag)
+    return os << "struct " << to_struct_tag_type(type).get_identifier();
+  else if(id == ID_c_enum_tag)
+    return os << "c_enum " << to_c_enum_tag_type(type).get_identifier();
   else if(id == ID_symbol_type)
     return os << "symbol_type " << to_symbol_type(type).get_identifier();
   else
