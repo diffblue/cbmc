5a0feaa1a7278d25d3730b5994eaa94a170668a4
Matthias Güdemann <matthias.guedemann@diffblue.com>
2018-09-20 16:46:48 +0200

    Add unit test to check for generic local variables
    The test checks that a local variable has the correct generic type and
    instantiation.

---
+++ jbmc/unit/java_bytecode/java_bytecode_parse_generics/GenericLocalVar.java
@@ -0,0 +1,5 @@
+public class GenericLocalVar {
+  public void f() {
+    java.util.ArrayList<Double> l = null;
+  }
+}
---
+++ jbmc/unit/java_bytecode/java_bytecode_parse_generics/parse_lvtt_generic_local_vars.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************\
+
+ Module: Unit tests for parsing generic local variables from the LVTT
+
+ Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#include <java-testing-utils/load_java_class.h>
+#include <java-testing-utils/require_type.h>
+#include <testing-utils/catch.hpp>
+
+SCENARIO(
+  "parse_lvtt_generic_local_vars",
+  "[core][java_bytecode][java_bytecode_parse_generics]")
+{
+  const symbol_tablet &new_symbol_table = load_java_class(
+    "GenericLocalVar", "./java_bytecode/java_bytecode_parse_generics");
+
+  const std::string var_name("java::GenericLocalVar.f:()V::1::l");
+  REQUIRE(new_symbol_table.has_symbol(var_name));
+
+  WHEN("Local variable has an entry in the LVTT")
+  {
+    THEN("The type should be generic and instantiated with Double")
+    {
+      const symbolt &var_symbol = new_symbol_table.lookup_ref(var_name);
+      java_generic_typet local_var_type =
+        require_type::require_java_generic_type(
+          var_symbol.type,
+          {{require_type::type_argument_kindt::Inst,
+            "java::java.lang.Double"}});
+    }
+  }
+}
