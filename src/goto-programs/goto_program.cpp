466f12b3dc480b19bb0fd7db0788a5b86cd051f4
Peter Schrammel <peter.schrammel@diffblue.com>
2018-09-24 11:27:00 +0100

    Move contents of goto_program_template.cpp to goto_program.cpp
    goto_program_template.cpp seems to be a relict of times
    where we had goto_program_template.h

--- src/goto-programs/goto_program.cpp
+++ src/goto-programs/goto_program.cpp
@@ -698,3 +698,31 @@ bool goto_programt::equals(const goto_programt &other) const
 
   return true;
 }
+
+std::ostream &operator<<(std::ostream &out, goto_program_instruction_typet t)
+{
+  switch(t)
+  {
+    case NO_INSTRUCTION_TYPE: out << "NO_INSTRUCTION_TYPE"; break;
+    case GOTO: out << "GOTO"; break;
+    case ASSUME: out << "ASSUME"; break;
+    case ASSERT: out << "ASSERT"; break;
+    case OTHER: out << "OTHER"; break;
+    case DECL: out << "DECL"; break;
+    case DEAD: out << "DEAD"; break;
+    case SKIP: out << "SKIP"; break;
+    case START_THREAD: out << "START_THREAD"; break;
+    case END_THREAD: out << "END_THREAD"; break;
+    case LOCATION: out << "LOCATION"; break;
+    case END_FUNCTION: out << "END_FUNCTION"; break;
+    case ATOMIC_BEGIN: out << "ATOMIC_BEGIN"; break;
+    case ATOMIC_END: out << "ATOMIC_END"; break;
+    case RETURN: out << "RETURN"; break;
+    case ASSIGN: out << "ASSIGN"; break;
+    case FUNCTION_CALL: out << "FUNCTION_CALL"; break;
+    default:
+      out << "?";
+  }
+
+  return out;
+}
