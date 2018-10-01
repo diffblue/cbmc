e21f5ac142ee6d35021057d7333f45c7cff9abd9
Michael Tautschnig <tautschn@amazon.com>
2018-10-01 08:31:21 +0100

    Only test for __float80 on architectures where it is available
    Our C front-end carefully selects the architectures where it should be
    supported, but regression tests were entirely general.

--- regression/ansi-c/gcc_float_types1/main.c
+++ regression/ansi-c/gcc_float_types1/main.c
@@ -1,9 +1,12 @@
 // for gcc, __float80 and __float128 are typedefs
 // for clang, __float128 is a keyword, and __float80 doesn't exist.
 
-#ifdef __clang__
+#if defined(__clang__) || \
+    !(defined(__ia64__) || defined(__x86_64__) || defined(__i386__))
 int __float80;
+#if (defined(__hppa__) || defined(__powerpc__))
 __float128 f128;
+#endif
 #else
 __float80 f80;
 __float128 f128;
