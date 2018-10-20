10226d2e03d8d7589d0cf52d0676912fd8e4f0f7
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-10-20 23:04:32 +0100

    refactoring: factor out analysis

--- src/goto-analyzer/static_verifier.cpp
+++ src/goto-analyzer/static_verifier.cpp
@@ -31,177 +31,184 @@ bool static_verifier(
   message_handlert &message_handler,
   std::ostream &out)
 {
-  std::size_t pass=0, fail=0, unknown=0;
+  std::size_t pass = 0, fail = 0, unknown = 0;
 
   namespacet ns(goto_model.symbol_table);
 
   messaget m(message_handler);
   m.status() << "Checking assertions" << messaget::eom;
 
-  if(options.get_bool_option("json"))
+  struct resultt
   {
-    json_arrayt json_result;
+    // clang-format off
+    enum { TRUE, FALSE, BOTTOM, UNKNOWN } status;
+    // clang-format on
+    source_locationt source_location;
+    irep_idt function_id;
+  };
 
-    forall_goto_functions(f_it, goto_model.goto_functions)
+  std::vector<resultt> results;
+
+  for(const auto &f : goto_model.goto_functions.function_map)
+  {
+    const auto &symbol = ns.lookup(f.first);
+
+    m.progress() << "Checking " << symbol.display_name() << messaget::eom;
+
+    if(!f.second.body.has_assertion())
+      continue;
+
+    forall_goto_program_instructions(i_it, f.second.body)
     {
-      const auto &symbol = ns.lookup(f_it->first);
-
-      m.progress() << "Checking " << symbol.display_name() << messaget::eom;
-
-      if(!f_it->second.body.has_assertion())
+      if(!i_it->is_assert())
         continue;
 
-      forall_goto_program_instructions(i_it, f_it->second.body)
+      exprt e(i_it->guard);
+      auto dp = ai.abstract_state_before(i_it);
+      const ai_domain_baset &domain(*dp);
+      domain.ai_simplify(e, ns);
+
+      results.push_back(resultt());
+      auto &result = results.back();
+
+      if(e.is_true())
       {
-        if(!i_it->is_assert())
-          continue;
-
-        exprt e(i_it->guard);
-        auto dp = ai.abstract_state_before(i_it);
-        const ai_domain_baset &domain(*dp);
-        domain.ai_simplify(e, ns);
-
-        json_objectt &j=json_result.push_back().make_object();
-
-        if(e.is_true())
-        {
-          j["status"]=json_stringt("SUCCESS");
-          ++pass;
-        }
-        else if(e.is_false())
-        {
-          j["status"]=json_stringt("FAILURE (if reachable)");
-          ++fail;
-        }
-        else if(domain.is_bottom())
-        {
-          j["status"]=json_stringt("SUCCESS (unreachable)");
-          ++pass;
-        }
-        else
-        {
-          j["status"]=json_stringt("UNKNOWN");
-          ++unknown;
-        }
-
-        j["sourceLocation"]=json(i_it->source_location);
+        result.status = resultt::TRUE;
+        ++pass;
       }
+      else if(e.is_false())
+      {
+        result.status = resultt::FALSE;
+        ++fail;
+      }
+      else if(domain.is_bottom())
+      {
+        result.status = resultt::BOTTOM;
+        ++pass;
+      }
+      else
+      {
+        result.status = resultt::UNKNOWN;
+        ++unknown;
+      }
+
+      result.source_location = i_it->source_location;
+      result.function_id = f.first;
     }
+  }
+
+  if(options.get_bool_option("json"))
+  {
     m.status() << "Writing JSON report" << messaget::eom;
+
+    json_arrayt json_result;
+
+    for(const auto &result : results)
+    {
+      json_objectt &j = json_result.push_back().make_object();
+
+      switch(result.status)
+      {
+      case resultt::TRUE:
+        j["status"] = json_stringt("SUCCESS");
+        break;
+
+      case resultt::FALSE:
+        j["status"] = json_stringt("FAILURE (if reachable)");
+        break;
+
+      case resultt::BOTTOM:
+        j["status"] = json_stringt("SUCCESS (unreachable)");
+        break;
+
+      case resultt::UNKNOWN:
+        j["status"] = json_stringt("UNKNOWN");
+        ++unknown;
+        break;
+      }
+
+      j["sourceLocation"] = json(result.source_location);
+    }
+
     out << json_result;
   }
   else if(options.get_bool_option("xml"))
   {
+    m.status() << "Writing XML report" << messaget::eom;
+
     xmlt xml_result;
 
-    forall_goto_functions(f_it, goto_model.goto_functions)
+    for(const auto &result : results)
     {
-      const auto &symbol = ns.lookup(f_it->first);
+      xmlt &x = xml_result.new_element("result");
 
-      m.progress() << "Checking " << symbol.display_name() << messaget::eom;
-
-      if(!f_it->second.body.has_assertion())
-        continue;
-
-      forall_goto_program_instructions(i_it, f_it->second.body)
+      switch(result.status)
       {
-        if(!i_it->is_assert())
-          continue;
+      case resultt::TRUE:
+        x.set_attribute("status", "SUCCESS");
+        break;
 
-        exprt e(i_it->guard);
-        auto dp = ai.abstract_state_before(i_it);
-        const ai_domain_baset &domain(*dp);
-        domain.ai_simplify(e, ns);
+      case resultt::FALSE:
+        x.set_attribute("status", "FAILURE (if reachable)");
+        break;
 
-        xmlt &x=xml_result.new_element("result");
+      case resultt::BOTTOM:
+        x.set_attribute("status", "SUCCESS (unreachable)");
+        break;
 
-        if(e.is_true())
-        {
-          x.set_attribute("status", "SUCCESS");
-          ++pass;
-        }
-        else if(e.is_false())
-        {
-          x.set_attribute("status", "FAILURE (if reachable)");
-          ++fail;
-        }
-        else if(domain.is_bottom())
-        {
-          x.set_attribute("status", "SUCCESS (unreachable)");
-          ++pass;
-        }
-        else
-        {
-          x.set_attribute("status", "UNKNOWN");
-          ++unknown;
-        }
-
-        x.set_attribute("file", id2string(i_it->source_location.get_file()));
-        x.set_attribute("line", id2string(i_it->source_location.get_line()));
-        x.set_attribute(
-          "description",
-          id2string(i_it->source_location.get_comment()));
+      case resultt::UNKNOWN:
+        x.set_attribute("status", "UNKNOWN");
       }
+
+      x.set_attribute("file", id2string(result.source_location.get_file()));
+      x.set_attribute("line", id2string(result.source_location.get_line()));
+      x.set_attribute(
+        "description", id2string(result.source_location.get_comment()));
     }
 
-    m.status() << "Writing XML report" << messaget::eom;
     out << xml_result;
   }
   else if(options.get_bool_option("text"))
   {
-    forall_goto_functions(f_it, goto_model.goto_functions)
+    irep_idt last_function_id;
+
+    for(const auto &result : results)
     {
-      const auto &symbol = ns.lookup(f_it->first);
-
-      m.progress() << "Checking " << symbol.display_name() << messaget::eom;
-
-      if(!f_it->second.body.has_assertion())
-        continue;
-
-      out << "******** Function " << symbol.display_name() << '\n';
-
-      forall_goto_program_instructions(i_it, f_it->second.body)
+      if(last_function_id != result.function_id)
       {
-        if(!i_it->is_assert())
-          continue;
+        if(!last_function_id.empty())
+          out << '\n';
+        last_function_id = result.function_id;
+        const auto &symbol = ns.lookup(last_function_id);
+        out << "******** Function " << symbol.display_name() << '\n';
+      }
 
-        exprt e(i_it->guard);
-        auto dp = ai.abstract_state_before(i_it);
-        const ai_domain_baset &domain(*dp);
-        domain.ai_simplify(e, ns);
+      out << '[' << result.source_location.get_property_id() << ']' << ' ';
 
-        out << '[' << i_it->source_location.get_property_id()
-            << ']' << ' ';
+      out << result.source_location;
 
-        out << i_it->source_location;
+      if(!result.source_location.get_comment().empty())
+        out << ", " << result.source_location.get_comment();
 
-        if(!i_it->source_location.get_comment().empty())
-          out << ", " << i_it->source_location.get_comment();
+      out << ": ";
 
-        out << ": ";
+      switch(result.status)
+      {
+      case resultt::TRUE:
+        out << "Success";
+        break;
 
-        if(e.is_true())
-        {
-          out << "Success";
-          pass++;
-        }
-        else if(e.is_false())
-        {
-          out << "Failure (if reachable)";
-          fail++;
-        }
-        else if(domain.is_bottom())
-        {
-          out << "Success (unreachable)";
-          pass++;
-        }
-        else
-        {
-          out << "Unknown";
-          unknown++;
-        }
+      case resultt::FALSE:
+        out << "Failure (if reachable)";
+        break;
 
-        out << '\n';
+      case resultt::BOTTOM:
+        out << "Success (unreachable)";
+        break;
+
+      case resultt::UNKNOWN:
+        out << "Unknown";
+        break;
       }
 
       out << '\n';
@@ -209,58 +216,46 @@ bool static_verifier(
   }
   else
   {
-    forall_goto_functions(f_it, goto_model.goto_functions)
+    irep_idt last_function_id;
+
+    for(const auto &result : results)
     {
-      if(!f_it->second.body.has_assertion())
-        continue;
-
-      const auto &symbol = ns.lookup(f_it->first);
-
-      m.result() << "******** Function " << symbol.display_name()
-                 << messaget::eom;
-
-      forall_goto_program_instructions(i_it, f_it->second.body)
+      if(last_function_id != result.function_id)
       {
-        if(!i_it->is_assert())
-          continue;
+        if(!last_function_id.empty())
+          out << '\n';
+        last_function_id = result.function_id;
+        const auto &symbol = ns.lookup(last_function_id);
+        out << "******** Function " << symbol.display_name() << '\n';
+      }
 
-        exprt e(i_it->guard);
-        auto dp = ai.abstract_state_before(i_it);
-        const ai_domain_baset &domain(*dp);
-        domain.ai_simplify(e, ns);
+      m.result() << '[' << result.source_location.get_property_id() << ']'
+                 << ' ';
 
-        m.result() << '[' << i_it->source_location.get_property_id() << ']'
-                   << ' ';
+      m.result() << result.source_location;
 
-        m.result() << i_it->source_location;
+      if(!result.source_location.get_comment().empty())
+        m.result() << ", " << result.source_location.get_comment();
 
-        if(!i_it->source_location.get_comment().empty())
-          m.result() << ", " << i_it->source_location.get_comment();
+      m.result() << ": ";
 
-        m.result() << ": ";
+      switch(result.status)
+      {
+      case resultt::TRUE:
+        m.result() << m.green << "SUCCESS" << m.reset;
+        break;
 
-        if(e.is_true())
-        {
-          m.result() << m.green << "SUCCESS" << m.reset;
-          pass++;
-        }
-        else if(e.is_false())
-        {
-          m.result() << m.red << "FAILURE" << m.reset << " (if reachable)";
-          fail++;
-        }
-        else if(domain.is_bottom())
-        {
-          m.result() << m.green << "SUCCESS" << m.reset << " (unreachable)";
-          pass++;
-        }
-        else
-        {
-          m.result() << m.yellow << "UNKNOWN" << m.reset;
-          unknown++;
-        }
+      case resultt::FALSE:
+        m.result() << m.red << "FAILURE" << m.reset << " (if reachable)";
+        break;
 
-        m.result() << messaget::eom;
+      case resultt::BOTTOM:
+        m.result() << m.green << "SUCCESS" << m.reset << " (unreachable)";
+        break;
+
+      case resultt::UNKNOWN:
+        m.result() << m.yellow << "UNKNOWN" << m.reset;
+        break;
       }
 
       m.result() << messaget::eom;
