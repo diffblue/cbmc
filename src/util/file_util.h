318922e5fda263a914d85dcc7f64b9a62424173e
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-10-21 11:02:07 +0100

    comments on future replacements of util/file_util.h functions

--- src/util/file_util.h
+++ src/util/file_util.h
@@ -12,14 +12,17 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <string>
 
+// C++17 will allow us to use std::filesystem::path::remove_all
 void delete_directory(const std::string &path);
 
+// C++17 will allow us to use std::filesystem::current_path
 std::string get_current_working_directory();
 
+// C++17 will allow us to use std::filesystem::path(dir).append(file)
 std::string concat_dir_file(const std::string &directory,
                             const std::string &file_name);
 
-// C++17 will allow us to use std::filesystem::is_directory()
+// C++17 will allow us to use std::filesystem::is_directory
 bool is_directory(const std::string &path);
 
 #endif // CPROVER_UTIL_FILE_UTIL_H
