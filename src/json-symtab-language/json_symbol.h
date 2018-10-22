3b3c4f3fd9f4d24d00ec7c7f79a46d0ae91ac8f2
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2018-10-22 15:55:28 +0100

    Fix a mistake with the Makefile after files were moved into a new folder.

--- src/json-symtab-language/json_symbol.h
+++ src/json-symtab-language/json_symbol.h
@@ -9,8 +9,8 @@ Author: Chris Smowton, chris.smowton@diffblue.com
 #ifndef CPROVER_UTIL_JSON_SYMBOL_H
 #define CPROVER_UTIL_JSON_SYMBOL_H
 
-#include "json.h"
-#include "symbol.h"
+#include <util/json.h>
+#include <util/symbol.h>
 
 symbolt symbol_from_json(const jsont &);
 
