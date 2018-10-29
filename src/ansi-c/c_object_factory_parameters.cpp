7f95dd81d18893d770a4f6bef4b6c46407658890
Daniel Poetzl <daniel.poetzl@diffblue.com>
2018-10-29 13:52:05 +0000

    Add c object factory parameters

---
+++ src/ansi-c/c_object_factory_parameters.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Poetzl
+
+\*******************************************************************/
+
+#include "c_object_factory_parameters.h"
+
+void parse_c_object_factory_options(const cmdlinet &cmdline, optionst &options)
+{
+  parse_object_factory_options(cmdline, options);
+}
