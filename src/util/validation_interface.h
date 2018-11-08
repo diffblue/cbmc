d11cece5c474f8a277e0ad46f4e02d36493e5747
Daniel Poetzl <daniel.poetzl@diffblue.com>
2018-11-08 17:50:57 +0000

    Add macros for common command line interface for goto model validation

---
+++ src/util/validation_interface.h
@@ -0,0 +1,19 @@
+/*******************************************************************\
+
+Module: Goto program validation common command line options
+
+Author: Daniel Poetzl
+
+\*******************************************************************/
+
+#ifndef CPROVER_UTIL_VALIDATION_INTERFACE_H
+#define CPROVER_UTIL_VALIDATION_INTERFACE_H
+
+#define OPT_VALIDATE "(validate-goto-model)"
+
+#define HELP_VALIDATE                                                          \
+  " --validate-goto-model        enable additional well-formedness checks on " \
+  "the\n"                                                                      \
+  "                              goto program\n"
+
+#endif /* CPROVER_UTIL_VALIDATION_INTERFACE_H */
