fa39d19e9de42f71381f5518ccb161e828543719
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-11-12 15:02:10 +0000

    Add optional_lookup utility function
    This provides a more ergonomic interface over map::find

---
+++ src/util/optional_utils.h
@@ -0,0 +1,28 @@
+/*******************************************************************\
+
+ Module: functions that are useful with optionalt
+
+ Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#ifndef CPROVER_UTIL_OPTIONAL_UTILS_H
+#define CPROVER_UTIL_OPTIONAL_UTILS_H
+
+#include "optional.h"
+
+/// Lookup a key in a map, if found return the associated value,
+/// nullopt otherwise
+template <typename map_like_collectiont, typename keyt>
+auto optional_lookup(const map_like_collectiont &map, const keyt &key)
+  -> optionalt<decltype(map.find(key)->second)>
+{
+  auto const it = map.find(key);
+  if(it != map.end())
+  {
+    return it->second;
+  }
+  return nullopt;
+}
+
+#endif // CPROVER_UTIL_OPTIONAL_UTILS_H
