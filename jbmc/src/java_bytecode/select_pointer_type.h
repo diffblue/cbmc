53126060ed61862d2637d6eb70c2ebe36355fb6e
svorenova <maria.svorenova@diffblue.com>
2018-11-13 10:54:41 +0000

    Move documentation of public functions to the header

--- jbmc/src/java_bytecode/select_pointer_type.h
+++ jbmc/src/java_bytecode/select_pointer_type.h
@@ -36,6 +36,16 @@ class select_pointer_typet
 
 public:
   virtual ~select_pointer_typet() = default;
+
+  /// Select what type should be used for a given pointer type. For the base
+  /// class we just use the supplied type. Derived classes can override this
+  /// behaviour to provide more sophisticated type selection. Generic
+  /// parameters are replaced with their concrete type.
+  /// \param pointer_type: The pointer type replace
+  /// \param generic_parameter_specialization_map map of types for all generic
+  /// parameters in the current scope
+  /// \param ns Namespace for type lookups
+  /// \return A pointer type where the subtype may have been modified
   virtual pointer_typet convert_pointer_type(
     const pointer_typet &pointer_type,
     const generic_parameter_specialization_mapt
@@ -47,6 +57,24 @@ public:
     const irep_idt &parameter_name,
     const namespacet &ns) const;
 
+  /// Specialize generic parameters in a pointer type based on the current map
+  /// of parameters -> types. We specialize generics if the pointer is a java
+  /// generic parameter or an array with generic parameters (java generic types
+  /// are specialized recursively, their concrete types are already stored in
+  /// the map and will be retrieved when needed e.g., to initialize fields).
+  /// Example:
+  /// - generic type: T
+  /// - map: T -> U; U -> String
+  /// - result: String
+  ///
+  /// - generic type: T[]
+  /// - map: T -> U; U -> String
+  /// - result: String
+  /// \param pointer_type pointer to be specialized
+  /// \param generic_parameter_specialization_map map of types for all generic
+  /// parameters in the current scope
+  /// \return pointer type where generic parameters are replaced with concrete
+  /// types, if set in the current scope
   pointer_typet specialize_generics(
     const pointer_typet &pointer_type,
     const generic_parameter_specialization_mapt
