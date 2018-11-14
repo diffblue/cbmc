75b18015b98a1700ce865c80cba935a6f5ea193b
svorenova <maria.svorenova@diffblue.com>
2018-11-14 10:32:48 +0000

    Update function docs

--- jbmc/src/java_bytecode/select_pointer_type.h
+++ jbmc/src/java_bytecode/select_pointer_type.h
@@ -37,14 +37,14 @@ class select_pointer_typet
 public:
   virtual ~select_pointer_typet() = default;
 
-  /// Select what type should be used for a given pointer type. For the base
+  /// Select what type should be used for a given pointer type. In the base
   /// class we just use the supplied type. Derived classes can override this
-  /// behaviour to provide more sophisticated type selection. Generic
-  /// parameters are replaced with their concrete type.
-  /// \param pointer_type: The pointer type replace
-  /// \param generic_parameter_specialization_map map of types for all generic
-  /// parameters in the current scope
-  /// \param ns Namespace for type lookups
+  /// behavior to provide more sophisticated type selection. Generic
+  /// parameters are replaced with their specialized type.
+  /// \param pointer_type: The pointer type to convert
+  /// \param generic_parameter_specialization_map: Map of types for all generic
+  ///   parameters in the current scope
+  /// \param ns: Namespace for type lookups
   /// \return A pointer type where the subtype may have been modified
   virtual pointer_typet convert_pointer_type(
     const pointer_typet &pointer_type,
@@ -52,19 +52,22 @@ public:
       &generic_parameter_specialization_map,
     const namespacet &ns) const;
 
-  /// Get alternative types for a method parameter. For the base class we
-  /// just return an empty set. Derived classes can override this behaviour
-  /// to provide more sophisticated alternative type identification.
+  /// Get alternative types for a method parameter, e.g., based on the casts in
+  /// the function body. In the base class we just return an empty set.
+  /// Derived classes can override this behaviour to provide more
+  /// sophisticated alternative type identification.
   virtual std::set<symbol_typet> get_parameter_alternative_types(
     const irep_idt &function_name,
     const irep_idt &parameter_name,
     const namespacet &ns) const;
 
   /// Specialize generic parameters in a pointer type based on the current map
-  /// of parameters -> types. We specialize generics if the pointer is a java
-  /// generic parameter or an array with generic parameters (java generic types
-  /// are specialized recursively, their concrete types are already stored in
-  /// the map and will be retrieved when needed e.g., to initialize fields).
+  /// of parameters -> types. We specialize generics only if the pointer is a
+  /// java generic parameter or an array with generic parameters. More
+  /// general generic types such as `MyGeneric<T>` are specialized
+  /// indirectly in \ref java_object_factoryt, their concrete types are already
+  /// stored in the map and will be retrieved when needed e.g., to initialize
+  /// fields.
   /// Example:
   /// - generic type: T
   /// - map: T -> U; U -> String
@@ -72,14 +75,14 @@ public:
   ///
   /// - generic type: T[]
   /// - map: T -> U; U -> String
-  /// - result: String
-  /// \param pointer_type pointer to be specialized
-  /// \param generic_parameter_specialization_map map of types for all generic
-  /// parameters in the current scope
-  /// \param visited_nodes set of parameter names already considered in
+  /// - result: String[]
+  /// \param pointer_type: The pointer to be specialized
+  /// \param generic_parameter_specialization_map: Map of types for all
+  ///   generic parameters in the current scope
+  /// \param visited_nodes: Set of parameter names already considered in
   ///   recursion, used to avoid infinite recursion
-  /// \return pointer type where generic parameters are replaced with concrete
-  /// types, if set in the current scope
+  /// \return Pointer type where generic parameters are replaced with
+  ///   specialized types (if set in the current scope)
   pointer_typet specialize_generics(
     const pointer_typet &pointer_type,
     const generic_parameter_specialization_mapt
