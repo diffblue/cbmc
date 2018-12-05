bc281cc199c6b80a1ab424e233d93fd9a5b0e882
Petr Bauch <petr.bauch@DB0276.ox.diffblue.com>
2018-12-05 10:43:25 +0000

    Symbol consistency between goto programs and symbol table
    Iterate over all symbols in a goto program and for each one check that it is
    present in the symbol table as well. Includes a unit test for the check.

--- src/goto-programs/goto_program.cpp
+++ src/goto-programs/goto_program.cpp
@@ -14,6 +14,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <ostream>
 #include <iomanip>
 
+#include <util/find_symbols.h>
 #include <util/std_expr.h>
 #include <util/validate.h>
 
@@ -677,6 +678,26 @@ void goto_programt::instructiont::validate(
   validate_full_expr(guard, ns, vm);
 
   const symbolt *table_symbol;
+  DATA_CHECK_WITH_DIAGNOSTICS(
+    vm,
+    !ns.lookup(function, table_symbol),
+    id2string(function) + " not found",
+    source_location);
+
+  auto expr_symbol_finder = [&](const exprt &e) {
+    find_symbols_sett typetags;
+    find_type_symbols(e.type(), typetags);
+    find_symbols(e, typetags);
+    const symbolt *symbol;
+    for(const auto &identifier : typetags)
+    {
+      DATA_CHECK_WITH_DIAGNOSTICS(
+        vm,
+        !ns.lookup(identifier, symbol),
+        id2string(identifier) + " not found",
+        source_location);
+    }
+  };
 
   switch(type)
   {
@@ -708,6 +729,7 @@ void goto_programt::instructiont::validate(
       targets.empty(),
       "assert instruction should not have a target",
       source_location);
+    std::for_each(guard.depth_begin(), guard.depth_end(), expr_symbol_finder);
     break;
   case OTHER:
     break;
@@ -772,6 +794,7 @@ void goto_programt::instructiont::validate(
       code.get_statement() == ID_function_call,
       "function call instruction should contain a call statement",
       source_location);
+    std::for_each(code.depth_begin(), code.depth_end(), expr_symbol_finder);
     break;
   case THROW:
     break;
