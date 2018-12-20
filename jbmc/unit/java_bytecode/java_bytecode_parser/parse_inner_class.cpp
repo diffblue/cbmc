28fbcce828e8f8c2cc05c67242c4cdce471aacf4
Thomas Spriggs <thomas.spriggs@diffblue.com>
2018-12-20 13:15:03 +0000

    Rerun inner class unit test with outer and inner loaded first

--- jbmc/unit/java_bytecode/java_bytecode_parser/parse_inner_class.cpp
+++ jbmc/unit/java_bytecode/java_bytecode_parser/parse_inner_class.cpp
@@ -10,97 +10,103 @@
 #include <java_bytecode/java_types.h>
 #include <testing-utils/catch.hpp>
 
+#include <string>
+#include <vector>
+
 SCENARIO(
   "Parsing inner java classes",
   "[core][java_bytecode][java_bytecode_parser]")
 {
-  GIVEN("A class file of an inner class.")
+  for(const auto &load_first : std::vector<std::string>{"Outer$Inner", "Outer"})
   {
-    WHEN("Parsing an inner class called \"Inner\".")
-    {
-      const symbol_tablet &symbol_table = load_java_class(
-        "Outer$Inner", "./java_bytecode/java_bytecode_parser");
-      const symbolt *class_symbol = symbol_table.lookup("java::Outer$Inner");
-      REQUIRE(class_symbol);
-      const java_class_typet *class_type =
-        type_try_dynamic_cast<java_class_typet>(class_symbol->type);
-      REQUIRE(class_type);
+    const symbol_tablet &symbol_table =
+      load_java_class(load_first, "./java_bytecode/java_bytecode_parser");
 
-      THEN("The inner class should have the name \"java::Outer$Inner\".")
+    GIVEN("A class file of an inner class and " + load_first + " loaded first.")
+    {
+      WHEN("Parsing an inner class called \"Inner\".")
       {
-        REQUIRE(id2string(class_type->get_name()) == "java::Outer$Inner");
-      }
-      THEN("The inner class should have the inner name \"Inner\".")
-      {
-        REQUIRE(id2string(class_type->get_inner_name()) == "Inner");
-      }
-      THEN("The inner class is not considered to be an anonymous class.")
-      {
-        REQUIRE_FALSE(class_type->get_is_anonymous_class());
-      }
-      THEN("The inner class has the outer class \"Outer\".")
-      {
-        REQUIRE(id2string(class_type->get_outer_class()) == "Outer");
+        const symbolt *class_symbol = symbol_table.lookup("java::Outer$Inner");
+        REQUIRE(class_symbol);
+        const java_class_typet *class_type =
+          type_try_dynamic_cast<java_class_typet>(class_symbol->type);
+        REQUIRE(class_type);
+
+        THEN("The inner class should have the name \"java::Outer$Inner\".")
+        {
+          REQUIRE(id2string(class_type->get_name()) == "java::Outer$Inner");
+        }
+        THEN("The inner class should have the inner name \"Inner\".")
+        {
+          REQUIRE(id2string(class_type->get_inner_name()) == "Inner");
+        }
+        THEN("The inner class is not considered to be an anonymous class.")
+        {
+          REQUIRE_FALSE(class_type->get_is_anonymous_class());
+        }
+        THEN("The inner class has the outer class \"Outer\".")
+        {
+          REQUIRE(id2string(class_type->get_outer_class()) == "Outer");
+        }
       }
     }
-  }
-  GIVEN("A class file of a class which is not an inner class.")
-  {
-    WHEN("Parsing an outer class.")
+    GIVEN(
+      "A class file of a class which is not an inner class and " + load_first +
+      " loaded first.")
     {
-      const symbol_tablet &symbol_table =
-        load_java_class("Outer", "./java_bytecode/java_bytecode_parser");
-      const symbolt *class_symbol = symbol_table.lookup("java::Outer");
-      REQUIRE(class_symbol);
-      const java_class_typet *class_type =
-        type_try_dynamic_cast<java_class_typet>(class_symbol->type);
-      REQUIRE(class_type);
+      WHEN("Parsing an outer class.")
+      {
+        const symbolt *class_symbol = symbol_table.lookup("java::Outer");
+        REQUIRE(class_symbol);
+        const java_class_typet *class_type =
+          type_try_dynamic_cast<java_class_typet>(class_symbol->type);
+        REQUIRE(class_type);
 
-      THEN("The outer class should have the name \"java::Outer\".")
-      {
-        REQUIRE(id2string(class_type->get_name()) == "java::Outer");
-      }
-      THEN("The outer class should not have an inner name.")
-      {
-        REQUIRE(id2string(class_type->get_inner_name()).empty());
-      }
-      THEN("The outer class is not considered to be an anonymous class.")
-      {
-        REQUIRE_FALSE(class_type->get_is_anonymous_class());
-      }
-      THEN("The outer class does not have an outer class.")
-      {
-        REQUIRE(id2string(class_type->get_outer_class()).empty());
+        THEN("The outer class should have the name \"java::Outer\".")
+        {
+          REQUIRE(id2string(class_type->get_name()) == "java::Outer");
+        }
+        THEN("The outer class should not have an inner name.")
+        {
+          REQUIRE(id2string(class_type->get_inner_name()).empty());
+        }
+        THEN("The outer class is not considered to be an anonymous class.")
+        {
+          REQUIRE_FALSE(class_type->get_is_anonymous_class());
+        }
+        THEN("The outer class does not have an outer class.")
+        {
+          REQUIRE(id2string(class_type->get_outer_class()).empty());
+        }
       }
     }
-  }
-  GIVEN("A class file of an anonymous class.")
-  {
-    WHEN("Parsing an anonymous class.")
+    GIVEN(
+      "A class file of an anonymous class and " + load_first + " loaded first.")
     {
-      const symbol_tablet &symbol_table = load_java_class(
-        "Outer$1", "./java_bytecode/java_bytecode_parser");
-      const symbolt *class_symbol = symbol_table.lookup("java::Outer$1");
-      REQUIRE(class_symbol);
-      const java_class_typet *class_type =
-        type_try_dynamic_cast<java_class_typet>(class_symbol->type);
-      REQUIRE(class_type);
+      WHEN("Parsing an anonymous class.")
+      {
+        const symbolt *class_symbol = symbol_table.lookup("java::Outer$1");
+        REQUIRE(class_symbol);
+        const java_class_typet *class_type =
+          type_try_dynamic_cast<java_class_typet>(class_symbol->type);
+        REQUIRE(class_type);
 
-      THEN("The anonymous class should have the name \"java::Outer$1\".")
-      {
-        REQUIRE(id2string(class_type->get_name()) == "java::Outer$1");
-      }
-      THEN("The anonymous class should not have an inner name.")
-      {
-        REQUIRE(id2string(class_type->get_inner_name()).empty());
-      }
-      THEN("The anonymous class is considered to be an anonymous class.")
-      {
-        REQUIRE(class_type->get_is_anonymous_class());
-      }
-      THEN("The anonymous class does not have an outer class.")
-      {
-        REQUIRE(id2string(class_type->get_outer_class()).empty());
+        THEN("The anonymous class should have the name \"java::Outer$1\".")
+        {
+          REQUIRE(id2string(class_type->get_name()) == "java::Outer$1");
+        }
+        THEN("The anonymous class should not have an inner name.")
+        {
+          REQUIRE(id2string(class_type->get_inner_name()).empty());
+        }
+        THEN("The anonymous class is considered to be an anonymous class.")
+        {
+          REQUIRE(class_type->get_is_anonymous_class());
+        }
+        THEN("The anonymous class does not have an outer class.")
+        {
+          REQUIRE(id2string(class_type->get_outer_class()).empty());
+        }
       }
     }
   }
