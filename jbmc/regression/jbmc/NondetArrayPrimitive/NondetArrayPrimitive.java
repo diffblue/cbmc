9041b6eb0f2a7b88b2225bf82ff740deefe2d27c
Joel Allred <joel.allred@diffblue.com>
2018-12-31 10:17:23 +0000

    Tests for loop-less array init

---
+++ jbmc/regression/jbmc/NondetArrayPrimitive/NondetArrayPrimitive.java
@@ -0,0 +1,69 @@
+class NondetArrayPrimitive
+{
+  void intArray(int[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42) {
+      assert false;
+    }
+  }
+
+  void floatArray(float[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42.0) {
+      assert false;
+    }
+  }
+
+  void charArray(char[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 'f') {
+      assert false;
+    }
+  }
+
+  void doubleArray(double[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42.0) {
+      assert false;
+    }
+  }
+
+  void longArray(long[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42) {
+      assert false;
+    }
+  }
+
+  void shortArray(short[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42) {
+      assert false;
+    }
+  }
+
+  void byteArray(byte[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == 42) {
+      assert false;
+    }
+  }
+
+  void boolArray(boolean[] array)
+  {
+    if (array != null && array.length > 1500 && array[1500] == true) {
+      assert false;
+    }
+  }
+
+  void intArrayMulti(int[][] array)
+  {
+    if (array != null &&
+        array.length > 2 &&
+        array[2].length > 50 &&
+        array[2][50] == 42) {
+      assert false;
+    }
+  }
+
+}
