0d96b0d08fece7d8e539e063010c428d252d4f9a
Owen Jones <owen.jones@diffblue.com>
2019-01-02 18:15:00 +0000

    Add test for string initializers

---
+++ jbmc/regression/janalyzer/string-initializer/Basic1.java
@@ -0,0 +1,10 @@
+class Basic1
+{
+    public static void main(String[] args) {
+        String s = "Hello ";
+        s +=  "World!";
+        if(s.equals("Hello World!"))
+            System.out.printf("Hello World!");
+    }
+};
+
