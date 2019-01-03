052a8d329a1a98a798402a469ea24d6628ba7664
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-03 16:21:50 +0000

    improve test for signed left-shift overflows

--- regression/cbmc/Overflow_Leftshift1/main.c
+++ regression/cbmc/Overflow_Leftshift1/main.c
@@ -1,11 +1,21 @@
 int main()
 {
   unsigned char x;
+
+  // signed, owing to promotion, and may overflow
   unsigned r=x << ((sizeof(unsigned)-1)*8);
+
+  // signed, owing to promotion, and cannot overflow
   r=x << ((sizeof(unsigned)-1)*8-1);
+
+  // unsigned
   r=(unsigned)x << ((sizeof(unsigned)-1)*8);
 
-  int s=-1 << ((sizeof(int)-1)*8);
-  s=-256 << ((sizeof(int)-1)*8);
+  // negative value or zero, not an overflow
+  int s = -x << ((sizeof(int) - 1) * 8);
+
+  // overflow
+  s = 1 << x;
+
   return 0;
 }
