748bb0084489dbfefb69b312a869bf4a2acacfce
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-08 12:41:34 +0000

    avoid floating-point arithmetic in fixed-point test

--- regression/cbmc/Fixedbv1/main.c
+++ regression/cbmc/Fixedbv1/main.c
@@ -5,13 +5,13 @@ int main() {
   int y;
 
   x=2;
-  x-=0.6;
+  x -= (__CPROVER_fixedbv[64][32])0.6;
   y=x; // this yields 1.4, which is cut off
 
   assert(y==1);
 
   x=2;
-  x-=0.4;
+  x -= (__CPROVER_fixedbv[64][32])0.4;
   y=x; // this yields 1.6, which is cut off, too!
 
   assert(y==1);
