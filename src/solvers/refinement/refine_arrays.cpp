f281bc33c461b3d543ae98693906b0db8518da43
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-10 18:11:39 +0000

    remove dependency on string_refinement_invariantt
    The refinement solver a part of the string solver, but can live on its own,
    and thus, shouldn't use its exception class.

--- src/solvers/refinement/refine_arrays.cpp
+++ src/solvers/refinement/refine_arrays.cpp
@@ -15,7 +15,6 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/std_expr.h>
 #include <util/find_symbols.h>
 
-#include <solvers/refinement/string_refinement_invariant.h>
 #include <solvers/sat/satcheck.h>
 
 /// generate array constraints
@@ -55,9 +54,6 @@ void bv_refinementt::arrays_overapproximated()
     if(current.id()==ID_implies)
     {
       implies_exprt imp=to_implies_expr(current);
-      DATA_INVARIANT(
-        imp.operands().size()==2,
-        string_refinement_invariantt("implies must have two operands"));
       exprt implies_simplified=get(imp.op0());
       if(implies_simplified==false_exprt())
       {
@@ -70,8 +66,7 @@ void bv_refinementt::arrays_overapproximated()
     {
       or_exprt orexp=to_or_expr(current);
       INVARIANT(
-        orexp.operands().size()==2,
-        string_refinement_invariantt("only treats the case of a binary or"));
+        orexp.operands().size() == 2, "only treats the case of a binary or");
       exprt o1=get(orexp.op0());
       exprt o2=get(orexp.op1());
       if(o1==true_exprt() || o2 == true_exprt())
@@ -95,10 +90,7 @@ void bv_refinementt::arrays_overapproximated()
       lazy_array_constraints.erase(it++);
       break;
     default:
-      INVARIANT(
-        false,
-        string_refinement_invariantt("error in array over approximation "
-          "check"));
+      INVARIANT(false, "error in array over approximation check");
     }
   }
 
