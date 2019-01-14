abebf052e0053f82fea488e12d8200b49409bfad
Michael Tautschnig <tautschn@amazon.com>
2019-01-14 21:34:49 +0000

    Avoid evaluation-order warnings with Visual Studio
    Constructing the arguments in place triggers a warning with Visual Studio. Just
    construct them in a fixed order upfront and move each of them.

--- src/analyses/ai.cpp
+++ src/analyses/ai.cpp
@@ -95,11 +95,15 @@ jsont ai_baset::output_json(
     std::ostringstream out;
     i_it->output(out);
 
-    json_objectt location{
-      {"locationNumber", json_numbert(std::to_string(i_it->location_number))},
-      {"sourceLocation", json_stringt(i_it->source_location().as_string())},
-      {"abstractState", abstract_state_before(i_it)->output_json(*this, ns)},
-      {"instruction", json_stringt(out.str())}};
+    json_numbert location_number_json(std::to_string(i_it->location_number));
+    json_stringt source_location_json(i_it->source_location.as_string());
+    jsont abstract_state_json(abstract_state_before(i_it)->output_json(*this, ns));
+    json_stringt instruction_json(out.str());
+    json_objectt location(
+      {{"locationNumber", std::move(location_number_json)},
+       {"sourceLocation", std::move(source_location_json)},
+       {"abstractState", std::move(abstract_state_json)},
+       {"instruction", std::move(instruction_json)}});
 
     contents.push_back(std::move(location));
   }
