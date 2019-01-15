07bbc9c6092bab7733049cd66d812b2a0309a095
Thomas Spriggs <thomas.spriggs@diffblue.com>
2019-01-15 15:58:35 +0000

    Add unit test on json_object as output for std::transform
    This unit test, tests that the functionality added to `json_objectt` in
    the previous commit enables `json_object` to be used as the output
    container of `std::transform`. It also tests that `std::inserter` can be
    used to construct an `std::insert_iterator<json_objectt>` from a
    `json_objectt`. This test is included, because it is good to test new
    functionality.

---
+++ unit/util/json_object.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************\
+
+Module: Catch tests for json_objectt
+
+Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#include <testing-utils/catch.hpp>
+#include <util/json.h>
+#include <util/optional.h>
+#include <util/optional_utils.h>
+
+#include <algorithm>
+#include <iterator>
+
+SCENARIO(
+  "Test that json_objectt is compatible with STL algorithms",
+  "[core][util][json]")
+{
+  GIVEN("An empty json_objectt")
+  {
+    json_objectt object;
+    THEN("std::transform can be used to insert into the json_object.")
+    {
+      const std::vector<std::string> input_values = {"one", "two", "three"};
+      const std::insert_iterator<json_objectt> insert_it =
+        std::inserter(object, object.end());
+      std::transform(
+        input_values.begin(),
+        input_values.end(),
+        insert_it,
+        [](const std::string &number) {
+          return make_pair(number, json_stringt{number});
+        });
+
+      const optionalt<jsont> one = optional_lookup(object, "one");
+      REQUIRE(one);
+      REQUIRE(one->value == "one");
+      const optionalt<jsont> two = optional_lookup(object, "two");
+      REQUIRE(two);
+      REQUIRE(two->value == "two");
+      const optionalt<jsont> three = optional_lookup(object, "three");
+      REQUIRE(three);
+      REQUIRE(three->value == "three");
+    }
+  }
+}
