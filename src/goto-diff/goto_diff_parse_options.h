623ff5c5f0283656aea926584818b73ca124318b
Michael Tautschnig <tautschn@amazon.com>
2019-01-16 14:59:32 +0000

    Remove unnecessary use of "virtual"
    These functions are neither provided by any base classes nor is anyone
    inheriting from these classes.

--- src/goto-diff/goto_diff_parse_options.h
+++ src/goto-diff/goto_diff_parse_options.h
@@ -59,16 +59,14 @@ protected:
   ui_message_handlert ui_message_handler;
   goto_diff_languagest languages2;
 
-  virtual void get_command_line_options(optionst &options);
+  void get_command_line_options(optionst &options);
 
   virtual int get_goto_program(
     const optionst &options,
     goto_diff_languagest &languages,
     goto_modelt &goto_model);
 
-  virtual bool process_goto_program(
-    const optionst &options,
-    goto_modelt &goto_model);
+  bool process_goto_program(const optionst &options, goto_modelt &goto_model);
 
   void preprocessing();
 };
