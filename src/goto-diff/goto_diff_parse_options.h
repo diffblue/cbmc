750e2f32e1363a2ae33b0d3ad23ef5bfdef7ae51
Michael Tautschnig <tautschn@amazon.com>
2019-01-16 15:15:14 +0000

    Remove useless method declaration
    "preprocessing" is not actually defined.

--- src/goto-diff/goto_diff_parse_options.h
+++ src/goto-diff/goto_diff_parse_options.h
@@ -67,8 +67,6 @@ protected:
     goto_modelt &goto_model);
 
   bool process_goto_program(const optionst &options, goto_modelt &goto_model);
-
-  void preprocessing();
 };
 
 #endif // CPROVER_GOTO_DIFF_GOTO_DIFF_PARSE_OPTIONS_H
