e2f2c3189a42f9e65a26a6b895ab02984448ff8a
Peter Schrammel <peter.schrammel@diffblue.com>
2019-01-19 13:14:26 +0000

    Add goto trace storage
    In all properties mode, traces are currently output
    at the very end. Hence, we have to store them.

---
+++ src/goto-checker/goto_trace_storage.h
@@ -0,0 +1,41 @@
+/*******************************************************************\
+
+Module: Goto Trace Storage
+
+Author: Daniel Kroening, Peter Schrammel
+
+\*******************************************************************/
+
+/// \file
+/// Goto Trace Storage
+
+#ifndef CPROVER_GOTO_CHECKER_GOTO_TRACE_STORAGE_H
+#define CPROVER_GOTO_CHECKER_GOTO_TRACE_STORAGE_H
+
+#include <goto-programs/goto_trace.h>
+
+class goto_trace_storaget
+{
+public:
+  explicit goto_trace_storaget(const namespacet &);
+  goto_trace_storaget(const goto_trace_storaget &) = delete;
+
+  const goto_tracet &insert(goto_tracet &&);
+
+  const std::vector<goto_tracet> &all() const;
+  const goto_tracet &operator[](const irep_idt &property_id) const;
+
+  const namespacet &get_namespace() const;
+
+protected:
+  /// the namespace related to the traces
+  const namespacet &ns;
+
+  /// stores the traces
+  std::vector<goto_tracet> traces;
+
+  // maps property ID to index in traces
+  std::unordered_map<irep_idt, std::size_t> property_id_to_trace_index;
+};
+
+#endif // CPROVER_GOTO_CHECKER_GOTO_TRACE_STORAGE_H
