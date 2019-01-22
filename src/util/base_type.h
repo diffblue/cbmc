e84ac77d223980647640d58f8fef355f7ba2add4
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-22 08:56:31 +0000

    deprecate base_type_eq and type_eq
    Both are no longer needed as symbol_typet() is no longer in use.  Simply use
    type1 == type2 instead.

--- src/util/base_type.h
+++ src/util/base_type.h
@@ -12,15 +12,19 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_UTIL_BASE_TYPE_H
 #define CPROVER_UTIL_BASE_TYPE_H
 
+#include "deprecate.h"
+
 class exprt;
 class typet;
 class namespacet;
 
+DEPRECATED("Use == instead")
 bool base_type_eq(
   const typet &type1,
   const typet &type2,
   const namespacet &ns);
 
+DEPRECATED("Use == instead")
 bool base_type_eq(
   const exprt &expr1,
   const exprt &expr2,
--- src/util/type_eq.h
+++ src/util/type_eq.h
@@ -13,9 +13,12 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_UTIL_TYPE_EQ_H
 #define CPROVER_UTIL_TYPE_EQ_H
 
+#include "deprecate.h"
+
 class namespacet;
 class typet;
 
+DEPRECATED("Use == instead")
 bool type_eq(const typet &type1, const typet &type2, const namespacet &ns);
 
 #endif // CPROVER_UTIL_TYPE_EQ_H
