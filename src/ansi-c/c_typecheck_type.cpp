ea539edd7d2a260fc553ea5965df67dce15612d3
Michael Tautschnig <tautschn@amazon.com>
2019-01-25 15:25:15 +0000

    clang-format reverted code - must be reverted in future as well

--- src/ansi-c/c_typecheck_type.cpp
+++ src/ansi-c/c_typecheck_type.cpp
@@ -609,27 +609,26 @@ void c_typecheck_baset::typecheck_array_type(array_typet &type)
       }
 
       // Need to pull out! We insert new symbol.
-      unsigned count=0;
+      unsigned count = 0;
       irep_idt temp_identifier;
       std::string suffix;
 
       do
       {
-        suffix="$array_size"+std::to_string(count);
-        temp_identifier=id2string(current_symbol.name)+suffix;
+        suffix = "$array_size" + std::to_string(count);
+        temp_identifier = id2string(current_symbol.name) + suffix;
         count++;
-      }
-      while(symbol_table.symbols.find(temp_identifier)!=
-            symbol_table.symbols.end());
+      } while(symbol_table.symbols.find(temp_identifier) !=
+              symbol_table.symbols.end());
 
       // add the symbol to symbol table
       auxiliary_symbolt new_symbol;
-      new_symbol.name=temp_identifier;
-      new_symbol.pretty_name=id2string(current_symbol.pretty_name)+suffix;
-      new_symbol.base_name=id2string(current_symbol.base_name)+suffix;
-      new_symbol.type=size.type();
+      new_symbol.name = temp_identifier;
+      new_symbol.pretty_name = id2string(current_symbol.pretty_name) + suffix;
+      new_symbol.base_name = id2string(current_symbol.base_name) + suffix;
+      new_symbol.type = size.type();
       new_symbol.type.set(ID_C_constant, true);
-      new_symbol.value=size;
+      new_symbol.value = size;
       new_symbol.location = size_source_location;
       new_symbol.mode = mode;
 
@@ -638,14 +637,14 @@ void c_typecheck_baset::typecheck_array_type(array_typet &type)
       // produce the code that declares and initializes the symbol
       symbol_exprt symbol_expr;
       symbol_expr.set_identifier(temp_identifier);
-      symbol_expr.type()=new_symbol.type;
+      symbol_expr.type() = new_symbol.type;
 
       code_declt declaration(symbol_expr);
       declaration.add_source_location() = size_source_location;
 
       code_assignt assignment;
       assignment.lhs()=symbol_expr;
-      assignment.rhs()=size;
+      assignment.rhs() = size;
       assignment.add_source_location() = size_source_location;
 
       // store the code
