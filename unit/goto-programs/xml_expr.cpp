10f8089d4f0f1a545a04c630e53f0ac14abe7ab1
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-31 10:31:59 +0000

    Fix binary represenation in XML generated from constant_exprt
    We no longer use binary as bitvector representation, and thus, conversion
    must happen.

--- unit/goto-programs/xml_expr.cpp
+++ unit/goto-programs/xml_expr.cpp
@@ -12,6 +12,7 @@ Author: Michael Tautschnig
 #include <util/config.h>
 #include <util/namespace.h>
 #include <util/std_expr.h>
+#include <util/std_types.h>
 #include <util/symbol_table.h>
 
 #include <goto-programs/xml_expr.h>
@@ -23,8 +24,17 @@ TEST_CASE("Constant expression to XML")
   const symbol_tablet symbol_table;
   const namespacet ns(symbol_table);
 
-  const constant_exprt number = from_integer(0xFF, unsignedbv_typet(8));
-  const xmlt x = xml(number, ns);
+  const constant_exprt number_ubv = from_integer(0xFF, unsignedbv_typet(8));
+  const xmlt x_ubv = xml(number_ubv, ns);
 
-  REQUIRE(x.get_attribute("binary") == "11111111");
+  REQUIRE(x_ubv.get_attribute("binary") == "11111111");
+
+  fixedbv_typet fixedbv_type;
+  fixedbv_type.set_width(8);
+  fixedbv_type.set_integer_bits(6);
+
+  const constant_exprt number_fbv = from_integer(0x3, fixedbv_type);
+  const xmlt x_fbv = xml(number_fbv, ns);
+
+  REQUIRE(x_fbv.get_attribute("binary") == "00001100");
 }
