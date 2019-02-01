c7053d4c3a564fa84465e0f6167dae07977f6886
Michael Tautschnig <tautschn@amazon.com>
2019-02-01 09:14:49 +0000

    Do not introduce byte_extract_exprt during flattening
    A prior lowering pass may have removed them (for example, if they occur within
    unbounded arrays).

--- src/solvers/flattening/boolbv_member.cpp
+++ src/solvers/flattening/boolbv_member.cpp
@@ -9,9 +9,6 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "boolbv.h"
 
 #include <util/base_type.h>
-#include <util/byte_operators.h>
-#include <util/arith_tools.h>
-#include <util/c_types.h>
 
 bvt boolbvt::convert_member(const member_exprt &expr)
 {
@@ -20,59 +17,40 @@ bvt boolbvt::convert_member(const member_exprt &expr)
 
   const bvt &struct_bv=convert_bv(struct_op);
 
-  if(struct_op_type.id()==ID_union)
+  const irep_idt &component_name = expr.get_component_name();
+  const struct_union_typet::componentst &components =
+    to_struct_union_type(struct_op_type).components();
+
+  std::size_t offset = 0;
+
+  for(const auto &c : components)
   {
-    return convert_bv(
-      byte_extract_exprt(byte_extract_id(),
-                         struct_op,
-                         from_integer(0, index_type()),
-                         expr.type()));
-  }
-  else
-  {
-    INVARIANT(
-      struct_op_type.id() == ID_struct,
-      "member_exprt should denote access to a union or struct");
+    const typet &subtype = c.type();
+    const std::size_t sub_width = boolbv_width(subtype);
 
-    const irep_idt &component_name=expr.get_component_name();
-    const struct_typet::componentst &components=
-      to_struct_type(struct_op_type).components();
-
-    std::size_t offset=0;
-
-    for(const auto &c : components)
+    if(c.get_name() == component_name)
     {
-      const typet &subtype = c.type();
-      std::size_t sub_width=boolbv_width(subtype);
+      DATA_INVARIANT_WITH_DIAGNOSTICS(
+        base_type_eq(subtype, expr.type(), ns),
+        "component type shall match the member expression type",
+        subtype.pretty(),
+        expr.type().pretty());
+      INVARIANT(
+        offset + sub_width <= struct_bv.size(),
+        "bitvector part corresponding to element shall be contained within the "
+        "full aggregate bitvector");
 
-      if(c.get_name() == component_name)
-      {
-        DATA_INVARIANT_WITH_DIAGNOSTICS(
-          base_type_eq(subtype, expr.type(), ns),
-          "struct component type shall match the member expression type",
-          subtype.pretty(),
-          expr.type().pretty());
-
-        bvt bv;
-        bv.resize(sub_width);
-        INVARIANT(
-          offset + sub_width <= struct_bv.size(),
-          "bitvector part corresponding to struct element shall be contained "
-          "within the full struct bitvector");
-
-        for(std::size_t i=0; i<sub_width; i++)
-          bv[i]=struct_bv[offset+i];
-
-        return bv;
-      }
-
-      offset+=sub_width;
+      return bvt(
+        struct_bv.begin() + offset, struct_bv.begin() + offset + sub_width);
     }
 
-    INVARIANT_WITH_DIAGNOSTICS(
-      false,
-      "struct type shall contain component accessed by member expression",
-      expr.find_source_location(),
-      id2string(component_name));
+    if(struct_op_type.id() != ID_union)
+      offset += sub_width;
   }
+
+  INVARIANT_WITH_DIAGNOSTICS(
+    false,
+    "struct type shall contain component accessed by member expression",
+    expr.find_source_location(),
+    id2string(component_name));
 }
