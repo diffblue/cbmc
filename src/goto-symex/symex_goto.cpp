7fbc6b030f41efd207e35c5ef9eb4bd31ed7902d
Peter Schrammel <peter.schrammel@diffblue.com>
2019-02-09 11:23:19 +0000

    Add callback to decide on interrupting goto-symex
    Required for incremental loop unwinding.

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -288,6 +288,7 @@ void goto_symext::symex_goto(statet &state)
 
     if(should_stop_unwind(state.source, state.call_stack(), unwind))
     {
+      // we break the loop
       loop_bound_exceeded(state, new_guard);
 
       // next instruction
@@ -297,6 +298,11 @@ void goto_symext::symex_goto(statet &state)
 
     if(new_guard.is_true())
     {
+      // we continue executing the loop
+      if(check_break(loop_id, false, state, new_guard, unwind))
+      {
+        should_pause_symex = true;
+      }
       symex_transition(state, goto_target, true);
       return; // nothing else to do
     }
@@ -514,6 +520,17 @@ void goto_symext::symex_goto(statet &state)
       }
     }
   }
+}
+
+bool goto_symext::check_break(
+  const irep_idt &id,
+  bool is_function,
+  statet &state,
+  const exprt &cond,
+  unsigned unwind)
+{
+  // dummy implementation
+  return false;
 }
 
 void goto_symext::merge_gotos(statet &state)
