30a8a70f9d526386f5bb79d7312dca1c24695fef
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-02-23 19:10:53 +0000

    clang-format trace_show_function_calls/main.c

--- regression/cbmc/trace_show_function_calls/main.c
+++ regression/cbmc/trace_show_function_calls/main.c
@@ -1,22 +1,20 @@
 int function_to_call(int a)
 {
-	int local = 1;
-	return a+local;
+  int local = 1;
+  return a + local;
 }
-
 
 int main()
 {
-	int a;
-	unsigned int b;
-	a = 0;
-	a = -100;
-	a = 2147483647;
-	b = a*2;
-	a = -2147483647;
-	a = function_to_call(a);
-	b = function_to_call(b);
-	
-	__CPROVER_assert(0,"");
+  int a;
+  unsigned int b;
+  a = 0;
+  a = -100;
+  a = 2147483647;
+  b = a * 2;
+  a = -2147483647;
+  a = function_to_call(a);
+  b = function_to_call(b);
 
+  __CPROVER_assert(0, "");
 }
