c86cf4a02bb42c3192a9f99fb7fc0bbbab6f8d43
Michael Tautschnig <tautschn@amazon.com>
2019-02-28 10:54:13 +0000

    Remove link_to_library(goto_functions, symbol_table, ...)
    There are no in-tree uses of this function. External users should use
    link_to_library(goto_model, ...). This is in preparation of further changes that
    will only support the goto_modelt-variant.

--- src/goto-programs/link_to_library.h
+++ src/goto-programs/link_to_library.h
@@ -18,18 +18,9 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/deprecate.h>
 #include <util/irep.h>
 
-class goto_functionst;
 class goto_modelt;
 class message_handlert;
 class symbol_tablet;
-
-DEPRECATED(SINCE(2019, 2, 28, "Use link_to_library(goto_model, ...) instead"))
-void link_to_library(
-  symbol_tablet &,
-  goto_functionst &,
-  message_handlert &,
-  const std::function<
-    void(const std::set<irep_idt> &, symbol_tablet &, message_handlert &)> &);
 
 void link_to_library(
   goto_modelt &,
