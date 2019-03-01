3092d8df2a7b42dab291332ff0921448f15b0039
Romain Brenguier <romain.brenguier@gmail.com>
2019-03-01 09:37:13 +0000

    Add a --total option to benchmark_to_spreadsheet.js
    This adds an option to the script to take the total of the time taken by
    the benchmarks instead of the results for each individual functions

--- scripts/benchmark/benchmark_to_spreadsheet.js
+++ scripts/benchmark/benchmark_to_spreadsheet.js
@@ -3,14 +3,21 @@ let program = require('commander');
 
 program
     .arguments('<jsonFile>')
+    .option('-t, --total', 'Only print the total time')
     .action(function (jsonFile) {
         let lineReader = require('readline').createInterface({
             input: require('fs').createReadStream(jsonFile)
         });
-        console.log("class file; function name; execution time (ms);"
-        + "success; goals covered; total number of goals");
+        const printTotalTime = typeof program.total != 'undefined';
+        let totalTime = 0.0;
+        if (!printTotalTime)
+            console.log("class file; function name; execution time (ms);"
+                        + "success; goals covered; total number of goals");
         lineReader.on('line', function (data) {
             let json = JSON.parse(data.toString());
+            totalTime += parseFloat(json.execTime);
+            if (printTotalTime)
+                return;
             console.log(
                 json.classFile +";" +
                 json['function'].replace(/;/g, "_") + "; " +
@@ -19,5 +26,10 @@ program
                 ((typeof json.goals != 'undefined')? json.goals.goalsCovered : "0") + ";" +
                 ((typeof json.goals != 'undefined')? json.goals.totalGoals : ""));
         });
+        if (printTotalTime) {
+            lineReader.on('close', function () {
+                console.log(jsonFile + "; " + totalTime)
+            });
+        }
     })
     .parse(process.argv);
