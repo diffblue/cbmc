0d32a43dc7c15504a780101b351fbe2da8b93a8d
Antonia Lechner <antonia.lechner@diffblue.com>
2019-03-21 17:10:14 +0000

    Move nondet documentation to header file
    Following the new coding standard

--- src/util/nondet.cpp
+++ src/util/nondet.cpp
@@ -13,26 +13,6 @@ Author: Diffblue Ltd.
 #include <util/fresh_symbol.h>
 #include <util/symbol.h>
 
-/// Gets a fresh nondet choice in range (min_value, max_value). GOTO generated
-/// resembles:
-/// ```
-/// int_type name_prefix::nondet_int = NONDET(int_type)
-/// ASSUME(name_prefix::nondet_int >= min_value)
-/// ASSUME(name_prefix::nondet_int <= max_value)
-/// ```
-/// \param min_value: Minimum value (inclusive) of returned int.
-/// \param max_value: Maximum value (inclusive) of returned int.
-/// \param name_prefix: Prefix for the fresh symbol name generated (should be
-///   function id)
-/// \param int_type: The type of the int used to non-deterministically choose
-///   one of the switch cases.
-/// \param mode: Mode (language) of the symbol to be generated.
-/// \param source_location: The location to mark the generated int with.
-/// \param symbol_table: The global symbol table.
-/// \param [out] instructions: Output instructions are written to
-///   'instructions'. These declare, nondet-initialise and range-constrain (with
-///   assume statements) a fresh integer.
-/// \return Returns a symbol expression for the resulting integer.
 symbol_exprt generate_nondet_int(
   const mp_integer &min_value,
   const mp_integer &max_value,
@@ -74,16 +54,6 @@ symbol_exprt generate_nondet_int(
   return nondet_symbol;
 }
 
-/// Pick nondeterministically between imperative actions 'switch_cases'.
-/// \param name_prefix: Name prefix for fresh symbols (should be function id)
-/// \param switch_cases: List of codet objects to execute in each switch case.
-/// \param int_type: The type of the int used to non-deterministically choose
-///   one of the switch cases.
-/// \param mode: Mode (language) of the symbol to be generated.
-/// \param source_location: The location to mark the generated int with.
-/// \param symbol_table: The global symbol table.
-/// \return Returns a nondet-switch choosing between switch_cases. The resulting
-///   switch block has no default case.
 code_blockt generate_nondet_switch(
   const irep_idt &name_prefix,
   const alternate_casest &switch_cases,
