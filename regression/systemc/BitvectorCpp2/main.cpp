1ad5898e62057b622616f2d4e6f7520c5e42313b
Michael Tautschnig <tautschn@amazon.com>
2019-03-31 16:46:38 +0000

    C++: Add support for indexed access to bitvectors
    As a non-standard extension, we permit indexed access to bitvectors.
    This translates to bit-level updates, which the typechecker now
    generates.

--- regression/systemc/BitvectorCpp2/main.cpp
+++ regression/systemc/BitvectorCpp2/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
   __CPROVER::signedbv<4> x(15);
   __CPROVER::signedbv<4> y;
   y = 13;
-  x[1] = 0; //TODO: currently not supported
+  x[1] = 0;
   assert(x == y);
 
   return 0;
