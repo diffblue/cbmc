17191e053010f5d8f4bba141ea5156ef6db6bc8d
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-04-15 14:43:12 +0100

    Set up the interface for fp_target map
    The type and the function to return the map.

--- src/goto-programs/remove_function_pointers.h
+++ src/goto-programs/remove_function_pointers.h
@@ -16,11 +16,22 @@ Date: June 2003
 
 #include <util/irep.h>
 
+#include <map>
+
+#include "remove_const_function_pointers.h"
+
 class goto_functionst;
 class goto_programt;
 class goto_modelt;
 class message_handlert;
 class symbol_tablet;
+
+using possible_fp_targetst = remove_const_function_pointerst::functionst;
+using possible_fp_targets_mapt = std::map<irep_idt, possible_fp_targetst>;
+
+possible_fp_targets_mapt get_function_pointer_targets(
+  message_handlert &message_handler,
+  goto_modelt &goto_model);
 
 // remove indirect function calls
 // and replace by case-split
