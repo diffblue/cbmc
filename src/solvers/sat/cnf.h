6602ca94e6bb6604cef94c1d29dc3e2004547154
Norbert Manthey <nmanthey@amazon.de>
2019-04-18 21:41:17 +0200

    cnf_dumper: define class
    This commit defines a simple class that can store a CNF formula in a
    simple and raw format, such that this formula can be dumped whenever
    a SAT call is issued.
    
    This class is made part of cnf_solvert, so that any SAT backend can
    easily be extended to dump CNFs just before calling the solver.
    
    To activate the cnf_dumper, the environment variable
    CBMC_CNF_DUMP_FILE_PREFIX has to be specified, and point to a writable
    location and file name prefix. This prefix is extended with the number
    of the cnf_dump class instantiation, the number of the dump of that
    instantiation, as well as the PID of the dumping process, e.g.
    
    /tmp/t/cbmc-main_1_5_14106.cnf
    
    Signed-off-by: Norbert Manthey <nmanthey@amazon.de>

--- src/solvers/sat/cnf.h
+++ src/solvers/sat/cnf.h
@@ -14,6 +14,13 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <solvers/prop/prop.h>
 
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
 class cnft:public propt
 {
 public:
@@ -66,6 +73,89 @@ protected:
   }
 };
 
+/** simple container for CNF formula to easily dump it */
+class cnf_dumpert
+{
+  bool active = false;
+  std::vector< std::vector<int> > formula;
+  std::size_t dumps = 0;
+  std::size_t writer = 0;
+  std::size_t maxV;
+  std::string file_prefix;
+
+  public:
+  cnf_dumpert()
+  {
+    static std::size_t created_writer = 0;
+    created_writer++;
+    const char *prefix = getenv("CBMC_CNF_DUMP_FILE_PREFIX");
+    if(!prefix)
+      return;
+    active = true;
+    file_prefix = prefix;
+    writer = created_writer;
+  }
+
+  /** add the given clause to the formula */
+  void add_clause(const bvt &bv)
+  {
+    if(!active)
+      return;
+
+    formula.emplace_back(std::vector<int>());
+    forall_literals(it, bv)
+      if(!it->is_false()) {
+        maxV = maxV > it->var_no() ? maxV : it->var_no();
+        formula.back().push_back(it->sign() ? (-it->var_no()) : (it->var_no()));
+      }
+  }
+
+  /** dump the CNF to the specified location*/
+  void dump_formula(std::string hint = "")
+  {
+    // make sure different calls get different names
+    dumps++;
+
+    std::stringstream filename;
+    filename << file_prefix;
+    filename << "_" << writer << "_" << dumps << "_" << getpid() << ".cnf";
+
+    std::ofstream out(filename.str());
+
+    if(!out) {
+      std::cout << "warn: failed to open file " << filename.str()
+                << " for writing" << std::endl;
+      return;
+    }
+
+    // write header
+    out << "c CNF created with CBMC CNF streamer of writer " << writer
+        << ", dump " << dumps << std::endl
+        << "p cnf " << maxV << " " << formula.size() << std::endl;
+
+    // write formula
+    std::size_t count = 0;
+    std::stringstream output_block;
+    for(const auto &clause : formula)
+    {
+      // dump clause into string stream (faster than file IO)
+      for(const int &l : clause)
+        output_block << l << " ";
+      output_block << "0" << std::endl;
+
+      // print the block once in a while
+      if(++count % CNF_DUMP_BLOCK_SIZE == 0)
+      {
+        out << output_block.str();
+        output_block.str("");
+      }
+    }
+
+    // make sure the final block is dumped as well
+    out << output_block.str();
+  }
+};
+
 class cnf_solvert:public cnft
 {
 public:
@@ -83,6 +173,8 @@ protected:
   enum class statust { INIT, SAT, UNSAT, ERROR };
   statust status;
   size_t clause_counter;
+
+  cnf_dumpert cnf_dumper;
 };
 
 #endif // CPROVER_SOLVERS_SAT_CNF_H
