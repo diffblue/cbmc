d323552c0555b150713a2564d7bb2044a9f4f5ef
Peter Schrammel <peter.schrammel@diffblue.com>
2019-04-28 22:22:17 +0100

    Bugfix: Use XML_UI if xml-interface is enabled
    This feature was broken by a previous parse_optionst
    refactoring. We can either enable XML_UI if xml-interface
    is enabled or give ui_message_handlert a set_ui() method
    to set the UI after cmdline has been fully parsed.

--- src/util/ui_message.cpp
+++ src/util/ui_message.cpp
@@ -65,18 +65,17 @@ ui_message_handlert::ui_message_handlert(
   const std::string &program)
   : ui_message_handlert(
       nullptr,
-      cmdline.isset("xml-ui") ? uit::XML_UI : cmdline.isset("json-ui")
-                                                ? uit::JSON_UI
-                                                : uit::PLAIN,
+      cmdline.isset("xml-ui") || cmdline.isset("xml-interface")
+        ? uit::XML_UI
+        : cmdline.isset("json-ui") ? uit::JSON_UI : uit::PLAIN,
       program,
       cmdline.isset("flush"),
-      cmdline.isset("timestamp")
-        ? cmdline.get_value("timestamp") == "monotonic"
-            ? timestampert::clockt::MONOTONIC
-            : cmdline.get_value("timestamp") == "wall"
-                ? timestampert::clockt::WALL_CLOCK
-                : timestampert::clockt::NONE
-        : timestampert::clockt::NONE)
+      cmdline.isset("timestamp") ? cmdline.get_value("timestamp") == "monotonic"
+                                     ? timestampert::clockt::MONOTONIC
+                                     : cmdline.get_value("timestamp") == "wall"
+                                         ? timestampert::clockt::WALL_CLOCK
+                                         : timestampert::clockt::NONE
+                                 : timestampert::clockt::NONE)
 {
   if(get_ui() == uit::PLAIN)
   {
