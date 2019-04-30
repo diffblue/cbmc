524c13ff2dc1dc3f9100e9bd1f020606d0b23656
Romain Brenguier <romain.brenguier@gmail.com>
2019-04-30 08:10:38 +0100

    Improve test for String.intern
    We add a version of the test which should fail in addition to the one
    that should pass.

--- jbmc/regression/strings-smoke-tests/java_intern/test_intern.java
+++ jbmc/regression/strings-smoke-tests/java_intern/test_intern.java
@@ -1,11 +1,26 @@
 public class test_intern
 {
-   public static void main()
-   {
-      String s1 = "abc";
-      String s3 = "abc";
-      String x = s1.intern();
-      String y = s3.intern();
-      assert(x == y);
+    public static void testPass()
+    {
+        // Arrange
+        String s1 = "abc";
+        String s2 = "abc";
+        // Act
+        String x = s1.intern();
+        String y = s2.intern();
+        // Assert
+        assert(x == y);
+    }
+
+    public static void testFail()
+    {
+        // Arrange
+        String s1 = "abc";
+        String s2 = "abd";
+        // Act
+        String x = s1.intern();
+        String y = s2.intern();
+        // Assert
+        assert(x == y);
    }
 }
