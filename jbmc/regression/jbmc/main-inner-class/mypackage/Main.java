159c6e3424300299c4c33214f562879b82152251
Peter Schrammel <peter.schrammel@diffblue.com>
2019-06-04 21:24:53 +0100

    Add test for main method in inner class
    Java can run with a main method inside an inner class.
    We need a test for this case.

---
+++ jbmc/regression/jbmc/main-inner-class/mypackage/Main.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Main {
+    public static class Inner {
+        public static void main(String[] args) {
+            System.out.println("Hallo");
+        }
+    }
+}
