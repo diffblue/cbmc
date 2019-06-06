ace9eee4fb83ad7c52eba39193a6ba1402e88abf
Romain Brenguier <romain.brenguier@gmail.com>
2019-06-06 13:15:00 +0100

    Use symex_level0t::operator() instead of set_level_0
    Calling the renaming makes additional checks compared to the low level
    update of the ssa_exprt and the return type is more informative.

--- src/goto-symex/symex_start_thread.cpp
+++ src/goto-symex/symex_start_thread.cpp
@@ -67,15 +67,15 @@ void goto_symext::symex_start_thread(statet &state)
     ssa_exprt lhs(pair.second.first.get_original_expr());
 
     // get L0 name for current thread
-    lhs.set_level_0(t);
-    const irep_idt &l0_name = lhs.get_identifier();
+    renamedt<ssa_exprt, L0> l0_lhs = symex_level0t{}(std::move(lhs), ns , t);
+    const irep_idt &l0_name = l0_lhs.get().get_identifier();
     std::size_t l1_index = path_storage.get_unique_l1_index(l0_name, 0);
     CHECK_RETURN(l1_index == 0);
 
     // set up L1 name
-    state.level1.insert(lhs, 0);
+    state.level1.insert(l0_lhs.get(), 0);
 
-    const ssa_exprt lhs_l1 = state.rename_ssa<L1>(std::move(lhs), ns).get();
+    const ssa_exprt lhs_l1 = state.rename_ssa<L1>(l0_lhs.get(), ns).get();
     const irep_idt l1_name = lhs_l1.get_l1_object_identifier();
     // store it
     new_thread.call_stack.back().local_objects.insert(l1_name);
