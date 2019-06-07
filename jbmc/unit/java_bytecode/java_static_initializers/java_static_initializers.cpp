b4b4c99005a0b202aa9fa76a9e76b306b3694815
Jeannie Moulton <jeannie.moulton@diffblue.com>
2019-06-07 17:25:01 +0100

    Add missing scoping brackets to GIVEN.
    The GIVENs were missing scoping brackets.

--- jbmc/unit/java_bytecode/java_static_initializers/java_static_initializers.cpp
+++ jbmc/unit/java_bytecode/java_static_initializers/java_static_initializers.cpp
@@ -12,16 +12,21 @@ Author: Diffblue Ltd.
 SCENARIO("is_clinit_function", "[core][java_static_initializers]")
 {
   GIVEN("A function id that represents a clinit")
-  THEN("is_clinit_function should return true.")
   {
-    const std::string input = "com.something.package.TestClass.<clinit>:()V";
-    REQUIRE(is_clinit_function(input));
+    THEN("is_clinit_function should return true.")
+    {
+      const std::string input = "com.something.package.TestClass.<clinit>:()V";
+      REQUIRE(is_clinit_function(input));
+    }
   }
   GIVEN("A function id that does not represent a clinit")
-  THEN("is_clinit_function should return false.")
   {
-    const std::string input = "com.something.package.TestClass.<notclinit>:()V";
-    REQUIRE_FALSE(is_clinit_function(input));
+    THEN("is_clinit_function should return false.")
+    {
+      const std::string input =
+        "com.something.package.TestClass.<notclinit>:()V";
+      REQUIRE_FALSE(is_clinit_function(input));
+    }
   }
 }
 
@@ -30,16 +35,20 @@ SCENARIO(
   "[core][java_static_initializers]")
 {
   GIVEN("A function id that represents a user-specified clinit")
-  THEN("is_user_specified_clinit_function should return true.")
   {
-    const std::string input =
-      "com.something.package.TestClass::user_specified_clinit";
-    REQUIRE(is_user_specified_clinit_function(input));
+    THEN("is_user_specified_clinit_function should return true.")
+    {
+      const std::string input =
+        "com.something.package.TestClass::user_specified_clinit";
+      REQUIRE(is_user_specified_clinit_function(input));
+    }
   }
   GIVEN("A function id that does not represent a user-specified clinit")
-  THEN("is_clinit_function should return false.")
   {
-    const std::string input = "com.something.package.TestClass::not_it";
-    REQUIRE_FALSE(is_user_specified_clinit_function(input));
+    THEN("is_clinit_function should return false.")
+    {
+      const std::string input = "com.something.package.TestClass::not_it";
+      REQUIRE_FALSE(is_user_specified_clinit_function(input));
+    }
   }
 }
