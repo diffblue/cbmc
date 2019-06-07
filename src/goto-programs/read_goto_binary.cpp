diffblue/cbmc

commit 5be1db8eb6a282aeda9c872406d8629af4768f77

OSX fat binary reader: differentiate Java class files
Both use the less-than-ideally-random magic number 0xCAFEBABE, which means we must
tell them apart more rigorously. Here I use the same method as GNU file, which notes
that the number of possible architectures in one fat binary is <= 18, while the Java
class file version field that occupies the same position is >= 43.

src/goto-programs/read_goto_binary.cpp
@@ -68,19 +68,24 @@ static bool read_goto_binary(
   std::ifstream in(filename, std::ios::binary);
   #endif
 
+  messaget message(message_handler);
+
   if(!in)
   {
-    messaget message(message_handler);
     message.error() << "Failed to open `" << filename << "'"
                     << messaget::eom;
     return true;
   }
 
-  char hdr[4];
-  hdr[0]=in.get();
-  hdr[1]=in.get();
-  hdr[2]=in.get();
-  hdr[3]=in.get();
+  char hdr[8];
+  in.read(hdr, 8);
+  if(!in)
+  {
+    message.error() << "Failed to read header from `" << filename << "'"
+                    << messaget::eom;
+    return true;
+  }
+
   in.seekg(0);
 
   if(hdr[0]==0x7f && hdr[1]=='G' && hdr[2]=='B' && hdr[3]=='F')
@@ -114,7 +119,7 @@ static bool read_goto_binary(
       messaget(message_handler).error() << s << messaget::eom;
     }
   }
-  else if(is_osx_fat_magic(hdr))
+  else if(is_osx_fat_header(hdr))
   {
     messaget message(message_handler);
 
@@ -226,7 +231,7 @@ bool is_goto_binary(
       // ignore any errors
     }
   }
-  else if(is_osx_fat_magic(hdr))
+  else if(is_osx_fat_header(hdr))
   {
     // this _may_ have a goto binary as hppa7100LC architecture
     try

unit/Makefile
@@ -27,6 +27,7 @@ SRC += analyses/ai/ai.cpp \
        goto-programs/goto_program_table_consistency.cpp \
        goto-programs/goto_program_validate.cpp \
        goto-programs/goto_trace_output.cpp \
+       goto-programs/osx_fat_reader.cpp \
        goto-programs/remove_returns.cpp \
        goto-programs/xml_expr.cpp \
        goto-symex/ssa_equation.cpp \

unit/goto-programs/osx_fat_reader.cpp
@@ -1,0 +1,67 @@
+/*******************************************************************\
+
+Module: Unit tests for osx_fat_binary
+
+Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#include <testing-utils/use_catch.h>
+
+#include <goto-programs/osx_fat_reader.h>
+#ifdef __APPLE__
+#include <util/exception_utils.h>
+#endif
+
+#include <fstream>
+
+TEST_CASE("OSX fat binary reader", "[core][goto-programs][osx_fat_reader]")
+{
+  std::ifstream class_file("goto-programs/Hello.class");
+  std::ifstream fat_binary("goto-programs/hello_fat");
+
+  char class_header[8];
+  char fat_binary_header[8];
+
+  class_file.read(class_header, 8);
+  fat_binary.read(fat_binary_header, 8);
+
+  REQUIRE(class_file);
+  REQUIRE(fat_binary);
+
+  REQUIRE(is_osx_fat_header(fat_binary_header));
+  REQUIRE(!is_osx_fat_header(class_header));
+
+  class_file.seekg(0);
+  fat_binary.seekg(0);
+
+  std::ostringstream fat_binary_output;
+  stream_message_handlert fat_binary_output_handler(fat_binary_output);
+  std::ostringstream class_file_output;
+  stream_message_handlert class_file_output_handler(class_file_output);
+
+#ifdef __APPLE__
+
+  REQUIRE_THROWS_AS(
+    (osx_fat_readert{class_file, class_file_output_handler}),
+    deserialization_exceptiont);
+  osx_fat_readert fat_binary_reader(fat_binary, fat_binary_output_handler);
+  REQUIRE(fat_binary_output.str().empty());
+  REQUIRE(!fat_binary_reader.has_gb());
+
+#else
+
+  osx_fat_readert class_file_reader(class_file, class_file_output_handler);
+  REQUIRE(!class_file_reader.has_gb());
+  REQUIRE(
+    class_file_output.str() ==
+    "Cannot read OSX fat archive on this platform\n");
+
+  osx_fat_readert fat_binary_reader(fat_binary, fat_binary_output_handler);
+  REQUIRE(!fat_binary_reader.has_gb());
+  REQUIRE(
+    fat_binary_output.str() ==
+    "Cannot read OSX fat archive on this platform\n");
+
+#endif
+}

src/goto-programs/osx_fat_reader.cpp
@@ -40,18 +40,36 @@ Author:
 
 #include <util/run.h>
 
-bool is_osx_fat_magic(char hdr[4])
+struct fat_header_prefixt
 {
-  uint32_t *magic=reinterpret_cast<uint32_t*>(hdr);
+  uint32_t magic;
+  uint32_t n_architectures;
+};
 
-  switch(*magic)
-  {
-  case CPROVER_FAT_MAGIC:
-  case CPROVER_FAT_CIGAM:
-    return true;
-  }
+static uint32_t u32_to_native_endian(uint32_t input)
+{
+  const uint8_t *input_as_bytes = reinterpret_cast<uint8_t *>(&input);
+  return (((uint32_t)input_as_bytes[0]) << 24) |
+         (((uint32_t)input_as_bytes[1]) << 16) |
+         (((uint32_t)input_as_bytes[2]) << 8) |
+         (((uint32_t)input_as_bytes[3]) << 0);
+}
 
-  return false;
+bool is_osx_fat_header(char header_bytes[8])
+{
+  struct fat_header_prefixt *header =
+    reinterpret_cast<struct fat_header_prefixt *>(header_bytes);
+
+  // Unfortunately for us, both Java class files and Mach fat binaries use the
+  // magic number 0xCAFEBABE. Therefore we must also check the second field,
+  // number of architectures, is in a sensible range (I use at 1 <= archs < 20,
+  // the same criterion used by `GNU file`).
+  // Luckily the class file format stores the file version here, which cannot
+  // fall in this range.
+  uint32_t n_architectures_native =
+    u32_to_native_endian(header->n_architectures);
+  return u32_to_native_endian(header->magic) == CPROVER_FAT_MAGIC &&
+         n_architectures_native >= 1 && n_architectures_native < 20;
 }
 
 osx_fat_readert::osx_fat_readert(
@@ -68,12 +86,12 @@ osx_fat_readert::osx_fat_readert(
   if(!in)
     throw system_exceptiont("failed to read OSX fat header");
 
-  if(!is_osx_fat_magic(reinterpret_cast<char*>(&(fh.magic))))
-    throw deserialization_exceptiont("OSX fat header malformed (magic)");
+  if(!is_osx_fat_header(reinterpret_cast<char *>(&(fh.magic))))
+    throw deserialization_exceptiont("OSX fat header malformed");
 
   static_assert(
     sizeof(fh.nfat_arch) == 4, "fat_header::nfat_arch is of type uint32_t");
-  unsigned narch=__builtin_bswap32(fh.nfat_arch);
+  unsigned narch = u32_to_native_endian(fh.nfat_arch);
 
   for(unsigned i=0; !has_gb_arch && i<narch; ++i)
   {
@@ -86,9 +104,9 @@ osx_fat_readert::osx_fat_readert(
       sizeof(fa.cputype) == 4 && sizeof(fa.cpusubtype) == 4 &&
         sizeof(fa.size) == 4,
       "This requires a specific fat architecture");
-    int cputype=__builtin_bswap32(fa.cputype);
-    int cpusubtype=__builtin_bswap32(fa.cpusubtype);
-    unsigned size=__builtin_bswap32(fa.size);
+    int cputype = u32_to_native_endian(fa.cputype);
+    int cpusubtype = u32_to_native_endian(fa.cpusubtype);
+    unsigned size = u32_to_native_endian(fa.size);
 
     has_gb_arch=cputype==CPU_TYPE_HPPA &&
                 cpusubtype==CPU_SUBTYPE_HPPA_7100LC &&

src/goto-programs/osx_fat_reader.h
@@ -37,7 +37,7 @@ private:
   bool has_gb_arch;
 };
 
-bool is_osx_fat_magic(char hdr[4]);
+bool is_osx_fat_header(char hdr[8]);
 
 class osx_mach_o_readert
 {
@@ -74,6 +74,6 @@ private:
   void process_sections_64(uint32_t nsects, bool need_swap);
 };
 
-bool is_osx_mach_object(char hdr[4]);
+bool is_osx_mach_object(char hdr[8]);
 
 #endif // CPROVER_GOTO_PROGRAMS_OSX_FAT_READER_H

commit 0aa279dfc493ed79cf9c6c2e7932be3e18f13562

Fix is_goto_binary to pass extended Mach-O header to osx_fat_reader
This resolves a mistake introduced in 5be1db8eb6a282aeda9c872406d8629af4768f77, which tried to differentiate
Mach-O fat binaries from Java class files, but only changed one user to pass the required extended header.

We also extend the unit tests to verify GBFs are detected in both Mach-O and ELF containers.

src/goto-programs/read_goto_binary.cpp
@@ -205,11 +205,10 @@ bool is_goto_binary(
   // 1. goto binaries, marked with 0x7f GBF
   // 2. ELF binaries, marked with 0x7f ELF
 
-  char hdr[4];
-  hdr[0]=in.get();
-  hdr[1]=in.get();
-  hdr[2]=in.get();
-  hdr[3]=in.get();
+  char hdr[8];
+  in.read(hdr, 8);
+  if(!in)
+    return false;
 
   if(hdr[0]==0x7f && hdr[1]=='G' && hdr[2]=='B' && hdr[3]=='F')
   {

unit/Makefile
@@ -27,6 +27,7 @@ SRC += analyses/ai/ai.cpp \
        goto-programs/goto_program_table_consistency.cpp \
        goto-programs/goto_program_validate.cpp \
        goto-programs/goto_trace_output.cpp \
+       goto-programs/is_goto_binary.cpp \
        goto-programs/osx_fat_reader.cpp \
        goto-programs/remove_returns.cpp \
        goto-programs/xml_expr.cpp \

unit/goto-programs/is_goto_binary.cpp
@@ -1,0 +1,72 @@
+/*******************************************************************\
+
+Module: Unit tests for is_goto_binary
+
+Author: Diffblue Ltd.
+
+\*******************************************************************/
+
+#include <testing-utils/use_catch.h>
+
+#include <goto-programs/read_goto_binary.h>
+#include <util/message.h>
+
+#include <sstream>
+
+TEST_CASE("is_goto_binary", "[core][goto-programs][is_goto_binary]")
+{
+  std::string class_file("goto-programs/Hello.class");
+  std::string fat_macho("goto-programs/hello_fat_macho");
+  std::string fat_macho_with_gbf("goto-programs/hello_fat_macho_with_gbf");
+  std::string elf("goto-programs/hello_elf");
+  std::string elf_with_gbf("goto-programs/hello_elf_with_gbf");
+
+  std::ostringstream fat_macho_output;
+  stream_message_handlert fat_macho_output_handler(fat_macho_output);
+  std::ostringstream fat_macho_with_gbf_output;
+  stream_message_handlert fat_macho_with_gbf_output_handler(
+    fat_macho_with_gbf_output);
+  std::ostringstream elf_output;
+  stream_message_handlert elf_output_handler(elf_output);
+  std::ostringstream elf_with_gbf_output;
+  stream_message_handlert elf_with_gbf_output_handler(elf_with_gbf_output);
+  std::ostringstream class_file_output;
+  stream_message_handlert class_file_output_handler(class_file_output);
+
+  bool class_file_is_gbf =
+    is_goto_binary(class_file, class_file_output_handler);
+  REQUIRE(!class_file_is_gbf);
+  REQUIRE(class_file_output.str().empty());
+
+  bool elf_has_gbf = is_goto_binary(elf, elf_output_handler);
+  bool elf_with_gbf_has_gbf =
+    is_goto_binary(elf_with_gbf, elf_with_gbf_output_handler);
+  REQUIRE(!elf_has_gbf);
+  REQUIRE(elf_with_gbf_has_gbf);
+  REQUIRE(elf_output.str().empty());
+  REQUIRE(elf_with_gbf_output.str().empty());
+
+  bool fat_macho_has_gbf = is_goto_binary(fat_macho, fat_macho_output_handler);
+  bool fat_macho_with_gbf_has_gbf =
+    is_goto_binary(fat_macho_with_gbf, fat_macho_with_gbf_output_handler);
+
+#ifdef __APPLE__
+
+  REQUIRE(!fat_macho_has_gbf);
+  REQUIRE(fat_macho_output.str().empty());
+  REQUIRE(fat_macho_with_gbf_has_gbf);
+  REQUIRE(fat_macho_with_gbf_output.str().empty());
+
+#else
+
+  REQUIRE(!fat_macho_has_gbf);
+  REQUIRE(
+    fat_macho_output.str() == "Cannot read OSX fat archive on this platform\n");
+
+  REQUIRE(!fat_macho_with_gbf_has_gbf);
+  REQUIRE(
+    fat_macho_with_gbf_output.str() ==
+    "Cannot read OSX fat archive on this platform\n");
+
+#endif
+}

unit/goto-programs/osx_fat_reader.cpp
@@ -18,25 +18,34 @@ Author: Diffblue Ltd.
 TEST_CASE("OSX fat binary reader", "[core][goto-programs][osx_fat_reader]")
 {
   std::ifstream class_file("goto-programs/Hello.class");
-  std::ifstream fat_binary("goto-programs/hello_fat");
+  std::ifstream fat_macho("goto-programs/hello_fat_macho");
+  std::ifstream fat_macho_with_gbf("goto-programs/hello_fat_macho_with_gbf");
 
   char class_header[8];
-  char fat_binary_header[8];
+  char fat_macho_header[8];
+  char fat_macho_with_gbf_header[8];
 
   class_file.read(class_header, 8);
-  fat_binary.read(fat_binary_header, 8);
+  fat_macho.read(fat_macho_header, 8);
+  fat_macho_with_gbf.read(fat_macho_with_gbf_header, 8);
 
   REQUIRE(class_file);
-  REQUIRE(fat_binary);
+  REQUIRE(fat_macho);
+  REQUIRE(fat_macho_with_gbf);
 
-  REQUIRE(is_osx_fat_header(fat_binary_header));
+  REQUIRE(is_osx_fat_header(fat_macho_header));
+  REQUIRE(is_osx_fat_header(fat_macho_with_gbf_header));
   REQUIRE(!is_osx_fat_header(class_header));
 
   class_file.seekg(0);
-  fat_binary.seekg(0);
+  fat_macho.seekg(0);
+  fat_macho_with_gbf.seekg(0);
 
-  std::ostringstream fat_binary_output;
-  stream_message_handlert fat_binary_output_handler(fat_binary_output);
+  std::ostringstream fat_macho_output;
+  stream_message_handlert fat_macho_output_handler(fat_macho_output);
+  std::ostringstream fat_macho_with_gbf_output;
+  stream_message_handlert fat_macho_with_gbf_output_handler(
+    fat_macho_with_gbf_output);
   std::ostringstream class_file_output;
   stream_message_handlert class_file_output_handler(class_file_output);
 
@@ -45,9 +54,15 @@ TEST_CASE("OSX fat binary reader", "[core][goto-programs][osx_fat_reader]")
   REQUIRE_THROWS_AS(
     (osx_fat_readert{class_file, class_file_output_handler}),
     deserialization_exceptiont);
-  osx_fat_readert fat_binary_reader(fat_binary, fat_binary_output_handler);
-  REQUIRE(fat_binary_output.str().empty());
-  REQUIRE(!fat_binary_reader.has_gb());
+
+  osx_fat_readert fat_macho_reader(fat_macho, fat_macho_output_handler);
+  REQUIRE(fat_macho_output.str().empty());
+  REQUIRE(!fat_macho_reader.has_gb());
+
+  osx_fat_readert fat_macho_with_gbf_reader(
+    fat_macho_with_gbf, fat_macho_with_gbf_output_handler);
+  REQUIRE(fat_macho_with_gbf_output.str().empty());
+  REQUIRE(fat_macho_with_gbf_reader.has_gb());
 
 #else
 
@@ -57,10 +72,16 @@ TEST_CASE("OSX fat binary reader", "[core][goto-programs][osx_fat_reader]")
     class_file_output.str() ==
     "Cannot read OSX fat archive on this platform\n");
 
-  osx_fat_readert fat_binary_reader(fat_binary, fat_binary_output_handler);
-  REQUIRE(!fat_binary_reader.has_gb());
+  osx_fat_readert fat_macho_reader(fat_macho, fat_macho_output_handler);
+  REQUIRE(!fat_macho_reader.has_gb());
   REQUIRE(
-    fat_binary_output.str() ==
+    fat_macho_output.str() == "Cannot read OSX fat archive on this platform\n");
+
+  osx_fat_readert fat_macho_with_gbf_reader(
+    fat_macho_with_gbf, fat_macho_with_gbf_output_handler);
+  REQUIRE(!fat_macho_with_gbf_reader.has_gb());
+  REQUIRE(
+    fat_macho_with_gbf_output.str() ==
     "Cannot read OSX fat archive on this platform\n");
 
 #endif