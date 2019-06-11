5a8aea0aea55836b906d79d16fd60d9613df1e9a
Peter Schrammel <peter.schrammel@diffblue.com>
2019-06-11 21:28:49 +0100

    Show parse tree for given main class in janalyzer
    To match behaviour of JBMC.

--- jbmc/src/janalyzer/janalyzer_parse_options.cpp
+++ jbmc/src/janalyzer/janalyzer_parse_options.cpp
@@ -375,6 +375,26 @@ int janalyzer_parse_optionst::doit()
     return CPROVER_EXIT_USAGE_ERROR;
   }
 
+  // Shows the parse tree of the main class
+  if(cmdline.isset("show-parse-tree"))
+  {
+    std::unique_ptr<languaget> language = get_language_from_mode(ID_java);
+    CHECK_RETURN(language != nullptr);
+    language->set_language_options(options);
+    language->set_message_handler(ui_message_handler);
+
+    log.status() << "Parsing ..." << messaget::eom;
+
+    if(static_cast<java_bytecode_languaget *>(language.get())->parse())
+    {
+      log.error() << "PARSING ERROR" << messaget::eom;
+      return CPROVER_EXIT_PARSE_ERROR;
+    }
+
+    language->show_parse(std::cout);
+    return CPROVER_EXIT_SUCCESS;
+  }
+
   lazy_goto_modelt lazy_goto_model =
     lazy_goto_modelt::from_handler_object(*this, options, ui_message_handler);
   lazy_goto_model.initialize(cmdline.args, options);
