2c5ef55be0b3852d0425e85eb3e1d9f1af08c60a
Romain Brenguier <romain.brenguier@gmail.com>
2019-06-13 16:38:06 +0100

    Inline variable that is immediately moved

--- src/goto-symex/symex_assign.cpp
+++ src/goto-symex/symex_assign.cpp
@@ -465,9 +465,9 @@ void goto_symext::symex_assign_symbol(
                                symex_config.allow_pointer_unsoundness)
                              .get();
 
-  exprt ssa_full_lhs = add_to_lhs(full_lhs, l2_lhs);
   state.record_events.push(false);
-  const exprt l2_full_lhs = state.rename(std::move(ssa_full_lhs), ns).get();
+  const exprt l2_full_lhs =
+    state.rename(add_to_lhs(full_lhs, l2_lhs), ns).get();
   state.record_events.pop();
 
   // do the assignment
