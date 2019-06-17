a7fa7f6732956bb690390ad83e69643323a566c3
Jeannie Moulton <jeannie.moulton@diffblue.com>
2019-06-17 16:04:46 +0100

    Add overrides to trace building for java.
    For java, we wish to validate the trace using the
    check_trace_assumptions function and the "validate-trace" command line
    option, so we need to override the functions that build the trace.

--- jbmc/src/java_bytecode/java_single_path_symex_checker.h
+++ jbmc/src/java_bytecode/java_single_path_symex_checker.h
@@ -12,9 +12,13 @@ Author: Daniel Kroening, Peter Schrammel
 #ifndef CPROVER_JAVA_BYTECODE_JAVA_SINGLE_PATH_SYMEX_CHECKER_H
 #define CPROVER_JAVA_BYTECODE_JAVA_SINGLE_PATH_SYMEX_CHECKER_H
 
+#include <goto-checker/bmc_util.h>
+#include <goto-checker/counterexample_beautification.h>
 #include <goto-checker/single_path_symex_checker.h>
+#include <goto-symex/build_goto_trace.h>
 
 #include "java_bmc_util.h"
+#include "java_trace_validation.h"
 
 class java_single_path_symex_checkert : public single_path_symex_checkert
 {
@@ -32,6 +36,10 @@ public:
     single_path_symex_checkert::setup_symex(symex);
     java_setup_symex(options, goto_model, symex);
   }
+
+  goto_tracet build_full_trace() const override;
+  goto_tracet build_shortest_trace() const override;
+  goto_tracet build_trace(const irep_idt &property_id) const override;
 };
 
 #endif // CPROVER_JAVA_BYTECODE_JAVA_SINGLE_PATH_SYMEX_CHECKER_H
