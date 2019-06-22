4a1b7cbc8426ffc3abf1747927a152f68a386ed3
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-06-21 18:56:51 -0700

    added create_directory(path)
    This replaces mkdir(...), which differs on Unix and Windows.

--- src/util/file_util.h
+++ src/util/file_util.h
@@ -27,4 +27,9 @@ std::string concat_dir_file(const std::string &directory,
 // C++17 will allow us to use std::filesystem::is_directory
 bool is_directory(const std::string &path);
 
+/// Create a directory with given path
+/// C++17 will allow us to use std::filesystem::create_directory
+/// \return true iff the directory was created
+bool create_directory(const std::string &path);
+
 #endif // CPROVER_UTIL_FILE_UTIL_H
