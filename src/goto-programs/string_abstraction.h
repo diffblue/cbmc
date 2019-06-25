e8f3459d3d87b8deb095370a4e11d67d809b6951
Joel Allred <joel.allred@diffblue.com>
2019-06-25 13:22:58 +0100

    Document string_abstractiont

--- src/goto-programs/string_abstraction.h
+++ src/goto-programs/string_abstraction.h
@@ -19,6 +19,11 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include "goto_model.h"
 
+/// Replace all uses of `char *` by a struct that carries that string, and also
+/// the underlying allocation and the C string length.
+/// This will become useful (with some modifications) for supporting strings in
+/// the C frontend, as the string solver expects a struct that bundles the
+/// string length and the underlying character array.
 class string_abstractiont:public messaget
 {
 public:
