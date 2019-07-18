1e9632417124504dd4e650d03364876894b8565c
Daniel Poetzl <daniel.poetzl@diffblue.com>
2019-07-18 12:34:15 +0100

    Create aux symbol with a different name than symbols in namespace
    This adds another version of get_fresh_aux_symbol which takes a namespace and
    produces symbol names that are different from any of the symbols in the
    namespace. This can be used to e.g. generate fresh symbols during symex that
    don't clash with any of the existing symbols.

--- src/util/fresh_symbol.h
+++ src/util/fresh_symbol.h
@@ -16,6 +16,7 @@ Author: Chris Smowton, chris.smowton@diffblue.com
 
 #include "irep.h"
 
+class namespacet;
 class source_locationt;
 class symbolt;
 class symbol_table_baset;
@@ -27,6 +28,15 @@ symbolt &get_fresh_aux_symbol(
   const std::string &basename_prefix,
   const source_locationt &source_location,
   const irep_idt &symbol_mode,
+  symbol_table_baset &symbol_table);
+
+symbolt &get_fresh_aux_symbol(
+  const typet &type,
+  const std::string &name_prefix,
+  const std::string &basename_prefix,
+  const source_locationt &source_location,
+  const irep_idt &symbol_mode,
+  const namespacet &ns,
   symbol_table_baset &symbol_table);
 
 #endif // CPROVER_UTIL_FRESH_SYMBOL_H
