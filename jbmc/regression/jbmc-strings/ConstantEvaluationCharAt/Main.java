225d4ffaac76c9e2c2706340380ea941377be4b3
Yumi Bagge <yumi.bagge@diffblue.com>
2019-07-26 18:46:57 +0100

    Clang reformat

--- jbmc/regression/jbmc-strings/ConstantEvaluationCharAt/Main.java
+++ jbmc/regression/jbmc-strings/ConstantEvaluationCharAt/Main.java
@@ -1,7 +1,7 @@
 public class Main {
-    public void constantCharAt() {
-        StringBuilder sb = new StringBuilder("abc");
-        char c = sb.charAt(1);
-        assert c == 'b';
-    }
+  public void constantCharAt() {
+    StringBuilder sb = new StringBuilder("abc");
+    char c = sb.charAt(1);
+    assert c == 'b';
+  }
 }
--- jbmc/regression/jbmc-strings/ConstantEvaluationIsEmpty/Main.java
+++ jbmc/regression/jbmc-strings/ConstantEvaluationIsEmpty/Main.java
@@ -1,12 +1,11 @@
 public class Main {
-    public void constantIsEmptyPass() {
-        String s = "";
-        assert s.isEmpty();
-    }
+  public void constantIsEmptyPass() {
+    String s = "";
+    assert s.isEmpty();
+  }
 
-    public void constantIsEmptyFail() {
-        String s = "abc";
-        assert s.isEmpty();
-    }
-
+  public void constantIsEmptyFail() {
+    String s = "abc";
+    assert s.isEmpty();
+  }
 }
