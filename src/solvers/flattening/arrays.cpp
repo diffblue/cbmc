diffblue/cbmc

commit 2fbe5e1e29ec625c8e15bcbeebe6923b4d184cc5

Rename lambda_exprt to array_comprehension_exprt
This kind of exprt does not represent lambda functions, but it
represents an array by specifying a range and a function operating on
the array indices.
This is a type of list/array comprehension, so calling it
array_comprehension_exprt is more descriptive of what this type is used
for.

src/solvers/flattening/boolbv.cpp
@@ -294,7 +294,7 @@ bvt boolbvt::convert_bitvector(const exprt &expr)
   else if(expr.id()==ID_complex_imag)
     return convert_complex_imag(to_complex_imag_expr(expr));
   else if(expr.id()==ID_lambda)
-    return convert_lambda(to_lambda_expr(expr));
+    return convert_lambda(to_array_comprehension_expr(expr));
   else if(expr.id()==ID_array_of)
     return convert_array_of(to_array_of_expr(expr));
   else if(expr.id()==ID_let)
@@ -316,7 +316,7 @@ bvt boolbvt::convert_bitvector(const exprt &expr)
   return conversion_failed(expr);
 }
 
-bvt boolbvt::convert_lambda(const lambda_exprt &expr)
+bvt boolbvt::convert_lambda(const array_comprehension_exprt &expr)
 {
   std::size_t width=boolbv_width(expr.type());
 

src/solvers/flattening/boolbv.h
@@ -28,7 +28,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 class extractbit_exprt;
 class extractbits_exprt;
-class lambda_exprt;
+class array_comprehension_exprt;
 class member_exprt;
 
 class boolbvt:public arrayst
@@ -149,7 +149,7 @@ protected:
   virtual bvt convert_complex(const complex_exprt &expr);
   virtual bvt convert_complex_real(const complex_real_exprt &expr);
   virtual bvt convert_complex_imag(const complex_imag_exprt &expr);
-  virtual bvt convert_lambda(const lambda_exprt &expr);
+  virtual bvt convert_lambda(const array_comprehension_exprt &expr);
   virtual bvt convert_let(const let_exprt &);
   virtual bvt convert_array_of(const array_of_exprt &expr);
   virtual bvt convert_union(const union_exprt &expr);

src/util/simplify_expr_array.cpp
@@ -48,12 +48,12 @@ bool simplify_exprt::simplify_index(exprt &expr)
   {
     // simplify (lambda i: e)(x) to e[i/x]
 
-    const lambda_exprt &lambda_expr = to_lambda_expr(array);
+    const auto &comprehension = to_array_comprehension_expr(array);
 
-    if(expr.op1().type() == lambda_expr.arg().type())
+    if(expr.op1().type() == comprehension.arg().type())
     {
-      exprt tmp = lambda_expr.body();
-      replace_expr(lambda_expr.arg(), expr.op1(), tmp);
+      exprt tmp = comprehension.body();
+      replace_expr(comprehension.arg(), expr.op1(), tmp);
       expr.swap(tmp);
       simplify_rec(expr);
       return false;

src/util/std_expr.h
@@ -4462,10 +4462,13 @@ inline cond_exprt &to_cond_expr(exprt &expr)
 
 /// \brief Expression to define a mapping from an argument (index) to elements.
 /// This enables constructing an array via an anonymous function.
-class lambda_exprt : public binary_exprt
+class array_comprehension_exprt : public binary_exprt
 {
 public:
-  explicit lambda_exprt(symbol_exprt arg, exprt body, array_typet _type)
+  explicit array_comprehension_exprt(
+    symbol_exprt arg,
+    exprt body,
+    array_typet _type)
     : binary_exprt(std::move(arg), ID_lambda, std::move(body), std::move(_type))
   {
   }
@@ -4502,35 +4505,38 @@ public:
 };
 
 template <>
-inline bool can_cast_expr<lambda_exprt>(const exprt &base)
+inline bool can_cast_expr<array_comprehension_exprt>(const exprt &base)
 {
   return base.id() == ID_lambda;
 }
 
-inline void validate_expr(const lambda_exprt &value)
+inline void validate_expr(const array_comprehension_exprt &value)
 {
-  validate_operands(value, 2, "'Lambda' must have two operands");
+  validate_operands(value, 2, "'Array comprehension' must have two operands");
 }
 
-/// \brief Cast an exprt to a \ref lambda_exprt
+/// \brief Cast an exprt to a \ref array_comprehension_exprt
 ///
-/// \a expr must be known to be \ref lambda_exprt.
+/// \a expr must be known to be \ref array_comprehension_exprt.
 ///
 /// \param expr: Source expression
-/// \return Object of type \ref lambda_exprt
-inline const lambda_exprt &to_lambda_expr(const exprt &expr)
+/// \return Object of type \ref array_comprehension_exprt
+inline const array_comprehension_exprt &
+to_array_comprehension_expr(const exprt &expr)
 {
   PRECONDITION(expr.id() == ID_lambda);
-  const lambda_exprt &ret = static_cast<const lambda_exprt &>(expr);
+  const array_comprehension_exprt &ret =
+    static_cast<const array_comprehension_exprt &>(expr);
   validate_expr(ret);
   return ret;
 }
 
-/// \copydoc to_lambda_expr(const exprt &)
-inline lambda_exprt &to_lambda_expr(exprt &expr)
+/// \copydoc to_array_comprehension_expr(const exprt &)
+inline array_comprehension_exprt &to_array_comprehension_expr(exprt &expr)
 {
   PRECONDITION(expr.id() == ID_lambda);
-  lambda_exprt &ret = static_cast<lambda_exprt &>(expr);
+  array_comprehension_exprt &ret =
+    static_cast<array_comprehension_exprt &>(expr);
   validate_expr(ret);
   return ret;
 }

commit b5ad3e432db7c944c9029efcecafb015fee07daa

introduce ID_array_comprehension
The ID is introduced to avoid an inconsistency between class name and the ID
that was introduced with 2fbe5e1e29.

This will allow the introduction of lambda expressions, to mean nameless
(mathematical) functions.

src/solvers/flattening/arrays.cpp
@@ -83,7 +83,7 @@ void arrayst::collect_indices(const exprt &expr)
 {
   if(expr.id()!=ID_index)
   {
-    if(expr.id() == ID_lambda)
+    if(expr.id() == ID_array_comprehension)
       array_comprehension_args.insert(
         to_array_comprehension_expr(expr).arg().get_identifier());
 
@@ -228,7 +228,7 @@ void arrayst::collect_arrays(const exprt &a)
     arrays.make_union(a, a.op0());
     collect_arrays(a.op0());
   }
-  else if(a.id() == ID_lambda)
+  else if(a.id() == ID_array_comprehension)
   {
   }
   else
@@ -470,7 +470,7 @@ void arrayst::add_array_constraints(
     return add_array_constraints_array_of(index_set, to_array_of_expr(expr));
   else if(expr.id() == ID_array)
     return add_array_constraints_array_constant(index_set, to_array_expr(expr));
-  else if(expr.id() == ID_lambda)
+  else if(expr.id() == ID_array_comprehension)
   {
     return add_array_constraints_comprehension(
       index_set, to_array_comprehension_expr(expr));

src/solvers/flattening/boolbv.cpp
@@ -290,8 +290,8 @@ bvt boolbvt::convert_bitvector(const exprt &expr)
     return convert_complex_real(to_complex_real_expr(expr));
   else if(expr.id()==ID_complex_imag)
     return convert_complex_imag(to_complex_imag_expr(expr));
-  else if(expr.id()==ID_lambda)
-    return convert_lambda(to_array_comprehension_expr(expr));
+  else if(expr.id() == ID_array_comprehension)
+    return convert_array_comprehension(to_array_comprehension_expr(expr));
   else if(expr.id()==ID_array_of)
     return convert_array_of(to_array_of_expr(expr));
   else if(expr.id()==ID_let)
@@ -313,7 +313,7 @@ bvt boolbvt::convert_bitvector(const exprt &expr)
   return conversion_failed(expr);
 }
 
-bvt boolbvt::convert_lambda(const array_comprehension_exprt &expr)
+bvt boolbvt::convert_array_comprehension(const array_comprehension_exprt &expr)
 {
   std::size_t width=boolbv_width(expr.type());
 

src/solvers/flattening/boolbv.h
@@ -149,7 +149,7 @@ protected:
   virtual bvt convert_complex(const complex_exprt &expr);
   virtual bvt convert_complex_real(const complex_real_exprt &expr);
   virtual bvt convert_complex_imag(const complex_imag_exprt &expr);
-  virtual bvt convert_lambda(const array_comprehension_exprt &expr);
+  virtual bvt convert_array_comprehension(const array_comprehension_exprt &);
   virtual bvt convert_let(const let_exprt &);
   virtual bvt convert_array_of(const array_of_exprt &expr);
   virtual bvt convert_union(const union_exprt &expr);

src/util/irep_ids.def
@@ -223,6 +223,7 @@ IREP_ID_ONE(ieee_float_equal)
 IREP_ID_ONE(ieee_float_notequal)
 IREP_ID_ONE(isnan)
 IREP_ID_ONE(lambda)
+IREP_ID_ONE(array_comprehension)
 IREP_ID_ONE(array_of)
 IREP_ID_ONE(array_equal)
 IREP_ID_ONE(array_set)

src/util/simplify_expr_array.cpp
@@ -49,7 +49,7 @@ simplify_exprt::simplify_index(const index_exprt &expr)
     }
   }
 
-  if(array.id() == ID_lambda)
+  if(array.id() == ID_array_comprehension)
   {
     // simplify (lambda i: e)(x) to e[i/x]
 

src/util/std_expr.h
@@ -4481,8 +4481,6 @@ inline cond_exprt &to_cond_expr(exprt &expr)
 /// function, respectively. The range is given by the type of the expression,
 /// which has to be an \ref array_typet (which includes a value for
 /// `array_size`).
-/// For legacy reasons, the ID of an array_comprehension_exprt is ID_lambda,
-/// even though it cannot be used to represent arbitrary lambda functions.
 class array_comprehension_exprt : public binary_exprt
 {
 public:
@@ -4490,7 +4488,11 @@ public:
     symbol_exprt arg,
     exprt body,
     array_typet _type)
-    : binary_exprt(std::move(arg), ID_lambda, std::move(body), std::move(_type))
+    : binary_exprt(
+        std::move(arg),
+        ID_array_comprehension,
+        std::move(body),
+        std::move(_type))
   {
   }
 
@@ -4528,7 +4530,7 @@ public:
 template <>
 inline bool can_cast_expr<array_comprehension_exprt>(const exprt &base)
 {
-  return base.id() == ID_lambda;
+  return base.id() == ID_array_comprehension;
 }
 
 inline void validate_expr(const array_comprehension_exprt &value)
@@ -4545,7 +4547,7 @@ inline void validate_expr(const array_comprehension_exprt &value)
 inline const array_comprehension_exprt &
 to_array_comprehension_expr(const exprt &expr)
 {
-  PRECONDITION(expr.id() == ID_lambda);
+  PRECONDITION(expr.id() == ID_array_comprehension);
   const array_comprehension_exprt &ret =
     static_cast<const array_comprehension_exprt &>(expr);
   validate_expr(ret);
@@ -4555,7 +4557,7 @@ to_array_comprehension_expr(const exprt &expr)
 /// \copydoc to_array_comprehension_expr(const exprt &)
 inline array_comprehension_exprt &to_array_comprehension_expr(exprt &expr)
 {
-  PRECONDITION(expr.id() == ID_lambda);
+  PRECONDITION(expr.id() == ID_array_comprehension);
   array_comprehension_exprt &ret =
     static_cast<array_comprehension_exprt &>(expr);
   validate_expr(ret);