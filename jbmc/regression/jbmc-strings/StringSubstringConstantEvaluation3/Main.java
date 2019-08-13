e56041f4cf250f77a010b7231c74a3fa6051b1a9
Daniel Poetzl <daniel.poetzl@diffblue.com>
2019-08-13 18:38:14 +0100

    Add String.substring() and StringBuilder.substring() regression error tests
    Add regression tests that check that the constant propagator does not simplify
    substring() invocations with only constant arguments when the indices are out of
    bounds.

---
+++ jbmc/regression/jbmc-strings/StringSubstringConstantEvaluation3/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public void test1() {
+    String s1 = "abc";
+    s1.substring(-1);
+  }
+
+  public void test2() {
+    String s1 = "abc";
+    s1.substring(4);
+  }
+
+  public void test3() {
+    String s1 = "abc";
+    s1.substring(-1, 4);
+  }
+}
