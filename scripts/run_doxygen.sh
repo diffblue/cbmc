bca2236f1116a106e09153a0186135ec39254b53
thk123 <thomas@diffblue.com>
2019-08-23 14:15:48 +0100

    Allow specifying the version of doxygen used by the script

--- scripts/run_doxygen.sh
+++ scripts/run_doxygen.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+doxygen_executable=${1:-doxygen}
+
 # Check doxygen version
 EXPECTED_VERSION="1.8.16"
-doxygen --version | grep ^$EXPECTED_VERSION > /dev/null
+$doxygen_executable --version | grep ^$EXPECTED_VERSION > /dev/null
 if [ $? -ne 0 ]
 then
   echo "WARNING: Using wrong version of doxygen.\
   The list of expected warnings is for version $EXPECTED_VERSION."
+  $doxygen_executable --version
+
 fi
 
 # Run doxygen and filter warnings
 SCRIPT_FOLDER=`dirname $0`
 cd $SCRIPT_FOLDER/../src
-doxygen 2>&1 | ../scripts/filter_expected_warnings.py ../scripts/expected_doxygen_warnings.txt
+$doxygen_executable 2>&1 | ../scripts/filter_expected_warnings.py ../scripts/expected_doxygen_warnings.txt
