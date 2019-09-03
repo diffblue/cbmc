67c6e3dd586906c4ec5f3bb7580a3995d7f82440
Thomas Spriggs <thomas.spriggs@diffblue.com>
2019-09-03 11:17:16 +0100

    Keep initialise functions value nil until completely made
    By avoiding setting the value of the initialise function until the code
    is completely generated we can use a check on whether the value is nil
    for checking whether the generation is complete.

--- jbmc/src/java_bytecode/java_entry_point.cpp
+++ jbmc/src/java_bytecode/java_entry_point.cpp
@@ -54,18 +54,6 @@ static void create_initialize(symbol_table_baset &symbol_table)
 
   initialize.type = java_method_typet({}, java_void_type());
 
-  code_blockt init_code;
-
-  namespacet ns(symbol_table);
-
-  symbol_exprt rounding_mode=
-    ns.lookup(CPROVER_PREFIX "rounding_mode").symbol_expr();
-
-  init_code.add(
-    code_assignt(rounding_mode, from_integer(0, rounding_mode.type())));
-
-  initialize.value=init_code;
-
   symbol_table.add(initialize);
 }
 
@@ -127,7 +115,13 @@ static void java_static_lifetime_init(
 {
   symbolt &initialize_symbol =
     symbol_table.get_writeable_ref(INITIALIZE_FUNCTION);
-  code_blockt &code_block=to_code_block(to_code(initialize_symbol.value));
+  code_blockt code_block;
+
+  const symbol_exprt rounding_mode =
+    symbol_table.lookup_ref(CPROVER_PREFIX "rounding_mode").symbol_expr();
+  code_block.add(
+    code_assignt{rounding_mode, from_integer(0, rounding_mode.type())});
+
   object_factory_parameters.function_id = initialize_symbol.name;
 
   // If there are strings given using --string-input-value, we need to add
@@ -266,6 +260,7 @@ static void java_static_lifetime_init(
       }
     }
   }
+  initialize_symbol.value = std::move(code_block);
 }
 
 /// Checks whether the given symbol is a valid java main method
