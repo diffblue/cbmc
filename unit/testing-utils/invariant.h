fafafbd90cbb29b194dd790bc87bb5e404ab6726
Thomas Spriggs <thomas.spriggs@diffblue.com>
2019-09-03 19:33:23 +0100

    Add catch framework matcher for `invariant_failuret` testing
    This utility supports writing more succinct requirements on
    `invariant_failuret` being thrown and the reason for which it was
    thrown.

---
+++ unit/testing-utils/invariant.h
@@ -0,0 +1,28 @@
+/// Author: Diffblue Ltd.
+
+#ifndef CPROVER_TESTING_UTILS_INVARIANT_H
+#define CPROVER_TESTING_UTILS_INVARIANT_H
+
+#include <testing-utils/use_catch.h>
+#include <util/invariant.h>
+
+#include <string>
+
+class invariant_failure_containingt
+  : public Catch::MatcherBase<invariant_failedt>
+{
+public:
+  explicit invariant_failure_containingt(std::string expected);
+  bool match(const invariant_failedt &exception) const override;
+  std::string describe() const override;
+
+private:
+  std::string expected;
+};
+
+/// Returns a matcher which matches an invariant_failedt exception, where the
+/// `.what()` returns a string containing \p expected.
+invariant_failure_containingt
+invariant_failure_containing(std::string expected);
+
+#endif // CPROVER_TESTING_UTILS_INVARIANT_H
