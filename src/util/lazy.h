cd07c794ec043208d0dbceb297183cbfb5543d01
Romain Brenguier <romain.brenguier@gmail.com>
2019-09-05 13:40:42 +0100

    Introduce lazy class
    This generalizes a pattern for which we have several potential usage.

---
+++ src/util/lazy.h
@@ -0,0 +1,46 @@
+/*******************************************************************\
+
+Module: Util
+
+Author: Romain Brenguier, romain.brenguier@diffblue.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_UTIL_LAZY_H
+#define CPROVER_UTIL_LAZY_H
+
+#include <functional>
+#include <util/optional.h>
+
+template <typename valuet>
+class lazyt
+{
+public:
+  /// Delay the computation of \p fun to the next time the \c force method
+  /// is called.
+  static lazyt from_fun(std::function<valuet()> fun)
+  {
+    return lazyt{std::move(fun)};
+  }
+
+  /// Force the computation of the value. If it was already computed,
+  /// return the same result.
+  valuet force()
+  {
+    if(value)
+      return *value;
+    value = evaluation_function();
+    return *value;
+  }
+
+private:
+  optionalt<valuet> value;
+  std::function<valuet()> evaluation_function;
+
+  explicit lazyt(std::function<valuet()> fun)
+    : evaluation_function(std::move(fun))
+  {
+  }
+};
+
+#endif // CPROVER_UTIL_LAZY_H
