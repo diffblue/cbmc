ac681ab906cad57adfe05b698896f6e3aa0d6d6e
Jeannie Moulton <jeannie.moulton@diffblue.com>
2019-10-02 15:29:28 +0100

    Unit test create_parameter_symbols

--- jbmc/unit/java_bytecode/java_bytecode_convert_method/convert_method.cpp
+++ jbmc/unit/java_bytecode/java_bytecode_convert_method/convert_method.cpp
@@ -954,3 +954,69 @@ TEST_CASE(
   REQUIRE(variables[1][0].symbol_expr.get(ID_C_base_name) == "this$0");
   REQUIRE(variables[2][0].symbol_expr.get(ID_C_base_name) == "other");
 }
+
+TEST_CASE(
+  "create_parameter_symbols",
+  "[core][java_bytecode][java_bytecode_convert_method]"
+  "[create_parameter_symbols]")
+{
+  // Arrange
+  const irep_idt method_id = "someClass.someMethod";
+  // The parameters should be already populated, with names, ids
+  code_typet::parametert this_param;
+  this_param.set_this();
+  this_param.type() = java_lang_object_type();
+  this_param.set_identifier(id2string(method_id) + "::this");
+  this_param.set_base_name("this");
+  code_typet::parametert ref_to_inner;
+  ref_to_inner.type() = java_lang_object_type();
+  ref_to_inner.set_identifier(id2string(method_id) + "::this$0");
+  ref_to_inner.set_base_name("this$0");
+  code_typet::parametert other_param;
+  other_param.type() = java_lang_object_type();
+  other_param.set_identifier(id2string(method_id) + "::other");
+  other_param.set_base_name("other");
+  java_method_typet::parameterst parameters{
+    this_param, ref_to_inner, other_param};
+
+  variablest variables;
+
+  symbol_tablet symbol_table;
+  REQUIRE(symbol_table.symbols.empty());
+
+  // Act
+  create_parameter_symbols(parameters, variables, symbol_table);
+
+  // Assert side effects on symbol table
+  REQUIRE(symbol_table.symbols.size() == 3);
+  const symbolt this_symbol =
+    symbol_table.lookup_ref(id2string(method_id) + "::this");
+  REQUIRE(this_symbol.name == id2string(method_id) + "::this");
+  REQUIRE(this_symbol.base_name == "this");
+  REQUIRE(this_symbol.type == java_lang_object_type());
+  REQUIRE(this_symbol.mode == ID_java);
+  const symbolt inner_symbol =
+    symbol_table.lookup_ref(id2string(method_id) + "::this$0");
+  REQUIRE(inner_symbol.name == id2string(method_id) + "::this$0");
+  REQUIRE(inner_symbol.base_name == "this$0");
+  REQUIRE(inner_symbol.type == java_lang_object_type());
+  REQUIRE(inner_symbol.mode == ID_java);
+  const symbolt other_symbol =
+    symbol_table.lookup_ref(id2string(method_id) + "::other");
+  REQUIRE(other_symbol.name == id2string(method_id) + "::other");
+  REQUIRE(other_symbol.base_name == "other");
+  REQUIRE(other_symbol.type == java_lang_object_type());
+  REQUIRE(other_symbol.mode == ID_java);
+
+  // Assert side effects on variables
+  REQUIRE(variables.size() == 3);
+  REQUIRE(
+    variables[0][0].symbol_expr.get_identifier() ==
+    id2string(method_id) + "::this");
+  REQUIRE(
+    variables[1][0].symbol_expr.get_identifier() ==
+    id2string(method_id) + "::this$0");
+  REQUIRE(
+    variables[2][0].symbol_expr.get_identifier() ==
+    id2string(method_id) + "::other");
+}
