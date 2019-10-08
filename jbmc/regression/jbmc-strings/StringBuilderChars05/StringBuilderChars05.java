b44fd4109e117343732716d9c9b687cc0f5bcde5
Daniel Poetzl <daniel.poetzl@diffblue.com>
2019-10-08 12:02:18 +0100

    Adapt existing StringBuilder tests
    Activate two tests marked FUTURE that can meanwhile be solved by the string
    solver. Also adapt tests so they cannot be solved via constant propagation, and
    load the models library.

--- jbmc/regression/jbmc-strings/StringBuilderChars05/StringBuilderChars05.java
+++ jbmc/regression/jbmc-strings/StringBuilderChars05/StringBuilderChars05.java
@@ -1,10 +1,11 @@
 public class StringBuilderChars05
 {
-   public static void main(String[] args)
+   public static void test(boolean choice)
    {
-      StringBuilder buffer = new StringBuilder("Diffblue Limited");
+      String s = choice ? "Diffblue Limited" : "Diffblue Unlimited";
+      StringBuilder buffer = new StringBuilder(s);
       buffer.setCharAt(0, 'H');
       buffer.setCharAt(6, 'T');
-      assert buffer.toString().equals("HiffBllTe Limited");
+      assert !choice || buffer.toString().equals("HiffBllTe Limited");
    }
 }
