7e4cdd5bbebc6a9f26f5ceb6df49f33bb80d817f
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2019-10-25 18:37:03 +0100

    Add function pointer restriction regression tests

---
+++ regression/goto-instrument/restrict-function-pointer-to-multiple-functions/test.c
@@ -0,0 +1,66 @@
+#include <assert.h>
+
+typedef int (*fptr_t)(int);
+
+fptr_t get_f(void);
+
+void use_f(fptr_t fptr)
+{
+  assert(fptr(10) >= 10);
+}
+
+void select_f(void);
+void select_g(void);
+void select_h(void);
+
+int main(void)
+{
+  select_f();
+  use_f(get_f());
+  select_g();
+  use_f(get_f());
+}
+
+int f(int x)
+{
+  return x + 1;
+}
+int g(int x)
+{
+  return x;
+}
+int h(int x)
+{
+  return x - 1;
+}
+
+int g_select_function = 0;
+
+void select_f(void)
+{
+  g_select_function = 0;
+}
+void select_g(void)
+{
+  g_select_function = 1;
+}
+void select_h(void)
+{
+  g_select_function = 2;
+}
+
+fptr_t get_f(void)
+{
+  if(g_select_function == 0)
+  {
+    return f;
+  }
+  else if(g_select_function == 1)
+  {
+    return g;
+  }
+  else
+  {
+    return h;
+  }
+}
---
+++ regression/goto-instrument/restrict-function-pointer-to-single-function-incorrectly/test.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+
+typedef int (*fptr_t)(int);
+
+fptr_t get_f(void);
+
+void use_f(fptr_t fptr)
+{
+  assert(fptr(10) == 11);
+}
+
+int main(void)
+{
+  use_f(get_f());
+}
+
+int f(int x)
+{
+  return x + 1;
+}
+int g(int x)
+{
+  return x;
+}
+
+// this is just here to distinguish the behavior from FP removal, which'd include g
+int g_always_false_cond = 1;
+
+fptr_t get_f(void)
+{
+  if(!g_always_false_cond)
+  {
+    return f;
+  }
+  else
+  {
+    return g;
+  }
+}
