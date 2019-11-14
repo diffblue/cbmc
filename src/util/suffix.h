b5dee9ac8174252796fd313cf5d81c81fa1158c2
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-11-14 18:09:03 +0000

    C++20 replacements for has_suffix/has_prefix
    This clarifies the intended meaning of the existing functions.

--- src/util/suffix.h
+++ src/util/suffix.h
@@ -12,6 +12,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include <string>
 
+// C++20 will have std::string::ends_with
+
 inline bool has_suffix(const std::string &s, const std::string &suffix)
 {
   if(suffix.size()>s.size())
