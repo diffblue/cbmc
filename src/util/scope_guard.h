7dda2661a4405ca132fd090eb5289adbdc632543
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2020-01-29 14:43:33 +0000

    WIP Make scope guard work with C++11

--- src/util/scope_guard.h
+++ src/util/scope_guard.h
@@ -17,25 +17,26 @@ template <typename OnErrorHandler>
 class on_exit_scope_with_exceptiont
 {
   OnErrorHandler handler;
-  int uncaught_exceptions_at_scope_start;
+  std::exception_ptr uncaught_exception_at_scope_start;
   bool cancelled = false;
 
 public:
   explicit on_exit_scope_with_exceptiont(OnErrorHandler handler)
     : handler(std::move(handler)),
-      uncaught_exceptions_at_scope_start(std::uncaught_exceptions())
+      uncaught_exception_at_scope_start(std::current_exception())
   {
   }
 
   on_exit_scope_with_exceptiont(const on_exit_scope_with_exceptiont &) = delete;
   on_exit_scope_with_exceptiont(on_exit_scope_with_exceptiont &&other) noexcept
     : handler{std::move(other.handler)},
-      uncaught_exceptions_at_scope_start{
-        other.uncaught_exceptions_at_scope_start},
-      cancelled{false}
+      uncaught_exception_at_scope_start{
+        std::move(other.uncaught_exception_at_scope_start)},
+      cancelled{other.cancelled}
   {
     other.cancelled = true;
   }
+
   on_exit_scope_with_exceptiont &
   operator=(const on_exit_scope_with_exceptiont &) = delete;
   on_exit_scope_with_exceptiont &
@@ -44,10 +45,11 @@ public:
   ~on_exit_scope_with_exceptiont()
   {
     // there was an uncaught exception in the scope of this guard if
-    // the number of uncaught exceptions has increased since its creation.
+    // the current exception now is different than the current exception
+    // at the time of creating this object.
     if(
       !cancelled &&
-      std::uncaught_exceptions() > uncaught_exceptions_at_scope_start)
+      std::current_exception() != uncaught_exception_at_scope_start)
     {
       handler();
     }
