b8e774c91f4addf1427a65437aa7cd66319d9272
Chris Smowton <chris@smowton.net>
2020-02-06 16:52:10 +0000

    Ensure string is non-null, radix is in range before parsing integers
    Previously we passed the input to parseInt/parseLong straight to the built-in string solver functions,
    meaning it was possible to crash jbmc by passing an out-of-range constant radix or for the solver to
    produce crazy results when it regarded the radix as a free choice. Now the models apply the constraint
    that it falls in a sensible range.
    
    I add a test for parseint with a free radix, but am unable to add one for parselong yet as it takes
    too long to solve.

---
+++ jbmc/regression/strings-smoke-tests/java_parselong_unknown_radix/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void testMe(String input, int radix) {
+
+        // Railroad the solver a bit to speed up solving
+        // (note use of > and < to dodge constant propagation)
+        if(input == null || input.length() != 1 || input.charAt(0) != 'k' || radix < 30 || radix > 30)
+            return;
+
+	long value = Long.parseLong(input, radix);
+	if(value == 20)
+	    assert false;
+        else
+            assert false;
+
+    }
+
+}
