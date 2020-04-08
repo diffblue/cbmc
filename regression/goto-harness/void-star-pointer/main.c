1bbd0f80b0d94e5388fd993c6974f63fbbd08119
Thomas Kiley <thomas.kiley@diffblue.com>
2020-04-08 17:47:14 +0100

    Add a test for array of void* pointers

--- regression/goto-harness/void-star-pointer/main.c
+++ regression/goto-harness/void-star-pointer/main.c
@@ -1,6 +1,14 @@
 #include <assert.h>
+#include <stdbool.h>
 
 void test_function(void *input)
 {
   assert(input == 0);
+}
+
+void test_void_array(void *input_array[10])
+{
+  __CPROVER_assume(input_array != 0);
+  assert(input_array[0] == 0);
+  assert(false);
 }
