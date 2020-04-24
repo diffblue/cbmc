9384b911adab0977f5687df63cb6c50aaed37797
Thomas Kiley <thomas.kiley@diffblue.com>
2020-04-24 18:06:27 +0100

    Add additional tests for more types

--- regression/cbmc/float-trace/test.c
+++ regression/cbmc/float-trace/test.c
@@ -1,12 +1,20 @@
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 
 int main()
 {
   float zero = 0.0f;
   float one = 1.0f;
-  double dzero = 0.0;
   float complex = 0x1p+37f;
+  float inf = INFINITY;
+  float nan = NAN;
+
+  double dzero = 0.0;
+  double done = 1.0;
+  double dinf = INFINITY;
+  double dnan = NAN;
+  double dcomplex = -5.56268e-309;
 
   assert(false);
 
