50fae45ad51a24b0c52cc9e772d1f6ae7fd75573
Thomas Kiley <thomas.kiley@diffblue.com>
2020-05-22 17:56:20 +0100

    Add missing test coverage for unary operations

--- unit/util/interval/eval.cpp
+++ unit/util/interval/eval.cpp
@@ -36,14 +36,20 @@ SCENARIO("Unary eval on intervals", "[core][analyses][interval][eval]")
     }
   }
 
-  WHEN("Unary operations to an interval")
+  WHEN("Unary operations to an single element interval")
   {
     constant_interval_exprt five =
       constant_interval_exprt(from_integer(5, signedbv_typet(32)));
+    const constant_interval_exprt &max_interval =
+      constant_interval_exprt(max_exprt{signedbv_typet(32)});
+    const constant_interval_exprt &min_interval =
+      constant_interval_exprt(min_exprt{signedbv_typet(32)});
 
     THEN("When we apply unary addition to it, nothing should happen")
     {
       REQUIRE(five.eval(ID_unary_plus) == five);
+      REQUIRE(min_interval.eval(ID_unary_minus).has_no_lower_bound());
+      REQUIRE(max_interval.eval(ID_unary_minus).has_no_upper_bound());
     }
 
     THEN("When we apply unary subtraction to it, it should be negated")
@@ -52,6 +58,9 @@ SCENARIO("Unary eval on intervals", "[core][analyses][interval][eval]")
         numeric_cast<mp_integer>(five.eval(ID_unary_minus).get_lower());
       REQUIRE(negated_val.has_value());
       REQUIRE(negated_val.value() == -5);
+
+      REQUIRE(max_interval.eval(ID_unary_minus).has_no_lower_bound());
+      REQUIRE(min_interval.eval(ID_unary_minus).has_no_upper_bound());
     }
 
     THEN("When we apply bitwise negation to it, is should be bitwise negated")
@@ -60,6 +69,42 @@ SCENARIO("Unary eval on intervals", "[core][analyses][interval][eval]")
         numeric_cast<mp_integer>(five.eval(ID_bitnot).get_lower());
       REQUIRE(bitwise_negated_val.has_value());
       REQUIRE(bitwise_negated_val.value() == (~5));
+    }
+  }
+  WHEN("Unary operations to an single element interval")
+  {
+    constant_interval_exprt five_to_eight = constant_interval_exprt(
+      from_integer(5, signedbv_typet(32)), from_integer(8, signedbv_typet(32)));
+
+    THEN("When we apply unary addition to it, nothing should happen")
+    {
+      REQUIRE(five_to_eight.eval(ID_unary_plus) == five_to_eight);
+    }
+
+    THEN("When we apply unary subtraction to it, it should be negated")
+    {
+      auto negated_val = numeric_cast<mp_integer>(
+        five_to_eight.eval(ID_unary_minus).get_lower());
+      REQUIRE(negated_val.has_value());
+      REQUIRE(negated_val.value() == -8);
+
+      auto upper_value = numeric_cast<mp_integer>(
+        five_to_eight.eval(ID_unary_minus).get_upper());
+      REQUIRE(upper_value.has_value());
+      REQUIRE(upper_value.value() == -8);
+    }
+
+    THEN("When we apply bitwise negation to it, is should be bitwise negated")
+    {
+      auto bitwise_negated_val =
+        numeric_cast<mp_integer>(five_to_eight.eval(ID_bitnot).get_lower());
+      REQUIRE(bitwise_negated_val.has_value());
+      REQUIRE(bitwise_negated_val.value() == (-9 /* ~5 */));
+
+      auto upper_value =
+        numeric_cast<mp_integer>(five_to_eight.eval(ID_bitnot).get_upper());
+      REQUIRE(bitwise_negated_val.has_value());
+      REQUIRE(bitwise_negated_val.value() == (-6 /* ~8 */));
     }
   }
 }
