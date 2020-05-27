c1f60ad7810d7306bd93a099887648bb693d5df7
Thomas Kiley <thomas.kiley@diffblue.com>
2020-05-27 16:41:14 +0100

    Tidy up shift test

--- unit/util/interval/shift.cpp
+++ unit/util/interval/shift.cpp
@@ -15,23 +15,17 @@
 #define V_(X) (bvrep2integer(X.c_str(), 32, true))
 #define CEV(X) (from_integer(mp_integer(X), signedbv_typet(32)))
 
-SCENARIO("shift interval domain", "[core][analyses][interval][shift]")
+TEST_CASE("shift interval domain", "[core][analyses][interval][shift]")
 {
-  GIVEN("Two simple signed intervals")
+  SECTION("Left shift")
   {
-    symbol_tablet symbol_table;
-    namespacet ns(symbol_table);
-
-    WHEN("Something")
-    {
-      THEN("Something else")
-      {
-        REQUIRE(
-          constant_interval_exprt::left_shift(
-            constant_interval_exprt(CEV(4), CEV(8)),
-            constant_interval_exprt(CEV(1))) ==
-          constant_interval_exprt(CEV(8), CEV(16)));
-      }
-    }
+    const constant_interval_exprt four_to_eight(CEV(4), CEV(8));
+    const constant_interval_exprt one(CEV(1));
+    REQUIRE(
+      constant_interval_exprt::left_shift(four_to_eight, one) ==
+      constant_interval_exprt(CEV(8), CEV(16)));
+  }
+  SECTION("Right shift")
+  {
   }
 }
