6a6a6bdfb46a7790afb6ea5c5a52453eaa676699
Thomas Spriggs <thomas.spriggs@diffblue.com>
2020-07-13 15:00:51 +0100

    Add more robust type name to type identifier conversion
    In order to certain that we don't emmit C harnesses containing invalid
    identifiers.
    
    The unit test includes both a function to generate the string of all
    printable characters and the same string as a string literal in order to
    show that the string does include all printable characters and to show
    what these characters are.

--- unit/ansi-c/type2name.cpp
+++ unit/ansi-c/type2name.cpp
@@ -8,6 +8,8 @@ Author: Thomas Spriggs
 
 #include <testing-utils/use_catch.h>
 
+#include <numeric>
+
 extern std::string type_name2type_identifier(const std::string &name);
 
 TEST_CASE(
@@ -23,4 +25,41 @@ TEST_CASE(
 {
   CHECK(type_name2type_identifier("char*") == "char_ptr_");
   CHECK(type_name2type_identifier("foo{bar}") == "foo_start_sub_bar_end_sub_");
+}
+
+/**
+ * @return A string containing all 7-bit ascii printable characters.
+ */
+static std::string all_printable_characters()
+{
+  const char first = 32;
+  const char last = 127;
+  std::string printable_characters(last - first, ' ');
+  std::iota(printable_characters.begin(), printable_characters.end(), first);
+  return printable_characters;
+}
+
+TEST_CASE(
+  "type_name2type_identifier invalid characters",
+  "[core][ansi-c][type_name2type_identifier]")
+{
+  const std::string printable_characters = all_printable_characters();
+  CHECK(
+    printable_characters ==
+    R"( !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`)"
+    R"(abcdefghijklmnopqrstuvwxyz{|}~)");
+  CHECK(
+    type_name2type_identifier(printable_characters) ==
+    "ptr_0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz_"
+    "start_sub_end_sub_");
+}
+
+TEST_CASE(
+  "type_name2type_identifier leading digits",
+  "[core][ansi-c][type_name2type_identifier]")
+{
+  CHECK(
+    type_name2type_identifier(
+      "0123456789_banana_0123456789_split_0123456789") ==
+    "banana_0123456789_split_0123456789");
 }
