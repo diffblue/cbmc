9b9c48ff84115b8d2caa1315deea97637608e892
Thomas Spriggs <thomas.spriggs@diffblue.com>
2020-08-18 18:23:08 +0100

    Add github actions clang-format job
    This was formerly a Travis CI job. Setting it up using Github actions
    gives us a user interface which integrates into the rest of github
    better. This is a step towards having all jobs seup in Github actions
    and removing Travis.

---
+++ .github/workflows/pull-request-check-clang-format.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Stop on errors
+set -e
+
+# Log information about the run of this check.
+echo "Pull request's base branch is: ${BASE_BRANCH}"
+echo "Pull request's merge branch is: ${MERGE_BRANCH}"
+echo "Pull request's source branch is: ${GITHUB_HEAD_REF}"
+clang-format-7 --version
+
+# The checkout action leaves us in detatched head state. The following line
+# names the checked out commit, for simpler reference later.
+git checkout -b ${MERGE_BRANCH}
+
+# Build list of files to ignore 
+while read file ; do EXCLUDES+="':(top,exclude)$file' " ; done < .clang-format-ignore
+
+# Make sure we can refer to ${BASE_BRANCH} by name
+git checkout ${BASE_BRANCH}
+git checkout ${MERGE_BRANCH}
+
+# Find the commit on which the PR is based.
+MERGE_BASE=$(git merge-base ${BASE_BRANCH} ${MERGE_BRANCH})
+echo "Checking for formatting errors introduced since $MERGE_BASE"
+
+# Do the checking. "eval" is used so that quotes (as inserted into $EXCLUDES
+# above) are not interpreted as parts of file names.
+eval git-clang-format-7 --binary clang-format-7 $MERGE_BASE -- $EXCLUDES
+git diff > formatted.diff
+if [[ -s formatted.diff ]] ; then
+  echo 'Formatting error! The following diff shows the required changes'
+  echo 'Use the raw log to get a version of the diff that preserves spacing'
+  cat formatted.diff
+  exit 1
+fi
+echo 'No formatting errors found'
+exit 0
