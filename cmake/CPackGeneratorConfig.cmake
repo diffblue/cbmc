26804402eed1436f95ef96b8e56154a2d9135e19
Thomas Spriggs <thomas.spriggs@diffblue.com>
2020-08-27 21:15:27 +0100

    Switch from python3 to generic python dependency

--- cmake/CPackGeneratorConfig.cmake
+++ cmake/CPackGeneratorConfig.cmake
@@ -5,5 +5,5 @@ message(STATUS "Configuring for generator ${CPACK_GENERATOR} and TOPLEVEL_TAG ${
 
 if("${CPACK_TOPLEVEL_TAG}" STREQUAL "Linux-Source")
   message(STATUS "Adding python dependency")
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS},python3")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS},python")
 endif()
