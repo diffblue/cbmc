7ca937e9f90eaf302ff1ce8bcd97f1518b8cd744
Thomas Spriggs <thomas.spriggs@diffblue.com>
2020-08-28 19:31:08 +0100

    Add node dependency

--- cmake/CPackGeneratorConfig.cmake
+++ cmake/CPackGeneratorConfig.cmake
@@ -4,6 +4,9 @@
 message(STATUS "Configuring for generator ${CPACK_GENERATOR} and TOPLEVEL_TAG ${CPACK_TOPLEVEL_TAG}")
 
 if("${CPACK_TOPLEVEL_TAG}" STREQUAL "Linux-Source")
-  message(STATUS "Adding python dependency")
+  message(STATUS "Adding build time dependencies")
+  # Python for various scripts including testing and linting.
   set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS},python,python3")
+  # Node for benchmark scripts
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS},nodejs")
 endif()
