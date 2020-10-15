6e7e2270a48fded8cc53b0905d477f44d5f1d9e1
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2020-10-15 18:30:50 +0100

    Simplify and explain goto_unwind chain.sh

--- regression/goto-unwind/chain.sh
+++ regression/goto-unwind/chain.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+##################################################################################################################
+#
+#                          goto-unwind chain.sh
+#
+# Arguments: goto_cc goto_unwind cbmc is_windows (goto_unwind_arg)* C-file
+#
+# The first 3 arguments are the paths to the goto_cc, goto_unwind and cbmc binaries to use.
+# The 4th one is true/false indicating whether or not we are on windows (in
+# which case goto_cc will use the cl command-line interface so the logic is slightly different).
+# Any further arguments are passed on to goto-unwind, except for the last one which will be the C file
+# we compile and run goto-unwind on.
+#
+# What actually happens here is basically:
+#   goto-cc test.c -o test.gb
+#   goto-unwind test.gb test-out.gb $args
+#   cbmc test-out.gb
+##################################################################################################################
+
 set -e
 
+if [[ $# < 5 ]]; then
+  echo >&2 "Expected at least 5 arguments: goto-cc, goto_unwind, cbmc, is_windows and at least a C file to test"
+fi
 goto_cc=$1
 goto_unwind=$2
 cbmc=$3
@@ -9,44 +30,17 @@ is_windows=$4
 
 name=${*:$#}
 
-if [[ x$name == x ]];  then
-  name=${name%.c}
-fi
+name=${name%.c}
 
 args=${*:5:$#-5}
 
-if [[ "${is_windows}" == "true" && x$name != x ]]; then
-  $goto_cc "main.gb" ${name}
-  name="main"
-  mv "${name}.exe" "${name}.gb"
-elif [[ "${is_windows}" == "true" ]]; then
+if [[ "${is_windows}" == "true" ]]; then
   $goto_cc "${name}.c"
   mv "${name}.exe" "${name}.gb"
-elif [[ x$name != x ]]; then
-  $goto_cc -o "main.gb" ${name}
-  echo "name: ${name}"
-  name="main"
 else
   $goto_cc -o "${name}.gb" "${name}.c"
 fi
 
 rm -f "${name}-mod.gb"
 $goto_unwind ${args} "${name}.gb" "${name}-mod.gb"
-if [ ! -e "${name}-mod.gb" ] ; then
-  cp "$name.gb" "${name}-mod.gb"
-elif echo $args | grep -q -- "--dump-c-type-header" ; then
-  cat "${name}-mod.gb"
-  mv "${name}.gb" "${name}-mod.gb"
-elif echo $args | grep -q -- "--dump-c" ; then
-  mv "${name}-mod.gb" "${name}-mod.c"
-
-  if [[ "${is_windows}" == "true" ]]; then
-    $goto_cc "${name}-mod.c"
-    mv "${name}-mod.exe" "${name}-mod.gb"
-  else
-    $goto_cc -o "${name}-mod.gb" "${name}-mod.c"
-  fi
-
-  rm "${name}-mod.c"
-fi
 $cbmc "${name}-mod.gb"
