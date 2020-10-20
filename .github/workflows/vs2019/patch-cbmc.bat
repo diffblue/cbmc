75ce37f54a418a3395f36a25ada46b10f6c140a1
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2020-10-20 16:33:40 +0100

    Remove deprecated windows package building scripts.
    The scripts removed are from an early attempt to build
    and package a CBMC distribution on windows, that has
    now been deprecated since we delegate to CMake (CPack)
    to build a windows installer.

--- .github/workflows/vs2019/patch-cbmc.bat
+++
@@ -1,9 +0,0 @@
-REM Patch cbmc for Visual Studio
-
-REM Set up PATH to find Git Bash shell tools
-PATH=%PATH%;"c:\Program Files\Git\usr\bin"
-
-REM Patch CBMC with Visual Studio patches
-
-REM Currently nothing to patch
-REM patch -p1 < .github\workflows\vs2019\PATCHFILE.patch
