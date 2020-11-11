e605bb4a70b9c18b85feb40f27a9adf3deca6c79
Michael Tautschnig <tautschn@amazon.com>
2020-11-11 22:16:01 +0000

    Remove deprecated goto_programt::instructiont methods
    These were deprecated in 2019. All remaining in-tree uses are updated
    according to the instructions provided with the deprecated methods.

--- src/goto-programs/goto_program.h
+++ src/goto-programs/goto_program.h
@@ -353,106 +353,12 @@ public:
       clear(SKIP);
     }
 
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_return() instead"))
-    void make_return() { clear(RETURN); }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_skip() instead"))
-    void make_skip() { clear(SKIP); }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_location() instead"))
-    void make_location(const source_locationt &l)
-    { clear(LOCATION); source_location=l; }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_throw() instead"))
-    void make_throw() { clear(THROW); }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_catch() instead"))
-    void make_catch() { clear(CATCH); }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_assertion() instead"))
-    void make_assertion(const exprt &g) { clear(ASSERT); guard=g; }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_assumption() instead"))
-    void make_assumption(const exprt &g) { clear(ASSUME); guard=g; }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_assignment() instead"))
-    void make_assignment() { clear(ASSIGN); }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_other() instead"))
-    void make_other(const codet &_code) { clear(OTHER); code=_code; }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_decl() instead"))
-    void make_decl() { clear(DECL); }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_dead() instead"))
-    void make_dead() { clear(DEAD); }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_atomic_begin() instead"))
-    void make_atomic_begin() { clear(ATOMIC_BEGIN); }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_atomic_end() instead"))
-    void make_atomic_end() { clear(ATOMIC_END); }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_end_function() instead"))
-    void make_end_function() { clear(END_FUNCTION); }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_incomplete_goto() instead"))
-    void make_incomplete_goto(const code_gotot &_code)
-    {
-      clear(INCOMPLETE_GOTO);
-      code = _code;
-    }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_goto() instead"))
-    void make_goto(targett _target)
-    {
-      clear(GOTO);
-      targets.push_back(_target);
-    }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_goto() instead"))
-    void make_goto(targett _target, const exprt &g)
-    {
-      make_goto(_target);
-      guard=g;
-    }
-
     void complete_goto(targett _target)
     {
       PRECONDITION(type == INCOMPLETE_GOTO);
       code.make_nil();
       targets.push_back(_target);
       type = GOTO;
-    }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_assignment() instead"))
-    void make_assignment(const code_assignt &_code)
-    {
-      clear(ASSIGN);
-      code=_code;
-    }
-
-    DEPRECATED(SINCE(2019, 2, 13, "use goto_programt::make_decl() instead"))
-    void make_decl(const code_declt &_code)
-    {
-      clear(DECL);
-      code=_code;
-    }
-
-    DEPRECATED(
-      SINCE(2019, 2, 13, "use goto_programt::make_function_call() instead"))
-    void make_function_call(const code_function_callt &_code)
-    {
-      clear(FUNCTION_CALL);
-      code=_code;
     }
 
     bool is_goto         () const { return type==GOTO;          }
