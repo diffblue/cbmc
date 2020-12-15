05e5be3ea83faafca25934dc993a950f3e2f304b
Michael Tautschnig <tautschn@amazon.com>
2020-12-15 14:28:33 +0000

    Return value removal: handle missing declarations more gracefully
    If a function is used before it is defined, a signature of int f(void)
    is assumed. Then trying to use the (possibly non-existent) return value
    fails during return-statement removal. In such cases, just assume a
    non-deterministic value is being returned.
    
    Found by running C-Reduce on a CSmith-generated example.

---
+++ regression/cbmc/return7/main.c
@@ -0,0 +1,18 @@
+void a()
+{
+  // Uses the implicit signature of undefined functions: int c(void)
+  int b;
+  b = c();
+  __CPROVER_assert(b == 0, "expected to fail");
+}
+void c(void)
+{
+  // Actually... don't return anything
+  // So the results will be non-deterministic
+}
+
+int main(int argc, char **argv)
+{
+  a();
+  return 0;
+}
