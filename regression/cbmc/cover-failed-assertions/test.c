bad0d162647f25128b848ee6d820cd95e4925985
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2020-12-17 15:49:26 +0000

    Add comments to tests

--- regression/cbmc/cover-failed-assertions/test.c
+++ regression/cbmc/cover-failed-assertions/test.c
@@ -5,6 +5,10 @@ int main()
   int *ptr = malloc(sizeof(*ptr));
   int a;
 
+  // pointer check would detect the dereference of a null pointer here
+  // default --cover lines behaviour is to treat non-cover assertions as
+  // assumptions instead, so in the ptr == NULL case we don't get past this line
+  // leading to failed coverage for the body of the if statement down below
   a = *ptr;
 
   if(ptr == NULL)
