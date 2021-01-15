3e3e85771cfc5eb33a32acdbcdc51d665f30d026
Daniel Kroening <dkr@amazon.com>
2021-01-15 10:36:27 +0000

    clang __builtin_rotateleftX/__builtin_rotaterightX
    This adds clang's rotate builtins.

---
+++ regression/cbmc/clang_builtins/rotate.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdint.h>
+
+#define rol(type, x, d)                                                        \
+  ((type)(((x) << (d)) | ((x) >> ((8 * sizeof(type)) - (d)))))
+
+#define ror(type, x, d)                                                        \
+  ((type)(((x) >> (d)) | ((x) << ((8 * sizeof(type)) - (d)))))
+
+#ifdef __clang__
+void check_left8(void)
+{
+  uint8_t op;
+  assert(__builtin_rotateleft8(op, 1) == rol(uint8_t, op, 1));
+  assert(__builtin_rotateleft8(op, 2) == rol(uint8_t, op, 2));
+  assert(__builtin_rotateleft8(op, 3) == rol(uint8_t, op, 3));
+  assert(__builtin_rotateleft8(op, 4) == rol(uint8_t, op, 4));
+}
+
+void check_left16(void)
+{
+  uint16_t op;
+  assert(__builtin_rotateleft16(op, 1) == rol(uint16_t, op, 1));
+  assert(__builtin_rotateleft16(op, 2) == rol(uint16_t, op, 2));
+  assert(__builtin_rotateleft16(op, 3) == rol(uint16_t, op, 3));
+  assert(__builtin_rotateleft16(op, 4) == rol(uint16_t, op, 4));
+}
+
+void check_left32(void)
+{
+  uint32_t op;
+  assert(__builtin_rotateleft32(op, 1) == rol(uint32_t, op, 1));
+  assert(__builtin_rotateleft32(op, 2) == rol(uint32_t, op, 2));
+  assert(__builtin_rotateleft32(op, 3) == rol(uint32_t, op, 3));
+  assert(__builtin_rotateleft32(op, 4) == rol(uint32_t, op, 4));
+}
+
+void check_left64(void)
+{
+  uint64_t op;
+  assert(__builtin_rotateleft64(op, 1) == rol(uint64_t, op, 1));
+  assert(__builtin_rotateleft64(op, 2) == rol(uint64_t, op, 2));
+  assert(__builtin_rotateleft64(op, 3) == rol(uint64_t, op, 3));
+  assert(__builtin_rotateleft64(op, 4) == rol(uint64_t, op, 4));
+}
+
+void check_right8(void)
+{
+  uint8_t op;
+  assert(__builtin_rotateright8(op, 1) == ror(uint8_t, op, 1));
+  assert(__builtin_rotateright8(op, 2) == ror(uint8_t, op, 2));
+  assert(__builtin_rotateright8(op, 3) == ror(uint8_t, op, 3));
+  assert(__builtin_rotateright8(op, 4) == ror(uint8_t, op, 4));
+}
+
+void check_right16(void)
+{
+  uint16_t op;
+  assert(__builtin_rotateright16(op, 1) == ror(uint16_t, op, 1));
+  assert(__builtin_rotateright16(op, 2) == ror(uint16_t, op, 2));
+  assert(__builtin_rotateright16(op, 3) == ror(uint16_t, op, 3));
+  assert(__builtin_rotateright16(op, 4) == ror(uint16_t, op, 4));
+}
+
+void check_right32(void)
+{
+  uint32_t op;
+  assert(__builtin_rotateright32(op, 1) == ror(uint32_t, op, 1));
+  assert(__builtin_rotateright32(op, 2) == ror(uint32_t, op, 2));
+  assert(__builtin_rotateright32(op, 3) == ror(uint32_t, op, 3));
+  assert(__builtin_rotateright32(op, 4) == ror(uint32_t, op, 4));
+}
+
+void check_right64(void)
+{
+  uint64_t op;
+  assert(__builtin_rotateright64(op, 1) == ror(uint64_t, op, 1));
+  assert(__builtin_rotateright64(op, 2) == ror(uint64_t, op, 2));
+  assert(__builtin_rotateright64(op, 3) == ror(uint64_t, op, 3));
+  assert(__builtin_rotateright64(op, 4) == ror(uint64_t, op, 4));
+}
+#endif
+
+int main(void)
+{
+#ifdef __clang__
+  check_left8();
+  check_left16();
+  check_left32();
+  check_left64();
+  check_right8();
+  check_right16();
+  check_right32();
+  check_right64();
+#endif
+}
