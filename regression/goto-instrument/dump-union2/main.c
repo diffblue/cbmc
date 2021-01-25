3a2454576f3324df63d50d45b636990e6f06b745
Michael Tautschnig <tautschn@amazon.com>
2021-01-25 10:43:05 +0000

    Partly revert "Simplify initializer to remove byte_update"
    This reverts commit ac31213d57dbfcfba29909de3460254ead632d4a, which can
    break dump-c output: the simplified expression may have padding
    initialised to non-zero values. This works fine for any analysis, but
    breaks dump-c as padding is not included in the output.
    
    Found via tests generated CSmith, a minimised version of which is
    included as a regression test.

---
+++ regression/goto-instrument/dump-union2/main.c
@@ -0,0 +1,27 @@
+#include <assert.h>
+
+struct S2
+{
+  signed char f0;
+  unsigned short f1;
+};
+
+union U10 {
+  unsigned short f0;
+  struct S2 f3;
+};
+
+union U10 g_2110 = {.f0 = 53747};
+
+union U6 {
+  signed f0 : 3;
+};
+
+union U6 g_1197 = {1L};
+
+int main()
+{
+  assert(g_2110.f0 == 53747);
+
+  return 0;
+}
