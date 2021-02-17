541cd8a1cd08d6223f4a3dcbd9a5cc865bca4853
Thomas Spriggs <thomas.spriggs@diffblue.com>
2021-02-17 18:01:28 +0000

    Use `set_initial_value` in `format` unit test
    To avoid directly setting the operands.

--- unit/util/format.cpp
+++ unit/util/format.cpp
@@ -17,6 +17,6 @@ TEST_CASE("Format a declaration statement.", "[core][util][format]")
   const signedbv_typet int_type{32};
   code_declt declaration{symbol_exprt{"foo", int_type}};
   REQUIRE(format_to_string(declaration) == "decl signedbv[32] foo;");
-  declaration.add_to_operands(int_type.zero_expr());
+  declaration.set_initial_value({int_type.zero_expr()});
   REQUIRE(format_to_string(declaration) == "decl signedbv[32] foo = 0;");
 }
