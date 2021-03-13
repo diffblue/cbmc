13ddc84a6111534d3ed67000b2646cce6b658e2f
Daniel Kroening <dkr@amazon.com>
2021-03-13 09:32:53 +0000

    Introduce ansi-c/c_expr.h
    This introduces a new header file, ansi-c/c_expr.h, which is to collect
    expressions that are specific to and internal to the C frontend.

---
+++ src/ansi-c/c_expr.h
@@ -0,0 +1,17 @@
+/*******************************************************************\
+
+Module: API to expression classes that are internal to the C frontend
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_ANSI_C_C_EXPR_H
+#define CPROVER_ANSI_C_C_EXPR_H
+
+/// \file ansi-c/c_expr.h
+/// API to expression classes that are internal to the C frontend
+
+#include <util/std_code.h>
+
+#endif // CPROVER_ANSI_C_C_EXPR_H
