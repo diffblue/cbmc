0e6b393f1b56e36a50b4d7b24e41f4a9efd70e7a
Michael Tautschnig <tautschn@amazon.com>
2021-03-15 18:35:32 +0000

    Enable extract_type_header regression tests
    This test isn't strictly dependent on memory-analyzer being available,
    but is only of interest if that is the case.

--- regression/extract_type_header/chain.sh
+++ regression/extract_type_header/chain.sh
@@ -14,13 +14,12 @@ name=${name%.c}
 args=${*:6:$#-6}
 
 if [[ "${is_windows}" == "true" ]]; then
-  $goto_cc "${name}.c"
-  mv "${name}.exe" "${name}.gb"
+  $goto_cc "${name}.c" "/Fe${name}.gb"
 else
   $goto_cc -o "${name}.gb" "${name}.c"
 fi
 
-export PATH=$PATH:"$(pwd)../../../src/goto-instrument/"
+export PATH=$PATH:"$(cd $(dirname $goto_instrument) && pwd)"
 $python_script "${name}.gb" "${name}.c" "header.h"
 cat "header.h"
 rm -f "header.h"
