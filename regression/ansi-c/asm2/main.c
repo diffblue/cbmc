111b51e6410a164c1377513a6641d4dc996b8f4f
Michael Tautschnig <tautschn@amazon.com>
2021-03-15 09:42:39 +0000

    C front-end: support inline (and its variants) after asm
    GCC supports volatile, inline, goto as asm-qualifiers:
    https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html.

--- regression/ansi-c/asm2/main.c
+++ regression/ansi-c/asm2/main.c
@@ -3,10 +3,11 @@
 int main()
 {
   int x, y;
-  #ifdef __GNUC__
+#ifdef __GNUC__
   asm goto ("jc %l[error];"
       : : "r"(x), "r"(&y) : "memory" : error);
-  #endif
+  asm __inline volatile("jc %l[error];" : : "r"(x), "r"(&y) : "memory" : error);
+#endif
 error:
   return 0;
 }
