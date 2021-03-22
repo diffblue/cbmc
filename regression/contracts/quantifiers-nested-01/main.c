33b412b8afb9dbd835752c236e97fb58321f8b46
ArenBabikian <aren.babikian@mail.mcgill.ca>
2021-03-22 18:39:56 +0000

    Add support for nested quantifiers in function contracts
    This adds support for nested quantifiers in funtion contracts.
    We recursively handle quantifiers that are nested within other
    quantifiers or within Boolean connectives.

---
+++ regression/contracts/quantifiers-nested-01/main.c
@@ -0,0 +1,24 @@
+// clang-format off
+int f1(int *arr) __CPROVER_ensures(__CPROVER_forall {
+  int i;
+  __CPROVER_forall
+  {
+    int j;
+    (0 <= i && i < 10 && i <= j && j < 10) ==> arr[i] <= arr[j]
+  }
+})
+// clang-format on
+{
+  for(int i = 0; i < 10; i++)
+  {
+    arr[i] = i;
+  }
+
+  return 0;
+}
+
+int main()
+{
+  int arr[10];
+  f1(arr);
+}
