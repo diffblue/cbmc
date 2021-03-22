ef2f3e16b90bc7042871972f2e228dd2cf3ea504
Thomas Given-Wilson <thomas.givenwilson@diffblue.com>
2021-03-22 15:04:15 +0000

    Add unit tests for bounded_size
    This adds unit tests for the bounded_size function for
    exprs. Note that the pathological case is designed to break
    incorrect implementations of bounded size and is intentionally
    a malformed exprt.

--- unit/util/expr.cpp
+++ unit/util/expr.cpp
@@ -36,3 +36,36 @@ SCENARIO("bitfield-expr-is-zero", "[core][util][expr]")
     }
   }
 }
+
+TEST_CASE("Bounded size is computer correctly", "[core][util][expr]")
+{
+  // Helper types and functions for constructing test expressions.
+  const typet type = signedbv_typet(32);
+  std::function<exprt(size_t)> make_expression;
+  make_expression = [&](size_t size) -> exprt {
+    PRECONDITION(size != 0);
+    if(size <= 1)
+      return from_integer(1, type);
+    if(size == 2)
+      return unary_minus_exprt(from_integer(1, type));
+    return plus_exprt(from_integer(1, type), make_expression(size - 2));
+  };
+  // Actual test cases.
+  REQUIRE(make_expression(1).bounded_size(10) == 1);
+  REQUIRE(make_expression(2).bounded_size(10) == 2);
+  REQUIRE(make_expression(3).bounded_size(10) == 3);
+
+  REQUIRE(make_expression(30).bounded_size(10) < 13);
+  REQUIRE(make_expression(30).bounded_size(10) >= 10);
+}
+
+TEST_CASE("Bounded size versus pathological example", "[core][util][expr]")
+{
+  const typet type = signedbv_typet(32);
+  exprt pathology = plus_exprt(from_integer(1, type), from_integer(1, type));
+  // Create an infinite exprt by self reference!
+  pathology.add_to_operands(pathology);
+  // If bounded size is not checking the bound this will never terminate.
+  REQUIRE(pathology.bounded_size(10) < 13);
+  REQUIRE(pathology.bounded_size(10) >= 10);
+}
