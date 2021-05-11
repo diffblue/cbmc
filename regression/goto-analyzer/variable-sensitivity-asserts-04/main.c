e1bff612f5c0d7471956342d13cb44276cc4b7d3
Jez Higgins <jez@jezuk.co.uk>
2021-05-11 16:42:23 +0100

    Regression tests to capture behaviour of assertions

---
+++ regression/goto-analyzer/variable-sensitivity-asserts-04/main.c
@@ -0,0 +1,21 @@
+
+int main(void)
+{
+  int x = 1;
+  int y = 3;
+  int nondet;
+
+  if(nondet)
+    y = 4;
+  if(nondet)
+    x = 5;
+
+  __CPROVER_assert(x != y, "x != y");
+  __CPROVER_assert(y != x, "y != x");
+  __CPROVER_assert(!(x != y), "!(x != y)");
+  __CPROVER_assert(!(y != x), "!(y != x)");
+  __CPROVER_assert(x == y, "x == y");
+  __CPROVER_assert(y == x, "y == x");
+  __CPROVER_assert(!(x == y), "!(x == y)");
+  __CPROVER_assert(!(y == x), "!(y == x)");
+}
