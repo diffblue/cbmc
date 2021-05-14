8e533b70ab70298b8b515009574b198c0a98e2d7
Thomas Given-Wilson <thomas.givenwilson@diffblue.com>
2021-05-14 11:59:17 +0100

    Change chain.sh to allow for custom goto binary building
    This changes the chain.sh (and adds a READFME.md to explain)
    so that a test can be written that uses its own script to build
    the goto binary.

--- regression/goto-cc-goto-analyzer/chain.sh
+++ regression/goto-cc-goto-analyzer/chain.sh
@@ -9,12 +9,17 @@ is_windows=$3
 options=${*:4:$#-4}
 name=${*:$#}
 name=${name%.c}
+buildgoto=${name}.sh
 
-if [[ "${is_windows}" == "true" ]]; then
-  "${goto_cc}" "${name}.c"
-  mv "${name}.exe" "${name}.gb"
+if test -f ${buildgoto}; then
+  ./${buildgoto} ${goto_cc} ${is_windows}
 else
-  "${goto_cc}" "${name}.c" -o "${name}.gb"
+  if [[ "${is_windows}" == "true" ]]; then
+    "${goto_cc}" "${name}.c"
+    mv "${name}.exe" "${name}.gb"
+  else
+    "${goto_cc}" "${name}.c" -o "${name}.gb"
+  fi
 fi
 
 "${goto_analyzer}" "${name}.gb" ${options}
