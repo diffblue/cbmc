3f20567aa67b4b9612a1206021a3aed805ba0a75
Felipe R. Monteiro <felisous@amazon.com>
2021-06-10 15:50:40 -0400

    Handling function pointers in contracts instrumentation
    During instrumentation for enforcing/replacing function contracts,
    all calls to function pointers were not handled. We now consider
    that a function call might be a dereference and properly handle it
    during instrumentation.
    
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

---
+++ regression/contracts/function-calls-05/main.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdlib.h>
+
+static int add_operator(const int *a, const int *b)
+{
+  return (*a + *b);
+}
+
+// clang-format off
+int foo(int *x, int *y)
+  __CPROVER_requires(x != NULL &&  y != NULL)
+  __CPROVER_ensures(__CPROVER_return_value == 10)
+// clang-format on
+{
+  int (*sum)(const int *, const int *) = add_operator;
+  return sum(x, y);
+}
+
+int main()
+{
+  int x = 5;
+  int y = 5;
+  assert(foo(&x, &y) == 10);
+  return 0;
+}
