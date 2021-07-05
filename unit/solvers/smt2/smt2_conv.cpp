665eaada9c94d0d16c1964cdaad70a79f86383be
Thomas Spriggs <thomas.spriggs@diffblue.com>
2021-07-05 21:37:02 +0100

    Add unit test of `smt2_convt::convert_identifier`
    So that the proper working of this function can be checked
    independently from the rest of the smt2 support.

---
+++ unit/solvers/smt2/smt2_conv.cpp
@@ -0,0 +1,18 @@
+// Author: Diffblue Ltd.
+
+#include <testing-utils/use_catch.h>
+
+#include <solvers/smt2/smt2_conv.h>
+
+TEST_CASE(
+  "smt2_convt::convert_identifier character escaping.",
+  "[core][solvers][smt2]")
+{
+  const auto no_escaping_characters = "abcdefghijklmnopqrstuvwxyz0123456789$";
+  CHECK(
+    smt2_convt::convert_identifier(no_escaping_characters) ==
+    no_escaping_characters);
+  CHECK(smt2_convt::convert_identifier("\\") == "&92;");
+  CHECK(smt2_convt::convert_identifier("|") == "&124;");
+  CHECK(smt2_convt::convert_identifier("&") == "&38;");
+}
