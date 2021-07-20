cb2457b5fac8b85fb4202ff0ad50797066d0b723
Thomas Given-Wilson <thomas.givenwilson@diffblue.com>
2021-07-20 14:25:04 +0100

    Add KNOWNBUG regression test for printing special operators
    This adds a KNOWNBUG regression test for printing special operators
    in traces. This is also a bug in XML output but not JSON.

---
+++ regression/cbmc/ACSL/fail-operators.c
@@ -0,0 +1,36 @@
+void boolean()
+{
+  __CPROVER_bool a, b;
+  __CPROVER_assert(!(a ≡ b) == (a == b), "≥");
+  __CPROVER_assert(!(a ≢ b) == (a != b), "≢");
+  __CPROVER_assert(!(a ⇒ b) == (!a || b), "⇒");
+  __CPROVER_assert(!(a ⇔ b) == (a == b), "⇔");
+  __CPROVER_assert(!(a ∧ b) == (a && b), "∧");
+  __CPROVER_assert(!(a ∨ b) == (a || b), "∨");
+  __CPROVER_assert(!(a ⊻ b) == (a != b), "⊻");
+  __CPROVER_assert(!(¬ a) == (!a), "¬");
+}
+
+void relations()
+{
+  int a, b;
+  __CPROVER_assert(!(a ≥ b) == (a >= b), "≥");
+  __CPROVER_assert(!(a ≤ b) == (a <= b), "≤");
+  __CPROVER_assert(!(a ≡ b) == (a == b), "≥");
+  __CPROVER_assert(!(a ≢ b) == (a != b), "≢");
+  __CPROVER_assert(!(− a) == (-a), "−");
+}
+
+void quantifiers()
+{
+  __CPROVER_assert(!(∀ int i; 1) == 1, "∀");
+  __CPROVER_assert(!(∃ int i; 1) == 1, "∃");
+  __CPROVER_assert(!(\lambda int i; i + 1)(1) == 2, "lambda");
+}
+
+int main()
+{
+  boolean();
+  relations();
+  quantifiers();
+}
