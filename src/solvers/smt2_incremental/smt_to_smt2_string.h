5dd2519aab1de7df40604ac90c18fa06106bcac7
Thomas Spriggs <thomas.spriggs@diffblue.com>
2021-07-23 12:26:24 +0100

    Extend doxygen to specify the targeted format standard
    In order to be clear about the kind of strings which should be
    generated.

--- src/solvers/smt2_incremental/smt_to_smt2_string.h
+++ src/solvers/smt2_incremental/smt_to_smt2_string.h
@@ -1,7 +1,9 @@
 // Author: Diffblue Ltd.
 
 /// \file
-/// Streaming SMT data structures to a string based output stream.
+/// Streaming SMT data structures to a string based output stream. The generated
+/// output strings are intended to be valid input for SMT2 solvers compliant
+/// with the SMT-LIB standard version 2.6.
 
 #ifndef CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT_TO_SMT2_STRING_H
 #define CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT_TO_SMT2_STRING_H
