6cc4f98af0bbd93b7d585ee023c9ed7f58d96c63
Michael Tautschnig <tautschn@amazon.com>
2021-11-05 19:48:28 +0000

    goto-symex: initialisation of pointers is no longer necessary
    goto-symex now treats empty value sets as dereferences to all current
    objects. Therefore, there is no need to initialise the value set for
    pointer-typed objects upon declaration.

--- regression/cbmc/nondet-pointer/nondet-pointer1.c
+++ regression/cbmc/nondet-pointer/nondet-pointer1.c
@@ -5,9 +5,13 @@ int main()
   int x = 123;
   int *p = &x;
   int *q = nondet_pointer();
+  int *r;
 
   if(p == q)
     __CPROVER_assert(*q == 123, "value of *q");
+
+  if(p == r)
+    __CPROVER_assert(*r == 123, "value of *q");
 
   return 0;
 }
