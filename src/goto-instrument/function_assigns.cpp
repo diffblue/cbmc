171d0c2524ca6ed5b453f5cbf751870659783f12
Michael Tautschnig <tautschn@amazon.com>
2021-12-04 19:37:57 +0000

    Clang-format renamed file

--- src/goto-instrument/function_assigns.cpp
+++ src/goto-instrument/function_assigns.cpp
@@ -22,7 +22,7 @@ void function_assignst::get_assigns(
   const goto_programt::const_targett i_it,
   assignst &assigns)
 {
-  const goto_programt::instructiont &instruction=*i_it;
+  const goto_programt::instructiont &instruction = *i_it;
 
   if(instruction.is_assign())
   {
@@ -44,34 +44,33 @@ void function_assignst::get_assigns_function(
   const exprt &function,
   assignst &assigns)
 {
-  if(function.id()==ID_symbol)
+  if(function.id() == ID_symbol)
   {
-    const irep_idt &identifier=to_symbol_expr(function).get_identifier();
+    const irep_idt &identifier = to_symbol_expr(function).get_identifier();
 
-    function_mapt::const_iterator fm_it=
-      function_map.find(identifier);
+    function_mapt::const_iterator fm_it = function_map.find(identifier);
 
-    if(fm_it!=function_map.end())
+    if(fm_it != function_map.end())
     {
       // already done
       assigns.insert(fm_it->second.begin(), fm_it->second.end());
       return;
     }
 
-    goto_functionst::function_mapt::const_iterator
-      f_it=goto_functions.function_map.find(identifier);
+    goto_functionst::function_mapt::const_iterator f_it =
+      goto_functions.function_map.find(identifier);
 
-    if(f_it==goto_functions.function_map.end())
+    if(f_it == goto_functions.function_map.end())
       return;
 
     local_may_aliast local_may_alias(f_it->second);
 
-    const goto_programt &goto_program=f_it->second.body;
+    const goto_programt &goto_program = f_it->second.body;
 
     forall_goto_program_instructions(i_it, goto_program)
       get_assigns(local_may_alias, i_it, assigns);
   }
-  else if(function.id()==ID_if)
+  else if(function.id() == ID_if)
   {
     get_assigns_function(to_if_expr(function).true_case(), assigns);
     get_assigns_function(to_if_expr(function).false_case(), assigns);
