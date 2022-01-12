164eea569bf11ef73457e53cd9d756d609a809b3
Michael Tautschnig <tautschn@amazon.com>
2022-01-12 22:04:01 +0000

    Actually test linker script processing
    The goto-cc personality does not invoke linker script processing, hence
    the tests located in the ansi-c folder were a no-op. Moving them to the
    goto-gcc folder showed that they wouldn't work as is, which is also
    fixed now:
    
    1) Add -static so as not to generate dynamic linking headers, which
    those linker scripts leave no room for.
    2) Actually detect failure of linker script parsing by enabling warnings
    and testing for them.
    3) Make sure ls_parse.py is available on the PATH.
    4) Linker scripts aren't supported by Apple's linker.

--- regression/goto-gcc/linker_script_start+end/main.c
+++ regression/goto-gcc/linker_script_start+end/main.c
@@ -2,11 +2,13 @@ extern char src_start[];
 extern char src_end[];
 extern char dst_start[];
 
-void *memcpy(void *dest, void *src, unsigned n){
+void *memcpy(void *dest, void *src, unsigned n)
+{
   return (void *)0;
 }
 
-int main(){
+int main()
+{
   memcpy(dst_start, src_start, (unsigned)src_end - (unsigned)src_start);
   return 0;
 }
