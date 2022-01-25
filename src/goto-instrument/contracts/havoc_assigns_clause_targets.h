a61b669c875f5c409fd42a2fc1ddd3f423fc63ce
Remi Delmas <delmasrd@amazon.com>
2022-01-25 02:42:42 +0000

    CONTRACTS: Make havoc_assigns_clause_targets a class that inherits from instrument_assigns_clauset.
    Eliminate code duplication for CAR calculation, target validity assertions
    properly handle condition cleaning, local statics when havocking
    assigns clauses.

--- src/goto-instrument/contracts/havoc_assigns_clause_targets.h
+++ src/goto-instrument/contracts/havoc_assigns_clause_targets.h
@@ -12,19 +12,23 @@ Author: Remi Delmas, delmasrd@amazon.com
 #ifndef CPROVER_GOTO_INSTRUMENT_CONTRACTS_HAVOC_ASSIGNS_CLAUSE_TARGETS_H
 #define CPROVER_GOTO_INSTRUMENT_CONTRACTS_HAVOC_ASSIGNS_CLAUSE_TARGETS_H
 
+#include "instrument_spec_assigns.h"
 #include <util/expr.h>
 
 class namespacet;
 class symbol_tablet;
 class goto_programt;
+class goto_functionst;
 class message_handlert;
 
-/// Generates havocking instructions for target expressions of a
+bool is_assigns_clause_replacement_tracking_comment(const irep_idt &comment);
+
+/// Class to generate havocking instructions for target expressions of a
 /// function contract's assign clause (replacement).
 ///
 /// \param replaced_function_id Name of the replaced function
-/// \param targets Assigns clause targets
-/// \param dest Destination program where the instructions are generated
+/// \param goto_functions Other functions forming the GOTO model
+/// \param targets Assigns clause targets for the replaced function
 /// \param source_location Source location of the replaced function call
 ///        (added to all generated instructions)
 /// \param mode Language mode to use for newly generated symbols
@@ -49,17 +53,55 @@ class message_handlert;
 /// of their addresses, and havoc them in a second time.
 /// Snapshotting and havocking are both guarded by the validity of the target.
 ///
-void havoc_assigns_clause_targets(
-  const irep_idt &replaced_function_id,
-  const std::vector<exprt> &targets,
-  goto_programt &dest,
-  // context parameters
-  const source_locationt &source_location,
-  const irep_idt &mode,
-  namespacet &ns,
-  symbol_tablet &st,
-  message_handlert &message_handler);
+class havoc_assigns_clause_targetst : public instrument_spec_assignst
+{
+public:
+  havoc_assigns_clause_targetst(
+    const irep_idt &_function_id,
+    const std::vector<exprt> &_targets,
+    const goto_functionst &_functions,
+    const source_locationt &_source_location,
+    symbol_tablet &_st,
+    message_handlert &_message_handler)
+    : instrument_spec_assignst(_function_id, _functions, _st, _message_handler),
+      targets(_targets),
+      source_location(_source_location)
+  {
+  }
 
-bool is_assigns_clause_replacement_tracking_comment(const irep_idt &comment);
+  /// Generates the assigns clause replacement instructions in dest.
+  void get_instructions(goto_programt &dest);
 
+private:
+  /// \brief Generates instructions to conditionally havoc
+  /// the given `car_exprt`.
+  ///
+  /// Generates these instructions for a `__CPROVER_POINTER_OBJECT(...)` target:
+  ///
+  /// ```
+  /// IF !__car_writable GOTO skip_target
+  /// OTHER havoc_object(_car_lb)
+  /// skip_target: SKIP
+  /// DEAD __car_writable
+  /// DEAD __car_lb
+  /// DEAD __car_ub
+  /// ```
+  ///
+  /// And generate these instructions otherwise:
+  ///
+  /// ```
+  /// IF !__car_writable GOTO skip_target
+  /// ASSIGN * ((<target.type()> *)_car_lb) = nondet_<target.type()>;
+  /// skip_target: SKIP
+  /// DEAD __car_writable
+  /// DEAD __car_lb
+  /// DEAD __car_ub
+  /// ```
+  /// Adds a special comment on the havoc instructions in order to trace back
+  /// the havoc to the replaced function.
+  void havoc_if_valid(car_exprt car, goto_programt &dest);
+
+  const std::vector<exprt> &targets;
+  const source_locationt &source_location;
+};
 #endif // CPROVER_GOTO_INSTRUMENT_CONTRACTS_HAVOC_ASSIGNS_CLAUSE_TARGETS_H
