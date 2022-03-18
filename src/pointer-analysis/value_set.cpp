e098f863ebdd96993ff7e642bc1d0aa716be6a12
Michael Tautschnig <tautschn@amazon.com>
2022-03-18 13:11:51 +0000

    Full slicing: fix bugs in handling array_equal
    This statement has three operands, where the third holds the comparison
    result. Make sure value sets know about this statement.

--- src/pointer-analysis/value_set.cpp
+++ src/pointer-analysis/value_set.cpp
@@ -1613,8 +1613,7 @@ void value_sett::apply_code_rec(
   {
     // can be ignored, we don't expect side effects here
   }
-  else if(statement=="cpp_delete" ||
-          statement=="cpp_delete[]")
+  else if(statement == ID_cpp_delete || statement == ID_cpp_delete_array)
   {
     // does nothing
   }
@@ -1649,6 +1648,9 @@ void value_sett::apply_code_rec(
   {
   }
   else if(statement==ID_array_replace)
+  {
+  }
+  else if(statement == ID_array_equal)
   {
   }
   else if(statement==ID_assume)
