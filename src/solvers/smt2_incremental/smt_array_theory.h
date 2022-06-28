257bfc4633d3c5b43ab3dfb9e43edfff0858a58e
Enrico Steffinlongo <enrico.steffinlongo@diffblue.com>
2022-06-28 12:32:30 +0100

    Add SMT2 array theory store function

--- src/solvers/smt2_incremental/smt_array_theory.h
+++ src/solvers/smt2_incremental/smt_array_theory.h
@@ -16,6 +16,20 @@ public:
     static void validate(const smt_termt &array, const smt_termt &index);
   };
   static const smt_function_application_termt::factoryt<selectt> select;
+
+  struct storet final
+  {
+    static const char *identifier();
+    static smt_sortt return_sort(
+      const smt_termt &array,
+      const smt_termt &index,
+      const smt_termt &value);
+    static void validate(
+      const smt_termt &array,
+      const smt_termt &index,
+      const smt_termt &value);
+  };
+  static const smt_function_application_termt::factoryt<storet> store;
 };
 
 #endif // CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT_ARRAY_THEORY_H
