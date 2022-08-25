e8f4da8570bc4614f9bfa0ffb9262addfc2ca88d
Remi Delmas <delmasrd@amazon.com>
2022-08-25 00:04:55 -0400

    CONTACTS: update regression test suite for DFCC
    Updated tests
    - update assigns-enforce-malloc-zero
    - update assigns-local-composite
    - update assigns-replace-ignored-return-value
    - update assigns-replace-malloc-zero
    - update assigns-slice-targets
    - update assigns-slice-targets
    - update assigns_enforce_01
    - update assigns_enforce_02
    - update assigns_enforce_03
    - update assigns_enforce_04
    - update assigns_enforce_05
    - update assigns_enforce_06
    - update assigns_enforce_07

--- regression/contracts/assigns_enforce_07/main.c
+++ regression/contracts/assigns_enforce_07/main.c
@@ -1,18 +1,39 @@
-void f1(int *x1, int *y1, int *z1) __CPROVER_assigns(*x1, *y1, *z1)
+void f1(int *x, int *y, int *z) __CPROVER_assigns(*x, *y)
 {
-  f2(x1, y1, z1);
+  *x = *x + 1;
+  *y = *y + 1;
 }
 
-void f2(int *x2, int *y2, int *z2) __CPROVER_assigns(*x2, *y2, *z2)
+void f2(int *x, int *y, int *z) __CPROVER_assigns(*x, *y)
 {
-  f3(x2, y2, z2);
+  *x = *x + 1;
+  *y = *y + 1;
 }
 
-void f3(int *x3, int *y3, int *z3) __CPROVER_assigns(*y3, *z3)
+void f3(int *x, int *y, int *z) __CPROVER_assigns(*x, *y, *z)
 {
-  *x3 = *x3 + 1;
-  *y3 = *y3 + 1;
-  *z3 = *z3 + 1;
+  *x = *x + 1;
+  *y = *y + 1;
+  *z = *z + 1;
+}
+
+void f(int *x, int *y, int *z) __CPROVER_assigns(*x, *y)
+{
+  for(int i = 0; i < 3; ++i)
+  {
+    if(i == 0)
+    {
+      f1(x, y, z);
+    }
+    if(i == 1)
+    {
+      f2(x, y, z);
+    }
+    if(i == 2)
+    {
+      f3(x, y, z);
+    }
+  }
 }
 
 int main()
@@ -20,22 +41,6 @@ int main()
   int p = 1;
   int q = 2;
   int r = 3;
-
-  for(int i = 0; i < 3; ++i)
-  {
-    if(i == 0)
-    {
-      f1(&p, &q, &r);
-    }
-    if(i == 1)
-    {
-      f2(&p, &q, &r);
-    }
-    if(i == 2)
-    {
-      f3(&p, &q, &r);
-    }
-  }
-
+  f(&p, &q, &r);
   return 0;
 }
