e6fc280373d9b906202babe4f1c8a14252825340
Remi Delmas <delmasrd@amazon.com>
2022-09-06 18:26:09 -0400

    Make dirtyt traverse operands of OTHER instructions

--- src/analyses/dirty.h
+++ src/analyses/dirty.h
@@ -14,11 +14,12 @@ Date: March 2013
 #ifndef CPROVER_ANALYSES_DIRTY_H
 #define CPROVER_ANALYSES_DIRTY_H
 
-#include <unordered_set>
-
-#include <util/std_expr.h>
 #include <util/invariant.h>
+#include <util/std_expr.h>
+
 #include <goto-programs/goto_functions.h>
+
+#include <unordered_set>
 
 /// Dirty variables are ones which have their address taken so we can't
 /// reliably work out where they may be assigned and are also considered shared
@@ -65,7 +66,7 @@ public:
   bool operator()(const irep_idt &id) const
   {
     die_if_uninitialized();
-    return dirty.find(id)!=dirty.end();
+    return dirty.find(id) != dirty.end();
   }
 
   bool operator()(const symbol_exprt &expr) const
@@ -100,14 +101,12 @@ protected:
 
   // variables whose address is taken
   std::unordered_set<irep_idt> dirty;
-
+  void search_other(const goto_programt::instructiont &instruction);
   void find_dirty(const exprt &expr);
   void find_dirty_address_of(const exprt &expr);
 };
 
-inline std::ostream &operator<<(
-  std::ostream &out,
-  const dirtyt &dirty)
+inline std::ostream &operator<<(std::ostream &out, const dirtyt &dirty)
 {
   dirty.output(out);
   return out;
@@ -119,7 +118,8 @@ class incremental_dirtyt
 {
 public:
   void populate_dirty_for_function(
-    const irep_idt &id, const goto_functionst::goto_functiont &function);
+    const irep_idt &id,
+    const goto_functionst::goto_functiont &function);
 
   bool operator()(const irep_idt &id) const
   {
