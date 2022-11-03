714882a2eaad5bbe7950d1f5e2a27113d322f21d
Daniel Kroening <dkr@amazon.com>
2022-11-03 09:47:18 +0000

    fx

--- src/cprover/constant_propagation.cpp
+++ src/cprover/constant_propagation.cpp
@@ -11,6 +11,22 @@ Author: Daniel Kroening, dkr@amazon.com
 
 #include "constant_propagation.h"
 
+#include <util/format_expr.h>
+
+#include <iostream>
+
+#include <unordered_map>
+
 void constant_propagation(std::vector<exprt> &constraints)
 {
+  for(auto &c : constraints)
+  {
+    std::cout << "C: " << format(c) << "\n";
+  }
+
+  // map from addresses to values
+  std::unordered_map<exprt, exprt> constants;
+
+  // iterate until saturation
+
 }
