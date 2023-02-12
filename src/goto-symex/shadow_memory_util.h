eedd91b9e73d06ae51187377872bec9f8fc719f5
Peter Schrammel <peter.schrammel@diffblue.com>
2023-02-12 11:33:48 +0000

    Add util for cleaning pointer argument expr
    Clean the given pointer expression so that it has the right
    shape for being used for identifying shadow memory.
    This handles some quirks regarding array sizes containing
    L2 symbols and string constants not having char-pointer type.

--- src/goto-symex/shadow_memory_util.h
+++ src/goto-symex/shadow_memory_util.h
@@ -15,11 +15,21 @@ Author: Peter Schrammel
 #include <util/irep.h>
 
 class exprt;
+class typet;
 
 /// Extracts the field name identifier from a string expression,
 /// e.g. as passed as argument to a __CPROVER_field_decl_local call.
 /// \param string_expr The string argument expression
 /// \return The identifier denoted by the string argument expression
 irep_idt extract_field_name(const exprt &string_expr);
+
+/// Clean the given pointer expression so that it has the right
+/// shape for being used for identifying shadow memory.
+/// This handles some quirks regarding array sizes containing
+/// L2 symbols and string constants not having char-pointer type.
+/// \param expr The pointer to the original memory, e.g. as passed to
+///    __CPROVER_field_get.
+/// \param type The followed type of expr.
+void clean_pointer_expr(exprt &expr, const typet &type);
 
 #endif // CPROVER_GOTO_SYMEX_SHADOW_MEMORY_STATE_H
