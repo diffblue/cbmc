04886f52e3a3cae14ebdf3f247ec6b44c75dcdb9
Michael Tautschnig <tautschn@amazon.com>
2023-02-13 14:16:15 +0000

    C library check: silence warnings about GNU-style line markers
    Clang 15 complains about GNU-style line markers (`# <number>` instead of
    `#line <number>`), which we don't need to do anything about.
    
    Fixes: #7525

--- src/ansi-c/library_check.sh
+++ src/ansi-c/library_check.sh
@@ -14,7 +14,8 @@ for f in "$@"; do
     perl -p -i -e 's/(__atomic_)/s$1/' __libcheck.c
     $CC -std=gnu99 -E -include library/cprover.h -D__CPROVER_bool=_Bool -D__CPROVER_thread_local=__thread -DLIBRARY_CHECK -o __libcheck.i __libcheck.c
     $CC -S -Wall -Werror -pedantic -Wextra -std=gnu99 __libcheck.i \
-      -o __libcheck.s -Wno-unused-label -Wno-unknown-pragmas
+      -o __libcheck.s -Wno-unused-label -Wno-unknown-pragmas \
+      -Wno-gnu-line-marker -Wno-unknown-warning-option
     ec="${?}"
     rm __libcheck.s __libcheck.i __libcheck.c
     [ "${ec}" -eq 0 ] || exit "${ec}"
