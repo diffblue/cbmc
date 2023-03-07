01fd5ea12977c51333845cabf26a0d9acb865556
Michael Tautschnig <tautschn@amazon.com>
2023-03-07 13:36:51 +0000

    De-duplicate contracts regression tests
    Reduce the maintenance burden by using tags to run contracts tests both
    with and without DFCC. A first round of filtering was done as follows:
    
    ```
    cd regression/contracts-dfcc ; sed -i '1s/$/ dfcc-only/' */*.desc
    cd ../contracts
    for d in * ; do
      [ -d $d ] || continue
      [ -d ../contracts-dfcc/$d ] || continue
      cp -a ../contracts-dfcc/$d ../contracts-dfcc/X.$d
      sed -i '1s/ dfcc-only$//' ../contracts-dfcc/X.$d/*.desc
      sed -i '3s/--dfcc main //' ../contracts-dfcc/X.$d/*.desc
      if diff -urN $d ../contracts-dfcc/X.$d ; then
        sed -i '1s/ dfcc-only$//' ../contracts-dfcc/$d/*.desc
        git rm -r $d
      fi
      rm -r ../contracts-dfcc/X.$d
    done
    ```

--- regression/contracts/entry_point/main.c
+++
@@ -1,9 +0,0 @@
-int foo(char *arr, unsigned int size)
-  // clang-format off
-__CPROVER_requires(__CPROVER_is_fresh(arr, size))
-__CPROVER_assigns(arr &&size > 0: arr[0])
-// clang-format on
-{
-  if(arr && size > 0)
-    arr[0] = 1;
-}
