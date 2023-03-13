45fbdbe32bfb5609da4716e36eef79a059e20931
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2023-03-13 12:03:46 +0000

    Rename object files so that we don't have collisions between different object files coming from different projects.
    Fixes #7586
    
    Co-authored-by: Enrico Steffinlongo "enrico.steffinlongo@diffblue.com"
    Co-authored-by: Fotis Koutoulakis "fotis.koutoulakis@diffblue.com"

--- src/libcprover-cpp/add_dependencies.sh
+++ src/libcprover-cpp/add_dependencies.sh
@@ -15,11 +15,36 @@ DESTINATION=$1
 shift
 LIB_LIST=$@
 
+# We need to do the following renaming of object files because of translation
+# unit name collisions which affect linking against the final artefact. For more
+# details, please look at https://github.com/diffblue/cbmc/issues/7586 .
+
 # For each library to add:
 for lib in ${LIB_LIST}; do
+  # We will unpack and rename all .o of dependent libraries marking them with
+  # their library name to avoid clashes.
+  LIBNAME=$(basename ${lib} .a)
+
+  # Remove previous unpacked folders and create a new fresh one to work in.
+  rm -rf ${LIBNAME}
+  mkdir ${LIBNAME}
+  cd ${LIBNAME}  
+
   # Unpack the library
   ${AR_COMMAND} -x ${lib}
+
+  # Rename all object file in the library prepending "${LIBNAME}_" to avoid
+  # clashes.
+  for obj in *.o; do
+    mv ${obj} "${LIBNAME}_${obj}"
+  done
+
+  # Move back to the working directory.
+  cd ..
 done
 
 # Append all the unpacked files to the destination library
-${AR_COMMAND} -rcs ${DESTINATION} *.o
+${AR_COMMAND} -rcs ${DESTINATION} **/*.o
+
+# TODO: See if we need to do some cleanup in order to save cache space for
+# Github actions runners.
