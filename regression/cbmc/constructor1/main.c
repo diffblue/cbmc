7c41e4d3698807987aa46e809837004a53b1a8d4
Michael Tautschnig <tautschn@amazon.com>
2023-03-13 13:48:16 +0000

    Constructor attribute is not required with forward declaration
    GCC happily accepts constructor (and destructor) definitions when an
    earlier forward declaration did not have any such attribute.

--- regression/cbmc/constructor1/main.c
+++ regression/cbmc/constructor1/main.c
@@ -1,9 +1,11 @@
 #include <assert.h>
 
 #ifdef __GNUC__
-int x;
+int x, y;
 
+// forward declaration with and without attribute is ok
 static __attribute__((constructor)) void format_init(void);
+static void other_init(void);
 
 static __attribute__((constructor))
 void format_init(void)
@@ -11,12 +13,18 @@ void format_init(void)
   x=42;
   return;
 }
+
+static __attribute__((constructor)) void other_init(void)
+{
+  y = 42;
+}
 #endif
 
 int main()
 {
 #ifdef __GNUC__
   assert(x==42);
+  assert(y == 42);
 #endif
   return 0;
 }
