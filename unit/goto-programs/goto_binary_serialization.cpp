a077f7a44317e5482334d306edd84b9d64225cb8
Enrico Steffinlongo <enrico.steffinlongo@diffblue.com>
2023-03-31 15:47:03 +0100

    Add test to check goto_historyt is serialized
    Add a test checking that `goto_historyt` can be serialized and
    deserialized correctly.

--- unit/goto-programs/goto_binary_serialization.cpp
+++ unit/goto-programs/goto_binary_serialization.cpp
@@ -47,3 +47,55 @@ TEST_CASE(
     deserialized_function_map.find(function_name) !=
     deserialized_function_map.end());
 }
+
+TEST_CASE(
+  "Test goto_historyt is saved and restored",
+  "[core][goto-programs][goto-binary]")
+{
+  temporary_filet tmp_file{"serialization", ".gb"};
+
+  goto_modelt model{};
+
+  irep_idt foo_name{"foo"};
+  auto &foo_function = add_goto_function(foo_name, model);
+  foo_function.history.add(goto_transform_kindt::adjust_float_expressions);
+  const auto &foo_transforms = foo_function.history.transforms();
+
+  irep_idt bar_name{"bar"};
+  auto &bar_function = add_goto_function(bar_name, model);
+  bar_function.history.add(goto_transform_kindt::mm_io);
+  const auto &bar_transforms = bar_function.history.transforms();
+
+  irep_idt buz_name{"buz"};
+  auto &buz_function = add_goto_function(buz_name, model);
+  buz_function.history.add(goto_transform_kindt::adjust_float_expressions);
+  buz_function.history.add(goto_transform_kindt::mm_io);
+  const auto &buz_transforms = buz_function.history.transforms();
+
+  write_goto_binary(tmp_file(), model, null_message_handler);
+
+  auto deserialized_model = read_goto_binary(tmp_file(), null_message_handler);
+
+  REQUIRE(deserialized_model.has_value());
+  const auto &deserialized_function_map =
+    deserialized_model->goto_functions.function_map;
+  REQUIRE(deserialized_function_map.size() == 3);
+
+  const auto deserialized_foo_function =
+    deserialized_function_map.find(foo_name);
+  REQUIRE(deserialized_foo_function != deserialized_function_map.end());
+  REQUIRE(
+    deserialized_foo_function->second.history.transforms() == foo_transforms);
+
+  const auto deserialized_bar_function =
+    deserialized_function_map.find(bar_name);
+  REQUIRE(deserialized_bar_function != deserialized_function_map.end());
+  REQUIRE(
+    deserialized_bar_function->second.history.transforms() == bar_transforms);
+
+  const auto deserialized_buz_function =
+    deserialized_function_map.find(buz_name);
+  REQUIRE(deserialized_buz_function != deserialized_function_map.end());
+  REQUIRE(
+    deserialized_buz_function->second.history.transforms() == buz_transforms);
+}
