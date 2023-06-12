edea1bcfe28ed783e998d746a2c62667dfd13227
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2023-06-12 10:54:39 +0100

    Add regression tests for basic functionality of goto-bmc

---
+++ regression/goto-bmc/goto-bmc-symex-ready-goto/chain.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+cbmc=$1
+goto_bmc=$2
+
+options=${*:3:$#-3}
+name=${*:$#}
+base_name=${name%.c}
+
+"${cbmc}" --export-symex-ready-goto "${base_name}.goto.symex_ready" "${name}"
+"${goto_bmc}" "${base_name}.goto.symex_ready" "${options}"
