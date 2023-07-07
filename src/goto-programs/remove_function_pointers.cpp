3c05f0bf419abf75656b3a142fa94a0d51d88a1e
Michael Tautschnig <tautschn@amazon.com>
2023-07-07 20:06:08 +0000

    Fix typo in function-pointer removal
    The name of the type is "unsignedbv", not "unsigned."

--- src/goto-programs/remove_function_pointers.cpp
+++ src/goto-programs/remove_function_pointers.cpp
@@ -109,14 +109,14 @@ static bool arg_is_type_compatible(
 
   // any integer-vs-enum-vs-pointer is ok
   if(
-    call_type.id() == ID_signedbv || call_type.id() == ID_unsigned ||
+    call_type.id() == ID_signedbv || call_type.id() == ID_unsignedbv ||
     call_type.id() == ID_bool || call_type.id() == ID_c_bool ||
     call_type.id() == ID_c_enum_tag || call_type.id() == ID_c_enum ||
     call_type.id() == ID_pointer)
   {
     return function_type.id() == ID_signedbv ||
-           function_type.id() == ID_unsigned || function_type.id() == ID_bool ||
-           function_type.id() == ID_c_bool ||
+           function_type.id() == ID_unsignedbv ||
+           function_type.id() == ID_bool || function_type.id() == ID_c_bool ||
            function_type.id() == ID_pointer ||
            function_type.id() == ID_c_enum ||
            function_type.id() == ID_c_enum_tag;
