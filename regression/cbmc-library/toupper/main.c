275084d20c056871c9e90cca2770f684d26761ae
Michael Tautschnig <tautschn@amazon.com>
2023-09-29 11:45:30 +0000

    C library: Refine and improve stdio models
    Fixes portability to FreeBSD, which redefines several functions as
    macros that would only conditionally call that function. Also, ensure
    that stdin/stdout/stderr point to valid objects when those are
    fdopen'ed.

--- regression/cbmc-library/toupper/main.c
+++ regression/cbmc-library/toupper/main.c
@@ -3,8 +3,12 @@
 
 int main()
 {
+#if !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__NetBSD__)
+  // We would need to model conversion tables, where each of the BSDs has their
+  // peculiar approach.
   int x;
   int r = toupper(x);
   assert(r <= x);
+#endif
   return 0;
 }
