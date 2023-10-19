c3c7d84532d36cce9dbc82f3ca2648e878ed8d65
Michael Tautschnig <tautschn@amazon.com>
2023-10-19 20:37:12 +0000

    Experimenting with different adder implementations
    The experiments were performed using the following example:
    ```
     #ifndef SIZE
     #define SIZE 100
     #endif
    
     #ifndef UNSAT
     int main()
     {
       int a[SIZE], b[SIZE];
       for(int i=0; i < SIZE; ++i)
         __CPROVER_assert(a[i] + b[i] > a[i], "");
     }
     #else
     #include <limits.h>
    
     int main()
     {
       int a[SIZE], b[SIZE];
       for(int i=0; i < SIZE; ++i)
       {
         __CPROVER_assert(
           b[i] <= 0 ||
           (a[i] >= (INT_MAX >> 28) || b[i] >= (INT_MAX >> 28)) ||
           (a[i] <= (INT_MIN >> 1) || b[i] <= (INT_MIN >> 1)) ||
           a[i] + b[i] > a[i], "");
       }
     }
     #endif
    ```
    See https://tinyurl.com/adder-comparison
    (https://docs.google.com/spreadsheets/d/1ckrIwxe5QVUu12rs3OaY3o7CQ-UqFnEWvcEOf8cw580/)
    for results.

--- src/solvers/flattening/bv_utils.h
+++ src/solvers/flattening/bv_utils.h
@@ -232,6 +232,31 @@ protected:
   [[nodiscard]] std::pair<bvt, literalt>
   adder(const bvt &op0, const bvt &op1, literalt carry_in);
 
+  [[nodiscard]] std::pair<bvt, literalt> optimized_ripple_carry_adder(
+    const bvt &op0,
+    const bvt &op1,
+    literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt>
+  carry_lookahead_adder(const bvt &op0, const bvt &op1, literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt>
+  simple_ripple_carry_adder(const bvt &op0, const bvt &op1, literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt> carry_lookahead_4_bit_adder(
+    const bvt &op0,
+    const bvt &op1,
+    literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt>
+  kogge_stone_adder(const bvt &op0, const bvt &op1, literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt>
+  brent_kung_adder(const bvt &op0, const bvt &op1, literalt carry_in);
+
+  [[nodiscard]] std::pair<bvt, literalt>
+  sklansky_adder(const bvt &op0, const bvt &op1, literalt carry_in);
+
   [[nodiscard]] bvt adder_no_overflow(
     const bvt &op0,
     const bvt &op1,
