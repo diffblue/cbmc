c225302f55c39d819aaea394ed2d8edadd361f46
Thomas Spriggs <thomas.spriggs@diffblue.com>
2023-11-14 16:47:56 +0000

    Use `[[nodiscard]]` attribute for all build systems
    As we now require `C++17` support, the `[[nodiscard]]` attribute is
    required to be supported across all platforms.

--- src/util/nodiscard.h
+++ src/util/nodiscard.h
@@ -9,17 +9,6 @@ Author: Diffblue Ltd.
 #ifndef CPROVER_UTIL_NODISCARD_H
 #define CPROVER_UTIL_NODISCARD_H
 
-#if __has_cpp_attribute(nodiscard)
-#  ifdef __clang__
-#    pragma GCC diagnostic ignored "-Wc++1z-extensions"
-#  endif
-// NOLINTNEXTLINE(whitespace/braces)
-#  define NODISCARD [[nodiscard]]
-#elif __has_cpp_attribute(gnu::warn_unused_result)
-// NOLINTNEXTLINE(whitespace/braces)
-#  define NODISCARD [[gnu::warn_unused_result]]
-#else
-#  define NODISCARD
-#endif
+#define NODISCARD [[nodiscard]]
 
 #endif // CPROVER_UTIL_NODISCARD_H
