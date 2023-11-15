7818cad02986e125bd3532b712f60ac523504287
Michael Tautschnig <tautschn@amazon.com>
2023-11-15 16:03:04 +0000

    Avoid overload conflict when USE_STD_STRING is set
    We must not end up with two constructors that take the same argument
    type.

--- unit/solvers/smt2_incremental/smt_response_validation.cpp
+++ unit/solvers/smt2_incremental/smt_response_validation.cpp
@@ -22,7 +22,8 @@ TEST_CASE("response_or_errort storage", "[core][smt2_incremental]")
   SECTION("Error response")
   {
     const std::string message{"Test error message"};
-    const response_or_errort<smt_responset> error{message};
+    const response_or_errort<smt_responset> error{
+      std::vector<std::string>{{message}}};
     CHECK_FALSE(error.get_if_valid());
     CHECK(*error.get_if_error() == std::vector<std::string>{message});
   }
