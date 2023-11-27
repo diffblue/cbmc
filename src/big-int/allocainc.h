f3738af757f9b851ed11c202d12aae317da0beb0
Daniel Kroening <dkr@amazon.com>
2023-11-27 10:13:29 -0800

    The emscripten compiler has alloca.h
    Emscripten is a compiler that targets WASM.  To enable cross-compilation
    with emscripten, allocainc.h now uses alloca.h for this compiler.

--- src/big-int/allocainc.h
+++ src/big-int/allocainc.h
@@ -5,15 +5,17 @@
 #ifndef CPROVER_BIG_INT_ALLOCAINC_H
 #define CPROVER_BIG_INT_ALLOCAINC_H
 
-
+// clang-format off
 #if defined linux || defined __linux__		\
  || defined __sun				\
  || defined UWIN				\
  || defined osf1                                \
  || defined __MACH__                            \
- || defined __CYGWIN__
+ || defined __CYGWIN__                          \
+ || defined __EMSCRIPTEN__
+// clang-format on
 
-#include <alloca.h>
+#  include <alloca.h>
 
 #elif defined _MSC_VER    \
    || defined __BORLANDC__ \
