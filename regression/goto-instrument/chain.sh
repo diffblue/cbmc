27e312da1f57b4ba8b425963753c617220d35e7d
esteffin <enrylongo@gmail.com>
2023-12-19 17:46:49 +0000

    Removed extra arguments from goto-instrument regression and added mechanism to pass arguments to cbmc

--- regression/goto-instrument/chain.sh
+++ regression/goto-instrument/chain.sh
@@ -10,6 +10,15 @@ is_windows=$4
 sources=${*:$#}
 args=${*:5:$#-5}
 
+if [[ "$args" != *" _ "* ]]
+then
+  args_inst=$args
+  args_cbmc=""
+else
+  args_inst="${args%%" _ "*}"
+  args_cbmc="${args#*" _ "}"
+fi
+
 set -- $sources
 target=${*:$#}
 target=${target%.c}
@@ -21,7 +30,7 @@ else
 fi
 
 rm -f "${target}-mod.gb"
-$goto_instrument --no-malloc-may-fail ${args} "${target}.gb" "${target}-mod.gb"
+$goto_instrument ${args_inst} "${target}.gb" "${target}-mod.gb"
 if [ ! -e "${target}-mod.gb" ] ; then
   cp "${target}.gb" "${target}-mod.gb"
 elif echo $args | grep -q -- "--dump-c-type-header" ; then
@@ -39,5 +48,5 @@ elif echo $args | grep -q -- "--dump-c" ; then
 
   rm "${target}-mod.c"
 fi
-$goto_instrument --no-malloc-may-fail --show-goto-functions "${target}-mod.gb"
-$cbmc --no-standard-checks "${target}-mod.gb"
+$goto_instrument --show-goto-functions "${target}-mod.gb"
+$cbmc ${args_cbmc}  "${target}-mod.gb"
