67be193b5404d400c0dad4b99f39b69a46922bb8
Michael Tautschnig <tautschn@amazon.com>
2024-01-12 12:56:03 +0000

    Remove JSIL front-end
    We do not have any tests and JSIL is not being developed any further at
    https://vtss.doc.ic.ac.uk/research/javascript.html.

--- src/cbmc/cbmc_languages.cpp
+++ src/cbmc/cbmc_languages.cpp
@@ -18,18 +18,10 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <json-symtab-language/json_symtab_language.h>
 #include <statement-list/statement_list_language.h>
 
-#ifdef HAVE_JSIL
-#  include <jsil/jsil_language.h>
-#endif
-
 void cbmc_parse_optionst::register_languages()
 {
   register_language(new_ansi_c_language);
   register_language(new_statement_list_language);
   register_language(new_cpp_language);
   register_language(new_json_symtab_language);
-
-#ifdef HAVE_JSIL
-  register_language(new_jsil_language);
-#endif
 }
