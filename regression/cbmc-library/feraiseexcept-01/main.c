d5043589d3d5582926ffb8d82bb1c1cb148ef964
Daniel Kroening <dkr@amazon.com>
2024-03-09 08:56:20 -0800

    Visual Studio: fix feraiseexcept test failure
    We have a test that checks that our own implementation of feraiseexcept
    triggers an assertion failure when a non-zero argument is given.  This test
    now fails since Visual Studio has in implementation for feraiseexcept in the
    fenv.h header file.
    
    This commit disables the test when using Visual Studio.

--- regression/cbmc-library/feraiseexcept-01/main.c
+++ regression/cbmc-library/feraiseexcept-01/main.c
@@ -1,9 +1,18 @@
 #include <assert.h>
 #include <fenv.h>
 
+int nondet_int();
+
 int main()
 {
-  int exceptions;
+  int exceptions = nondet_int();
+#ifdef _MSC_VER
+  // Visual Studio's fenv.h includes an inlined implementation of
+  // feraiseexcept, which prevents us from using the one in
+  // our library.
+  __CPROVER_assert(0, "dummy assertion");
+#else
   feraiseexcept(exceptions);
+#endif
   return 0;
 }
