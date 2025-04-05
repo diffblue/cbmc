be065690ed2808a613771cb67e667a099a34ae45
Qinheping Hu <qinhh@amazon.com>
2025-04-05 05:38:23 +0000

    Replace conditions in quantified  statement expressions

--- regression/cbmc/Quantifiers-statement-expression/main.c
+++ regression/cbmc/Quantifiers-statement-expression/main.c
@@ -4,7 +4,7 @@ int main()
   // no side effects!
   int j = 0;
   int a[5] = {0 , 0, 0, 0, 0};
-  assert(__CPROVER_forall { int i;  ({ int j = i; i=i; if(i < 0 || i >4) i = 1;  ( a[i] < 5); }) });
+  assert(__CPROVER_forall { int i;  ({ int j = i; int cond = i < 0 || i >4; if(cond) i = 1;  ( a[i] < 5); }) });
   // clang-format on
 
   return 0;
