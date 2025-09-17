06a88d6fdd310aa5fa1c0c4382ebbcc252a96bb3
Michael Tautschnig <tautschn@amazon.com>
2025-09-17 12:34:48 +0000

    Consolidate symtab2gb regression tests
    Use an approach similar to contracts regression tests to extract
    parameters for multiple tools from a single line in the .desc file. This
    avoids overly fragmenting regression tests.

--- regression/symtab2gb/chain.sh
+++ regression/symtab2gb/chain.sh
@@ -4,6 +4,22 @@ set -e
 
 symtab2gb_exe=$1
 cbmc_exe=$2
+source="${@: -1}"
+goto_binary="$source.gb"
 
-$symtab2gb_exe "${@:3}"
-$cbmc_exe a.out
+args=${*:3:$#-3}
+if [[ "$args" == *" _ "* ]]
+then
+  args_symtab="${args%%" _ "*}"
+  args_cbmc="${args#*" _ "}"
+elif [[ "$args" == "_ "* ]]
+then
+  args_symtab=""
+  args_cbmc="${args#"_ "}"
+else
+  args_symtab=$args
+  args_cbmc=""
+fi
+
+$symtab2gb_exe "$source" ${args_symtab} --out "$goto_binary"
+$cbmc_exe ${args_cbmc} "$goto_binary"
